7a2203f0cb90e4739fe5cbf45966f0863e4f9bb7
Liudmyla Ilchenko <liudmyla.ilchenko7@gmail.com>
2020-07-13 17:30:37 +0300

    Added second column copy

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (const row of table.rows) {
+  const copy = row.children[1].cloneNode(true);
+
+  row.insertBefore(copy, row.children[4]);
+}
