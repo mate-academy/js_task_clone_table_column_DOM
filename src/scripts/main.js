b874d499b26678ecbc699b59543cc252caacedba
Alex Arabov <arabov@ukr.net>
2020-07-28 11:54:52 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const trList = document.querySelectorAll('tr');
+
+for (const i in trList) {
+  const newCel = trList[i].children[1].cloneNode(true);
+
+  trList[i].insertBefore(newCel, trList[i].lastElementChild);
+}
