5a167bcc18a271a93992ff5026e6df8f425c6756
Dennis Priadka <dennis.priadka@gmail.com>
2020-08-16 19:30:56 +0300

    +

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+for (const element of [...document.querySelectorAll('tr *:nth-child(2)')]) {
+  const clone = document.createElement(`${element.tagName}`);
+
+  clone.innerHTML = element.innerHTML;
+
+  element.parentNode.insertBefore(clone, element.parentNode.children[4]);
+};
