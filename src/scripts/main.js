fc08c53507592bfe85f097773226375138d4e6ac
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-08-20 16:59:50 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const tableRows = [...table.rows];
+
+const second = tableRows.map(i => i.cells[1]);
+
+for (let i = 0; i < tableRows.length; i++) {
+  tableRows[i].innerHTML += `<th> ${tableRows[i].cells[4].innerText} </th>`;
+  tableRows[i].cells[4].innerHTML = second[i].innerText;
+}
