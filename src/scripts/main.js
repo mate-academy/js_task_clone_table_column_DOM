ad736ff9fce7617354ff4498e136170bf2d798ad
Serhii Antonyshyn <serginio4422@gmail.com>
2020-08-21 15:45:49 +0300

    Solution 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,16 +6,7 @@ const list = document.querySelector('table');
 
 for (let i = 0; i < list.rows.length; i++) {
   const penultimateIndex = list.rows[i].cells.length - 1;
+  const cloneElement = list.rows[i].cells[1].cloneNode(true);
 
-  if (list.rows[i].cells[0].tagName === 'TH') {
-    const th = document.createElement('th');
-
-    th.textContent = list.rows[i].cells[1].textContent;
-    list.rows[i].insertBefore(th, list.rows[i].cells[penultimateIndex]);
-  } else {
-    const td = document.createElement('td');
-
-    td.textContent = list.rows[i].cells[1].textContent;
-    list.rows[i].insertBefore(td, list.rows[i].cells[penultimateIndex]);
-  }
+  list.rows[i].insertBefore(cloneElement, list.rows[i].cells[penultimateIndex]);
 }
