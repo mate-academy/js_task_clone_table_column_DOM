0b18b121977b63f862fc83c4b31a433967de7007
Elena-brv <leena.brv@gmail.com>
2020-08-22 20:47:06 +0300

    updated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,5 +3,5 @@
 const tr = document.querySelectorAll('tr');
 
 tr.forEach(row =>
-  row.insertBefore(row.cells[1].cloneNode(true), row.children[4])
+  row.insertBefore(row.cells[1].cloneNode(true), row.lastElementChild)
 );
