a168685d3dc0118c1595092ed44fd790060261a9
oivannikov <alex0502ivannikov@gmail.com>
2020-08-22 23:14:32 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+function addComumnTable(cloneElement, putElement) {
+  const rows = [...document.querySelectorAll('tr')];
+
+  for (let i = 0; i < rows.length; i++) {
+    rows[i].insertBefore(
+      rows[i].children[cloneElement].cloneNode(true),
+      rows[i].children[putElement]
+    );
+  }
+}
+
+addComumnTable(1, 4);
