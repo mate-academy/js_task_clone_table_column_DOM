887de4cfef7508d96b4851d4004be5fb58713441
kshvetsova <katshvetsova@gmail.com>
2020-08-23 02:15:15 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
+const list = document.querySelectorAll('tr');
+
+for (const tr of list) {
+  const copy = tr.cells[1].cloneNode(true);
+
+  tr.cells[4].before(copy);
+}
+
 // write your code here
