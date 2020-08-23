740b6bdaec2f73a1e3be36c2f56b0c72fc3b0fb1
nanccyy <lidiapriymak@gmail.com>
2020-08-24 03:30:37 +0800

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const positionRow = document.querySelectorAll('tr');
+
+const positions = [...positionRow].map(row => row.cells[1]);
+const finalCopy = [...positions];
+
+finalCopy.map((el, index) =>
+  positionRow[index].cells[3].insertAdjacentHTML('afterend', el.outerHTML));
