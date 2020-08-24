020f08d63d2eb9e53a258d5aaa73cced3420f65f
KatrinaSavvova <kat.savvek@gmail.com>
2020-08-24 16:03:28 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+const tfoot = document.querySelector('tfoot');
+const headTitle = thead.querySelector('tr');
+const bodyList = tbody.querySelectorAll('tr');
+const footTitle = tfoot.querySelector('tr');
+
+headTitle.children[4].before(headTitle.children[1].cloneNode(true));
+
+[...bodyList].forEach(element => {
+  return element.children[4].before(element.children[1].cloneNode(true));
+});
+
+footTitle.children[4].before(footTitle.children[1].cloneNode(true));
