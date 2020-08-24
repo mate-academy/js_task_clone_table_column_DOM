8a4cd7d2a727b5d4ffdfeceff9ea6fbc4d4ee684
nanccyy <lidiapriymak@gmail.com>
2020-08-24 15:12:13 +0800

    code fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,6 @@
 
 const positionRow = document.querySelectorAll('tr');
 
-const positions = [...positionRow].map(row => row.cells[1]);
-const finalCopy = [...positions];
-
-finalCopy.map((el, index) =>
-  positionRow[index].cells[3].insertAdjacentHTML('afterend', el.outerHTML));
+[...positionRow].forEach((row, index) =>
+  positionRow[index].cells[3].after(row.cells[1].cloneNode(true))
+);
