5db3c5af6dc5a179515745da4f1e6afe58b800b0
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-08-25 10:16:30 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,9 @@
 const table = document.querySelector('table');
 const tableRows = [...table.rows];
 
-const second = tableRows.map(i => i.cells[1]);
+const secondColumn = tableRows.map(i => i.cells[1]);
 
 for (let i = 0; i < tableRows.length; i++) {
   tableRows[i].innerHTML += `<th> ${tableRows[i].cells[4].innerText} </th>`;
-  tableRows[i].cells[4].innerHTML = second[i].innerText;
+  tableRows[i].cells[4].innerHTML = secondColumn[i].innerText;
 }
