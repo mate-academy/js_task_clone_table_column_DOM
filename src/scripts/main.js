5fe7107df19b2e1d048092e07cb2264733ab4590
Zarva <zarva6596@gmail.com>
2020-08-25 16:46:18 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
-// write your code here
+const tr = [...document.querySelectorAll('tr')];
+
+for (const a of tr) {
+  a.append(a.children[1].cloneNode(true));
+  a.append(a.children[4]);
+}
