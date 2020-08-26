74529b9043370cc67909623e96d08f7a20d5e212
Victor-Zinchenko-mate <65622349+Victor-Zinchenko-mate@users.noreply.github.com>
2020-08-26 20:24:33 +0300

    SolutionDomTask6

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,7 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('tr');
+
+for (let i = 0; i <= rows.length; i++) {
+  rows[i].children[4].before(rows[i].children[1].cloneNode(true));
+};
