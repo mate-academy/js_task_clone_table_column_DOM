1969e7b8a3651564af2193054716e087d1bb335e
nznmvs <nznmvs@gmail.com>
2020-08-27 18:33:38 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+function copyColumn(collection, numberOfColumn = 1) {
+  const rows = [...collection.rows];
+
+  for (const key of rows) {
+    key.lastElementChild.insertAdjacentHTML('beforebegin', `
+  <td> ${key.cells[numberOfColumn].textContent} </td>
+  `);
+  }
+}
+
+copyColumn(table);
