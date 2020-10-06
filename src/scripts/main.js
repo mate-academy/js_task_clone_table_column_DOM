580e1c3c9c5dca1d2f86f6bca55d2c808672f2d5
Alona <aionovaionova@gmail.com>
2020-10-06 15:33:43 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write your code here
+const rows = [...document.querySelectorAll('tr')];
+
+rows.forEach(row => {
+  let rowElements = row.querySelectorAll('td');
+
+  if (rowElements.length === 0) {
+    rowElements = row.querySelectorAll('th');
+
+    const newTh = document.createElement('th');
+
+    newTh.innerText = rowElements[1].innerText;
+
+    const lastColumn = rowElements[rowElements.length - 1];
+
+    delete rowElements[rowElements.length - 1];
+
+    row.append(newTh);
+    row.append(lastColumn);
+  } else {
+    const newTd = document.createElement('td');
+
+    newTd.innerText = rowElements[1].innerText;
+
+    const lastColumn = rowElements[rowElements.length - 1];
+
+    delete rowElements[rowElements.length - 1];
+
+    row.append(newTd);
+    row.append(lastColumn);
+  };
+});
