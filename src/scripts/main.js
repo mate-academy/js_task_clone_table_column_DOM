72d891d930a44f060f0dba7e26c9a561f6b7e794
Yazheviks <vicsergeyeva@gmail.com>
2020-10-06 17:50:04 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write your code here
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+const tfoot = document.querySelector('tfoot');
+
+const headRow = thead.querySelector('tr');
+const headThs = headRow.querySelectorAll('th');
+const bodyRows = tbody.querySelectorAll('tr');
+const footRow = tfoot.querySelector('tr');
+const footThs = footRow.querySelectorAll('th');
+
+function createTh(srcTh, tag, parent) {
+  const thCopy = document.createElement(tag);
+
+  thCopy.textContent = srcTh[1].textContent;
+  parent.insertBefore(thCopy, srcTh[srcTh.length - 1]);
+}
+
+createTh(headThs, 'th', headRow);
+
+bodyRows.forEach((tr) => {
+  const bodyThs = tr.querySelectorAll('td');
+
+  createTh(bodyThs, 'td', tr);
+});
+
+createTh(footThs, 'th', footRow);
