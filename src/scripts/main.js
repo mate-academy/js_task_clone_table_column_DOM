475615f1975ce66c7adad7008327d0a9fb3909ac
Alona <aionovaionova@gmail.com>
2020-10-06 15:36:52 +0300

    made it shorter

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,30 +4,21 @@ const rows = [...document.querySelectorAll('tr')];
 
 rows.forEach(row => {
   let rowElements = row.querySelectorAll('td');
+  let newTd;
 
   if (rowElements.length === 0) {
     rowElements = row.querySelectorAll('th');
 
-    const newTh = document.createElement('th');
-
-    newTh.innerText = rowElements[1].innerText;
-
-    const lastColumn = rowElements[rowElements.length - 1];
-
-    delete rowElements[rowElements.length - 1];
-
-    row.append(newTh);
-    row.append(lastColumn);
+    newTd = document.createElement('th');
   } else {
-    const newTd = document.createElement('td');
-
-    newTd.innerText = rowElements[1].innerText;
-
-    const lastColumn = rowElements[rowElements.length - 1];
-
-    delete rowElements[rowElements.length - 1];
-
-    row.append(newTd);
-    row.append(lastColumn);
+    newTd = document.createElement('td');
   };
+  newTd.innerText = rowElements[1].innerText;
+
+  const lastColumn = rowElements[rowElements.length - 1];
+
+  delete rowElements[rowElements.length - 1];
+
+  row.append(newTd);
+  row.append(lastColumn);
 });
