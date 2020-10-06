4121f149dc2a86b50b4e961dfe5785cf8ac0145a
Taras Sidak <tarasidak1990@gmail.com>
2020-10-06 17:59:31 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const tr = [...document.querySelectorAll('tr')];
+
+for (let i = 0; i < tr.length; i++) {
+  const position = tr[i].children[1].cloneNode(true);
+
+  tr[i].insertBefore(position, tr[i].children[4]);
+}
