78ea47acaffca5cc41276085ef82039b91e67c4c
Alona <aionovaionova@gmail.com>
2020-10-08 12:27:09 +0300

    made it shorter

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,9 @@
 'use strict';
 
-const rows = [...document.querySelectorAll('tr')];
+const table = document.querySelector('table');
 
-rows.forEach(row => {
-  let rowElements = row.querySelectorAll('td');
-  let newTd;
+for (let i = 0; i < table.rows.length; i++) {
+  const itemToCopy = table.rows[i].cells[1];
 
-  if (rowElements.length === 0) {
-    rowElements = row.querySelectorAll('th');
-
-    newTd = document.createElement('th');
-  } else {
-    newTd = document.createElement('td');
-  };
-  newTd.innerText = rowElements[1].innerText;
-
-  const lastColumn = rowElements[rowElements.length - 1];
-
-  delete rowElements[rowElements.length - 1];
-
-  row.append(newTd);
-  row.append(lastColumn);
-});
+  table.rows[i].children[4].before(itemToCopy.cloneNode(true));
+}
