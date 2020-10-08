008d09ca2e00bb26cf224149eb75a12748051b5a
Anastasia Chervona <av.chervona@gmail.com>
2020-10-08 22:33:42 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const insertIndex = 4;
+const columnIndex = 1;
+
+for (const row of table.rows) {
+  row.children[insertIndex].before(row.cells[columnIndex].cloneNode(true));
+}
