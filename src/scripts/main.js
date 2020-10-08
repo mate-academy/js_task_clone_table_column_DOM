8262d0b508a385f0145fd550114b92d05a8cae17
Yazheviks <vicsergeyeva@gmail.com>
2020-10-08 13:33:05 +0300

    shortened

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,28 +1,7 @@
 'use strict';
 
-const thead = document.querySelector('thead');
-const tbody = document.querySelector('tbody');
-const tfoot = document.querySelector('tfoot');
+const table = document.querySelector('table');
 
-const headRow = thead.querySelector('tr');
-const headThs = headRow.querySelectorAll('th');
-const bodyRows = tbody.querySelectorAll('tr');
-const footRow = tfoot.querySelector('tr');
-const footThs = footRow.querySelectorAll('th');
-
-function createTh(srcTh, tag, parent) {
-  const thCopy = document.createElement(tag);
-
-  thCopy.textContent = srcTh[1].textContent;
-  parent.insertBefore(thCopy, srcTh[srcTh.length - 1]);
+for (const row of table.rows) {
+  row.cells[4].before(row.cells[1].cloneNode(true));
 }
-
-createTh(headThs, 'th', headRow);
-
-bodyRows.forEach((tr) => {
-  const bodyThs = tr.querySelectorAll('td');
-
-  createTh(bodyThs, 'td', tr);
-});
-
-createTh(footThs, 'th', footRow);
