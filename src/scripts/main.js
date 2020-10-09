ecf5be92cb07874c3a100bdacf0e83c082f21bbc
Alina <alina90565@gmail.com>
2020-10-09 13:22:06 +0300

    done clone table column DOM task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const rows = [...document.querySelectorAll('tr')];
+
+rows.forEach(row => {
+  const { children } = row;
+  const tag = children[0].tagName.toLowerCase();
+
+  const cell = document.createElement(tag);
+
+  cell.textContent = children[1].textContent;
+
+  row.insertBefore(cell, children[children.length - 1]);
+});
