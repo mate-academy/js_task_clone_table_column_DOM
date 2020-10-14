b24ff693e356ff66df717df94933fadd65b167f5
Tania Omelko <tanyaomelko39@gmail.com>
2020-10-14 20:08:40 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const columnIndexFrom = 1;
+const columnIndexTo = 4;
+let count = 0;
+
+for (const row of table.rows) {
+  const i = table.rows[count].cells[columnIndexFrom];
+
+  row.insertBefore(i.cloneNode(true), row.children[columnIndexTo]);
+  count++;
+}
