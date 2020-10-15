ef3dff43b60659993571ea0a1183c170780ced40
andlap24 <andrew.laptev24@gmail.com>
2020-10-15 12:31:17 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('tr');
+const tableHead = document.querySelector('thead');
+const tableHeadList = tableHead.querySelectorAll('th');
+
+function cloneRow(fromIndex, toIndex) {
+  if (fromIndex < tableHeadList.length && toIndex < tableHeadList.length) {
+    for (let i = 0; i < rows.length; i++) {
+      rows[i]
+        .children[toIndex]
+        .before(rows[i].children[fromIndex].cloneNode(true));
+    }
+  } else {
+    throw new Error('Could not find element');
+  }
+}
+
+cloneRow(1, 4);
