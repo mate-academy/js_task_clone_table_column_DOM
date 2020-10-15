8aeaa1760458bca8eb6dfca696cb5f959a79bce8
Yuriy Putsan <putsan57@gmail.com>
2020-10-15 21:10:20 +0300

    copy position collumn

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (const row of table.rows) {
+  const [ , position,,, salary ] = row.cells;
+
+  salary.before(position.cloneNode(true));
+}
