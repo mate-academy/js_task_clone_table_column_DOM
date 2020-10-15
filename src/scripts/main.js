51e1f90ef50e34006709a44c9a175776ebe6d8fb
Anton <office.sklyarov@gmail.com>
2020-10-15 16:49:57 +0300

    added cloned position column

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
+// Cloned <thead> value and added to row
 
-// write your code here
+const tableHeader = document.querySelector('thead').children[0];
+const clonedRowName = tableHeader.children[1].cloneNode(true);
+
+tableHeader.children[4].before(clonedRowName);
+
+// Cloned <tbody> values and added to rows
+const tableBodyRows = document.querySelector('tbody').children;
+
+for (const position of tableBodyRows) {
+  const clonedPositionValue = position.children[1].cloneNode(true);
+
+  position.children[4].before(clonedPositionValue);
+}
+
+// Cloned <tfoot> value and added to row
+const tableFooter = document.querySelector('tfoot').children[0];
+const clonedFooterName = tableFooter.children[1].cloneNode(true);
+
+tableFooter.children[4].before(clonedFooterName);
