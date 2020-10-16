7920049b89521a5ad4dba9840292f30902d19176
Sofiia Kucherenko <kucherenko.sv13@gmail.com>
2020-10-16 14:33:04 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('tr');
+
+rows.forEach(cells => {
+  const copiedElement = cells.children[1];
+
+  cells.insertBefore(copiedElement.cloneNode(true), cells.children[4]);
+});
