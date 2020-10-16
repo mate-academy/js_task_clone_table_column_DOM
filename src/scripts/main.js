12f90dc573abd93895e132e567c16eb77e187c34
martakupka <marta-k2008@ukr.net>
2020-10-16 16:33:52 +0300

    Small changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const tableRows = document.querySelector('table').rows;
+const clonedColumnIndex = 1;
+const lastColumnIndex = 4;
 
 for (let i = 0; i < tableRows.length; i++) {
-  const clonedCell = tableRows[i].cells[1];
-  const lastCell = tableRows[i].cells[4];
+  const clonedCell = tableRows[i].cells[clonedColumnIndex];
+  const lastCell = tableRows[i].cells[lastColumnIndex];
 
   lastCell.before(clonedCell.cloneNode(true));
 }
