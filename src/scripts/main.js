be1fe19c94cc03eae0398e5bd942e9b2d2e43bcb
martakupka <marta-k2008@ukr.net>
2020-10-16 15:20:55 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write your code here
+const tableRows = document.querySelector('table').rows;
+
+for (let i = 0; i < tableRows.length; i++) {
+  const clonedCell = tableRows[i].cells[1];
+  const lastCell = tableRows[i].cells[4];
+
+  lastCell.before(clonedCell.cloneNode(true));
+}
