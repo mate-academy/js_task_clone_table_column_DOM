e883befcc6753433f389f0503f00a84bccfa136f
KirillLutsenko <mnrovski68@gmail.com>
2020-10-16 11:35:15 +0300

    Clone Table Column

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const indexOfCopiedColumn = 1;
+const indexOfInsertedColumn = 4;
+let count = 0;
+
+for (const row of table.rows) {
+  const cell = table.rows[count].cells[indexOfCopiedColumn];
+
+  row.insertBefore(cell.cloneNode(true), row.children[indexOfInsertedColumn]);
+  count++;
+}
