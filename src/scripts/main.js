f53987ed86198c9ebb45676dcb7825f47a2b9929
38063 <xeilen1112@gmail.com>
2020-10-17 15:26:41 +0300

    -solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const originColumnIndex = 1;
+const secondLast = table.rows[originColumnIndex].cells.length - 1;
+
+for (let i = 0; i < table.rows.length; i++) {
+  const tagName = table.rows[i].cells[originColumnIndex].tagName;
+
+  const columnElement = document.createElement(tagName);
+
+  columnElement.textContent = `${table.rows[i].cells[1].innerText}`;
+  table.rows[i].insertBefore(columnElement, table.rows[i].cells[secondLast]);
+}
