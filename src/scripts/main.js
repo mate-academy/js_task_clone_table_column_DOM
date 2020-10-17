0f61dd1df982b55ee86b55d43799a00e061a6349
Vitalik <ztx25sss@gmail.com>
2020-10-17 20:12:45 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const copy = 1;
+const paste = 4;
+
+for (const row of table.rows) {
+  const cellCopy = row.cells[copy];
+
+  row.cells[paste].before(cellCopy.cloneNode(true));
+}
