c22170dec032b45163a8f2cedbddcc80affcbdfb
Olexiy Bashmakov <olexiybashmakov@gmail.com>
2020-10-17 21:30:25 +0300

    clone table column

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (let i = 0; i < table.rows.length; i++) {
+  const cellToCopy = table.rows[i].children[1];
+
+  table.rows[i].children[4].before(cellToCopy.cloneNode(true));
+}
