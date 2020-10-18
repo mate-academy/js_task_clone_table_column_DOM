fcd48c474941031aeef4cc7b7fc565a0174ca47e
Lancaelot <Lancaelot@gmail.com>
2020-10-18 20:21:04 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
 // write your code here
+const tableRows = document.querySelectorAll('tr');
+
+tableRows.forEach(({ cells }) => {
+  const cellToCopy = cells[1];
+
+  const insertAfterIndex = 3;
+
+  cells[insertAfterIndex].after(cellToCopy.cloneNode(true));
+});
