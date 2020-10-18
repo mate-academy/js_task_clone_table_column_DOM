ef445fbb65b7545518b33422eb5b5f643b27b3ed
KirillLutsenko <mnrovski68@gmail.com>
2020-10-18 17:43:45 +0300

    use for instead for of

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,9 @@
 const table = document.querySelector('table');
 const indexOfCopiedColumn = 1;
 const indexOfInsertedColumn = 4;
-let count = 0;
 
-for (const row of table.rows) {
-  const cell = table.rows[count].cells[indexOfCopiedColumn];
+for (let i = 0; i < table.rows.length; i++) {
+  const cell = table.rows[i].cells[indexOfCopiedColumn];
 
-  row.insertBefore(cell.cloneNode(true), row.children[indexOfInsertedColumn]);
-  count++;
+  table.rows[i].cells[indexOfInsertedColumn].before(cell.cloneNode(true));
 }
