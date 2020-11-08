b6d336109fcc3ca9b3b1105e92ba315a230e0500
Anna Uska <annauska95@gmail.com>
2020-11-08 15:51:07 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const tHead = document.querySelector('thead');
+const tBody = document.querySelector('tbody');
+const tFoot = document.querySelector('tfoot');
+
+tHead.rows[0]
+  .insertBefore(tHead.rows[0].cells[1].cloneNode(true), tHead.rows[0].cells[4]);
+
+tFoot.rows[0]
+  .insertBefore(tFoot.rows[0].cells[1].cloneNode(true), tFoot.rows[0].cells[4]);
+
+for (const row of tBody.rows) {
+  row.insertBefore(row.cells[1].cloneNode(true), row.cells[4]);
+}
