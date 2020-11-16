a3d6ea4c2213ac0fc69477a33ce41d2db4957157
Olga Rubalska <olga.anikeieva@gmail.com>
2020-11-16 14:44:14 +0200

    Function insertColumn ->clone and paste table column

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+function insertColumn(table_) {
+  const [...rowCollection] = table_.rows;
+
+  rowCollection.forEach((element) => {
+    const cellToMove = element.cells[1];
+    const cellToInserBefore = element.lastElementChild;
+
+    cellToInserBefore.before(cellToMove.cloneNode(true));
+  });
+}
+
+insertColumn(table);
