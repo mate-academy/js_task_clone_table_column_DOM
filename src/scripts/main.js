02e584d270a597f9330bdcfc440494182930f29d
Lora Levitska <larakad94@gmail.com>
2021-01-04 21:16:43 +0200

    add copy column

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write your code here
+const tags = ['thead', 'tbody', 'tfoot'];
+
+function copyColumn(array) {
+  for (const selector of array) {
+    let copy;
+    const innerItem = document.querySelectorAll(`${selector} > tr`);
+
+    if (innerItem.length === 1) {
+      copy = innerItem[0].children[1];
+
+      innerItem[0].children[3].insertAdjacentHTML('afterend', `
+        <th>${copy.innerText}</th>
+      `);
+    } else {
+      for (const person of innerItem) {
+        const copyBody = person.children[1];
+
+        person.children[3].insertAdjacentHTML('afterend', `
+          <td>${copyBody.innerText}</td>
+        `);
+      }
+    }
+  }
+}
+
+copyColumn(tags);
