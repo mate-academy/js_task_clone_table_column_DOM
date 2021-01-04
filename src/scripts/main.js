d686428d9c6197750afcb69cb14efa01b24e2dfc
5Mountains <V.Piatyhor@gmail.com>
2021-01-05 00:10:39 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+[...table.rows].forEach(item => {
+  item.insertCell(4).innerHTML = item.cells[1].innerText;
+});
+
+const header = document.createElement('th');
+const footer = document.createElement('th');
+
+header.innerText = table.tHead.rows[0].cells[1].innerText;
+footer.innerText = table.tFoot.rows[0].cells[1].innerText;
+
+table.tHead.rows[0].cells[4].replaceWith(header);
+table.tFoot.rows[0].cells[4].replaceWith(footer);
