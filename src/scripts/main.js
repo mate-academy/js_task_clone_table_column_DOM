d72ad80b4eb96a9c8a1454f360d7753bb62668ef
Svitlana Haiduk <svitlana.gaiduk@gmail.com>
2021-01-04 17:22:56 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+table.rows[0].cells[3].insertAdjacentHTML('afterend',
+  `<th>${table.rows[0].cells[1].innerText}</th>`
+);
+
+for (let i = 1; i < table.rows.length - 1; i++) {
+  table.rows[i].cells[3].insertAdjacentHTML('afterend',
+    `<td>${table.rows[i].cells[1].innerText}</td>`
+  );
+}
+
+table.rows[table.rows.length - 1].cells[3].insertAdjacentHTML('afterend',
+  `<th>${table.rows[table.rows.length - 1].cells[1].innerText}</th>`
+);
