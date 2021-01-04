1e25cbad1b681b4e7c37e2cdcdd4bbca0473d3e1
Olesia <olesia.tk@gmail.com>
2021-01-04 13:19:52 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const tableHead = table.tHead;
+const tableFooter = table.tFoot;
+
+const colHead = tableHead.querySelectorAll('th');
+
+colHead[colHead.length - 1].before(colHead[1].cloneNode(true));
+
+const rowsBody = table.querySelectorAll('tr');
+
+for (let i = 1; i < rowsBody.length - 1; i++) {
+  const colBody = rowsBody[i].querySelectorAll(`td`);
+
+  colBody[colBody.length - 1].before(colBody[1].cloneNode(true));
+}
+
+const colFooter = tableFooter.querySelectorAll('th');
+
+colFooter[colFooter.length - 1].before(colFooter[1].cloneNode(true));
