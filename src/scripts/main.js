0d05e99b4066391baf2d18e5e3add0e11ebd5cac
troospieler <troospieler1990@gmail.com>
2021-01-06 16:47:17 +0200

    improved solution after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,9 @@
 'use strict';
 
-const tableHead = document.querySelector('thead');
-const headElement = tableHead.children[0].children[1];
+const [...rows] = document.querySelectorAll('tr');
 
-const [...tableHeadRow] = tableHead.children[0].children; // for index
-const index = tableHeadRow.length - 2; // index number found
-
-tableHead.children[0].children[index].after(headElement.cloneNode(true));
-
-const tableFoot = document.querySelector('tfoot');
-const footElement = tableHead.children[0].children[1];
-
-tableFoot.children[0].children[index].after(footElement.cloneNode(true));
-
-const tableBodyRows = document.querySelector('tbody').children;
-
-for (const row of tableBodyRows) {
+for (const row of rows) {
   const element = row.children[1];
 
-  row.children[index].after(element.cloneNode(true));
+  row.children[row.childElementCount - 2].after(element.cloneNode(true));
 }
