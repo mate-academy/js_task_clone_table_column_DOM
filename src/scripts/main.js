bbf26923fec1de869d22df838df694b6b95a3020
DarMatt <daryamatviienko@gmail.com>
2021-01-06 22:14:00 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const rows = [...document.querySelectorAll('tr')];
+let clone;
+
+rows.forEach(el => {
+  clone = el.children[1].cloneNode(true);
+
+  const copyRows = [...el.children];
+
+  copyRows.splice(4, 0, clone);
+  el.append(...copyRows);
+});
