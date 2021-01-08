2a0bbd38b0d49f33dbba53154ed18a79f0e80851
5Mountains <V.Piatyhor@gmail.com>
2021-01-08 21:53:06 +0200

    updated solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,14 +3,7 @@
 const table = document.querySelector('table');
 
 [...table.rows].forEach(item => {
-  item.insertCell(4).innerHTML = item.cells[1].innerText;
+  const clone = item.children[1].cloneNode(true);
+
+  item.children[4].before(clone);
 });
-
-const header = document.createElement('th');
-const footer = document.createElement('th');
-
-header.innerText = table.tHead.rows[0].cells[1].innerText;
-footer.innerText = table.tFoot.rows[0].cells[1].innerText;
-
-table.tHead.rows[0].cells[4].replaceWith(header);
-table.tFoot.rows[0].cells[4].replaceWith(footer);
