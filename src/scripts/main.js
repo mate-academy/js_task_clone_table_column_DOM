4e5aac22ba4f0b8c4f84cdadac2b869f20786b99
Roman Matsuk <the.show.of.ice.man@gmail.com>
2021-01-10 15:40:24 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write your code here
+
+const th = [...document.querySelectorAll('th')]
+  .filter(item => item.cellIndex === 1);
+const td = [...document.querySelectorAll('td')]
+  .filter(item => item.cellIndex === 1);
+
+function cloneColumn(column, place) {
+  for (const cell of column) {
+    cell.parentElement.children[place].before(cell.cloneNode(true));
+  }
+}
+
+cloneColumn(th, 4);
+cloneColumn(td, 4);
