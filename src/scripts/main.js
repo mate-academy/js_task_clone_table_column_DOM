cebce51b990acfe00374de5fca9cf2f43d9b6e1a
Nazarii <nazarrishchuk@gmail.com>
2021-01-12 01:20:29 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('tbody');
+const tableHeader = document.querySelector('thead').querySelector('tr');
+const tableFooter = document.querySelector('tfoot').querySelector('tr');
+
+function shiftColumn(tableElement, tegName) {
+  tableElement.cells[4].insertAdjacentHTML('beforebegin',
+    `${tegName}${tableElement.cells[1].textContent}`);
+};
+
+for (const row of table.rows) {
+  shiftColumn(row, '<td>');
+};
+
+shiftColumn(tableHeader, '<th>');
+shiftColumn(tableFooter, '<th>');
