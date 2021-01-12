c7319d73eb168b7ac284d75fb056ecc6c8eff297
Iryna Kozhura <ivladimirovna713@gmail.com>
2021-01-12 22:18:30 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const row = document.querySelectorAll('tr');
+
+for (let i = 0; i < row.length; i++) {
+  const td = table.rows[i].cells[1];
+  const lastCellInRow = table.rows[i].cells[4];
+  const cellDuplicate = td.cloneNode(true);
+
+  lastCellInRow.before(cellDuplicate);
+}
