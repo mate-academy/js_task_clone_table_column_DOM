3421800521f4e586b9580dc2e24912ea224ce5bf
Nazarii <nazarrishchuk@gmail.com>
2021-01-12 12:27:00 +0200

    Solution shorter code fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const table = document.querySelectorAll('tr');
+const tableRows = document.querySelectorAll('tr');
 
 function shiftColumn(tableElement, teg) {
   tableElement.cells[4].insertAdjacentHTML('beforebegin',
     `${teg}${tableElement.cells[1].textContent}`);
 };
 
-for (const row of table) {
+for (const row of tableRows) {
   let teg = '<td>';
 
   if (row.cells[0].tagName === 'TH') {
