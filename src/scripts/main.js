1f3a0d34a2bbc75299df90a3ce0039813a846116
dinexpod <work.toxa@gmail.com>
2021-01-18 20:45:27 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const rows = [...document.querySelectorAll('tr')];
+
+rows.forEach(e => {
+  const tdElements = e.querySelectorAll('td');
+
+  if (tdElements.length > 0) {
+    cloneFirstAndInsertBefore(tdElements);
+  } else {
+    cloneFirstAndInsertBefore(e.querySelectorAll('th'));
+  }
+});
+
+function cloneFirstAndInsertBefore(nodeList) {
+  const thClone = nodeList[1].cloneNode(true);
+  const lastChild = nodeList[nodeList.length - 1];
+
+  nodeList[1].parentNode.insertBefore(thClone, lastChild);
+}
