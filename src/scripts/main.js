bc6c34ff0f901399e7ca0e49af28845f1531f2af
OleksandrLisnevskyi <lisnevskyi@gmail.com>
2021-01-27 14:44:22 +0200

    Add solution clone_table_column_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
 // write your code here
+
+const header = document.querySelector('thead');
+const trHeaderCells = header.children[0].cells;
+const trHeader = header.children[0];
+const thHeader = document.createElement('th');
+
+thHeader.textContent = trHeaderCells[1].textContent;
+trHeader.insertBefore(thHeader, trHeaderCells[4]);
+
+const footer = document.querySelector('tfoot');
+const trFooterCells = footer.children[0].cells;
+const trFooter = footer.children[0];
+const thFooter = document.createElement('th');
+
+thFooter.textContent = trFooterCells[1].textContent;
+trFooter.insertBefore(thFooter, trFooterCells[4]);
+
+const tBody = document.querySelector('tbody');
+const trBody = tBody.children;
+
+for (let i = 0; i < trBody.length; i++) {
+  const tdBody = document.createElement('td');
+
+  tdBody.textContent = trBody[i].children[1].textContent;
+  trBody[i].insertBefore(tdBody, trBody[i].children[4]);
+}
