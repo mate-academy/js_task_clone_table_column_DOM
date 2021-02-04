592a07b888035f134109f1b9e5209ddde5f9df0b
DominoSl <artemenko.mik@gmail.com>
2021-02-04 14:01:32 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const headElements = [...document.querySelectorAll('table thead th')];
+const bodyElements = [...document.querySelectorAll('table tbody td')];
+const footerElements = [...document.querySelectorAll('table tfoot th')];
+
+headElements[headElements.length - 1].insertAdjacentElement(
+  'beforebegin', headElements[1].cloneNode(true));
+
+for (let j = 4; j < bodyElements.length; j += 5) {
+  bodyElements[j].insertAdjacentElement(
+    'beforebegin', bodyElements[j - 3].cloneNode(true));
+}
+
+footerElements[footerElements.length - 1].insertAdjacentElement(
+  'beforebegin', footerElements[1].cloneNode(true));
