c71ec252bece57ba248204ff65770ab07e0c8cb7
Artem Fedoseev <afedoseev359@gmail.com>
2021-02-11 20:30:12 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
 // write your code here
+const table = document.querySelector('table');
+
+[...table.children].forEach((block) => {
+  [...block.children].forEach((tr) => {
+    const newTd = document.createElement(`${tr.children[1].nodeName}`);
+
+    newTd.innerText = tr.children[1].innerText;
+    tr.insertBefore(newTd, tr.children[4]);
+  });
+});
