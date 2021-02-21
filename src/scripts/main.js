69776500585dea2c1b6f984908e1265a937f01ac
Yevgeniy Podobreev <podobr_2018@ukr.net>
2021-02-21 13:05:05 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
 // write your code here
+const tr = document.querySelectorAll('tr');
+
+for (let index = 0; index < tr.length; index++) {
+  const clone = document.createElement(`${tr[index].children[1].tagName}`);
+
+  clone.innerHTML = tr[index].children[1].innerHTML;
+  tr[index].insertBefore(clone, tr[index].children[4]);
+}
