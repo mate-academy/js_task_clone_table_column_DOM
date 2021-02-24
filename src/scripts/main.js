29d9e083bbb521d1ea5de03198c2a03edfae4bec
chri911 <delightsome@bigmir.net>
2021-02-24 19:23:05 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (let i = 0; i < table.rows.length; i++) {
+  (i === 0 || i === 13)
+    ? table.rows[i].cells[4].insertAdjacentHTML('beforebegin', `
+    <th>${table.rows[i].cells[1].innerText}</th>
+  `)
+    : table.rows[i].cells[4].insertAdjacentHTML('beforebegin', `
+  <td>${table.rows[i].cells[1].innerText}</td>
+`);
+}
