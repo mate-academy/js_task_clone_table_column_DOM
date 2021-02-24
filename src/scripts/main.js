4feecd36ebbf0686e188b52792cafca8435317d2
Bohdan Dobak <chudolipton@gmail.com>
2021-02-24 21:54:30 +0200

    solve task clone table column and in orderd to solve this task i made some loop for and another things which make tests green

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const tableRows = table.rows;
+
+for (let i = 0; i < tableRows.length; i++) {
+  const columnsToClone = tableRows[i].cells[1];
+  const cloneColumn = columnsToClone.cloneNode(true);
+
+  tableRows[i].insertBefore(cloneColumn, tableRows[i].cells[4]);
+}
