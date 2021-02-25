0e171c5ee366e0ea10813a51e2f499f9463e7254
morozoleh <olegmoroz93@gmail.com>
2021-02-25 08:11:14 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const tableHead = document.querySelector('thead');
+const tableBody = document.querySelector('tbody');
+const tableFoot = document.querySelector('tfoot');
+
+const clonedPositionHead = tableHead.firstElementChild.children[1]
+  .cloneNode(true);
+const clonedPositionFoot = tableFoot.firstElementChild.children[1]
+  .cloneNode(true);
+
+[...tableBody.children].forEach(elem => {
+  const clonedPositionBody = elem.children[1].cloneNode(true);
+
+  elem.lastElementChild.before(clonedPositionBody);
+});
+
+tableHead.firstElementChild.lastElementChild.before(clonedPositionHead);
+tableFoot.firstElementChild.lastElementChild.before(clonedPositionFoot);
