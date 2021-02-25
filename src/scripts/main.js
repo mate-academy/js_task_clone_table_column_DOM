9e4f5063637fd0ab4df4859131a18356e22c00a6
Misha Lysak <myhaillysak@gmail.com>
2021-02-25 19:00:50 +0200

    made one more answer for this task with more smaller code with use methods like cloneNode and insertBefore, with I already  read. and did this task one more time, because volodya tak skazav

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,43 @@
 'use strict';
 
-const tHead = document.querySelector('thead');
-const tBody = document.querySelector('tbody');
-const tFoot = document.querySelector('tfoot');
-const tr = document.querySelectorAll('tr');
+const table = document.querySelector('table');
+const tableRow = table.rows;
 
-const thForCopyInHead = document.createElement('th');
-const thForCopyInFoot = document.createElement('th');
+const indexOfRowForCopyColumnFrom = 1;
+const indexOfRowForCopyColumnTo = 4;
 
-const forCopyInHead = tHead.rows[0].cells[1].innerHTML;
-const forCopyInFoot = tFoot.rows[0].cells[1].innerHTML;
+for (let i = 0; i < table.rows.length; i++) {
+  const columnWhichWeCopy = table.rows[i].cells[indexOfRowForCopyColumnFrom];
+  const clonedColumn = columnWhichWeCopy.cloneNode(true);
 
-thForCopyInHead.append(forCopyInHead);
-thForCopyInFoot.append(forCopyInFoot);
-tHead.rows[0].cells[4].insertAdjacentElement('beforebegin', thForCopyInHead);
-tFoot.rows[0].cells[4].insertAdjacentElement('beforebegin', thForCopyInFoot);
-
-for (let i = 0; i < tr.length - 2; i++) {
-  const tdForCopy = document.createElement('td');
-  const forCopyInBody = tBody.rows[i].cells[1].innerHTML;
-
-  tdForCopy.append(forCopyInBody);
-  tBody.rows[i].cells[4].insertAdjacentElement('beforebegin', tdForCopy);
+  tableRow[i]
+    .insertBefore(clonedColumn, tableRow[i].cells[indexOfRowForCopyColumnTo]);
 }
+
+// <--------------one more answer--------------->
+
+// const tHead = document.querySelector('thead');
+// const tBody = document.querySelector('tbody');
+// const tFoot = document.querySelector('tfoot');
+// const tr = document.querySelectorAll('tr');
+
+// const thForCopyInHead = document.createElement('th');
+// const thForCopyInFoot = document.createElement('th');
+
+// const forCopyInHead = tHead.rows[0].cells[1].innerHTML;
+// const forCopyInFoot = tFoot.rows[0].cells[1].innerHTML;
+
+// thForCopyInHead.append(forCopyInHead);
+// thForCopyInFoot.append(forCopyInFoot);
+// tHead.rows[0].cells[4].insertAdjacentElement('beforebegin', thForCopyInHead);
+// tFoot.rows[0].cells[4].insertAdjacentElement('beforebegin', thForCopyInFoot);
+
+// for (let i = 0; i < tr.length - 2; i++) {
+//   const tdForCopy = document.createElement('td');
+//   const forCopyInBody = tBody.rows[i].cells[1].innerHTML;
+
+//   tdForCopy.append(forCopyInBody);
+//   tBody.rows[i].cells[4].insertAdjacentElement('beforebegin', tdForCopy);
+// }
 
 // write your code here
