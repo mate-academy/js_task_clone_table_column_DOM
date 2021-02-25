4d04c8323bf44180573b4470f730f92b329d774b
Bohdan Dobak <chudolipton@gmail.com>
2021-02-25 13:01:23 +0200

    rename variables names

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,10 +5,11 @@ const table = document.querySelector('table');
 const tableRows = table.rows;
 
 for (let i = 0; i < tableRows.length; i++) {
-  const secondCell = 1;
-  const fifthCell = 5;
-  const columnsToClone = tableRows[i].cells[secondCell];
+  const columnIndexToCopy = 1;
+  const columnIndexToInsert = 4;
+  const columnsToClone = tableRows[i].cells[columnIndexToCopy];
   const cloneColumn = columnsToClone.cloneNode(true);
 
-  tableRows[i].insertBefore(cloneColumn, tableRows[i].cells[fifthCell]);
+  tableRows[i]
+    .insertBefore(cloneColumn, tableRows[i].cells[columnIndexToInsert]);
 }
