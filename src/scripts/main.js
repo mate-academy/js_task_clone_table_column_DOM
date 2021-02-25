bdff34e3f25ed9ed40117868139efe1bd42320ad
chri911 <delightsome@bigmir.net>
2021-02-25 18:17:10 +0200

    another solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,12 +2,7 @@
 
 const table = document.querySelector('table');
 
-for (let i = 0; i < table.rows.length; i++) {
-  (i === 0 || i === 13)
-    ? table.rows[i].cells[4].insertAdjacentHTML('beforebegin', `
-    <th>${table.rows[i].cells[1].innerText}</th>
-  `)
-    : table.rows[i].cells[4].insertAdjacentHTML('beforebegin', `
-  <td>${table.rows[i].cells[1].innerText}</td>
-`);
-}
+[...table.rows].forEach(row =>
+  row.lastElementChild.before(
+    row.firstElementChild.nextElementSibling.cloneNode(true)
+  ));
