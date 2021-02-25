b210ce27e792aaf28dda0499068bc0afa089a134
Bohdan Dobak <chudolipton@gmail.com>
2021-02-25 12:42:00 +0200

    make variables for position of cells

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,8 +5,10 @@ const table = document.querySelector('table');
 const tableRows = table.rows;
 
 for (let i = 0; i < tableRows.length; i++) {
-  const columnsToClone = tableRows[i].cells[1];
+  const secondCell = 1;
+  const fifthCell = 5;
+  const columnsToClone = tableRows[i].cells[secondCell];
   const cloneColumn = columnsToClone.cloneNode(true);
 
-  tableRows[i].insertBefore(cloneColumn, tableRows[i].cells[4]);
+  tableRows[i].insertBefore(cloneColumn, tableRows[i].cells[fifthCell]);
 }
