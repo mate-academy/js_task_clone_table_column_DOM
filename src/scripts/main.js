b1d1d6ae156d0985837606f9f7671d34d133dde2
Sofiia Asieieva <sofiia.asieieva@gmail.com>
2021-03-04 17:43:11 +0200

    created the copyColumns function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const [...tr] = document.querySelectorAll('tr');
+
+function copyColumns(copy, which, insert) {
+  for (const row of copy) {
+    const copyCellule = row.children[which].cloneNode(true);
+
+    row.children[insert].before(copyCellule);
+  }
+}
+
+copyColumns(tr, 1, 4);
