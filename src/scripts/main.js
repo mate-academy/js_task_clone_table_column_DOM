dca03a0931fd6cd8c2a7b447581efffc4e650f17
Ping <4ivrik@ukr.net>
2021-03-05 20:23:18 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const cells = table.querySelectorAll('tr');
+
+cells.forEach(cell => {
+  return cell.children[3].after(cell.children[1].cloneNode(true));
+}
+);
