711bc6a166f4acadb311fe187a2111eaf5556a00
Ostap21 <57452764+Ostap21@users.noreply.github.com>
2021-03-06 18:12:38 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const tableRows = [...table.rows];
+
+const positionIndex = 1;
+const copyPosition = [];
+
+tableRows.forEach(element => {
+  copyPosition.push(element.cells[positionIndex].cloneNode(true));
+});
+
+tableRows.forEach((element, index, array) => {
+  element.insertBefore(copyPosition[index], element.cells[4]);
+});
