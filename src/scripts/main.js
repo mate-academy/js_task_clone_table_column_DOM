3169e5bd80aba96aa7ca536cfd6d4fd8042f514f
yura0seredyuk <yura.seredyuk0@gmail.com>
2021-03-06 14:14:36 +0200

    easier version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,21 +6,21 @@ const table = document.querySelector('table');
 const headTable = document.querySelector('thead');
 const bodyTable = table.querySelector('tbody');
 const footTable = table.querySelector('tfoot');
-const position = table.children[0].children[0].children[1];
+const position = table.rows[0].cells[1];
 
 headTable.children[0].insertBefore(
-  position.cloneNode(true), headTable.children[0].children[4]
+  position.cloneNode(true), headTable.rows[0].cells[4]
 );
 
 footTable.children[0].insertBefore(
-  position.cloneNode(true), footTable.children[0].children[4]
+  position.cloneNode(true), footTable.rows[0].cells[4]
 );
 
 const count = document.querySelectorAll('tr').length;
 
 for (let i = 0; i < count - 2; i++) {
   bodyTable.children[i].insertBefore(
-    bodyTable.children[i].children[1].cloneNode(true),
-    bodyTable.children[i].children[4]
+    bodyTable.rows[i].cells[1].cloneNode(true),
+    bodyTable.rows[i].cells[4]
   );
 }
