c698fa82746d03235d5adc20a972dc9932596f3f
Nikita Puhovskii <cheliojul@gmail.com>
2021-03-06 12:58:23 +0200

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const dashboard = document.body.querySelector('table');
+
+cloneColumn(dashboard, 1, 4);
+
+function cloneColumn(targetTable, fromIndex, toIndex) {
+  for (const row of targetTable.rows) {
+    row.children[toIndex].before(row.children[fromIndex].cloneNode(true));
+  }
+}
