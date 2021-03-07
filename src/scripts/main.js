f63f5e3d3d1213925cf6bb6e33d050ea59da6dc7
Anna <garsinaanna32778@gmail.com>
2021-03-07 16:30:33 +0200

    table row was clonned

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const tableRows = table.rows;
+
+for (const row of tableRows) {
+  row.children[3].after(row.children[1].cloneNode(true));
+}
