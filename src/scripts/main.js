58496d5a0a059d4ac087e479fc4247c71be3ea58
Yurs Chabarenko <yurschabarenko@Yurss-MacBook-Pro.local>
2021-03-08 14:48:12 +0200

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const tableRows = document.querySelector('table').rows;
+const cloneIndex = 1;
+const insertIndex = 4;
+
+for (const row of tableRows) {
+  row.children[insertIndex].before(row.children[cloneIndex].cloneNode(true));
+}
