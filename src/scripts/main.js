86a1bde3ab93c85869be3fb2ecb08e5fbd76a860
Yevhenii Yashnyi <y.yashnyi@gmail.com>
2021-03-08 19:58:24 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const tableRows = table.rows;
+
+const secondCell = 1;
+const lastCell = table.rows[0].cells.length - 1;
+
+function copyCell(copyCellFrom, pasteCellTo) {
+  for (const tableRow of tableRows) {
+    tableRow.children[pasteCellTo]
+      .before(tableRow.children[copyCellFrom].cloneNode(true));
+  }
+}
+
+copyCell(secondCell, lastCell);
