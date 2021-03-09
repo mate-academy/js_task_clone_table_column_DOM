d2ef0ebee79c1a35b50ebbad59cee344b6988a61
Ping <4ivrik@ukr.net>
2021-03-09 19:17:10 +0200

    more variables changed to fit the correct names

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,11 @@
 'use strict';
 
 const table = document.querySelector('table');
-const cells = [...table.rows];
+const rows = [...table.rows];
 
-cells.forEach(cell => {
-  const clonedColumn = cell.children[1].cloneNode(true);
-  const positionToPlace = cell.children[3];
+rows.forEach(row => {
+  const clonedColumn = row.children[1].cloneNode(true);
+  const positionToPlace = row.children[3];
 
   return positionToPlace.after(clonedColumn);
 });
