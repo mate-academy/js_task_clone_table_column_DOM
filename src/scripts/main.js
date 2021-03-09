d7967a3e5f3238b66d6c09e31e19afeb1757a355
yura0seredyuk <yura.seredyuk0@gmail.com>
2021-03-09 21:30:12 +0200

    some fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,24 +3,10 @@
 // write your code here
 
 const table = document.querySelector('table');
-const headTable = document.querySelector('thead');
-const bodyTable = table.querySelector('tbody');
-const footTable = table.querySelector('tfoot');
-const position = table.rows[0].cells[1];
+const tableRow = table.rows;
+const copyFrom = 4;
+const copyTo = 1;
 
-headTable.children[0].insertBefore(
-  position.cloneNode(true), headTable.rows[0].cells[4]
-);
-
-footTable.children[0].insertBefore(
-  position.cloneNode(true), footTable.rows[0].cells[4]
-);
-
-const count = document.querySelectorAll('tr').length;
-
-for (let i = 0; i < count - 2; i++) {
-  bodyTable.children[i].insertBefore(
-    bodyTable.rows[i].cells[1].cloneNode(true),
-    bodyTable.rows[i].cells[4]
-  );
+for (const row of tableRow) {
+  row.cells[copyFrom].before(row.cells[copyTo].cloneNode(true));
 }
