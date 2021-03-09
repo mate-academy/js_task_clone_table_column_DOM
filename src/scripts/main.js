fcaa4a3602c7fdae22d94aa890fc6f60a7f1504d
Ping <4ivrik@ukr.net>
2021-03-09 19:10:05 +0200

    variables added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const table = document.querySelector('table');
-const cells = table.querySelectorAll('tr');
+const cells = [...table.rows];
 
 cells.forEach(cell => {
-  return cell.children[3].after(cell.children[1].cloneNode(true));
-}
-);
+  const clonedColumn = cell.children[1].cloneNode(true);
+  const positionToPlace = cell.children[3];
+
+  return positionToPlace.after(clonedColumn);
+});
