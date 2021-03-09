ee62915ceee62d2f41795582487ef147018cd6ac
Ostap21 <57452764+Ostap21@users.noreply.github.com>
2021-03-09 19:30:37 +0200

    after mentor review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,10 +6,7 @@ const tableRows = [...table.rows];
 const positionIndex = 1;
 const copyPosition = [];
 
-tableRows.forEach(element => {
-  copyPosition.push(element.cells[positionIndex].cloneNode(true));
-});
-
 tableRows.forEach((element, index, array) => {
+  copyPosition.push(element.cells[positionIndex].cloneNode(true));
   element.insertBefore(copyPosition[index], element.cells[4]);
 });
