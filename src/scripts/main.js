c961b5111a280f881ae3a86c2cccda479f51d81d
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-03-09 22:10:45 +0200

    Variables renamed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,12 @@
 'use strict';
 
 const table = document.querySelector('table');
-const rows = table.querySelectorAll('tr');
+const rows = table.rows;
+const columnToCut = rows[0].children.length - 1;
+const columnPosition = 1;
 
 for (const row of rows) {
-  row.children[row.children.length - 1].before(row.children[1].cloneNode(true));
+  const positionToInsert = row.children[columnPosition].cloneNode(true);
+
+  row.children[columnToCut].before(positionToInsert);
 }
