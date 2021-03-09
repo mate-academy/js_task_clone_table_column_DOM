bad8b6331a12c1363d854615af654dae4b52adc3
Yevhenii Yashnyi <y.yashnyi@gmail.com>
2021-03-09 20:05:47 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,14 +3,12 @@
 const table = document.querySelector('table');
 const tableRows = table.rows;
 
-const secondCellIndex = 1;
-const lastCellIndex = table.rows[0].cells.length - 1;
+const copyIndex = 1;
+const pasteIndex = table.rows[0].cells.length - 1;
 
-function copyCell(copyCellFromIndex, pasteCellBeforeIndex) {
-  for (const tableRow of tableRows) {
-    tableRow.children[pasteCellBeforeIndex]
-      .before(tableRow.children[copyCellFromIndex].cloneNode(true));
-  }
+for (const tableRow of tableRows) {
+  const copiedCell = tableRow.children[copyIndex].cloneNode(true);
+  const newCell = tableRow.children[pasteIndex];
+
+  newCell.before(copiedCell);
 }
-
-copyCell(secondCellIndex, lastCellIndex);
