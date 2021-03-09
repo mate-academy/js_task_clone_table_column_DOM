981e3cadc629e554502a22b0fe24b877f942d696
Ivan Panchenko <ivanpanchenkorulit@gmail.com>
2021-03-09 19:39:33 +0200

    fixedAll

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('tr');
+const copyFrom = 1;
+const copyTo = 4;
+
+for (const row of rows) {
+  const clonedCell = row.cells[copyFrom].cloneNode(true);
+
+  const newCell = row.cells[copyTo];
+
+  newCell.before(clonedCell);
+};
