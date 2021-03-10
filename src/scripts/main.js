42c0b6657a8e954f9d56ddde56e6031e6f530f51
Oleg Bezriadin <muhachov86@gmail.com>
2021-03-10 14:24:23 +0200

    deleted magic number

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const table = document.querySelector('table');
+const property = document.querySelector('thead tr');
+
+const index = [...property.children].findIndex(td =>
+  td.innerHTML === 'Position');
 
 for (const human of table.rows) {
-  const position = human.cells[1].cloneNode(true);
+  const position = human.cells[index].cloneNode(true);
 
   human.lastElementChild.before(position);
 }
