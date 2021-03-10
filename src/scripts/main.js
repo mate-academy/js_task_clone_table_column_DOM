f7f35176c0714964bb56aa94ec938896e3637fcd
Ostap21 <57452764+Ostap21@users.noreply.github.com>
2021-03-10 15:40:01 +0200

    fix veriables

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,12 +3,12 @@
 const table = document.querySelector('table');
 const tableRows = [...table.rows];
 
-const indexCloneFrom = 1;
-const indexCLlneTo = 4;
+const placedCloneFrom = 1;
+const placedLocationTo = 4;
 
 tableRows.forEach(element => {
-  const tableElementClone = element.cells[indexCloneFrom].cloneNode(true);
+  const tableElementClone = element.cells[placedCloneFrom].cloneNode(true);
 
   element.insertBefore(tableElementClone,
-    element.cells[indexCLlneTo]);
+    element.cells[placedLocationTo]);
 });
