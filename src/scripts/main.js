83439f3a9783a76cbed61ad663ba80313d557292
Vitalii Skrynkovych <vitaliiskrynkovych@gmail.com>
2021-03-10 13:01:07 +0200

    changes after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,18 @@
 'use strict';
 
 // write your code here
-const tableBody = document.querySelector('tbody');
-const theadAgeTitle
-  = document.querySelector('thead > tr > th:nth-child(4)');
-const tfootAgeTitle
-  = document.querySelector('tfoot > tr > th:nth-child(4)');
+const table = document.querySelector('table');
 
-theadAgeTitle.insertAdjacentHTML('afterend', `
-<th>Position</th>
-`);
+function cloneElement(indexOfElementToClone, placeToAddClonedElement) {
+  let index = 0;
 
-tfootAgeTitle.insertAdjacentHTML('afterend', `
-<th>Position</th>
-`);
+  while (index < table.rows.length) {
+    const clonedElement
+    = table.rows[index].children[indexOfElementToClone].cloneNode(true);
 
-const rows = [...tableBody.querySelectorAll('tr')];
+    table.rows[index].children[placeToAddClonedElement].before(clonedElement);
+    index++;
+  }
+}
 
-rows.forEach((element) => {
-  const position = element.querySelector('td:nth-child(2)');
-  const age = element.querySelector('td:nth-child(4)');
-
-  age.insertAdjacentHTML('afterend', `
-    <td>${position.textContent}</td>
-  `);
-});
+cloneElement(1, 4);
