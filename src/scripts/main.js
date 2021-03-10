c436d02c9608982e41e67bc77b3d6588293ff0d7
Ostap21 <57452764+Ostap21@users.noreply.github.com>
2021-03-10 13:19:11 +0200

    fix magic number

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,10 +4,11 @@ const table = document.querySelector('table');
 const tableRows = [...table.rows];
 
 const positionIndex = 1;
+const locationIndex = 4;
 
 tableRows.forEach(element => {
   const tableElementClone = element.cells[positionIndex].cloneNode(true);
 
   element.insertBefore(tableElementClone,
-    element.cells[4]);
+    element.cells[locationIndex]);
 });
