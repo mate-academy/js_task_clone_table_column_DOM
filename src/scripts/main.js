559add2b27903ec97d655b57eafc4742df4d9ce8
Onishchuk <onishchuk.danil@gmail.com>
2021-03-18 16:58:49 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const allTd = [...document.querySelectorAll('td')];
+
+for (let i = 0; i < allTd.length; i += 5) {
+  const copy = allTd[i + 1].cloneNode(true);
+
+  allTd[i + 4].insertAdjacentHTML('beforebegin', `
+    <td>${copy.textContent}</td>
+  `);
+}
+
+const allTh = [...document.querySelectorAll('th')];
+
+for (let i = 0; i < allTh.length; i += 5) {
+  const copy = allTh[i + 1].cloneNode(true);
+
+  allTh[i + 4].insertAdjacentHTML('beforebegin', `
+    <th>${copy.textContent}</th>
+  `);
+}
