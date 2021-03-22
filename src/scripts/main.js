39f1cfda83663dbaa31ddf78127ae1c6716b497c
Vitalii Nepomiashchyi <nepomyaschyi1@gmail.com>
2021-03-22 21:04:50 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 // write your code here
+
+const table = document.querySelector('table');
+
+const rows = table.rows;
+
+for (const row of rows) {
+  const cells = [...row.cells];
+
+  row.insertAdjacentHTML('beforeend', `${cells[1].outerHTML}`);
+  row.append(cells[cells.length - 1]);
+}
