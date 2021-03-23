016a9213babe33aeeeb82de5676c2707300b1f31
Inna Sorochynska <innasoro@gmail.com>
2021-03-23 22:19:03 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,7 @@
 'use strict';
 
-// write your code here
+const tr = document.querySelectorAll('tr');
+
+for (let i = 0; i < tr.length; i++) {
+  tr[i].insertBefore(tr[i].cells[1].cloneNode(true), tr[i].lastElementChild);
+};
