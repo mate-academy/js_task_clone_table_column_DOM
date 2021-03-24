1b2ca0cb1161bb3d94a914b9c5c2d9b21b4ab864
Iryna Haiduk <$ git config --global user.name Iryna>
2021-03-24 22:37:18 +0200

    Added new Column to table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
 // write your code here
+const rows = document.querySelectorAll('tr');
+
+for (let i = 0; i < rows.length; i++) {
+  const secondElementClone = rows[i].firstElementChild.nextElementSibling
+    .cloneNode(true);
+  const lastElement = rows[i].lastElementChild;
+
+  rows[i].insertBefore(secondElementClone, lastElement);
+}
