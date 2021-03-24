525c404d58f1a3c868e4466a4ee9139bcf600228
Serhii-Nikitin <serg.niki@i.ua>
2021-03-24 22:49:11 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('tr');
+
+for (let i = 0; i < rows.length; i++) {
+  const cell = rows[i].children[1].cloneNode(true);
+
+  rows[i].insertBefore(cell, rows[i].children[4]);
+}
