a2310a01344aab9ad73a7e71c2a7862e489a5c98
Ruslan Kalyniak <krv241085krv@icloud.com>
2021-03-25 13:10:46 +0200

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write your code here
+
+const table = document.querySelector('table');
+
+const rows = table.rows;
+const indexToClone = 1;
+
+for (const row of rows) {
+  const rowCells = [...row.cells];
+  const indexToInsert = rowCells.length - 1;
+  const clone = rowCells[indexToClone].cloneNode(true);
+  const lastCell = rowCells[indexToInsert];
+
+  row.insertBefore(clone, lastCell);
+}
