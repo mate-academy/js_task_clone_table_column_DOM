4a6329fcd63f06fbfdd18cbf8bcf549537f8bde6
Alexander Levchenko <a.levchenko603@gmail.com>
2021-03-25 16:49:13 +0200

    first solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const tableOfWorkers = document.querySelector('table');
+
+const rowsOfTable = tableOfWorkers.rows;
+let copyOfPosition;
+
+for (const row of rowsOfTable) {
+  copyOfPosition = row.cloneNode(true).children[1];
+  row.insertBefore(copyOfPosition, row.lastElementChild);
+}
