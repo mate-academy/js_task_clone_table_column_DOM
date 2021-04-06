82349db3e81dd511f77faaef4f8857e52b71b2a2
Alex Fedorchenko <mralex.fed@gmail.com>
2021-04-06 16:56:11 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
-// write your code here
+const trNode = document.querySelectorAll('tr');
+const trArr = Array.from(trNode);
+
+for (let i = 0; i < trArr.length; i++) {
+  trNode[i].children[4].before(trNode[i].children[1].cloneNode(true));
+}
