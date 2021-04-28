1e345269c1628a03f87bd8d38a6299e03238531a
Ivan Major <tarstarcas@gmail.com>
2021-04-28 12:26:50 +0300

    Changes V1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,31 +2,8 @@
 
 const table = document.querySelector('table');
 
-for (let i = 0; i < table.children.length; i++) {
-  const tag = table.children[i].tagName;
+const rows = table.rows;
 
-  if (tag === 'THEAD') {
-    const rowHead = table.tHead.rows[0];
-
-    const cellHead = document.createElement('th');
-
-    cellHead.innerHTML = rowHead.cells[1].innerText;
-    rowHead.insertBefore(cellHead, rowHead.cells[4]);
-  } else if (tag === 'TFOOT') {
-    const rowFoot = table.tFoot.rows[0];
-
-    const cellFoot = document.createElement('th');
-
-    cellFoot.innerHTML = rowFoot.cells[1].innerText;
-    rowFoot.insertBefore(cellFoot, rowFoot.cells[4]);
-  } else {
-    const rowsBody = table.tBodies[0].rows;
-
-    for (let j = 0; j < rowsBody.length; j++) {
-      const cellBody = document.createElement('td');
-
-      cellBody.innerHTML = rowsBody[j].cells[1].innerText;
-      rowsBody[j].insertBefore(cellBody, rowsBody[j].cells[4]);
-    };
-  };
+for (const row of rows) {
+  row.insertBefore(row.cells[1].cloneNode(true), row.cells[4]);
 };
