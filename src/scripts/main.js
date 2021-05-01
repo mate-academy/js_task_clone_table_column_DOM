9658e319a019c7752c6ff005209117ed720b9d76
Kostrica <romiorw@ukr.net>
2021-05-01 13:49:06 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const tableRows = [...document
+  .querySelector('table')
+  .rows,
+];
+
+for (let i = 0; i < tableRows.length; i++) {
+  const cells = tableRows[i].cells;
+  const cloneСell = cells[1].cloneNode(true);
+
+  cells[3].insertAdjacentElement('afterend', cloneСell);
+}
