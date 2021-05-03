26199eb5609b0a9f2c4cead4f141b12dc6d11315
Olga Vasylevskaya <olgavasylevskaya@ukr.net>
2021-05-03 18:30:42 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const headTr = document.querySelector('thead').children[0];
+const footTr = document.querySelector('tfoot').children[0];
+const body = document.querySelector('tbody');
+
+headTr.children[4].before(headTr.children[1].cloneNode(true));
+footTr.children[4].before(footTr.children[1].cloneNode(true));
+
+for (let i = 0; i < body.children.length; i++) {
+  const position = body.children[i].children[1];
+
+  body.children[i].children[4].before(position.cloneNode(true));
+}
