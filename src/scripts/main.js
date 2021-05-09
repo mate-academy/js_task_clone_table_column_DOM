4595bee8ba437e4c6d43799b5766c32665e88130
Vlad Trundenko <vlad.trundenko@gmail.com>
2021-05-09 14:27:21 +0300

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const allRow = [...document.querySelectorAll('tr')];
+
+function clone(nameOfClone, insertAfter) {
+  const clonePosition = [...allRow[0].children]
+    .findIndex(item => item.outerText === nameOfClone);
+  const pastePosition = [...allRow[0].children]
+    .findIndex(item => item.outerText === insertAfter) + 1;
+
+  for (let i = 0; i < allRow.length; i++) {
+    allRow[i].insertBefore(allRow[i].children[clonePosition].cloneNode(true),
+      allRow[i].children[pastePosition]);
+  }
+}
+
+clone('Position', 'Age');
