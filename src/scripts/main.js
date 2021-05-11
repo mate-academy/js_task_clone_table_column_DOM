49627944e231403c3f53d5d2bb1934b8cb82683a
romandubovskyi <rdubowskij@gmail.com>
2021-05-11 17:52:09 +0300

    implemented column clonning

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const sourceColumnName = 'Position';
+const targetColumnName = 'Salary';
+const tableHeader = document.querySelector('thead');
+const tableRows = document.querySelectorAll('tr');
+const sourceIndex = findColumnIndex(
+  tableHeader.children,
+  sourceColumnName
+);
+const targetIndex = findColumnIndex(tableHeader.children, targetColumnName);
+
+for (let i = 0; i < tableRows.length; i++) {
+  const clonedElement = tableRows[i].children[sourceIndex].cloneNode(true);
+
+  tableRows[i].children[targetIndex].before(clonedElement);
+}
+
+function findColumnIndex(header, columnTitle) {
+  return [...header[0].children].map(element => element.innerText)
+    .indexOf(columnTitle);
+}
