0a559025e48e42b3be6a255adf0500d564ab0464
Ihor Karpyn <igor82200@gmail.com>
2021-05-15 12:01:42 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (const tableElement of table.children) {
+  for (const tr of tableElement.children) {
+    tr.children[3].innerText = tr.children[1].innerText;
+  }
+}
