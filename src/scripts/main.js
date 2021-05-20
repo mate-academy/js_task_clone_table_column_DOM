07f271516424f398692e05c0b4e25c501160c6ee
Eugene Khablenko <eugene.khablenko@gmail.com>
2021-05-20 13:46:37 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const list = document.querySelectorAll('tr');
+
+for (let i = 0; i < [...list].length; i++) {
+  const row = list[i];
+
+  const item = document.createElement(list[i].firstElementChild.tagName);
+
+  item.innerHTML = row.cells[1].innerHTML;
+  row.insertBefore(item, row.cells[row.cells.length - 1]);
+}
