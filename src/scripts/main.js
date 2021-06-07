2fc28143ad2a4809d4b80e9fb4f45f3d17237503
VasylPerepichai <vperepichay@gmail.com>
2021-06-07 10:33:18 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 // write your code here
+const table = document.querySelector('table');
+
+for (const row of table.rows) {
+  const newCell = row.cells[1].cloneNode(false);
+
+  newCell.innerHTML = row.cells[1].innerHTML;
+
+  const posElem = row.lastElementChild;
+
+  posElem.before(newCell);
+}
