eca4c8591792d0f9c661311049233cefe219615f
Tolik <tolikdolinskiy@gmail.com>
2021-06-21 11:22:04 +0300

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 // write your code here
+const thead = document.querySelector('thead tr');
+const tbody = document.querySelectorAll('tbody tr');
+const tfoot = document.querySelector('tfoot tr');
+
+const thHead = document.createElement('th');
+const thFoot = document.createElement('th');
+
+thHead.innerHTML = 'Position';
+thFoot.innerHTML = 'Position';
+
+thead.children[4].before(thHead);
+tfoot.children[4].before(thFoot);
+
+tbody.forEach((item) => {
+  const td = document.createElement('td');
+
+  td.innerHTML = item.children[1].textContent;
+  item.children[4].before(td);
+});
