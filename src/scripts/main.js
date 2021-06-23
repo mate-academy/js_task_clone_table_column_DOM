da5d05d396cd51c9686c071efe5840ce194e7765
Tolik <tolikdolinskiy@gmail.com>
2021-06-23 09:09:10 +0300

    change review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,9 @@
 'use strict';
 
 // write your code here
-const thead = document.querySelector('thead tr');
-const tbody = document.querySelectorAll('tbody tr');
-const tfoot = document.querySelector('tfoot tr');
+const table = document.querySelectorAll('tr');
 
-const thHead = document.createElement('th');
-const thFoot = document.createElement('th');
-
-thHead.innerHTML = 'Position';
-thFoot.innerHTML = 'Position';
-
-thead.children[4].before(thHead);
-tfoot.children[4].before(thFoot);
-
-tbody.forEach((item) => {
+table.forEach((item) => {
   const td = document.createElement('td');
 
   td.innerHTML = item.children[1].textContent;
