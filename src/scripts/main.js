60f8c3d01e37d07042836bf67a4dc346a919f4ec
John <jozaman96@gmail.com>
2021-07-06 16:49:47 +0300

    code optimization

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,16 @@
 'use strict';
 
+const makeCloneAndInsert = (row, fromCell, toCell) => {
+  return row.insertBefore(
+    row.children[fromCell].cloneNode(true), row.children[toCell]
+  );
+};
+
 const topRow = document.querySelector('thead').children[0];
 const bottomRow = document.querySelector('tfoot').children[0];
 
-topRow.insertBefore(
-  topRow.children[1].cloneNode(true), topRow.children[4]
-);
-
-bottomRow.insertBefore(
-  bottomRow.children[1].cloneNode(true), bottomRow.children[4]
-);
+makeCloneAndInsert(topRow, 1, 4);
+makeCloneAndInsert(bottomRow, 1, 4);
 
 let counter = 0;
 const bodyRowsAmount = document.querySelector('tbody').children.length;
@@ -17,9 +18,6 @@ const bodyRowsAmount = document.querySelector('tbody').children.length;
 while (counter < bodyRowsAmount) {
   const currentRow = document.querySelector('tbody').children[counter];
 
-  currentRow.insertBefore(
-    currentRow.children[1].cloneNode(true), currentRow.children[4]
-  );
-
+  makeCloneAndInsert(currentRow, 1, 4);
   counter++;
 }
