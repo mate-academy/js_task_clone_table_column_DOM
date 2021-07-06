173f94da06be037c70b4978c88d379ee533b9155
Artem Dzhereleiko <dgerik2015@gmail.com>
2021-07-06 17:59:28 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+[...table.children].forEach(item => cloneColumn(item));
+
+function cloneColumn(row) {
+  for (let i = 0; i < [...row.children].length; i++) {
+    row.rows[i].cells[4].before((row.rows[i].cells[1]).cloneNode(true));
+  }
+}
