b139e6ceda9eceb9145cf384626deb63750c8e2d
Dmytro Prontenko <dprontenko@gmail.com>
2021-07-07 19:13:16 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write your code here
+const tableRows = [...document.querySelector('table').rows];
+
+for (const row of tableRows) {
+  // console.log(row.lastElementChild);
+
+  if (row.parentElement.nodeName === 'THEAD'
+  || row.parentElement.nodeName === 'TFOOT') {
+    const parentSection = row.parentElement.children[0];
+    const newCol = document.createElement('th');
+
+    newCol.textContent = row.children[1].textContent;
+
+    parentSection.insertBefore(newCol, parentSection.lastElementChild);
+  };
+
+  if (row.parentElement.nodeName === 'TBODY') {
+    const parentSection = row.parentElement.children[row.rowIndex - 1];
+    const newCol = document.createElement('td');
+
+    newCol.textContent = row.children[1].textContent;
+
+    parentSection.insertBefore(newCol, parentSection.lastElementChild);
+  }
+};
