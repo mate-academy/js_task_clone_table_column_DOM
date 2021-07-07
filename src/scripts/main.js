ca4a3c7a8c8e7e7dd1fed722a7fd9009a9ae3d80
Artem Dzhereleiko <dgerik2015@gmail.com>
2021-07-07 11:17:39 +0300

    add var for magic numbers

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,17 @@
 'use strict';
 
 const table = document.querySelector('table');
+const fromCeil = 1;
+const toCeil = 4;
 
-[...table.children].forEach(partOfTable => cloneColumn(partOfTable));
+[...table.children].forEach(
+  partOfTable => cloneColumn(partOfTable, fromCeil, toCeil)
+);
 
-function cloneColumn(tablePart) {
+function cloneColumn(tablePart, fromCell, toCell) {
   for (let i = 0; i < [...tablePart.children].length; i++) {
-    tablePart.rows[i].cells[4]
-      .before((tablePart.rows[i].cells[1])
+    tablePart.rows[i].cells[toCell]
+      .before((tablePart.rows[i].cells[fromCell])
         .cloneNode(true));
   }
 }
