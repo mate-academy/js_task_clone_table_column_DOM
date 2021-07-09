1f64ddd73f5782cdfec579c3c01b15793da3b3a0
Bishko <yurik322412@gmail.com>
2021-07-09 15:26:32 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const tableElement = document.querySelector('table').children;
+const [thead, tbody, tfood] = tableElement;
+
+function cloneTableColumn(element) {
+  [...element].forEach(tr => {
+    tr.children[3].insertAdjacentHTML('afterend', tr.children[1].outerHTML);
+  });
+}
+
+cloneTableColumn(thead.children);
+cloneTableColumn(tbody.children);
+cloneTableColumn(tfood.children);
