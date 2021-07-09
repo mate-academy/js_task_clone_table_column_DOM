ba46072b547357481d8d47cf1542b7edcb30ca7d
David Dohru <tronsmartmy@gmail.com>
2021-07-09 17:56:44 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,6 @@
 'use strict';
 
-// write your code here
+const tableRow = [...document.querySelectorAll('tr')];
+
+tableRow.map(item =>
+  (item.children[4].before(item.children[1].cloneNode(true))));
