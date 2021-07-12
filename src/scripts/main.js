1523ee09a9ef84e24874217befa9fa0a6147279a
Oleksii Blezniuk <oleksii.blezniuk@gmail.com>
2021-07-12 21:01:00 +0300

    task is completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
 // write your code here
+const rows = [...document.querySelectorAll('tr')];
+const columnCopy = rows.map(el => {
+  return el.children[1].cloneNode(true);
+});
+
+rows.forEach((row, i) => {
+  row.insertBefore(columnCopy[i], row.children[4]);
+});
