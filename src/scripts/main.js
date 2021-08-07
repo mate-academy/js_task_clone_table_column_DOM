321aef7cd5e4f26ce4bd598e656626579c978d77
Raz-Dva <my.poshta.goncharov@gmail.com>
2021-08-07 06:18:32 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const elementTable = document.querySelector('table');
+const tableRows = [...elementTable.rows];
+
+for (let i = 0; i < tableRows.length; i++) {
+  const tableCells = [...tableRows[i].cells];
+
+  const newCell = elementTable.rows[i].insertCell(5);
+
+  newCell.outerHTML = tableCells[4].outerHTML;
+  tableCells[4].outerHTML = tableCells[1].outerHTML;
+}
