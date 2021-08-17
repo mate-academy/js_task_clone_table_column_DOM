185de6d502f33f7f74e25e3567210259123d56f1
Olga Bordiug <obordiug.gmail.com>
2021-08-17 18:51:17 +0300

    solution after comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,7 @@
 'use strict';
 
-const table = document.querySelector('table');
+const tableRows = document.querySelectorAll('tr');
 
-table.tHead.rows[0].lastElementChild
-  .before(table.tHead.rows[0].cells[1].cloneNode(true));
-
-for (let i = 1; i < table.rows.length - 1; i++) {
-  table.rows[i].lastElementChild.before(table.rows[i].cells[1].cloneNode(true));
+for (let i = 0; i < tableRows.length; i++) {
+  tableRows[i].lastElementChild.before(tableRows[i].cells[1].cloneNode(true));
 }
-
-table.tFoot.rows[0].lastElementChild
-  .before(table.tFoot.rows[0].cells[1].cloneNode(true));
