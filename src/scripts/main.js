163e58a1739ec1f5c1b01e5477f67639531caa98
Roman <romakelemen@gmail.com>
2021-09-05 14:35:53 +0300

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (const rowCount of table.rows) {
+  for (const cell of rowCount.children) {
+    if (cell.cellIndex === 1) {
+      const copyCell = cell.cloneNode(true);
+
+      rowCount.children[3].after(copyCell);
+    }
+  }
+}
