441ec7263da513df25120fbe6ef9d0b67f8bca0c
Tykhon Shuliak <tiandzh2013@gmail.com>
2021-09-08 16:10:04 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
 // write your code here
+
+const list = document.querySelectorAll('tr');
+
+for (let i = 0; i < list.length; i++) {
+  const item = list[i].children[1];
+
+  list[i].children[4].before(item.cloneNode(true));
+}
