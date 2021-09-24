3a224759477ee55959d948cb9b4546c9f5c1ebfb
MaxStets <weawer458@gmail.com>
2021-09-24 15:39:49 +0300

    Solution -- clone_table_column_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
 // write your code here
+const dashboard = document.querySelector('table');
+
+function copyPateColumn(nodeTable, numColumn = 1, insertColumn = 2) {
+  const rows = [...nodeTable.rows];
+
+  rows.map(row => {
+    const rowChildren = row.children;
+
+    const elTarget = rowChildren[insertColumn - 1];
+    const elCopy = rowChildren[numColumn - 1].cloneNode(true);
+
+    elTarget.insertAdjacentElement('beforebegin', elCopy);
+  });
+}
+
+copyPateColumn(dashboard, 2, 5);
