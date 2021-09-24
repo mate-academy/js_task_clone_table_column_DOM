a3dcbf855d7e0ab94797bbf30544cbe0d8f1a0ff
Andriy Shevtsiv <andriy.shevtsiv@gmail.com>
2021-09-24 13:06:44 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 // write your code here
+
+const table = document.querySelector('table');
+
+for (let i = 0; i < table.rows.length; i++) {
+  const row = table.rows[i];
+  const position = row.cells[1];
+  const salary = row.cells[4];
+
+  row.append(position.cloneNode(true));
+  row.append(salary);
+}
