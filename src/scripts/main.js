67fbfdbecffdfb33407fef2f9823557a7180e574
bohdan_kochetov <bodja1208@gmail.com>
2021-09-26 11:10:50 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const tr = document.querySelectorAll('tr');
+
+for (const key in tr) {
+  const item = tr[key].children[1];
+  const item2 = tr[key].children[3];
+  const clone = item.cloneNode(true);
+
+  item2.after(clone);
+}
