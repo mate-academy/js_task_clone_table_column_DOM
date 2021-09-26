c7752417002d2e8f43181cd2b883a1a78376a2fd
bohdan_kochetov <bodja1208@gmail.com>
2021-09-26 11:11:50 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,9 +2,9 @@
 
 const tr = document.querySelectorAll('tr');
 
-for (const key in tr) {
-  const item = tr[key].children[1];
-  const item2 = tr[key].children[3];
+for (const th in tr) {
+  const item = tr[th].children[1];
+  const item2 = tr[th].children[3];
   const clone = item.cloneNode(true);
 
   item2.after(clone);
