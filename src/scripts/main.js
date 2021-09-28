6856a81e8ec20f9fff179193e4cc4d6993c5900d
Dmytro Baltak <dmitrybaltak@gmail.com>
2021-09-28 21:40:24 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const tableEl = document.querySelector('table');
+// console.dir(tableEl.tBodies[0].children);
+
+function cloneTableColumn(table) {
+  [...table.children].forEach(node => {
+    [...node.rows].forEach(tr => {
+      const cellCopy = tr.cells[1].cloneNode(true);
+
+      tr.cells[tr.cells.length - 1].before(cellCopy);
+    });
+  });
+}
+
+cloneTableColumn(tableEl);
