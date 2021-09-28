2cf7e31495bea3f0eb2761845c2c743294a2cc47
Andriy Shevtsiv <andriy.shevtsiv@gmail.com>
2021-09-28 10:07:01 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,7 +7,7 @@ const table = document.querySelector('table');
 for (let i = 0; i < table.rows.length; i++) {
   const row = table.rows[i];
   const position = row.cells[1];
-  const salary = row.cells[4];
+  const salary = row.lastElementChild;
 
   row.append(position.cloneNode(true));
   row.append(salary);
