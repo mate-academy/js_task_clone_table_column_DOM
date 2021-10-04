c0a753d6e72a402ba9e3f4e02d399a02639a6993
Nazar Valchyshyn <nvalja@gmail.com>
2021-10-04 16:32:05 +0300

    made task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const table = [...document.querySelectorAll('tr')];
+
+for (const row of table) {
+  const item = row.children[1].cloneNode(true);
+  const tag = item.nodeName.toLowerCase();
+
+  row.children[4].insertAdjacentHTML('beforebegin', `
+
+    <${tag}>${item.textContent}</${tag}>
+  `);
+
+  // row.children[4].before(row.children[1].cloneNode(true));
+}
