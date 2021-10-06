dcb6afb7528b3ce5c12cd7fbb8bdd4dd08c72a40
Roman Prokopiv <rprokopiv@gmail.com>
2021-10-06 12:45:59 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const table = document.getElementsByTagName('table');
+
+for (const partTable of table[0].children) {
+  for (const row of partTable.children) {
+    const clonTeg = document.createElement(
+      row.children[1].nodeName.toLocaleLowerCase());
+
+    clonTeg.textContent = row.children[1].innerHTML;
+    row.children[4].before(clonTeg);
+  };
+};
