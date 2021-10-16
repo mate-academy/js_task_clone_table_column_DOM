fecfae2de03e3212ca03adf1bff6b04ee9d0d97c
Dima Narozhnyi <dmitriy.narozhnyi@gmail.com>
2021-10-16 18:33:19 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const dashboard = document.querySelector('table');
+const rows = dashboard.rows;
+
+function getColumn(columnNumberForCopy) {
+  const column = [];
+
+  for (let i = 0; i < rows.length; i++) {
+    column.push(rows[i].cells[columnNumberForCopy - 1]);
+  }
+
+  return column;
+}
+
+function createColumn(column, columnNumberAfterPast) {
+  for (let i = 0; i < rows.length; i++) {
+    rows[i].children[columnNumberAfterPast - 1]
+      .before(column[i].cloneNode(true));
+  }
+}
+createColumn(getColumn(2), 5);
