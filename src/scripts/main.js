9857321c540fb0fe80304f6c5a1c3b329078edbb
Serhii Dvorets <dvorets.sergey@gmail.com>
2021-10-24 15:28:24 +0300

    1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
 // write your code here
+const element = document.querySelectorAll('tr');
+
+[...element].map(item => {
+  const tag = (item.children[0].tagName).toLowerCase();
+  const newElement = document.createElement(tag);
+  const copy = item.children[1].textContent;
+
+  newElement.textContent = copy;
+  item.children[4].before(newElement);
+});
