f383251c17e7863ecad515f82b66ea99ebfd9727
Serhii Dvorets <dvorets.sergey@gmail.com>
2021-10-26 07:52:07 +0300

    2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,5 @@
 // write your code here
 const element = document.querySelectorAll('tr');
 
-[...element].map(item => {
-  const tag = (item.children[0].tagName).toLowerCase();
-  const newElement = document.createElement(tag);
-  const copy = item.children[1].textContent;
-
-  newElement.textContent = copy;
-  item.children[4].before(newElement);
-});
+[...element].forEach(item => 
+  item.lastElementChild.before(item.children[1].cloneNode(true)))
