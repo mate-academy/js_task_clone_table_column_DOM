7b52df83e723d63968a6c651592f8e451b093ad2
Dina Samoilova <s13.dina@gmail.com>
2021-11-08 22:47:42 +0200

    remove not need const

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,20 @@
 'use strict';
 
 const table = document.querySelector('table');
-const tabHead = table.tHead;
-const tabFoot = table.tFoot;
-const tabBody = table.children[1];
 
 const thHead = document.createElement('th');
 
 thHead.textContent = table.rows[0].cells[1].textContent;
-
-tabHead.children[0].children[4].before(thHead.cloneNode(true));
+table.tHead.children[0].children[4].before(thHead.cloneNode(true));
 
 const thFoot = document.createElement('th');
 
 thFoot.textContent = table.rows[table.rows.length - 1].cells[1].textContent;
+table.tFoot.children[0].children[4].before(thFoot.cloneNode(true));
 
-tabFoot.children[0].children[4].before(thFoot.cloneNode(true));
-
-for (let i = 0; i < tabBody.rows.length; i++) {
+for (let i = 0; i < table.children[1].rows.length; i++) {
   const td = document.createElement('td');
 
-  td.textContent = tabBody.rows[i].cells[1].textContent;
-
-  tabBody.children[i].children[4].before(td.cloneNode(true));
+  td.textContent = table.children[1].rows[i].cells[1].textContent;
+  table.children[1].children[i].children[4].before(td.cloneNode(true));
 }
