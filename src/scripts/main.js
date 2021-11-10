b715e2a171353ac1ab0920fded66a0a6099506cc
Dima Permiakov <dimapermyakov2532@gmail.com>
2021-11-10 16:17:01 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write your code here
+const list = document.querySelectorAll('tr');
+
+// [...list].map(elem => console.log(elem.children));
+
+for (const key in [...list]) {
+  list[key].insertBefore(list[key]
+    .children[1].cloneNode(true), list[key].children[4]);
+}
