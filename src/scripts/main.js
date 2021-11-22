aeecf9183f230fab7b4b97f9f140c1a617681d19
Andrey Grigoriev <nkgrand25@gmail.com>
2021-11-22 23:45:04 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+/* Main solution */
+
+const table = document.querySelector('table');
+const rows = table.rows;
+
+for (const row of rows) {
+  const cloned = row.cells[1].cloneNode(true);
+
+  row.insertBefore(cloned, row.children[4]);
+}
+
+/* Alternative solution */
+/*
+const rows = document.querySelectorAll('tr');
+
+[...rows].forEach(el => {
+  const cloned = [...el.children][1].cloneNode(true);
+
+  el.children[4].before(cloned);
+});
+ */
