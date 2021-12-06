f1b9d9db28e773b9682b97d4b80c00e566c9ff36
Pasechnik Vitaly <pasechnik.vitaly.11@gmail.com>
2021-12-06 16:05:45 +0200

    last

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,7 @@
 'use strict';
 
-const table = document.body.firstElementChild;
+const tr = document.querySelectorAll('tr');
 
-for (let i = 0; i < table.rows.length; i++) {
-  if (i === 0) {
-    const th = document.createElement('th');
-
-    th.innerText = table.rows[0].cells[1].innerText;
-    table.rows[0].cells[4].before(th);
-  } else {
-    const td = document.createElement('td');
-
-    td.innerText = table.rows[i].cells[1].innerText;
-    table.rows[i].cells[4].before(td);
-  };
+for (const item of tr) {
+  item.cells[4].before(item.cells[1].cloneNode(true));
 }
