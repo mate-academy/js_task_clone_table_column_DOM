1b6a3c7003e2551e56afb7212af8a9bc62478875
Vova Alputov <alputov1998@gmail.com>
2021-12-22 01:45:31 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const thead = document.querySelector('thead').children;
+const tbody = document.querySelector('tbody').children;
+const tfoot = document.querySelector('tfoot').children;
+
+function clone(tElem) {
+  [...tElem].forEach(element => {
+    const copy = element.children[1].cloneNode(true);
+
+    element.children[3].after(copy);
+  });
+};
+
+clone(thead);
+clone(tbody);
+clone(tfoot);
