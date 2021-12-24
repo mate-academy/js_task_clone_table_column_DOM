4f19da93d583c38a2177c72e85edf89968a2b3af
Pavlo Khashchevskyi <PikiMapo92@gmail.com>
2021-12-24 17:54:39 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+const collum = document.querySelector('tbody').children;
+const tHead = document.querySelector('thead').children[0];
+const tFoot = document.querySelector('tfoot').children[0];
+
+tHead.children[3].insertAdjacentHTML('afterend', `
+<th>${tHead.children[1].textContent}</th>
+`);
+
+tFoot.children[3].insertAdjacentHTML('afterend', `
+<th>${tFoot.children[1].textContent}</th>
+`);
+
+const newCollum = [...collum].map(element => element.children[1].textContent);
+
+for (const tr of collum) {
+  const x = tr.children[3];
+  const newWord = newCollum.shift();
+
+  x.insertAdjacentHTML('afterend', `
+     <td>${newWord}</td>
+   `);
+}
