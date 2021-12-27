0bac74b0bee79c82bd2210dffacd03062e530aa3
KHTRE <KHTREE@gmail.com>
2021-12-27 12:32:57 +0200

    works

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const lines = document.querySelectorAll('tr');
+
+for (let i = 0; i < lines.length; i++) {
+  const lineCells = lines[i];
+  const cellsValues = lines[i].cells;
+  const extraCell = document.createElement('td');
+  const extraCellBold = document.createElement('th');
+
+  if (i > 0 && i < lines.length - 1) {
+    lineCells.append(extraCell);
+    extraCell.textContent = cellsValues[4].textContent;
+  } else {
+    lineCells.append(extraCellBold);
+    extraCellBold.textContent = cellsValues[4].textContent;
+  }
+
+  cellsValues[4].textContent = cellsValues[1].textContent;
+}
