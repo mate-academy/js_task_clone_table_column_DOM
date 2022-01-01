6fc38413c923612bea6c9c805337485078af36b4
Anna Tovkailo <babylike24@gmail.com>
2022-01-01 21:45:38 +0200

    solution2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,29 +1,7 @@
 'use strict';
 
-const query = document.querySelectorAll('tr');
+const rows = document.querySelectorAll('tr');
 
-const headText = query[0].children[1].innerHTML;
-const newHeadItem = document.createElement('th');
-
-newHeadItem.innerHTML = headText;
-
-query[0].children[3].after(newHeadItem);
-
-for (let i = 1; i < query.length - 1; i++) {
-  const thisText = query[i].children[1].innerHTML;
-  const newItem = document.createElement('td');
-
-  newItem.innerHTML = thisText;
-
-  const thisString = query[i].querySelectorAll('td');
-
-  thisString[3].after(newItem);
+for (let i = 0; i < rows.length; i++) {
+  rows[i].children[3].after(rows[i].children[1].cloneNode(true));
 }
-
-const footer = document.querySelector('tfoot');
-const footerItems = (footer.children[0].children);
-const footerName = footerItems[1].innerHTML;
-const newfooterItem = document.createElement('th');
-
-newfooterItem.innerHTML = footerName;
-footerItems[3].after(newfooterItem);
