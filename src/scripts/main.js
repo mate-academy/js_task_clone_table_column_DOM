7356be54c15a62deec113270645518ee6a9a0c6a
Anna Morozevych <annyshka2210@gmail.com>
2022-01-03 17:10:38 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const items = document.querySelectorAll('tr');
+
+for (const item of items) {
+  let newItem;
+
+  switch (item.firstElementChild.tagName) {
+    case 'TH':
+      newItem = document.createElement('th');
+      break;
+
+    default:
+      newItem = document.createElement('td');
+  }
+
+  newItem.textContent = item.children[1].textContent;
+  item.children[4].before(newItem);
+}
