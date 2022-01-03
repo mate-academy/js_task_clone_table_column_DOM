711ab83578c2dcdc30fb03f0b9d406e1414c9f63
Maryna Saikun <marynasaikun@gmail.com>
2022-01-03 21:33:30 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const tableRow = document.querySelectorAll('tr');
+
+tableRow.forEach(row => {
+  const tdCopy = row.children[1].cloneNode(true);
+  const trCopy = [...row.children];
+
+  trCopy.splice(4, 0, tdCopy);
+  row.append(...trCopy);
+});
