2672feec93ed9c54e8d27b53624023b94a2e5d0c
Klets Serhii <sergklets75@gmail.com>
2022-01-08 12:19:59 +0200

    Solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 // write your code here
+const trs = [...document.querySelectorAll('tr')];
+
+trs.map(el => {
+  const children = [...el.children];
+  const positionCopy = document.createElement(children[1].tagName);
+
+  positionCopy.textContent = children[1].textContent;
+
+  const lastChild = children[children.length - 1];
+
+  lastChild.before(positionCopy);
+}
+);
