00d879e702b383f19d130bee6fa48420e5111528
Klets Serhii <sergklets75@gmail.com>
2022-01-08 12:28:24 +0200

    use cloneNode

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,12 +5,8 @@ const trs = [...document.querySelectorAll('tr')];
 
 trs.map(el => {
   const children = [...el.children];
-  const positionCopy = document.createElement(children[1].tagName);
-
-  positionCopy.textContent = children[1].textContent;
-
   const lastChild = children[children.length - 1];
 
-  lastChild.before(positionCopy);
+  lastChild.before(children[1].cloneNode(true));
 }
 );
