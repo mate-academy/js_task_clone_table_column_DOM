a00ecc37aaa99e5d5b35b0bb1905e4be97af0a13
Ievgen Volkov <neoodessa86@gmail.com>
2022-01-25 23:03:17 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
 // write your code here
+
+const table = document.querySelector('table');
+const tr = [...document.querySelectorAll('tr')];
+
+tr.map(function clone(item) {
+  if (item.parentElement === table.children[0]
+    || item.parentElement === table.children[2]) {
+    const cloneTh = document.createElement('th');
+
+    cloneTh.innerHTML = item.children[1].innerText;
+    item.children[3].after(cloneTh);
+  }
+
+  if (item.parentElement === table.children[1]) {
+    const cloneTd = document.createElement('td');
+
+    cloneTd.innerHTML = item.children[1].innerText;
+    item.children[4].before(cloneTd);
+  }
+});
