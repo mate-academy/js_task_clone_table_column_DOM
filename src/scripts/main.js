e3bf55d1766415d976e044e445bb5891a87c2463
Ivan Shulhan <92580512+IvanShulhan@users.noreply.github.com>
2022-01-29 13:39:58 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write your code here
+// const head = document.querySelector('thead');
+// const body = document.querySelector('tbody');
+// const footer = document.querySelector('tfoot');
+
+// function copyPaste(part) {
+//   const collection = part.children;
+
+//   for (let i = 0; i < collection.length; i++) {
+//     const row = collection[i];
+//     const copy = row.children[1].cloneNode(true);
+
+//     row.children[4].before(copy);
+//   }
+// }
+
+// copyPaste(head);
+// copyPaste(body);
+// copyPaste(footer);
+
+const rowList = document.querySelectorAll('tr');
+
+for (let i = 0; i < rowList.length; i++) {
+  const row = rowList[i];
+  const copy = row.children[1].cloneNode(true);
+
+  row.children[4].before(copy);
+}
