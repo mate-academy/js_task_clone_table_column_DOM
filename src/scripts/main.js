24011c50e357e7c16a301f7569b23811f3783ae3
Ivan Shulhan <92580512+IvanShulhan@users.noreply.github.com>
2022-01-31 14:07:22 +0200

    remove-comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,5 @@
 'use strict';
 
-// const head = document.querySelector('thead');
-// const body = document.querySelector('tbody');
-// const footer = document.querySelector('tfoot');
-
-// function copyPaste(part) {
-//   const collection = part.children;
-
-//   for (let i = 0; i < collection.length; i++) {
-//     const row = collection[i];
-//     const copy = row.children[1].cloneNode(true);
-
-//     row.children[4].before(copy);
-//   }
-// }
-
-// copyPaste(head);
-// copyPaste(body);
-// copyPaste(footer);
-
 const rowList = document.querySelectorAll('tr');
 
 for (let i = 0; i < rowList.length; i++) {
