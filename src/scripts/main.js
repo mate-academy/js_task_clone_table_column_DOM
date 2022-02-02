8aadbbbc3db02d407ac9c0c4c3bc0b765230d92f
Luhovyi <95681104+Luhovyi@users.noreply.github.com>
2022-02-02 17:16:23 +0200

    js_task_clone_table_column_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
 // write your code here
+const clone = [...document.querySelectorAll('tr')];
+
+clone.map(el => {
+  const children = [...el.children];
+  const lastChild = children[children.length - 1];
+
+  lastChild.before(children[1].cloneNode(true));
+}
+);
