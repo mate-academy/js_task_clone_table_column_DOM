ca50eec0a0f4eb153619e6eccd6d1f54c3dbec3c
Dima Cherednyk <dicherednyk@gmail.com>
2022-02-10 19:57:46 +0200

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write your code here
+const header = document.querySelector('thead');
+const head = header.children[0].children[1];
+
+const copy = document.createElement('th');
+
+copy.textContent = head.textContent;
+
+header.children[0].insertBefore(copy,
+  header.children[0].children[header.children[0].children.length - 1]);
+
+const tBody = document.querySelector('tbody');
+
+for (let i = 0; i < tBody.children.length; i++) {
+  const b = tBody.children[i].children[1];
+  const copy2 = document.createElement('td');
+
+  copy2.textContent = b.textContent;
+
+  tBody.children[i].insertBefore(copy2,
+    tBody.children[i].children[tBody.children[i].children.length - 1]);
+};
+
+const footer = document.querySelector('tfoot');
+const foot = header.children[0].children[1];
+const copy3 = document.createElement('th');
+
+copy3.textContent = foot.textContent;
+
+footer.children[0].insertBefore(copy3,
+  footer.children[0].children[footer.children[0].children.length - 1]);
