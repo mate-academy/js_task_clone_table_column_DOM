8949ac7c3a0ea77c7aeb9e8fd3f7401da61b0181
Дима Краснобокий <krasnoboky.dima@gmail.com>
2022-03-08 15:44:16 +0200

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const tHead = document.querySelector('thead').children[0];
+const tBody = document.querySelector('tbody').children;
+const tFoot = document.querySelector('tfoot').children[0];
+
+const allChilds = [tHead, tFoot, ...tBody];
+
+for (const child of allChilds) {
+  child.children[2].after(child.children[1].cloneNode(true));
+}
