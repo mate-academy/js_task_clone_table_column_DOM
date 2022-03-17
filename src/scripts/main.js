5bd95fab6a52321f4311e944731d61bd75474169
Yuri Taraday <KaIIRu3@gmail.com>
2022-03-17 19:02:06 +0200

    DOM clone

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelector('table').rows;
+
+for (const row of rows) {
+  const cell = row.children[1].cloneNode(true);
+
+  row.children[4].before(cell);
+}
