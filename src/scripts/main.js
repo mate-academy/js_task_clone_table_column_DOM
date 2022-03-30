6e04b222fdcb7d632e291a7f68fd5626478be79a
Hlib Schebalkov <glebusha228@gmail.com>
2022-03-30 22:14:51 +0300

    Splution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const tr = document.querySelectorAll('tr');
+
+for (let i = 0; i < tr.length; i++) {
+  const thLoop = tr[i].querySelector('th');
+  const tdLoop = tr[i].querySelector('td');
+
+  if (tr[i].contains(thLoop)) {
+    const th = tr[i].querySelectorAll('th')[1];
+
+    tr[i].children[3].after(th.cloneNode(true));
+  }
+
+  if (tr[i].contains(tdLoop)) {
+    const td = tr[i].querySelectorAll('td')[1];
+
+    tr[i].children[3].after(td.cloneNode(true));
+  }
+}
