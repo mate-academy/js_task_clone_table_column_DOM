d71bd6d389fb63977deb02007dbcc1e2350fa7b6
Hlib Schebalkov <glebusha228@gmail.com>
2022-04-04 20:30:09 +0300

    delete IF

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,18 +3,8 @@
 const tr = document.querySelectorAll('tr');
 
 for (let i = 0; i < tr.length; i++) {
-  const thLoop = tr[i].querySelector('th');
-  const tdLoop = tr[i].querySelector('td');
+  const child = tr[i].children[1];
+  const thClone = child.cloneNode(true);
 
-  if (tr[i].contains(thLoop)) {
-    const th = tr[i].querySelectorAll('th')[1];
-
-    tr[i].children[3].after(th.cloneNode(true));
-  }
-
-  if (tr[i].contains(tdLoop)) {
-    const td = tr[i].querySelectorAll('td')[1];
-
-    tr[i].children[3].after(td.cloneNode(true));
-  }
+  tr[i].children[3].after(thClone);
 }
