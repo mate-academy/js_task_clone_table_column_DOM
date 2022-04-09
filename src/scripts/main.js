138bcf54275b42b78a56ebc18754cc5ec229e9e6
Ostap Tomashivskyi <o.tomash650416@gmail.com>
2022-04-09 21:56:27 +0300

    Added column

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const trCollection = document.querySelectorAll('tr');
+
+for (let i = 0; i < trCollection.length; i++) {
+  const tableRow = trCollection[i];
+  const tableCell = tableRow.children[1].cloneNode(true);
+  const trChildren = tableRow.children;
+
+  trChildren[trChildren.length - 1].before(tableCell);
+}
