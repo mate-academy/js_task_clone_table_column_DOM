b99ddda553c989fbd9b4afd1cf0692ea2c648e18
Oleksii Pinchuk <oleksii.pin4uk@gmail.com>
2022-04-11 14:53:02 +0300

    table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const table = document.querySelectorAll('tr');
+
+for (const row of table) {
+  const cell = row.children[1].cloneNode(true);
+
+  row.children[3].after(cell);
+}
