fd62d4d9c811a57ce71838b08a695b61ee64b7ec
Denys Lipin <lipin.transstream@gmail.com>
2022-04-12 16:32:13 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (const row of table.rows) {
+  const elementName = row.children[1].innerText;
+
+  if (elementName === 'Position') {
+    const thElement = document.createElement('th');
+
+    thElement.innerText = elementName;
+    row.insertBefore(thElement, row.children[4]);
+  } else {
+    const tdElement = document.createElement('td');
+
+    tdElement.innerText = elementName;
+    row.insertBefore(tdElement, row.children[4]);
+  }
+}
