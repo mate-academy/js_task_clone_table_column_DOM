c8adc0100532ae7655781b7328cab5009a93e063
Max Shylenko <maxshylenko@gmail.com>
2022-04-14 15:28:38 +0200

    clone table column DOM is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const rows = [...table.rows];
+const numberOfCells = rows[0].cells.length;
+
+const getCloneOfCell = (listOfRows, numberOfCell) => {
+  const clone = [];
+
+  for (const row of listOfRows) {
+    clone.push(row.cells[numberOfCell]);
+  }
+
+  return clone;
+};
+
+const addCloneOfCell = (listOfRows, cloneOfCell) => {
+  let i = 0;
+
+  for (const row of listOfRows) {
+    while (i <= cloneOfCell.length) {
+      row.children[numberOfCells - 1].before(cloneOfCell[i].cloneNode(true));
+      i++;
+      break;
+    }
+  }
+};
+
+addCloneOfCell(rows, getCloneOfCell(rows, 1));
