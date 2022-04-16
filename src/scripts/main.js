211f7e84bd11fcbf93fdfdeb489d10ac521515b0
Александр Погребняк <pogrebnyak19999@gmail.com>
2022-04-16 18:51:03 +0300

    Refactor code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,32 +2,10 @@
 
 const table = document.querySelector('table');
 
-const content = [];
-const title = [];
+[...table.children].forEach(block => {
+  [...block.children].forEach(tr => {
+    const td = tr.children[1];
 
-document.querySelectorAll('table tr').forEach(item => {
-  if (item.querySelector('td')) {
-    const td = item.querySelectorAll('td')[1];
-
-    content.push(td.cloneNode(true));
-  } else {
-    const th = item.querySelectorAll('th')[1];
-
-    title.push(th.cloneNode(true));
-  }
-});
-
-let trCounter = 0;
-
-table.querySelectorAll('tbody tr').forEach(tr => {
-  tr.insertBefore(content[trCounter], tr.querySelectorAll('td')[4]);
-  trCounter++;
-});
-
-table.querySelectorAll('thead tr').forEach(tr => {
-  tr.insertBefore(title[0], tr.querySelectorAll('th')[4]);
-});
-
-table.querySelectorAll('tfoot tr').forEach(tr => {
-  tr.insertBefore(title[1], tr.querySelectorAll('th')[4]);
+    tr.children[4].before(td.cloneNode(true));
+  });
 });
