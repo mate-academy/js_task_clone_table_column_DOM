38070d21cfd82a1da4d52f3b830edae4f5505429
Александр Погребняк <pogrebnyak19999@gmail.com>
2022-04-16 18:17:18 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const content = [];
+const title = [];
+
+document.querySelectorAll('table tr').forEach(item => {
+  if (item.querySelector('td')) {
+    const td = item.querySelectorAll('td')[1];
+
+    content.push(td.cloneNode(true));
+  } else {
+    const th = item.querySelectorAll('th')[1];
+
+    title.push(th.cloneNode(true));
+  }
+});
+
+let trCounter = 0;
+
+table.querySelectorAll('tbody tr').forEach(tr => {
+  tr.insertBefore(content[trCounter], tr.querySelectorAll('td')[4]);
+  trCounter++;
+});
+
+table.querySelectorAll('thead tr').forEach(tr => {
+  tr.insertBefore(title[0], tr.querySelectorAll('th')[4]);
+});
+
+table.querySelectorAll('tfoot tr').forEach(tr => {
+  tr.insertBefore(title[1], tr.querySelectorAll('th')[4]);
+});
