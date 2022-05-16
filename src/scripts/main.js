ba475ba77696f8e74894c7dc6a8bb75c3d4aca56
ShapovalDenys <denys.igorevich@gmail.com>
2022-05-16 15:35:28 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write your code here
+// add header clone
+const tableHeader = document.querySelector('thead');
+const headerToClone = tableHeader.rows[0].cells[1].cloneNode(true);
+const headerRows = tableHeader.rows[0];
+
+headerRows.insertBefore(headerToClone, headerRows.cells[4]);
+
+// add body clone
+const tableBody = document.querySelector('tbody');
+const tableBodyRows = tableBody.rows;
+
+for (let i = 0; i < tableBodyRows.length; i++) {
+  const bodyToClone = tableBody.rows[i].cells[1].cloneNode(true);
+  const bodyRows = tableBody.rows[i];
+
+  bodyRows.insertBefore(bodyToClone, bodyRows.cells[4]);
+}
+
+// add footer clone
+const tableFooter = document.querySelector('tfoot');
+const footerToClone = tableFooter.rows[0].cells[1].cloneNode(true);
+const footerRows = tableFooter.rows[0];
+
+footerRows.insertBefore(footerToClone, footerRows.cells[4]);
