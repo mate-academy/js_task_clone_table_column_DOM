e61edc662dc61bb3e1b0e437ea1444a9013c5680
Kostiantyn <havryshkostya@gmail.com>
2022-05-19 15:31:59 +0400

    pls tell me its a good solution2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,10 @@
 'use strict';
 
-const tHead = document.querySelector('thead');
-const tFoot = document.querySelector('tfoot');
-const tBody = document.querySelector('tbody');
-const neededTh = document.querySelectorAll('th');
-const neededTd = document.querySelectorAll('td');
+const tr = document.querySelectorAll('tr');
 
-tHead.firstElementChild.insertBefore(neededTh[1].cloneNode(true), neededTh[4]);
-
-tFoot.firstElementChild.insertBefore(neededTh[6].cloneNode(true), neededTh[9]);
-
-let copiedItemCount = 1;
-let insertBeforeItemCount = 4;
-
-for (let i = 0; i < tBody.children.length; i++) {
-  tBody.children[i].insertBefore(neededTd[copiedItemCount]
-    .cloneNode(true), neededTd[insertBeforeItemCount]);
-  copiedItemCount += 5;
-  insertBeforeItemCount += 5;
+for (const row of tr) {
+  row.insertBefore(
+    row.cells[1].cloneNode(true),
+    row.cells[4]
+  );
 }
