8c6c12f0d4b904710cc43709d60a3b73eee90cd4
Oleksiy Lyashenko <oleksij.lyashenko@gmail.com>
2022-06-10 13:42:35 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('tr');
+
+[...rows].map(row => {
+  const childrenOfRow = row.children;
+
+  const cloneOfCell = childrenOfRow[1].cloneNode(childrenOfRow[1].textContent);
+
+  row.append(cloneOfCell);
+});
