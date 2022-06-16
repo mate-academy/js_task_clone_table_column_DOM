80cce4991bb78bd9c29502ec6161455a01356055
Artem Furdela <furdela.artem@gmail.com>
2022-06-16 21:36:55 +0300

    Table clone first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
 // write your code here
+
+const table = [...document.querySelector('table').rows];
+
+table.forEach(row => {
+  row.insertBefore(row.children[1].cloneNode(true), row.children[4]);
+});
