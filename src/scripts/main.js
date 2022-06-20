c1613cdae539587e2960c7d55b15f86f3f9db0bf
Mikhail Shchuka <mikhail.shchuka@gmail.com>
2022-06-20 22:59:12 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const tableElem = document.querySelector('table');
+const tHead = tableElem.firstElementChild;
+const tBodyList = [...tHead.nextElementSibling.children];
+const tFoot = tableElem.lastElementChild;
+
+const copyCol = (elem) => {
+  const item = elem.firstElementChild.nextElementSibling;
+
+  elem.lastElementChild.before(item.cloneNode(true));
+};
+
+copyCol(tHead.firstElementChild);
+copyCol(tFoot.firstElementChild);
+
+for (const item of tBodyList) {
+  copyCol(item);
+}
