12a56be794ec57430a9d1628c5c13b42966b4792
Vitaliy Anochin <anuhin.vital@gmail.com>
2022-06-20 11:35:55 +0300

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const element = document.querySelector('table');
+
+for (let i = 0; i < element.rows.length; i++) {
+  const column = element.rows[i].cells[1].cloneNode(true);
+
+  element.rows[i].insertBefore(column, element.rows[i].children[4]);
+}
