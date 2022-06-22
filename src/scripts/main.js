cc878466d8ffa1be9ea914dfb9161fb7045fb34a
Stanislav Topikha <stanislav.topikha@gmail.com>
2022-06-22 14:07:16 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const tableRows = document.querySelectorAll('tr');
+
+[...tableRows].forEach(row => {
+  const secondCellClone = row.children[1].cloneNode(true);
+
+  row.children[3].before(secondCellClone);
+});
