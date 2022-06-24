5bd80cfe45cf0fa845788d5b34aaa0290befbe54
Stanislav Topikha <stanislav.topikha@gmail.com>
2022-06-24 12:49:45 +0300

    Parse position fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,5 +5,5 @@ const tableRows = document.querySelectorAll('tr');
 [...tableRows].forEach(row => {
   const secondCellClone = row.children[1].cloneNode(true);
 
-  row.children[3].before(secondCellClone);
+  row.children[4].before(secondCellClone);
 });
