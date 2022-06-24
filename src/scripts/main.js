e7b5b602dcbab6dbd6ea3e0128529152d1e2846e
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2022-06-24 07:53:16 +0300

    firstCommit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
 // write your code here
+
+const x = document.querySelector('thead');
+
+for (const ch of x.children) {
+  ch.append(ch.children[1].cloneNode(true));
+  ch.insertBefore(ch.children[5], ch.children[3]);
+}
+
+const y = document.querySelector('tbody');
+
+for (const ch of y.children) {
+  ch.append(ch.children[1].cloneNode(true));
+  ch.insertBefore(ch.children[5], ch.children[3]);
+}
+
+const z = document.querySelector('tfoot');
+
+for (const ch of z.children) {
+  ch.append(ch.children[1].cloneNode(true));
+  ch.insertBefore(ch.children[5], ch.children[3]);
+}
