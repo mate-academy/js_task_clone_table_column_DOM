dac0f4557639ddc3dd1c6eed0b07dc0ebe456662
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2022-06-24 19:49:25 +0300

    thirdCommit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,23 +2,9 @@
 
 // write your code here
 
-const x = document.querySelector('thead');
+const table = document.querySelectorAll('tr');
 
-for (const ch of x.children) {
-  ch.append(ch.children[1].cloneNode(true));
-  ch.insertBefore(ch.children[5], ch.children[3]);
-}
-
-const y = document.querySelector('tbody');
-
-for (const ch of y.children) {
-  ch.append(ch.children[1].cloneNode(true));
-  ch.insertBefore(ch.children[5], ch.children[3]);
-}
-
-const z = document.querySelector('tfoot');
-
-for (const ch of z.children) {
+for (const ch of table) {
   ch.append(ch.children[1].cloneNode(true));
   ch.insertBefore(ch.children[5], ch.children[3]);
 }
