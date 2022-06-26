dc4473a6ee13fe1c7fdaf18eaf72fe315a59efd6
Pavel Maistrenko <maystrenko.p@gmail.com>
2022-06-26 19:53:03 +0300

    Soluion

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const rows = [...document.querySelectorAll('tr')];
+
+for (let i = 0; i < rows.length; i++) {
+  if (i === 0 || i === rows.length - 1) {
+    const thElement = document.createElement('th');
+    const itemRow = rows[i].querySelectorAll('th');
+
+    thElement.textContent = itemRow[1].innerText;
+    rows[i].insertBefore(thElement, rows[i].children[4]);
+  } else {
+    const tdElement = document.createElement('td');
+    const itemRow = rows[i].querySelectorAll('td');
+
+    tdElement.textContent = itemRow[1].innerText;
+    rows[i].insertBefore(tdElement, rows[i].children[4]);
+  }
+}
