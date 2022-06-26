a34a7aa0dd81a6a4a7474b3d9953e2a0f3fc884a
Tania Troshchuk <troschuk95@gmail.com>
2022-06-26 21:19:44 +0300

    Solution Task: Copy/Paste table column

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const allRows = document.querySelectorAll('tr');
+
+[...allRows].forEach(row => {
+  const copyCell = row.children[1].cloneNode(true);
+
+  row.children[4].before(copyCell);
+});
