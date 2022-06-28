2e4ed2b962cf8a56b08b07716522fbd29d5f70c9
PaulRodriguez2049 <paul.rodriguez.2049@gmail.com>
2022-06-28 20:58:16 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,70 @@
 'use strict';
-
 // write your code here
+
+const table = document.querySelector('table');
+const tbodyArray = [...document.querySelectorAll(`tbody tr`)];
+
+const data = [];
+
+tbodyArray.forEach(el => {
+  const innerText = el.innerText.split('\t');
+
+  data.push(innerText);
+});
+
+table.innerHTML = ``;
+
+const headerHandler = () => {
+  const thead = document.createElement(`thead`);
+
+  thead.innerHTML
+    = `<tr>
+      <th>Name</th>
+      <th>Position</th>
+      <th>Office</th>
+      <th>Age</th>
+      <th>Position</th>
+      <th>Salary</th>
+    </tr>`;
+  table.appendChild(thead);
+};
+
+const bodyHandler = () => {
+  const tbody = document.createElement(`tbody`);
+
+  table.appendChild(tbody);
+
+  data.forEach(el => {
+    const tr = document.createElement(`tr`);
+
+    tr.innerHTML
+      = `
+        <td>${el[0]}</td>
+        <td>${el[1]}</td>
+        <td>${el[2]}</td>
+        <td>${el[3]}</td>
+        <td>${el[1]}</td>
+        <td>${el[4]}</td>
+      `;
+    tbody.appendChild(tr);
+  });
+};
+
+const footerHandler = () => {
+  const tfoot = document.createElement(`tfoot`);
+
+  tfoot.innerHTML
+    = `<tr>
+      <th>Name</th>
+      <th>Position</th>
+      <th>Office</th>
+      <th>Age</th>
+      <th>Position</th>
+      <th>Salary</th>
+    </tr>`;
+  table.appendChild(tfoot);
+};
+
+headerHandler();
+bodyHandler();
+footerHandler();
