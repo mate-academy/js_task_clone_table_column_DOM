a8939838890b41daa724f68ffca20c013315a68e
silchyn <ivan.silchin@gmail.com>
2022-07-05 21:04:43 +0300

    cloned column

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,7 @@
 'use strict';
 
-// write your code here
+for (const part of document.querySelector('table').children) {
+  for (const row of part.children) {
+    row.children[4].before(row.children[1].cloneNode(true));
+  }
+}
