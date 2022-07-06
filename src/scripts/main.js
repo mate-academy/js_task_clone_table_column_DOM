b6f3cfb02e3fe30e07edb9c301f34f62e2aea19a
Oleksii Budnik <budnikoleksii@gmail.com>
2022-07-06 16:04:37 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('tr');
+
+for (const row of rows) {
+  const secondColumnCopy = row.children[1].cloneNode(true);
+
+  row.insertBefore(secondColumnCopy, row.children[4]);
+}
