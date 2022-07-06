b10e9a9a5cdb86429ea63e6515ea463135a72493
silchyn <ivan.silchin@gmail.com>
2022-07-06 10:47:04 +0300

    now iterating only through tr

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,5 @@
 'use strict';
 
-for (const part of document.querySelector('table').children) {
-  for (const row of part.children) {
-    row.children[4].before(row.children[1].cloneNode(true));
-  }
+for (const row of document.querySelectorAll('tr')) {
+  row.children[4].before(row.children[1].cloneNode(true));
 }
