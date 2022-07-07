9e017a5b249f75b5b9c3332e0f55eedc44ac569a
fledith <barateon023@gmail.com>
2022-07-07 16:14:23 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const thead = document.querySelector('thead').children;
+const tbody = document.querySelector('tbody').children;
+const tfoot = document.querySelector('tfoot').children;
+
+function copyRow(column, typeRow) {
+  for (const element of column) {
+    const item = document.createElement(typeRow);
+
+    item.textContent = element.children[1].textContent;
+
+    element.insertBefore(item, element.children[4]);
+  }
+}
+
+copyRow(thead, 'th');
+copyRow(tbody, 'td');
+copyRow(tfoot, 'th');
