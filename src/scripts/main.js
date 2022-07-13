d90f7ddecedf79d81c925ee57b33b8a8b8cee4e3
Oleksandr Kotliarov <de.fr09n@gmail.com>
2022-07-13 13:23:24 +0300

    refreshed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const rows = document.querySelectorAll('tr');
+const columnCopyId = 1;
+const columnPlaceId = 4;
 
 for (const row of rows) {
-  const copy = row.children[1].cloneNode(true);
+  const copy = row.children[columnCopyId].cloneNode(true);
 
-  row.children[3].after(copy);
+  row.children[columnPlaceId].before(copy);
 }
