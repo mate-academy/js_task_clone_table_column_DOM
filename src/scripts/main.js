b4bd34f73e91f2a8f69e910c50f6b12ac35fd51c
Taras Kozii <taras_kozii@ukr.net>
2022-07-18 22:23:11 +0300

    solution v01

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const cellsHead = table.tHead.rows[0].cells;
+const thHead = cellsHead[1];
+
+const cellsFooter = table.tFoot.rows[0].cells;
+const thFooter = cellsFooter[1];
+
+const trsBody = table.tBodies[0].rows;
+
+table.tHead.rows[0].children[4].before(thHead.cloneNode(true));
+table.tFoot.rows[0].children[4].before(thFooter.cloneNode(true));
+
+for (const elem of trsBody) {
+  const item = elem.children[1];
+
+  elem.children[4].before(item.cloneNode(true));
+}
