bf16df6115629e15b59c2fe80545f9226713d208
Olha Arama <arama_o_f@ukr.net>
2022-07-19 21:13:19 +0300

    Column was cloned

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (let i = 0; i < table.rows.length; i++) {
+  const cell = table.rows[i].cells[1].cloneNode(true);
+
+  table.rows[i].lastElementChild.before(cell);
+}
