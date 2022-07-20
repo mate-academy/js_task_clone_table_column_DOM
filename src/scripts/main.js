d5d771f27dcda8eefc8d2c4ef414712c034dfccc
Oleksii Zasiedka <alex.zasedka@gmail.com>
2022-07-20 17:51:06 +0300

    add variable for source index

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,7 @@ const tableRows = document.querySelectorAll('tr');
 
 tableRows.forEach((row) => {
   const placingIndex = row.children.length - 2;
+  const sourceIndex = 1;
 
-  row.children[placingIndex].after(row.children[1].cloneNode(true));
+  row.children[placingIndex].after(row.children[sourceIndex].cloneNode(true));
 });
