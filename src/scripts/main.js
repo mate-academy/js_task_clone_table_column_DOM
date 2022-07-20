1e90c1ef8a19cc6184acbf214718e9349ec37e50
lentyaeev <daniilshaliuk@gmail.com>
2022-07-20 15:25:24 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+let index = 0;
+
+for (const section of table.children) {
+  for (const row of section.children) {
+    const cell = row.children[1];
+    const newCell = row.insertCell(4);
+
+    if (index === 0 || index === 13) {
+      newCell.outerHTML = `<th>${cell.textContent}</th>`;
+    } else {
+      newCell.append(document.createTextNode(cell.textContent));
+    }
+    index++;
+  }
+}
