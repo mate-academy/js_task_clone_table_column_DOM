2c689423668621c1360bc8e10c91f8cad5b41b86
DashaPoliazz <dasha.poliazzz@gmail.com>
2022-07-21 15:09:15 +0300

    Table cell has been cloned

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write your code here
+const tableData = document.querySelectorAll('td');
+
+const selectorsToRender = ['thead > tr', 'tfoot > tr'];
+
+selectorsToRender.forEach(selector => {
+  const thToInsert = document.createElement('th');
+
+  thToInsert.innerText = 'Position';
+
+  document.querySelector(selector).children[3].after(thToInsert);
+});
+
+const dataToRender = [];
+
+for (let i = 1; i < tableData.length; i += 5) {
+  dataToRender.push(tableData[i].textContent);
+}
+
+[...document.querySelector('tbody').children]
+  .forEach((el, idx) => {
+    const td = document.createElement('td');
+
+    td.textContent = dataToRender[idx];
+
+    el.children[3].after(td);
+  });
