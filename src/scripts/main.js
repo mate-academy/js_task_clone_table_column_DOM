81f701b37e5150552a1e0c669392345aa6ba6218
Vlad Skuratovskyi <vladskur0369@gmail.com>
2022-07-21 20:00:09 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 // write your code here
+const trCore = document.querySelectorAll('tr');
+
+for (const item of trCore) {
+  const tCore = item.querySelectorAll('th')[1]
+    ? item.querySelectorAll('th')[1]
+    : item.querySelectorAll('td')[1];
+
+  const posCore = item.querySelectorAll('th')[3]
+    ? item.querySelectorAll('th')[3]
+    : item.querySelectorAll('td')[3];
+
+  posCore.after(tCore.cloneNode(true));
+}
