0852e4187038c229264e9c4da010e27b90fd8c33
Olha Stakhova <olha.stakhova@gmail.com>
2022-07-23 22:23:57 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,46 @@
 'use strict';
 
 // write your code here
+const tableHead = document.querySelector('thead');
+const headElement = tableHead.querySelectorAll('th');
+const tableBody = document.querySelector('tbody');
+const tableFooter = document.querySelector('tfoot');
+const footerElement = tableFooter.querySelectorAll('th');
+const peoplesData = document.querySelectorAll('tr');
+
+let template = '';
+let templateHead = '';
+let templatefooter = '';
+
+templateHead = `<tr>
+    <th>${headElement[0].textContent}</th>
+    <th>${headElement[1].textContent}</th>
+    <th>${headElement[2].textContent}</th>
+    <th>${headElement[3].textContent}</th>
+    <th>${headElement[1].textContent}</th>
+    <th>${headElement[4].textContent}</th>
+  </tr>`;
+
+for (let i = 1; i < peoplesData.length - 1; i += 1) {
+  template += `<tr>
+    <td>${peoplesData[i].children[0].textContent}</td>
+    <td>${peoplesData[i].children[1].textContent}</td>
+    <td>${peoplesData[i].children[2].textContent}</td>
+    <td>${peoplesData[i].children[3].textContent}</td>
+    <td>${peoplesData[i].children[1].textContent}</td>
+    <td>${peoplesData[i].children[4].textContent}</td>
+  </tr>`;
+}
+
+templatefooter = `<tr>
+<th>${footerElement[0].textContent}</th>
+<th>${footerElement[1].textContent}</th>
+<th>${footerElement[2].textContent}</th>
+<th>${footerElement[3].textContent}</th>
+<th>${footerElement[1].textContent}</th>
+<th>${footerElement[4].textContent}</th>
+</tr>`;
+
+tableHead.innerHTML = templateHead;
+tableBody.innerHTML = template;
+tableFooter.innerHTML = templatefooter;
