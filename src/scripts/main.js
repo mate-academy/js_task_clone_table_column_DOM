23373f9ef17b014d6fd62dde0fd148042ab8dc44
Matvii Kharchenko <matviy.kharchenko@gmail.com>
2022-07-27 19:30:23 +0300

    sol 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,31 @@
 'use strict';
 
-const tHead = document.querySelector('thead');
-const tBody = document.querySelector('tbody');
-const tFoot = document.querySelector('tfoot');
+// const tHead = document.querySelector('thead');
+// const tBody = document.querySelector('tbody');
+// const tFoot = document.querySelector('tfoot');
 
-function getRightColumn(section) {
-  const rowLength = section.rows.length;
-  const columnsLength = section.rows[0].cells.length;
+// function getRightColumn(section) {
+//   const rowLength = section.rows.length;
+//   const columnsLength = section.rows[0].cells.length;
 
-  for (let i = 0; i < rowLength; i++) {
-    const columnToCopy = section.rows[i].cells[1];
-    const clone = columnToCopy.cloneNode(true);
-    const lastColumn = section.rows[i].cells[columnsLength - 1];
+//   for (let i = 0; i < rowLength; i++) {
+//     const columnToCopy = section.rows[i].cells[1];
+//     const clone = columnToCopy.cloneNode(true);
+//     const lastColumn = section.rows[i].cells[columnsLength - 1];
 
-    lastColumn.insertAdjacentElement('beforebegin', clone);
-  }
+//     lastColumn.insertAdjacentElement('beforebegin', clone);
+//   }
+// }
+
+// getRightColumn(tHead);
+// getRightColumn(tBody);
+// getRightColumn(tFoot);
+
+const table = document.querySelector('table');
+const rows = table.rows;
+
+for (const row of rows) {
+  const clone = row.cells[1].cloneNode(true);
+
+  row.insertBefore(clone, row.cells[4]);
 }
-
-getRightColumn(tHead);
-getRightColumn(tBody);
-getRightColumn(tFoot);
