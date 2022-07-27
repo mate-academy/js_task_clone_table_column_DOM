151a186bd83c74fd862fdfd7386a9b96d9ba0044
Matvii Kharchenko <matviy.kharchenko@gmail.com>
2022-07-27 19:04:43 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const tHead = document.querySelector('thead');
+const tBody = document.querySelector('tbody');
+const tFoot = document.querySelector('tfoot');
+
+function getRightColumn(section) {
+  const rowLength = section.rows.length;
+  const columnsLength = section.rows[0].cells.length;
+
+  for (let i = 0; i < rowLength; i++) {
+    const columnToCopy = section.rows[i].cells[1];
+    const clone = columnToCopy.cloneNode(true);
+    const lastColumn = section.rows[i].cells[columnsLength - 1];
+
+    lastColumn.insertAdjacentElement('beforebegin', clone);
+  }
+}
+
+getRightColumn(tHead);
+getRightColumn(tBody);
+getRightColumn(tFoot);
