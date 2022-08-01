5cad7c7f9c4afe8b0c24e818cd935e9b303f3821
Yura Atamaniuk <atam3905@gmail.com>
2022-08-01 18:01:07 +0300

    cloneTask

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
 // write your code here
+const tableRow = document.querySelectorAll('tr');
+
+for (const item of tableRow) {
+  const newItem = item.children[1].cloneNode(true);
+
+  item.children[4].before(newItem);
+}
