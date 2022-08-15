8bb6c6e57a73ba148242e355a75bedd1ed89c4ec
Nazar Kulchevych <gamebandit38@gmail.com>
2022-08-15 17:46:59 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,7 @@
 'use strict';
 
-// write your code here
+const secondColumnElements = document.querySelectorAll('tr > :nth-child(2)');
+const fourthColumnElements = document.querySelectorAll('tr > :nth-child(4)');
+
+fourthColumnElements.forEach((element, i) => (
+  fourthColumnElements[i].after(secondColumnElements[i].cloneNode(true))));
