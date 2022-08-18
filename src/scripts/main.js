0259775bec825e1e06fe06107ce2fa0500fb7e17
Yuliia Parashchenko <yuliia.parashchenko@gmail.com>
2022-08-18 14:00:26 +0300

    Clone table column DOM task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const head = [ ...table.tHead.children ][0];
+
+addThCell(head);
+
+const foot = [ ...table.tFoot.children ][0];
+
+addThCell(foot);
+
+const body = { ...table.tBodies }['0'].children;
+const childrens = [ ...body ];
+
+for (let i = 0; i < childrens.length; i++) {
+  addCell(childrens, i);
+}
+
+function addThCell(row) {
+  const addElement = document.createElement('th');
+
+  addElement.textContent = row.children[1].textContent;
+  row.lastElementChild.before(addElement);
+}
+
+function addCell(row, index) {
+  const addElement = document.createElement('td');
+
+  addElement.textContent = row[index].children[1].textContent;
+  row[index].lastElementChild.before(addElement);
+}
