afbc54175aba8b51efd1abb303f6a5b330ddbc98
Maryna Mazur <marinemazur@gmail.com>
2022-08-18 11:18:55 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const tbodyChildren = document.querySelector('tbody').children;
+const thead = document.querySelector('thead').children[0];
+const tfoot = document.querySelector('tfoot').children[0];
+
+for (const tr of tbodyChildren) {
+  const cloneTr = tr.children[1].cloneNode(true);
+
+  tr.insertBefore(cloneTr, tr.children[4]);
+}
+
+function clone(element) {
+  const cloneValue = element.children[1].cloneNode(true);
+
+  element.insertBefore(cloneValue, element.children[4]);
+}
+
+clone(thead);
+clone(tfoot);
