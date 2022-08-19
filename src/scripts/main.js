828aa3ab66a8319350dc9f6466fc836c5e3f01bf
Oleksandr Brui <oleksandr.bruigit@gmail.com>
2022-08-19 19:12:56 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
 // write your code here
+
+const table = document.querySelector('table');
+const listRows = table.rows;
+
+[...listRows].forEach(tr => {
+  const arrCells = tr.cells;
+  // get length of cells
+  const lengthCells = arrCells.length;
+  // the number of the cell to which we will clone
+  const numberTd = 1;
+  // get td which will be clone
+  const td = tr.cells[numberTd];
+
+  tr.cells[lengthCells - 1].before(td.cloneNode(true));
+});
