cb2c41bc4ae36dd7aa622295bd60166c12c95def
Andriy Kachalkin <kachalkin1306@gmail.com>
2022-08-24 16:19:13 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+function copyColumn(from, to) {
+  const fromColumn = document.querySelectorAll(`tr > :nth-child(${from})`);
+  const toColumn = document.querySelectorAll(`tr > :nth-child(${to})`);
+
+  return toColumn.forEach((element, i) => (
+    toColumn[i].before(fromColumn[i].cloneNode(true))));
+};
+
+copyColumn(2, 5);
