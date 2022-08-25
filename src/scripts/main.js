27b7426bdfa2213773802a254ae5deaf99d0cf92
Yuliia Parashchenko <yuliia.parashchenko@gmail.com>
2022-08-25 14:15:35 +0300

    Solution after 1st review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,31 +2,9 @@
 
 const table = document.querySelector('table');
 
-const head = [ ...table.tHead.children ][0];
+for (let i = 0; i < table.rows.length; i++) {
+  const row = table.rows[i];
+  const addNode = row.children[1].cloneNode(true);
 
-addThCell(head);
-
-const foot = [ ...table.tFoot.children ][0];
-
-addThCell(foot);
-
-const body = { ...table.tBodies }['0'].children;
-const childrens = [ ...body ];
-
-for (let i = 0; i < childrens.length; i++) {
-  addCell(childrens, i);
-}
-
-function addThCell(row) {
-  const addElement = document.createElement('th');
-
-  addElement.textContent = row.children[1].textContent;
-  row.lastElementChild.before(addElement);
-}
-
-function addCell(row, index) {
-  const addElement = document.createElement('td');
-
-  addElement.textContent = row[index].children[1].textContent;
-  row[index].lastElementChild.before(addElement);
+  row.lastElementChild.before(addNode);
 }
