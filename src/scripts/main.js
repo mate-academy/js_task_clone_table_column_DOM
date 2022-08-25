478329383839d5e97071ab627da905697f9a5fdc
Elina Bahirova <eli4ka.bagirova@gmail.com>
2022-08-25 11:32:37 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
 // write your code here
+
+const head = document.querySelectorAll('thead > tr > th')[1];
+const foot = document.querySelectorAll('tfoot > tr > th')[1];
+let dataset = document.querySelectorAll('tbody > tr > td:first-child');
+
+dataset = [...dataset].map(element => element.nextElementSibling);
+
+const copyHead = document.createElement('th');
+
+copyHead.innerText = head.innerText;
+
+head.parentElement.append(copyHead);
+
+head.parentElement
+  .append(head.parentElement.lastElementChild.previousElementSibling);
+
+const copyFoot = document.createElement('th');
+
+copyFoot.innerText = foot.innerText;
+
+foot.parentElement.append(copyFoot);
+
+foot.parentElement
+  .append(foot.parentElement.lastElementChild.previousElementSibling);
+
+dataset.forEach(element => {
+  const copyData = document.createElement('td');
+
+  copyData.innerText = element.innerText;
+
+  element.parentElement.append(copyData);
+
+  element.parentElement
+    .append(element.parentElement.lastElementChild.previousElementSibling);
+});
