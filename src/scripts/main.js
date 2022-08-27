ecb2bb8a1f92c6d709f308e2421ac8f64b6a05f7
Dolhov Andrey <andrej.dolgov.042110@gmail.com>
2022-08-27 17:04:36 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const tHead = document.querySelector('thead').children[0];
+const tBody = document.querySelector('tbody').children;
+const tFoot = document.querySelector('tfoot').children[0];
+
+let counter = 0;
+
+function copy(elem) {
+  const trElem = elem.children[1].cloneNode(true);
+
+  elem.insertBefore(trElem, elem.children[4]);
+
+  if (counter === 0) {
+    for (const key of tBody) {
+      const trBody = key.children[1].cloneNode(true);
+
+      key.insertBefore(trBody, key.children[4]);
+    }
+  }
+  counter++;
+};
+
+copy(tHead);
+copy(tFoot);
