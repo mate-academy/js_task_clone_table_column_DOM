c2ef8a93254b55240ca981078c564efd875678ba
Oleksandr Malyshev <sanyokmalyshev@gmail.com>
2022-09-05 14:22:04 +0800

    Task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
+/* eslint-disable */
 
-// write your code here
+const thPosition = document.querySelector('thead tr th:nth-child(2)');
+const theadTr = document.querySelector('thead tr');
+const tfootTr = document.querySelector('tfoot tr');
+
+theadTr.insertBefore(thPosition.cloneNode(true), theadTr.children[4]);
+tfootTr.insertBefore(thPosition.cloneNode(true), tfootTr.children[4]);
+
+const allPositionsTd = [...document.querySelectorAll('tbody tr td:nth-child(2)')];
+const allTbodyTr = [...document.querySelectorAll('tbody tr')];
+
+for (let i = 0; i < allPositionsTd.length; i++) {
+  allTbodyTr[i].insertBefore(allPositionsTd[i].cloneNode(true), allTbodyTr[i].children[4]);
+}
+
+
