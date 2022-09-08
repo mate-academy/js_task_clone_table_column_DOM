5ebd3794e72c9f2715cca4f098ac62162d55dfc4
Oleksii <oleksii@it.com>
2022-09-08 21:34:09 +0300

    Solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const allEl = document.querySelectorAll('tr');
+
+for (let i = 0; i < allEl.length; i++) {
+  const element = (i === 0 || i === allEl.length - 1)
+    ? document.createElement('th')
+    : document.createElement('td');
+
+  element.innerHTML = allEl[i].children[1].innerHTML;
+  allEl[i].children[4].before(element);
+}
