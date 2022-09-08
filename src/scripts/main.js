b7ccb5c45db08a211ccb44d5c7008526a1efd485
Oleksii <oleksii@it.com>
2022-09-08 21:41:44 +0300

    Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const allEl = document.querySelectorAll('tr');
+const allTr = document.querySelectorAll('tr');
 
-for (let i = 0; i < allEl.length; i++) {
-  const element = (i === 0 || i === allEl.length - 1)
+for (let i = 0; i < allTr.length; i++) {
+  const element = (i === 0 || i === allTr.length - 1)
     ? document.createElement('th')
     : document.createElement('td');
 
-  element.innerHTML = allEl[i].children[1].innerHTML;
-  allEl[i].children[4].before(element);
+  element.innerHTML = allTr[i].children[1].innerHTML;
+  allTr[i].children[4].before(element);
 }
