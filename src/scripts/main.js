16a171a5293b999e211ba453ebf0c54600f21744
Boris Kerekesha <kerekesha90@ukr.net>
2022-09-08 10:13:07 +0300

    clone_table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const bodyTable = document.querySelector('tbody');
+const bodyHead = document.querySelector('thead');
+const bodyFloot = document.querySelector('tfoot');
+
+const createCopyColumns = function(collection) {
+  const rowsCollection = collection.rows;
+  const arrayFrom = [...rowsCollection];
+
+  for (const row of arrayFrom) {
+    const column = row.children[1].cloneNode(true);
+
+    row.children[4].before(column);
+  }
+};
+
+createCopyColumns(bodyTable);
+createCopyColumns(bodyHead);
+createCopyColumns(bodyFloot);
