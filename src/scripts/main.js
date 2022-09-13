ff105579d1ee0b87466ab8a6ea7347765b2e9a98
Inna <dragotaiv@gmail.com>
2022-09-13 18:01:42 +0300

    copy_paste

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const row = document.querySelectorAll('tr');
+
+for (const column of row) {
+  const copy = column.cells[1].cloneNode(true);
+
+  column.cells[4].before(copy);
+}
