0d99c14ae54ba42bef09241658d4c2a58d43d2f3
Dima Korol <korovaidima@gmail.com>
2022-09-16 12:53:22 +0200

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+let copiedCell;
+
+for (let i = 0; i < table.rows.length; i++) {
+  if (table.rows[i].cells[0].tagName === 'TH') {
+    copiedCell = document.createElement('th');
+  } else {
+    copiedCell = document.createElement('td');
+  }
+
+  copiedCell.textContent = `${table.rows[i].cells[1].textContent}`;
+  table.rows[i].insertBefore(copiedCell, table.rows[i].children[4]);
+}
