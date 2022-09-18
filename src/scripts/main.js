3cbbc939b01112bd92e45e8bb220a405ff41782c
Kostya Berehoviy <kostyaber3@gmail.com>
2022-09-18 20:04:27 +0300

    Clone Table Column Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const trs = document.querySelectorAll('tr');
+
+for (const tr of trs) {
+  let positionClone 
+  if (tr.children[1].tagName === 'TH') {
+    positionClone = document.createElement('th');
+  }
+  if (tr.children[1].tagName === 'TD') {
+    positionClone = document.createElement('td');
+  }
+
+  positionClone.innerHTML = tr.children[1].innerHTML;
+  tr.children[3].after(positionClone);
+}
