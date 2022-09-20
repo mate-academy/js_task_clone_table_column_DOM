34bbad56e8a977345c4c4ac4a79e5c6d68ec60b3
grafit-off <graf.off.it@gmail.com>
2022-09-20 22:04:11 +0300

    fix: removed unnecessary code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,15 +2,14 @@
 
 // write your code here
 const table = document.querySelector('table');
-const tableRows = [...table.querySelectorAll('tr')];
+const tableRows = [...table.rows];
 
-const pasteCellInSectionRowByIndex = (tableSection, index, cell) => {
-  tableSection.parentElement.rows[index].cells[3].after(cell);
+const pasteCellInRowByIndex = (index, cell) => {
+  table.rows[index].cells[3].after(cell);
 };
 
 tableRows.forEach((row, i) => {
   const cellClone = row.cells[1].cloneNode(true);
-  const tableSection = row.parentElement;
 
-  pasteCellInSectionRowByIndex(tableSection, i, cellClone);
+  pasteCellInRowByIndex(i, cellClone);
 });
