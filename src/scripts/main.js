9f6dd708af4e0f4d4e1f8b68a32217cb72f0b2b9
grafit-off <graf.off.it@gmail.com>
2022-09-20 21:52:21 +0300

    feat: column was copied and pasted

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 // write your code here
+const table = document.querySelector('table');
+const tableRows = [...table.querySelectorAll('tr')];
+
+const pasteCellInSectionRowByIndex = (tableSection, index, cell) => {
+  tableSection.parentElement.rows[index].cells[3].after(cell);
+};
+
+tableRows.forEach((row, i) => {
+  const cellClone = row.cells[1].cloneNode(true);
+  const tableSection = row.parentElement;
+
+  pasteCellInSectionRowByIndex(tableSection, i, cellClone);
+});
