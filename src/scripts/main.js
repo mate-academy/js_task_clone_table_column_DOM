be68bdd49e972f45feea54858fad9fd5093d34b8
Andriy Andrieiev <proph7000@gmail.com>
2022-09-25 20:11:05 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelector('table').rows;
+
+for (let i = 0; i < rows.length; i++) {
+  let cell = document.createElement('td');
+
+  if (i === 0 || i === rows.length - 1) {
+    cell = document.createElement('th');
+  } else {
+    cell = document.createElement('td');
+  }
+
+  cell.innerText = rows[i].children[1].innerText;
+
+  rows[i].children[4].before(cell);
+}
