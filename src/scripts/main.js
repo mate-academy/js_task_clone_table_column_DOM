8ee122fda88929c7437decb0aca0a8e526cb7835
dmitriyn3679 <dmitriyn3679@gmail.com>
2022-09-26 15:57:50 +0300

    done!

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-// write your code here
+// const table = document.querySelector('table')
+const tHead = document.querySelector('thead');
+const tBody = document.querySelector('tbody');
+const tFoot = document.querySelector('tfoot');
+const positionData = parseColumnData(tBody, 1);
+
+function parseColumnData(el, column) {
+  const data = [...el.rows].map(tRow => tRow.cells[column].innerText);
+
+  return data;
+};
+
+const createTitleColumn = el => {
+  const tableCol = document.createElement('th');
+
+  tableCol.insertAdjacentText('beforeend', 'Position');
+
+  const colAge = el.rows[0].cells[3];
+
+  colAge.after(tableCol);
+};
+
+const createDataColumns = (el, data) => {
+  for (let i = 0; i < data.length; i++) {
+    const tableCol = document.createElement('td');
+
+    tableCol.insertAdjacentText('beforeend', data[i]);
+
+    const colAge = el.rows[i].cells[3];
+
+    colAge.after(tableCol);
+  }
+};
+
+createTitleColumn(tHead);
+createTitleColumn(tFoot);
+createDataColumns(tBody, positionData);
