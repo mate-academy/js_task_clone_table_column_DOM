58b6b9d051434cd708ea8cb61e526f31706182b3
dmitriyn3679 <dmitriyn3679@gmail.com>
2022-09-27 21:08:06 +0300

    done!

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,39 +1,7 @@
 'use strict';
 
-// const table = document.querySelector('table')
-const tHead = document.querySelector('thead');
-const tBody = document.querySelector('tbody');
-const tFoot = document.querySelector('tfoot');
-const positionData = parseColumnData(tBody, 1);
+const table = document.querySelector('table');
 
-function parseColumnData(el, column) {
-  const data = [...el.rows].map(tRow => tRow.cells[column].innerText);
-
-  return data;
-};
-
-const createTitleColumn = el => {
-  const tableCol = document.createElement('th');
-
-  tableCol.insertAdjacentText('beforeend', 'Position');
-
-  const colAge = el.rows[0].cells[3];
-
-  colAge.after(tableCol);
-};
-
-const createDataColumns = (el, data) => {
-  for (let i = 0; i < data.length; i++) {
-    const tableCol = document.createElement('td');
-
-    tableCol.insertAdjacentText('beforeend', data[i]);
-
-    const colAge = el.rows[i].cells[3];
-
-    colAge.after(tableCol);
-  }
-};
-
-createTitleColumn(tHead);
-createTitleColumn(tFoot);
-createDataColumns(tBody, positionData);
+for (const node of table.rows) {
+  node.cells[3].after(node.cells[1].cloneNode(true));
+}
