c28b381f66c3b837e1b1227ca658be6bbe807a9b
Danil <dasha8854567@gmail.com>
2022-10-06 12:59:36 +0300

    commit1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const givePosition = [...document.querySelectorAll('tr')]
+  .map(person => person.children[1]);
+
+const giveAllTr = [...document.querySelectorAll('tr')];
+
+function getItem(str) {
+  if (str === givePosition[0].innerText
+  || str === givePosition[givePosition.length - 1].innerText) {
+    return `<th>${str}</th>`;
+  }
+
+  return `<td>${str}</td>`;
+}
+
+for (let i = 0; i < givePosition.length; i++) {
+  giveAllTr[i].children[giveAllTr[i].children.length - 2]
+    .insertAdjacentHTML('afterend', getItem(givePosition[i].innerText));
+};
