e0343f7962ca430c2db493b07f763f8c48a115c0
Ruslan Nedaiborshch <rnedaiborshch@gmail.com>
2022-10-13 10:40:05 +0300

    cod for task clone table column

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+function add(a, b) {
+  const row = document.querySelector(a);
+
+  for (let i = 0; i <= row.children.length - 1; i++) {
+    const text = row.rows[i].cells[1].innerHTML;
+
+    const item = document.createElement(b);
+
+    item.innerText = text;
+
+    row.rows[i].insertBefore(item, row.rows[i].children[4]);
+  }
+}
+
+add('tfoot', 'th');
+add('thead', 'th');
+add('tbody', 'th');
