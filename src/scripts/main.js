83bf061a39206cffa4ba5391bbafdd3058fc1ab2
Anastasiia Tilikina <anastasiia.tilikina@gmail.com>
2022-10-17 15:08:59 +0300

    Fix code style

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,14 @@
 'use strict';
 
 const tableData = createDataArr('tr');
-const table = () => {
-  const newTable = [];
+const table = createTable();
 
-  for (let i = 0; i < tableData.length; i++) {
-    (i === 0 || i === tableData.length - 1)
-      ? newTable.push(getRowTexts('th', i))
-      : newTable.push(getRowTexts('td', i));
+for (let i = 0; i < table.length; i++) {
+  if (isTh(i, table)) {
+    insertNewCol('th', i);
+  } else {
+    insertNewCol('td', i);
   }
-
-  return newTable;
-};
-
-for (let i = 0; i < table().length; i++) {
-  (i === 0 || i === table().length - 1)
-    ? insertNewCol('th', i)
-    : insertNewCol('td', i);
 }
 
 function findRow(tagName, index) {
@@ -35,11 +27,29 @@ function insertNewCol(tagName, index) {
   const newCol = document.createElement(tagName);
   const lastCol = findRow(tagName, index).splice(-1, 1)[0];
 
-  newCol.innerText = table()[index][1];
+  newCol.innerText = table[index][1];
 
   lastCol.insertAdjacentElement('beforebegin', newCol);
 }
 
 function createDataArr(tagName) {
   return [...document.querySelectorAll(tagName)];
+}
+
+function createTable() {
+  const newTable = [];
+
+  for (let i = 0; i < tableData.length; i++) {
+    if (isTh(i, tableData)) {
+      newTable.push(getRowTexts('th', i));
+    } else {
+      newTable.push(getRowTexts('td', i));
+    }
+  }
+
+  return newTable;
+}
+
+function isTh(index, arr) {
+  return index === 0 || index === arr.length - 1;
 }
