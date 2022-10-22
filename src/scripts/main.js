42f509efe399760c9f78757b65ba0465c3152cf5
Mykola Yakymiv <mykola8426@gmail.com>
2022-10-22 18:11:17 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const table = document.body.querySelector('table');
+
+for (const el of [...table.children]) {
+  const rows = el.children;
+
+  for (const row of rows) {
+    const secondColumnElement = row.children[1];
+    const elementBeforeInsert = row.children[row.children.length - 1];
+
+    row.insertBefore(secondColumnElement.cloneNode(true), elementBeforeInsert);
+  }
+}
