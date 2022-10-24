2ecd74a35fec829257af0cdd5d848ad364bf4080
Oleksii Bieliaiev <o.bieliaiev@icloud.com>
2022-10-24 15:46:30 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const tr = document.querySelectorAll('tr');
+
+tr.forEach(row => {
+  const copy = row.children[1].cloneNode(true);
+
+  row.children[3].after(copy);
+});
