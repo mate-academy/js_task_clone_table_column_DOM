185c2de7888b646fcbb0d0c2d5497d601513b7fe
Mykyta Krainik <nikinta2003@gmail.com>
2022-10-25 17:05:23 +0300

    Function cloneColumn was implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+function cloneColumn(table, colNo) {
+  for (const tablePart of table.children) {
+    for (const row of tablePart.rows) {
+      const cellClone = row.cells[colNo].cloneNode(true);
+
+      row.lastElementChild.before(cellClone);
+    }
+  }
+}
+
+const dashboard = document.querySelector('table');
+
+cloneColumn(dashboard, 1);
