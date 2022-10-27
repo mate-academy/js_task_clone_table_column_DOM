02514fe7f9a0af874d6f0ece572b25392a20763c
Yurii Teslia <teslya.yuriy@gmail.com>
2022-10-27 21:03:02 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const records = document.querySelectorAll('tr');
+
+for (const record of records) {
+  const itemCopy = document.createElement('td');
+
+  itemCopy.innerText = record.children[1].innerText;
+
+  record.insertBefore(itemCopy, record.children[4]);
+}
