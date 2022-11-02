dc8db5bcc9e25f9cef7cdab9a10a3e63e748cf1b
Ruslan Miroshnychenko <rusikm87@gmail.com>
2022-11-02 11:55:49 +0200

    Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
-// write your code here
+const tr = [...document.querySelectorAll('tr')];
+const addElements = [...document.querySelectorAll('tr > :nth-child(2)')];
+
+tr.forEach((el, i) => {
+  el.children[4].before(addElements[i].cloneNode(true));
+});
