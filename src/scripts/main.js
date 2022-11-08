18d8e85b39e9102d9fbd3ef9ed7fcd343caee754
Kate Riazantseva <katyarezvova96@gmail.com>
2022-11-08 17:21:11 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('tr');
+
+rows.forEach(row => {
+  const columTo = document.createElement('td');
+
+  columTo.textContent = row.cells[1].innerText;
+  row.insertBefore(columTo, row.cells[4]);
+
+  return row;
+});
