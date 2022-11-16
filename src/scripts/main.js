b8d5c3f23bad9bb71bfa35136b16d2db066fa19d
Taras Kovalchuk <taras.kovalcuk93@gmail.com>
2022-11-16 23:22:07 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table').children;
+
+for (const rows of table) {
+  const row = rows.querySelectorAll('tr');
+
+  for (const item of row) {
+    const position = item.children[1];
+    const copyPosition = position.cloneNode(true);
+
+    item.children[3].after(copyPosition);
+  }
+};
