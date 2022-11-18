e4de8735dd400561cb09a6607e0ad9724a229dcd
Taras Kovalchuk <taras.kovalcuk93@gmail.com>
2022-11-18 19:56:57 +0200

    Fixed after code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,10 @@
 'use strict';
 
-const table = document.querySelector('table').children;
+const rows = document.querySelectorAll('tr');
 
-for (const rows of table) {
-  const row = rows.querySelectorAll('tr');
+for (const item of rows) {
+  const position = item.children[1];
+  const copyPosition = position.cloneNode(true);
 
-  for (const item of row) {
-    const position = item.children[1];
-    const copyPosition = position.cloneNode(true);
-
-    item.children[3].after(copyPosition);
-  }
+  item.children[3].after(copyPosition);
 };
