27fdb758494d3b1fe3448b0c24ed4adc33b844d0
Bohdan Rudenko <RudBeee@gmail.com>
2022-11-18 13:07:59 +0200

    vol_1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const secondColumn = document.querySelector('table');
+
+const mass = [...secondColumn.rows];
+
+for (let i = 0; i <= mass.length; i++) {
+  if (i === 0 || i === mass.length - 1) {
+    const createTh = document.createElement('th');
+
+    createTh.textContent = secondColumn.rows[i].cells[1].innerHTML;
+    mass[i].insertBefore(createTh, mass[i].children[4]);
+  } else {
+    const createTd = document.createElement('td');
+
+    createTd.textContent = secondColumn.rows[i].cells[1].innerHTML;
+
+    mass[i].insertBefore(createTd, mass[i].children[4]);
+  }
+};
