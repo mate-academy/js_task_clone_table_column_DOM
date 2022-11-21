0d31ae3c7838f65c76b0e6702434d080ccc10174
Olena Shevchenko <shevochkas@gmail.com>
2022-11-21 18:51:06 +0200

    table column was cloned with js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const namePosition = document.querySelectorAll('thead tr th')[1];
+const newNamePosition = namePosition.cloneNode(true);
+
+const namePositionFoot = document.querySelectorAll('tfoot tr th')[1];
+const newNamePositionFoot = namePositionFoot.cloneNode(true);
+
+const theadTr = document.querySelector('table thead tr');
+const tfootTr = document.querySelector('table tfoot tr');
+
+theadTr.insertBefore(newNamePosition, theadTr.children[4]);
+tfootTr.insertBefore(newNamePositionFoot, tfootTr.children[4]);
+
+const tbodyTr = document.querySelectorAll('table tbody tr');
+
+tbodyTr.forEach(tr => {
+  const position = tr.children[1].outerHTML;
+
+  tr.children[3].insertAdjacentHTML('afterend', position);
+});
