73c8f538054988c9386772f5a4a1842c464d4eb3
«ifUride» <ifuride@gmail.com>
2022-11-23 12:44:48 +0100

    Removal of append operation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,5 @@ const tableRows = document.querySelectorAll('tr');
 for (const tr of tableRows) {
   const td = tr.children[1].cloneNode(true);
 
-  tr.appendChild(td);
   tr.insertBefore(td, tr.children[4]);
 }
