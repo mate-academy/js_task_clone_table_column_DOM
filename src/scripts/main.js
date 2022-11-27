99546219f6c039ed3251164fb95b53c6bda960b0
Vlad Fedorishchev <fedorischev23@gmail.com>
2022-11-27 14:48:27 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const theadElement = document.querySelector('thead');
+const tbodyElement = document.querySelector('tbody');
+const tfootElement = document.querySelector('tfoot');
+
+function cloneNameOfColumn(element, row, clonedCell, cloneToCell) {
+  const cloneOfElement = element.rows[row].cells[clonedCell].cloneNode(true);
+
+  element.rows[row].cells[cloneToCell].before(cloneOfElement);
+}
+
+function cloneRow(element, clonedCell, cloneToCell) {
+  const trElement = [...element.rows];
+
+  for (let i = 0; i < trElement.length; i++) {
+    const rowElement = trElement[i].cells[clonedCell].cloneNode(true);
+
+    trElement[i].cells[cloneToCell].before(rowElement);
+  }
+}
+
+cloneNameOfColumn(theadElement, 0, 1, 4);
+cloneNameOfColumn(tfootElement, 0, 1, 4);
+cloneRow(tbodyElement, 1, 4);
