b322469073bef753cb9fee192ca8e82b27cdb67b
Sergey Mironenko <serg1prime@gmail.com>
2022-11-28 01:27:14 +0200

    Task was done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+function createColumn(column, position) {
+  const table = document.querySelector('table');
+
+  [...table.children].map(parentElement => {
+    [...parentElement.children].map(childElement => {
+      let newElement;
+
+      parentElement === table.querySelector('tbody')
+        ? newElement = document.createElement('td')
+        : newElement = document.createElement('th');
+
+      newElement.textContent = childElement.children[column - 1].textContent;
+      childElement.insertBefore(newElement, childElement.children[position]);
+    });
+  });
+}
+
+createColumn(2, 4);
