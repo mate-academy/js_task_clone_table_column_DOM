51d5aad7ad16c3fe25568c6016b12b3041d999d7
Panianchuk M <panianchuk@gmail.com>
2022-11-29 23:49:08 +0200

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const tableRows = document.querySelectorAll('tr');
+
+makeColumnClone(1, 4, tableRows);
+
+function makeColumnClone(columnNumberOriginal, columnNumberClone, rows) {
+  rows.forEach(row => {
+    const clonePositionItem = row.children[columnNumberOriginal]
+      .cloneNode(true);
+
+    row.children[columnNumberClone - 1].after(clonePositionItem);
+  });
+}
