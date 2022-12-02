51d4e680cbd87fe41d235b9e9d37d5371d5f0218
Maria Bramnik <mariabramnik@gmail.com>
2022-12-02 22:58:58 +0200

    Fixes after code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,9 +2,13 @@
 
 const tableElement = document.querySelector('table');
 const cellsCount = tableElement.rows[0].children.length;
-for(let i = 0; i < tableElement.rows.length; i++){
-   let row = tableElement.rows[i];
-   let str =  row.cells[1].innerHTML;
-   let newCell = row.insertCell(cellsCount - 1);
-   newCell.innerHTML = str;
+
+for (let i = 0; i < tableElement.rows.length; i++) {
+  const row = tableElement.rows[i];
+  const str = row.cells[1].innerHTML;
+  const newCell = row.insertCell(cellsCount - 1);
+  newCell.innerHTML = str;
+  if(i === 0 || i === tableElement.rows.length - 1){
+    newCell.style.fontWeight = 'bold';
+   }
 }
