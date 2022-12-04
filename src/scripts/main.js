d6f6a0a9dfa771e552d425d1261795ca5a3a3840
Dmitriy-Kovalenko <dmitrijkovalenko@gmail.com>
2022-12-04 22:00:22 +0200

    clone Ok

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const table = document.getElementsByTagName('table');
+
+const rowsBody = table[0].tBodies[0].rows;
+const rowsHead = table[0].tHead.rows;
+const rowsFooter = table[0].tFoot.rows;
+
+const insertAfter = rowsBody[0].cells.length - 2;
+
+cloneColumn(rowsBody, 1, insertAfter);
+cloneColumn(rowsHead, 1, insertAfter);
+cloneColumn(rowsFooter, 1, insertAfter);
+
+function cloneColumn(rows, cloneIndex, afterPosition) {
+  for (const tr of rows) {
+    const clone = tr.cells[cloneIndex].cloneNode(true);
+
+    tr.children[afterPosition].after(clone);
+  }
+}
