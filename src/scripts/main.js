99e4e71151b859f094f6405fb99541e0189a953d
Deniskakaka <krollik1000000@gmail.com>
2022-12-19 19:12:34 +0200

    delete superfluous lines

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,8 +3,6 @@
 const addElement = (element, collection) => {
   const newElement = collection.item(1).cloneNode(true);
 
-  newElement.innerHTML = collection.item(1).innerHTML;
-
   element.parentNode
     .insertBefore(newElement, collection.item(collection.length - 1));
 };
@@ -26,12 +24,5 @@ addElement(elementsFoot
 const elementsTbody = document.querySelector('tbody').children;
 
 for (let i = 0; i < elementsTbody.length; i++) {
-  const lastelement = elementsTbody[i].children
-    .item(elementsTbody[i].children.length - 1);
-
-  const newElement = elementsTbody[i].children.item(1).cloneNode(true);
-
-  newElement.innerHTML = elementsTbody[i].children.item(1).innerHTML;
-
-  elementsTbody[i].insertBefore(newElement, lastelement);
+  addElement(elementsTbody[i].children.item(1), elementsTbody[i].children);
 }
