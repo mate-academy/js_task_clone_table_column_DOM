0e4312ae6e49363db7a1563628f639a05df252a8
Vlad Boikov <vladboikov95@hotmail.com>
2022-12-21 15:40:50 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,12 +3,7 @@
 const rows = document.querySelector('table').rows;
 
 for (let i = 0; i < rows.length; i++) {
-  let el = document.createElement('td');
+  const el = rows[i].children[1].cloneNode(true);
 
-  if (i === 0 || i === rows.length - 1) {
-    el = document.createElement('th');
-  }
-
-  el.textContent = rows[i].children[1].textContent;
   rows[i].insertBefore(el, rows[i].children[4]);
 }
