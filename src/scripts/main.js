61e6ad44e202e7ef15923080506f6a423d23603a
Dmitriy Sporysh <dopmoneyd@gmail.com>
2022-12-24 00:08:56 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const rootTable = document.querySelector('table');
+const arrayOfTr = [...rootTable.rows];
+
+arrayOfTr.forEach((tr, index) => {
+  const fromCopy = tr.cells[1];
+  const newTr = fromCopy.cloneNode(true);
+
+  function appendElements(tablePart = '') {
+    rootTable[tablePart].rows[0].children[4].before(newTr);
+  }
+
+  if (index === 0) {
+    appendElements('tHead');
+  } else if (index === arrayOfTr.length - 1) {
+    appendElements('tFoot');
+  } else {
+    rootTable.tBodies[0].children[index - 1].children[4].before(newTr);
+  }
+});
