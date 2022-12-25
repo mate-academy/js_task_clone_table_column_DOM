5bb45dc32ec656ac3f2fabb6484a53837cacfacd
Denis Bokov <denisbokov1703@gmail.com>
2022-12-25 15:15:21 +0100

    Solution (2)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,12 @@
 'use strict';
 
-// write your code here
-
 const tr = document.querySelectorAll('tr');
 
 for (let i = 0; i < tr.length; i++) {
   const position = tr[i];
 
-  const clonell = position.children[1].cloneNode(true);
+  const cloneElement = position.children[1].cloneNode(true);
 
-  position.children[4].before(clonell);
+  position.children[4].before(cloneElement);
 }
 
-// console.log(table)
