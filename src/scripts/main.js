685b2e9ae3fe959d22a134540a7e962d49dcc8b3
Aleks <offsset@gmail.com>
2022-12-26 22:48:56 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const line = document.querySelectorAll('tr');
+
+for (const key of line) {
+  let neww = document.createElement('th');
+
+  if (key.children[1].tagName === 'TD') {
+    neww = document.createElement('td');
+  }
+
+  neww.innerText = key.children[1].innerText;
+
+  key.children[4].before(neww);
+}
