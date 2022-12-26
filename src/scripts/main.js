d6d687453541b33b177f3a5719df0452730727e3
Kukharuk <its.up.to.you.dk@gmail.com>
2022-12-26 11:49:03 +0200

    cloneColumnDK

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const tr = document.querySelectorAll('tr');
+
+for (const item of tr) {
+  if (item.firstElementChild.tagName === 'TH') {
+    const th = document.createElement('th');
+
+    th.textContent = item.children[1].textContent;
+    item.insertBefore(th, item.lastElementChild);
+  } else {
+    const td = document.createElement('td');
+
+    td.textContent = item.children[1].textContent;
+    item.insertBefore(td, item.lastElementChild);
+  };
+}
