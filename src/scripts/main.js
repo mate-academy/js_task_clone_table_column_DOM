4d4069d2e0469d23172e4d7791fe3ccec72d9352
Nazarii Kyselevych <nazarkysel@gmail.com>
2022-12-31 19:02:39 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelector('table').rows;
+
+[...rows].forEach(({ cells }) => {
+  const cloneCeil = cells[1].cloneNode(true);
+  const lastCeil = cells[cells.length - 1];
+
+  lastCeil.before(cloneCeil);
+});
