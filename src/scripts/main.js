99f51d29bd84411904caeeb69c6a485b1ae7fb0e
Nick Busarhin <busarginkolia@gmail.com>
2022-12-31 21:36:20 +0200

    addedcolumn

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const rows = table.rows;
+const rowCollection = [...rows];
+
+for (const row of rowCollection) {
+  const data = [...row.cells];
+  const newElement
+    = data[0].innerText === 'Name'
+      ? document.createElement('th')
+      : document.createElement('td');
+
+  newElement.textContent = (data[1].innerText);
+
+  row.insertBefore(newElement, row.children[4]);
+}
