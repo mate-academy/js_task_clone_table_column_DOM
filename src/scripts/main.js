3b08da73087ddef63be6ebcd0dba42969b62444f
Ivan Vyhinnyi <ivan665303@gmail.com>
2023-01-01 17:45:54 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const tr = document.querySelectorAll('tr');
+
+for (const field of tr) {
+  const copy = field.cells[1].cloneNode(true);
+
+  field.children[3].after(copy);
+}
