30b4de1c68103fd23833ea714cd5f51f7a42786a
OksanaBaloh <obaloh1108@gmail.com>
2023-01-03 18:56:59 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const rows = [...document.querySelectorAll('tr')];
+
+for (const row of rows) {
+  const cell = row.cells[1].cloneNode(true);
+
+  row.lastElementChild.before(cell);
+}
