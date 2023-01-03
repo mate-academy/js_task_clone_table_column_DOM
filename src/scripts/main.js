f611b576fc409603e65d65ecff35cb027363d9e2
Iryna Pysanko <malva37@gmail.com>
2023-01-03 20:09:58 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const trElements = [...document.querySelectorAll('tr')];
+
+trElements.forEach((tr, i, arr) => {
+  let cloneCell = document.createElement('td');
+
+  if (tr.children[0].tagName === 'TH') {
+    cloneCell = document.createElement('th');
+  }
+
+  cloneCell.textContent = [...tr.children]
+    .find((el, index) => index === 1).textContent;
+  tr.children[4].before(cloneCell);
+});
