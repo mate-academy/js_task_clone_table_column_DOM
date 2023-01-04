85448d3339177164d15dcd8dc7fd26767d8fc51b
Anna Koval <anny.koval@icloud.com>
2023-01-04 22:34:01 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('tr');
+
+rows.forEach(row => {
+  const newEL = row.children[1].cloneNode(true);
+
+  row.insertBefore(newEL, row.children[4]);
+});
