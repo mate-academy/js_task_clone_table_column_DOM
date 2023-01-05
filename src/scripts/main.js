83785b03be811529c291055e1c5a5af2afbcd80c
Nazarii Kyselevych <nazarkysel@gmail.com>
2023-01-05 20:08:13 +0200

    fixed variable name

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,8 +3,8 @@
 const rows = document.querySelector('table').rows;
 
 [...rows].forEach(({ cells }) => {
-  const cloneCeil = cells[1].cloneNode(true);
-  const lastCeil = cells[cells.length - 1];
+  const cloneCell = cells[1].cloneNode(true);
+  const lastCell = cells[cells.length - 1];
 
-  lastCeil.before(cloneCeil);
+  lastCell.before(cloneCell);
 });
