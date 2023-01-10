03c8cbb59a5f68bd50e1355a935fbdf0b9e1b3fe
Karadzha Mykola <nicholaskaradzha@gmail.com>
2023-01-10 11:35:12 +0000

    clone table DOM task completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
 // write your code here
+const table = document.querySelector('table');
+
+for (let i = 0; i < table.rows.length; i++) {
+  const newColumnCell = (table.rows[i].cells[1]).cloneNode(true);
+  const lastColumnCell = table.rows[i].cells[table.rows[i].cells.length - 1];
+
+  table.rows[i].insertBefore(newColumnCell, lastColumnCell);
+}
