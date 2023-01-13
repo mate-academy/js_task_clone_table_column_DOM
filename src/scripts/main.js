4b009458ee711862b59195061b40b05397287c48
abobyr <artem.bobyr@dataart.com>
2023-01-13 19:24:07 +0200

    task is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+function cloneColumn(index) {
+  rows.forEach(row => {
+    const cell = row.children[index];
+
+    row.insertBefore(cell.cloneNode(true), row.children[4]);
+  });
+}
+
+const rows = document.querySelectorAll('tr');
+
+cloneColumn(1);
