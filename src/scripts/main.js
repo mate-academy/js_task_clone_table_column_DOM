363ecda55224c968fa72d5580c2c1821fbdfa831
Karadzha Mykola <nicholaskaradzha@gmail.com>
2023-01-14 12:38:17 +0000

    updated using more modern methods

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,6 @@ const table = document.querySelector('table');
 
 for (let i = 0; i < table.rows.length; i++) {
   const newColumnCell = (table.rows[i].cells[1]).cloneNode(true);
-  const lastColumnCell = table.rows[i].cells[table.rows[i].cells.length - 1];
 
-  table.rows[i].insertBefore(newColumnCell, lastColumnCell);
+  table.rows[i].lastElementChild.before(newColumnCell);
 }
