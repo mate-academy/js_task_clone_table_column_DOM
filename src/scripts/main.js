f7c13711f18d747bdaf564d13190eeaf41b4a90c
Vadym Prydatok <vadymprydatok@gmail.com>
2023-01-18 16:32:05 +0200

    clone_table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const cells = document.querySelectorAll('tr');
+
+for (const cell of cells) {
+  if (cell.children[1].tagName === 'TH') {
+    const newTh = document.createElement('th');
+
+    newTh.textContent = cell.children[1].textContent;
+    cell.insertBefore(newTh, cell.children[4]);
+  }
+
+  if (cell.children[1].tagName === 'TD') {
+    const newTd = document.createElement('td');
+
+    newTd.textContent = cell.children[1].textContent;
+    cell.insertBefore(newTd, cell.children[4]);
+  }
+}
