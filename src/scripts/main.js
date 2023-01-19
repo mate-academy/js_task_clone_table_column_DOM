83d1b1b1694f21a480e637c5e77cf22a1b69d2bc
Vadym Prydatok <vadymprydatok@gmail.com>
2023-01-19 11:27:35 +0200

    reduced_code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,18 +2,5 @@
 
 const cells = document.querySelectorAll('tr');
 
-for (const cell of cells) {
-  if (cell.children[1].tagName === 'TH') {
-    const newTh = document.createElement('th');
-
-    newTh.textContent = cell.children[1].textContent;
-    cell.insertBefore(newTh, cell.children[4]);
-  }
-
-  if (cell.children[1].tagName === 'TD') {
-    const newTd = document.createElement('td');
-
-    newTd.textContent = cell.children[1].textContent;
-    cell.insertBefore(newTd, cell.children[4]);
-  }
-}
+cells.forEach(cell =>
+  cell.insertBefore(cell.children[1].cloneNode(true), cell.children[4]));
