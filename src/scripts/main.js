4d1cc8d76ccc29979635df819fd11d2fd35fcfdd
Mykhailo Orlov <mihalikmihalik85@gmail.com>
2023-01-21 18:14:22 +0200

    tests good

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+[...table.rows].forEach(item => {
+  const clone = item.children[1].cloneNode(true);
+
+  item.lastElementChild.before(clone);
+});
