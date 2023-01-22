abed9c56b6520a2eaa1bd459369cae0ce4f42e5c
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-01-22 10:47:11 +0200

    Finish task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write your code here
+const staffTable = document.querySelector('table');
+
+cloneTablbeColumn(staffTable, 1, 4);
+
+function cloneTablbeColumn(table, originIndex, insertBeforeIndex) {
+  for (let i = 0; i < table.rows.length; i++) {
+    const row = table.rows[i];
+    const rowItems = row.children;
+    const rowItem = rowItems[originIndex];
+
+    if (rowItem.tagName === 'TH') {
+      const th = document.createElement('th');
+
+      th.textContent = rowItem.textContent;
+      row.insertBefore(th, rowItems[insertBeforeIndex]);
+    }
+
+    if (rowItem.tagName === 'TD') {
+      const td = document.createElement('td');
+
+      td.textContent = rowItem.textContent;
+      row.insertBefore(td, rowItems[insertBeforeIndex]);
+    }
+  }
+}
