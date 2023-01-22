2a8cc16e0dfc552ae6119b1dfd831cd237f62514
Olena Hapon <olenagapon90gmail.com>
2023-01-22 09:22:48 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('tr');
+
+rows.forEach(row => {
+  const position = row.children[1];
+
+  const newColumn = position.cloneNode(true);
+
+  row.children[3].after(newColumn);
+});
