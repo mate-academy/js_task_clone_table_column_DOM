af37ac532ba9a07365a9ed6ed8b96c733a988a0d
Anastasia Radchenko <radchenckooo@gmail.com>
2023-01-24 18:31:24 +0200

    Table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,18 +3,10 @@
 // write your code here
 const table = document.querySelectorAll('tr');
 
-/*
-for (let i = 0; i < table.length; i++) {
-  const td = tableBody.rows[i].cells[1];
-
-  console.log(td);
-}
-*/
-
 table.forEach((tr) => {
   const newCell = tr.insertCell(4);
 
-  const bodyId = tr.cells[1].innerHTML;
+  const cellElement = tr.cells[1].innerHTML;
 
-  newCell.append(bodyId);
+  newCell.append(cellElement);
 });
