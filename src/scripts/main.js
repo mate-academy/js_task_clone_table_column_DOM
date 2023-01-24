db89e6b3bab5b21c7d5e609044ce2f6b394ce468
Anastasia Radchenko <radchenckooo@gmail.com>
2023-01-24 16:42:31 +0200

    New Table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,33 +1,17 @@
 'use strict';
 
 // write your code here
-const table = document.querySelector('table');
-
-const header = table.querySelector('thead');
-const body = table.querySelector('tbody');
-const footer = table.querySelector('tfoot');
-
-const headerTR = header.querySelector('tr');
-const bodyTr = body.querySelectorAll('tr');
-const footerTR = footer.querySelector('tr');
-
-headerTR.insertAdjacentHTML('beforeend', `
-  <th>Position</th>
-`);
-
-footerTR.insertAdjacentHTML('beforeend', `
-  <th>Position</th>
-`);
+const table = document.querySelectorAll('tr');
 
 /*
-for (let i = 0; i < bodyTr.length; i++) {
+for (let i = 0; i < table.length; i++) {
   const td = tableBody.rows[i].cells[1];
 
   console.log(td);
 }
 */
 
-bodyTr.forEach((tr) => {
+table.forEach((tr) => {
   const newCell = tr.insertCell(4);
 
   const bodyId = tr.cells[1].innerHTML;
