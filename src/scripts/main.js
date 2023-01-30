dbf65237b33f9f03a7e2bf268e661f70fd3237af
Vadym Tsyndra <Mr.Tsyndra@gmail.com>
2023-01-30 11:45:23 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const tableRows = table.rows;
+
+for (const row of tableRows) {
+  const columnPosition = row.cells[1];
+
+  const columnPositionCopy = columnPosition.cloneNode(true);
+
+  const columnSalary = row.cells[4];
+
+  columnSalary.remove();
+
+  row.append(columnPositionCopy);
+
+  row.append(columnSalary);
+}
