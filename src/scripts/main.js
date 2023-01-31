e249defa5745dcf083dce5cc87c06525a37bfa6a
Ukrainian_panda <94929618+Ukrainiane-panda@users.noreply.github.com>
2023-01-31 15:17:32 +0200

    Added cloneNode method

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,5 @@
 const table = document.querySelectorAll('tr');
 
 for (const row of table) {
-  const cell = document.createElement('th');
-
-  cell.textContent = row.children[1].textContent;
-
-  row.children[3].after(cell);
+  row.children[3].after(row.children[1].cloneNode(true));
 };
