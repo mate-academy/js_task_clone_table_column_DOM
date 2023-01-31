a6c19f5027620c372d386f7dd30dbd00602bcd4f
Vadym Tsyndra <Mr.Tsyndra@gmail.com>
2023-01-31 14:36:37 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,14 +5,10 @@ const tableRows = table.rows;
 
 for (const row of tableRows) {
   const columnPosition = row.cells[1];
-
   const columnPositionCopy = columnPosition.cloneNode(true);
-
   const columnSalary = row.cells[4];
 
   columnSalary.remove();
-
   row.append(columnPositionCopy);
-
   row.append(columnSalary);
 }
