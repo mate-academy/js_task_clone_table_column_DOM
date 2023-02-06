27e5e7f997d4af5fe81ec5faf6295e9eb289bbfa
Kate <novak5683@gmail.com>
2023-02-06 21:10:50 +0100

    result

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const tbl = document.getElementsByTagName('table')[0];
+
+for (const node of tbl.children) {
+  for (const n of node.children) {
+    const extra = document.createElement(n.children[0].tagName);
+
+    extra.textContent = n.children[1].textContent;
+
+    const cols = [...[...n.children].slice(0, 4), extra,
+      ...[...n.children].slice(4)];
+
+    n.innerHTML = '';
+    cols.forEach(col => n.appendChild(col));
+  };
+}
