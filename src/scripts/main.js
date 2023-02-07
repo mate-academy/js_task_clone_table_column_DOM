ce3ffe88ac98fa708a718e452e36eb223081da07
Kate <novak5683@gmail.com>
2023-02-07 11:45:02 +0100

    result fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,9 @@
 'use strict';
 
-const tbl = document.getElementsByTagName('table')[0];
+const tr = document.querySelectorAll('tr');
 
-for (const node of tbl.children) {
-  for (const n of node.children) {
-    const extra = document.createElement(n.children[0].tagName);
+tr.forEach(item => {
+  const cloneElements = item.children[1].cloneNode(true);
 
-    extra.textContent = n.children[1].textContent;
-
-    const cols = [...[...n.children].slice(0, 4), extra,
-      ...[...n.children].slice(4)];
-
-    n.innerHTML = '';
-    cols.forEach(col => n.appendChild(col));
-  };
-}
+  item.children[4].before(cloneElements);
+});
