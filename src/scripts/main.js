12cae056c5e44a27814815321499b0a0b546d9d0
Ivan <90928916+vanyachyzh@users.noreply.github.com>
2023-02-10 01:01:32 +0200

    task was solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const currentH = table.tHead.children[0].cells[1].cloneNode(true);
+const currentF = table.tFoot.children[0].cells[1].cloneNode(true);
+const nextH = table.tHead.children[0].lastElementChild;
+const nextF = table.tFoot.children[0].lastElementChild;
+const rowsB = [...table.tBodies[0].children];
+
+nextH.before(currentH);
+nextF.before(currentF);
+
+rowsB.map(row => {
+  const currentB = row.cells[1].cloneNode(true);
+  const nextB = row.lastElementChild;
+
+  nextB.before(currentB);
+});
