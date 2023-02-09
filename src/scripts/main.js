8b5a96de0d1c86be34366fcc8bc1591252e29c34
Vasyl Pavlenko <genlus@ukr.net>
2023-02-09 14:58:39 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('tr');
+
+rows.forEach(row => {
+  const clone = row.children[1].cloneNode(true);
+
+  row.children[4].before(clone);
+});
