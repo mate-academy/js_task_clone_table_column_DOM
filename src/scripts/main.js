84b6a556fb5271c0c90a8fb9633bc46a2f0f80d0
Mykola Huryn <mukola197022@gmail.com>
2023-02-09 16:39:42 +0100

    js_task_clone_table_column_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const tr = document.querySelectorAll('tr');
+
+for (const i of tr) {
+  const copy = i.cells[1].cloneNode(true);
+
+  i.cells[4].before(copy);
+}
