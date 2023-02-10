e376cdcbd72263b1615144ec5ce93e05a07d0768
Ivan <90928916+vanyachyzh@users.noreply.github.com>
2023-02-10 15:11:44 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,8 @@
 'use strict';
 
-const table = document.querySelector('table');
-const currentH = table.tHead.children[0].cells[1].cloneNode(true);
-const currentF = table.tFoot.children[0].cells[1].cloneNode(true);
-const nextH = table.tHead.children[0].lastElementChild;
-const nextF = table.tFoot.children[0].lastElementChild;
-const rowsB = [...table.tBodies[0].children];
+const rowsB = document.querySelectorAll('tr');
 
-nextH.before(currentH);
-nextF.before(currentF);
-
-rowsB.map(row => {
+rowsB.forEach(row => {
   const currentB = row.cells[1].cloneNode(true);
   const nextB = row.lastElementChild;
 
