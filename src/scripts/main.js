c0d1d63101c3c903c6cf909c231963c4f6c44cfe
Oleksandr <98667890az@gmail.com>
2023-02-13 13:05:54 +0200

    clone_table_column_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const tableHeadRow = table.querySelector('thead').querySelector('tr');
+const tableBodyRow = table.querySelector('tbody').querySelectorAll('tr');
+const tableFootRow = table.querySelector('tfoot').querySelector('tr');
+
+const copyHead = document.createElement('th');
+
+copyHead.textContent = tableHeadRow.children[1].textContent;
+
+tableHeadRow.insertBefore(copyHead, tableHeadRow.children[4]);
+
+for (const row of tableBodyRow) {
+  const copyBody = document.createElement('td');
+
+  copyBody.textContent = row.children[1].textContent;
+
+  row.insertBefore(copyBody, row.children[4]);
+}
+
+const copyFoot = document.createElement('th');
+
+copyFoot.textContent = tableFootRow.children[1].textContent;
+
+tableFootRow.insertBefore(copyFoot, tableFootRow.children[4]);
