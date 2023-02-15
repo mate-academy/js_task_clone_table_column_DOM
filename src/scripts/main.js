8014df67119b9a5b31fdfd54547a1191e755d7db
KseniyaGonchar <kseniyadf888@gmail.com>
2023-02-15 18:54:23 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const list = document.querySelectorAll('tr');
+
+[...list].map(item => {
+  const addItem = item.children[1].cloneNode(true);
+
+  return item.children[3].after(addItem);
+});
