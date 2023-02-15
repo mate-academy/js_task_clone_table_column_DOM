f3bfb6c7dd7ba66caacd698052ca4f27675e60d3
Nazarii AlieKsieiev <nazarmetall@gmail.com>
2023-02-15 16:08:43 +0200

    Solution clone_table_column

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('tr');
+
+rows.forEach(row => {
+  const currentRow = row.cells[1].cloneNode(true);
+  const neighborRow = row.lastElementChild;
+
+  neighborRow.before(currentRow);
+});
