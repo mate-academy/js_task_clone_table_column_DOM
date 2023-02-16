6cfd12f55c60b0f1a9d110e914d537a2d86d6162
mykolachupyak <chupyak@gmail.com>
2023-02-16 21:11:28 +0200

    improvement

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const data = document.querySelectorAll('tr');
+const tableRows = document.querySelectorAll('tr');
 
-for (const element of data) {
+for (const element of tableRows) {
   const newNode = element.children[1].cloneNode(true);
 
   element.insertBefore(newNode, element.children[4]);
