dc00957e04ca2c1c1954c0f12eb6b9426a42e450
Nikitin Evgeny <ngenya81@gmail.com>
2023-02-17 10:38:23 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const lengthCollection = document.getElementsByTagName('tr');
+
+for (let i = 0; i < lengthCollection.length; i++) {
+  const collectionTr = document.getElementsByTagName('tr')[i];
+
+  const child = collectionTr.children[1];
+
+  const copyChild = child.cloneNode(true);
+
+  collectionTr.insertBefore(copyChild, collectionTr.children[4]);
+}
