c118cafe0be24c289985fad8c2c1815e8cc2f014
Kyrychenko Illia <i.kirichenko160804@gmail.com>
2023-02-19 01:35:53 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
 // write your code here
+const tableRows = document.querySelectorAll('tr');
+
+function getTableColumn(columnNumber) {
+  const cellsArray = [];
+
+  tableRows.forEach((row) => {
+    const rowCell = row.children[columnNumber - 1];
+
+    cellsArray.push(rowCell.innerHTML);
+  });
+
+  return cellsArray;
+}
+
+function copyPasteColumnAfter(columnNumber, cellsArray) {
+  for (let i = 0; i < cellsArray.length; i++) {
+    const tableHead = document.createElement('th');
+    const tableData = document.createElement('td');
+
+    const tableRowCollection = tableRows[i].children;
+
+    if (i === 0 || i === cellsArray.length - 1) {
+      tableHead.innerHTML = cellsArray[i];
+
+      tableRowCollection[columnNumber - 1].after(tableHead);
+    } else {
+      tableData.innerHTML = cellsArray[i];
+
+      tableRowCollection[columnNumber - 1].after(tableData);
+    }
+  }
+}
+
+const cellsArray = getTableColumn(2);
+
+copyPasteColumnAfter(4, cellsArray);
