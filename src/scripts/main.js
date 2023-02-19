c62187df1d0afe103488c9c36594c3590c57e313
Nikitin Evgeny <ngenya81@gmail.com>
2023-02-19 12:48:41 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,11 @@
 const employees = document.getElementsByTagName('tr');
 
 for (let i = 0; i < employees.length; i++) {
-  const collectionTr = document.getElementsByTagName('tr')[i];
+  const employee = employees[i];
 
-  const positionColumn = collectionTr.children[1];
+  const positionColumn = employee.children[1];
 
   const positionColumnCopy = positionColumn.cloneNode(true);
 
-  collectionTr.insertBefore(positionColumnCopy, collectionTr.children[4]);
+  employee.insertBefore(positionColumnCopy, employee.children[4]);
 }
