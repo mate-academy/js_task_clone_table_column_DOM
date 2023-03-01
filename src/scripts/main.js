946e26593618a73fece188f13ce9a045eaad0c5a
Serhii Nikolin <sergeynikolin@gmail.com>
2023-03-01 16:06:02 +0200

    Ready

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write your code here
+const tr = document.querySelectorAll('tr');
+
+for (const item of tr) {
+  const clone = item.children[1].cloneNode(true);
+  const lastColumn = item.lastElementChild;
+
+  lastColumn.before(clone);
+}
