cb55e45e4303d0837da534364297b9d990c65a35
JulyaPetrovskaya <119932772+JulyaPetrovskaya@users.noreply.github.com>
2023-03-05 13:25:47 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const rows = table.rows; 
+
+for (let i = 0; i < rows.length; i++) {
+  const secondCell = rows[i].cells[1]; 
+  const newCell = document.createElement('td'); 
+  newCell.textContent = secondCell.textContent; 
+  const lastCell = rows[i].cells[rows[i].cells.length - 1];
+  rows[i].insertBefore(newCell, lastCell);
+}
