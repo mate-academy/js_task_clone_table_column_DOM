0a5f060afa91d93159f875ad9d592abb637bd203
Bozhena Bodnar <bozhena2705bodnar@gmail.com>
2023-03-18 15:23:17 +0100

    create table with clons second row

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const rows = document.querySelectorAll('tr');
+
+for (let i = 0; i < table.rows.length; i++) {
+  const secondRow = table.rows[i].cells[1];
+  const copy = secondRow.cloneNode(true);
+
+  rows[i].insertBefore(copy, rows[i].children[4]);
+}
