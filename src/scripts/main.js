f9f6542a37fca606d8ca0848e5816369b40496b5
Sergey Lipikhin <football3@ukr.net>
2023-03-20 15:16:58 +0200

    Complete solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+function CloneElementOfRow(row) {
+  const cloneElement = row.cells[columnIndexToClone].cloneNode(true);
+  const recipientElement = row.cells[columnIndexRecipient];
+
+  recipientElement.insertAdjacentElement('beforebegin', cloneElement);
+}
+
+const tableElement = document.querySelector('table');
+
+const tableHeadElement = tableElement.tHead.rows[0];
+const tableBodyRowElements = tableElement.tBodies[0].rows;
+const tableFootElement = tableElement.tFoot.rows[0];
+
+const columnIndexToClone = 1;
+const columnIndexRecipient = tableHeadElement.cells.length - 1;
+
+CloneElementOfRow(tableHeadElement);
+CloneElementOfRow(tableFootElement);
+[...tableBodyRowElements].forEach(CloneElementOfRow);
