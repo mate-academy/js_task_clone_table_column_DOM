28672e78ec11bb3eab7747b76c7c362fd6995cd1
stetsenko_viktoriia <stetsenko.viktoriia@gmail.com>
2023-03-22 20:16:13 +0200

    column was cloned

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const tableRow = document.querySelectorAll('tr');
+
+for (const item of tableRow) {
+  const child = item.children[1].cloneNode(true);
+
+  item.children[4].before(child);
+}
