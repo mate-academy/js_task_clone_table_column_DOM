5c0cbb2624207f58113428e7e98a2049554ea5dd
Nadiya Ovsiyenko <nadia.ovsienko1009@gmail.com>
2023-03-22 11:57:01 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (const row of [...table.rows]) {
+  const position = (row.parentElement.tagName === 'THEAD'
+  || row.parentElement.tagName === 'TFOOT')
+    ? document.createElement('th')
+    : document.createElement('td');
+
+  position.textContent = row.cells[1].innerHTML;
+
+  row.lastElementChild.before(position);
+}
