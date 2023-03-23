19735e934b37c43c04f897e545a54e6d04d9312a
Oleksandr Plias <pls777@ukr.net>
2023-03-23 15:21:26 +0200

    Fixed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,5 @@
 'use strict';
 
-// const table = document.querySelector('table');
-
-// for (let i = 0; i < rows.length; i++) {
-//   const clone = table.rows[i].cells[1].cloneNode(true);
-
-//   table.rows[i].cells[4].insertAdjacentElement('beforebegin, clone');
-// }
-
 const table = document.querySelector('table');
 
 const rows = table.rows;
