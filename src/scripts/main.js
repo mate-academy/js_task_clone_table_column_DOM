871681a462663cc6a795a8d84d18a94844969b91
Maksym Avramenko <nonste@outlook.com>
2023-03-23 12:02:11 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const rows = table.querySelectorAll('tbody tr');
+const headers = table.querySelectorAll('thead th');
+const footers = table.querySelectorAll('tfoot th');
+const newHeaderElement = document.createElement('th');
+const newFooterElement = document.createElement('th');
+
+newHeaderElement.textContent = headers[1].textContent;
+newFooterElement.textContent = footers[1].textContent;
+
+headers[3].insertAdjacentElement('afterend', newHeaderElement);
+footers[3].insertAdjacentElement('afterend', newFooterElement);
+
+rows.forEach((row) => {
+  const cells = row.querySelectorAll('td');
+  const secondCellContent = cells[1].innerHTML;
+  const newCell = document.createElement('td');
+
+  newCell.innerHTML = secondCellContent;
+  row.insertBefore(newCell, cells[cells.length - 1]);
+});
