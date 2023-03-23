fb385e24e42481f91e44d714c28d8ca3d3678d2d
Maksym Avramenko <nonste@outlook.com>
2023-03-23 15:26:57 +0200

    code refactoring

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,7 @@
 'use strict';
 
-const table = document.querySelector('table');
-const rows = table.querySelectorAll('tbody tr');
-const headers = table.querySelectorAll('thead th');
-const footers = table.querySelectorAll('tfoot th');
-const newHeaderElement = document.createElement('th');
-const newFooterElement = document.createElement('th');
+const tableRows = document.querySelectorAll('tr');
 
-newHeaderElement.textContent = headers[1].textContent;
-newFooterElement.textContent = footers[1].textContent;
-
-headers[3].insertAdjacentElement('afterend', newHeaderElement);
-footers[3].insertAdjacentElement('afterend', newFooterElement);
-
-rows.forEach((row) => {
-  const cells = row.querySelectorAll('td');
-  const secondCellContent = cells[1].innerHTML;
-  const newCell = document.createElement('td');
-
-  newCell.innerHTML = secondCellContent;
-  row.insertBefore(newCell, cells[cells.length - 1]);
+tableRows.forEach((row) => {
+  row.insertBefore(row.children[1].cloneNode(true), row.children[4]);
 });
