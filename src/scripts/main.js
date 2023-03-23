ccf6e24ec2d5a6bb6b12479bd65bbd980198db3c
Nazar Kharkevych <nkharkevych@gmail.com>
2023-03-23 18:45:09 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (const row of table.rows) {
+  const clone = row.cells[1].cloneNode(true);
+
+  row.cells[4].before(clone);
+}
