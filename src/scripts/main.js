4d1683c7b6390f38a1ddeb982c078993d054c763
Andrii Kovalskyi <andrii.kovalskyi02@gmail.com>
2023-03-24 17:30:45 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const tableRows = document.querySelectorAll('tr');
+
+tableRows.forEach(tr => {
+  const td = tr.cells[1].cloneNode(true);
+
+  tr.cells[tr.cells.length - 2].after(td);
+});
