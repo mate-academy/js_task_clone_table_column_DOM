ecd381d310e3d56ca044e56b99205377c8301822
Daria <hamdsad8@gmail.com>
2023-03-25 12:35:17 +0200

    fixed table columns

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const tableRowsList = document.querySelectorAll('tr');
+const tableRows = [...tableRowsList];
+
+for (const tableRow of tableRows) {
+  const copiedCell = tableRow.children[1];
+  const tagName = copiedCell.tagName.toLowerCase();
+  const newCell = document.createElement(tagName);
+
+  newCell.textContent = copiedCell.textContent;
+
+  const lastChildIndex = tableRow.children.length - 1;
+
+  tableRow.insertBefore(newCell, tableRow.children[lastChildIndex]);
+}
