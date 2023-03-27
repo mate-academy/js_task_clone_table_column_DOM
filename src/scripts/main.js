d093eb07607cb17d59bb3f7317ff2ad46550ec10
Yaroslav Dudar <dudar.komod@gmail.com>
2023-03-27 20:26:55 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const tableChild = table.children;
+
+for (let i = 0; i < tableChild.length; i++) {
+  copyColumn(2, 4, tableChild[i]);
+};
+
+function copyColumn(columnToCopy, toPasteAfter, tPart) {
+  const copyIndex = columnToCopy - 1;
+
+  for (let i = 0; i < tPart.children.length; i++) {
+    const newCell = tPart.rows[i].cells[copyIndex];
+
+    tPart.rows[i]
+      .insertBefore(newCell.cloneNode(true),
+        tPart.rows[i].children[toPasteAfter]);
+  };
+};
