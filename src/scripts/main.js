420da7516334b112899853bf8461ece19a136cb3
Diana Snihur <dianasnigur2@gmail.com>
2023-03-28 19:12:51 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const trElement = document.querySelectorAll('tr');
+
+trElement.forEach(item => {
+  const positionElement = item.cells[1];
+
+  const lastCell = item.cells[item.cells.length - 1];
+
+  lastCell.parentNode.insertBefore(positionElement.cloneNode(true), lastCell);
+});
