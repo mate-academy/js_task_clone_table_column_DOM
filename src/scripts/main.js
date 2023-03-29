771b6efbf97e98abeaf4c472d3d4921b3cba9da3
Sergii Mishchenko <serge.mishchenko@gmail.com>
2023-03-29 11:54:29 +0300

    Table colomn was copied and pasted

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write your code here
+const tableAll = document.querySelector('table');
+
+for (let i = 0; i < tableAll.rows.length; i++) {
+  const trAll = document.querySelectorAll('tr');
+
+  let newElement;
+
+  if (i === 0 || i === (tableAll.rows.length - 1)) {
+    newElement = document.createElement('th');
+  }
+
+  if (i > 0 && i < (tableAll.rows.length - 1)) {
+    newElement = document.createElement('td');
+  }
+
+  trAll[i].append(newElement);
+
+  newElement.textContent = tableAll.rows[i].cells[1].textContent;
+
+  const salary = tableAll.rows[i].cells[4].textContent;
+
+  const position = tableAll.rows[i].cells[5].textContent;
+
+  tableAll.rows[i].cells[4].textContent = position;
+
+  tableAll.rows[i].cells[5].textContent = salary;
+}
