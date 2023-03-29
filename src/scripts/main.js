c79f9f139dc9b3de1f6d49cd764f6afc96db3478
Oleksandr Lytovchenko <jaster93him@gmail.com>
2023-03-29 17:19:54 +0300

    added clone table column

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const tab = document.querySelector('table');
+
+for (let i = 0; i < tab.rows.length; i++) {
+  const clone = tab.rows[i].cells[1].cloneNode(true);
+
+  tab.rows[i].cells[4].insertAdjacentElement('beforebegin', clone);
+}
