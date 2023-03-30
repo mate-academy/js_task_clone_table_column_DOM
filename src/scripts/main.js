73aeab300c6c001025767afdc1700da4b3facfc5
Roman Hrytsiuk <vasylguk7@gmail.com>
2023-03-30 22:44:00 +0300

    Initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const head = document.querySelector('tr');
+
+const headItem = document.querySelectorAll('th')[1];
+
+head.children[3].after(headItem.cloneNode(true));
+
+const body = document.querySelector('tbody');
+
+for (const item of body.children) {
+  item.children[3].after(item.children[1].cloneNode(true));
+}
+
+const foot = document.querySelector('tfoot');
+
+const footItem = foot.children[0].children[1];
+
+foot.children[0].children[3].after(footItem.cloneNode(true));
