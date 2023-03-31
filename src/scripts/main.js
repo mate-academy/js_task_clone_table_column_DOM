8932c06dd0d054d1ad93d0eae8fbe7da2f7002ba
Daryna Hnidash <melissa998877@gmail.com>
2023-03-31 19:25:52 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (let i = 0; i < table.rows.length; i++) {
+  const td = table.rows[i].cells[1].localName === 'td'
+    ? document.createElement('td')
+    : document.createElement('th');
+
+  td.innerText = table.rows[i].cells[1].textContent;
+  table.rows[i].append(td);
+}
+
+for (let i = 0; i < table.rows.length; i++) {
+  table.rows[i].append(table.rows[i].cells[4]);
+}
