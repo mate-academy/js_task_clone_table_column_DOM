c44b425faf61a336ffeecc040e1b5773e8e7d4d9
Daryna Hnidash <melissa998877@gmail.com>
2023-04-01 07:42:34 +0300

    optimized

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,9 @@
 'use strict';
 
-const table = document.querySelector('table');
+const tab = document.querySelector('table');
 
-for (let i = 0; i < table.rows.length; i++) {
-  const td = table.rows[i].cells[1].localName === 'td'
-    ? document.createElement('td')
-    : document.createElement('th');
+for (let i = 0; i < tab.rows.length; i++) {
+  const clone = tab.rows[i].cells[1].cloneNode(true);
 
-  td.innerText = table.rows[i].cells[1].textContent;
-  table.rows[i].append(td);
-}
-
-for (let i = 0; i < table.rows.length; i++) {
-  table.rows[i].append(table.rows[i].cells[4]);
+  tab.rows[i].cells[4].insertAdjacentElement('beforebegin', clone);
 }
