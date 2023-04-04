208784dc3e2180788fe379d0420061c302ddf367
Anna Peretiatko <tobossanna@gmail.com>
2023-04-04 20:04:37 +0200

    Clone table column solution v.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (let i = 0; i < table.rows.length; i++) {
+  const positionCell = table.rows[i].cells[1];
+  const salaryCell = table.rows[i].cells[4];
+  const newPositionCell = positionCell.cloneNode(true);
+
+  table.rows[i].insertBefore(newPositionCell, salaryCell);
+}
