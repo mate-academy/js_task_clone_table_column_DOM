3ef99cad143fe6e499072fb92d30ba40c52d4e7e
Pavlo Kostyshyn <kostishinpavlo24@gmail.com>
2023-04-07 12:40:12 +0300

    clone of table column was done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (let i = 0; i < table.rows.length; i++) {
+  const row = table.rows[i];
+  const copyCell = row.cells[1].cloneNode(true);
+
+  row.insertBefore(copyCell, row.cells[row.cells.length - 1]);
+}
