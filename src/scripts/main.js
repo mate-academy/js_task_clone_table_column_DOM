0ab83a5809c5d0fc2b2e20fd3d9cee70cacedbb2
Ilia Kolesnik <koleshnik160799@gmail.com>
2023-04-10 01:12:08 +0300

    Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write your code here
+const tr = document.querySelectorAll('tr');
+
+for(let i = 0; i < tr.length; i++) {
+  const secondItem = tr[i].children[1];
+  const lastItem = tr[i].children[tr[i].children.length - 1];
+  const tag = secondItem.tagName.toLowerCase();
+
+  lastItem.remove()
+
+  tr[i].insertAdjacentHTML('beforeend', `
+  <${tag}>${secondItem.innerText}</${tag}>
+  <${tag}>${lastItem.innerText}</${tag}>
+  `)
+}
