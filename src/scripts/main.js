cf92a4532a48ff9fd9ea7343ba611cc3a4350f4a
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2023-04-10 13:51:33 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const rows = table.rows;
+const arrOfData = [];
+
+[...rows].forEach(row => {
+  arrOfData.push(row.cells[1].innerHTML);
+});
+
+[...rows].forEach((row, i) => {
+  let cell;
+
+  if (i === 0 || i === arrOfData.length - 1) {
+    cell = document.createElement('th');
+    cell.textContent = arrOfData[i];
+    row.insertBefore(cell, row.cells[5]);
+  } else {
+    cell = row.insertCell(5);
+    cell.textContent = arrOfData[i];
+  }
+});
