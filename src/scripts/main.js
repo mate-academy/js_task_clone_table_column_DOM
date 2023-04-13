9036dbe04c4dfd07d35b38e4a4310298b458e76b
AnastassiaH <nastyagurska@gmail.com>
2023-04-13 13:23:03 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // write your code here
+const table = document.querySelector('table');
+
+[...table.children].forEach(item => {
+  const rows = [...item.children];
+
+  rows.forEach(row => {
+    const cellToCopy = row.children[1];
+    const copy = cellToCopy.cloneNode(true);
+
+    row.children[3].after(copy);
+  });
+});
