6c61fde2c60e19884d36d6eb7b99ce14a832ff7f
popeye88 <aleksei.taran@gmail.com>
2023-04-17 16:22:50 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const tHeaders = document.querySelectorAll('thead th');
+const tBodyRows = document.querySelectorAll('tbody tr');
+const tFooters = document.querySelectorAll('tfoot th');
+
+tHeaders[4].insertAdjacentElement('beforebegin', tHeaders[1].cloneNode(true));
+
+tBodyRows.forEach(row => {
+  const newCell = row.children[1].cloneNode(true);
+
+  row.insertBefore(newCell, row.children[4]);
+});
+
+tFooters[4].insertAdjacentElement('beforebegin', tFooters[1].cloneNode(true));
