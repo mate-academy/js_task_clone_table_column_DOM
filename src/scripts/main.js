eb63efcda3e4d8f52e3d64a5438561fb56a92eae
DanielSorokowski <sorokowskidaniel0@gmail.com>
2023-04-17 13:29:37 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+[...table.rows].forEach((item, index) => {
+  const element = table.rows[index].cells[1];
+  const copy = document.createElement(element.tagName);
+
+  copy.innerText = element.innerText;
+
+  table.rows[index].insertBefore(copy, table.rows[index].cells[4]);
+})
