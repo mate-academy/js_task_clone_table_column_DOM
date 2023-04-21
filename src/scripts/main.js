234d41fb2337a1450e63bd4bb5c19ca97a5cd649
Victor Buldenko <gookidoo@gmail.com>
2023-04-21 15:21:31 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('TABLE');
+
+[...table.rows].forEach(row => {
+  const cloneSecondCell = row.cells[1].cloneNode(true);
+  const lastCellInRow = row.cells[row.cells.length - 1];
+
+  lastCellInRow.insertAdjacentElement('beforebegin', cloneSecondCell);
+});
