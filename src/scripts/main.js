74c59cfbe4f1341e001badc2c1753caa675c40a2
Nadiein Oleh <olegnadein1994@gmail.com>
2023-04-21 15:23:00 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const tbodies = [...table.tBodies];
+const cellsInColumn2 = [];
+
+const copyAndPaste = (copy, pasteAfter) => {
+  pasteAfter.after(copy.cloneNode(true));
+};
+
+copyAndPaste(table.rows[0].cells[1], table.rows[0].cells[3]);
+copyAndPaste(table.rows[13].cells[1], table.rows[13].cells[3]);
+
+tbodies.forEach((tbody) => {
+  [...tbody.rows].forEach((row) => {
+    cellsInColumn2.push(row.cells[1]);
+  });
+
+  [...tbody.rows].forEach((row, index) => {
+    const cell5 = row.insertCell(4);
+
+    cell5.innerHTML = cellsInColumn2[index].innerHTML;
+  });
+});
