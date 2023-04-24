6559ebef4a95d79201dd14db3d6b2c2b8d837d4c
Hanna Vasylieva <ann0950368600@gmail.com>
2023-04-24 20:02:29 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('table tbody tr');
+
+rows.forEach(row => {
+  const newCell = document.createElement('td');
+
+  newCell.textContent = row.cells[1].textContent;
+
+  row.insertBefore(newCell, row.cells[row.cells.length - 1]);
+});
+
+const tfoot = document.querySelector('table tfoot tr');
+const newFooterCell = document.createElement('th');
+
+newFooterCell.textContent = 'Position';
+tfoot.insertBefore(newFooterCell, tfoot.cells[tfoot.cells.length - 1]);
+
+const thead = document.querySelector('table thead tr');
+const newHeaderCell = document.createElement('th');
+
+newHeaderCell.textContent = 'Position';
+thead.insertBefore(newHeaderCell, thead.cells[thead.cells.length - 1]);
