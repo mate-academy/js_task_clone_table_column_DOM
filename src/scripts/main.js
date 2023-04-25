9590479eab895abc9db4d350c165c09b4533cd73
Vladyslav Fedenko <vadondrummer@gmail.com>
2023-04-26 00:03:03 +0300

    fix2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,27 +6,17 @@ const header = (table[0]);
 const tBody = (table[1]);
 const tFooter = (table[2]);
 
-function findPosition(element, index) {
-  const rows = element.children[index];
-  const insertPosition = element.children[index].children[4];
-  let copy = document.createElement('th');
+function copy(tablePart) {
+  const row = tablePart.children;
 
-  if (element.children.length > 1) {
-    copy = document.createElement('td');
+  for (let i = 0; i < row.length; i++) {
+    const position = row[i].children[1];
+    const salary = row[i].children[4];
+    const positionCopy = position.cloneNode(true);
+
+    row[i].insertBefore(positionCopy, salary);
   }
-
-  copy.innerText = rows.children[1].innerText;
-  element.children[index].insertBefore(copy, insertPosition);
-}
-
-for (let i = 0; i < tBody.children.length; i++) {
-  findPosition(tBody, i);
 };
-
-for (let i = 0; i < header.children.length; i++) {
-  findPosition(header, i);
-};
-
-for (let i = 0; i < tFooter.children.length; i++) {
-  findPosition(tFooter, i);
-};
+copy(header);
+copy(tBody);
+copy(tFooter);
