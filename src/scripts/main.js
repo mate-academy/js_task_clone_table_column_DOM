348cb36da2bc74423ca250df3925daf98a4ea8fe
Oksana Skuba <skuba.o.ua@gmail.com>
2023-04-28 17:02:24 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const tbody = table.querySelector('tbody');
+const thead = table.querySelector('thead');
+const tfoot = table.querySelector('tfoot');
+const rows = tbody.rows;
+
+[...rows].forEach(row => {
+  const td = row.querySelectorAll('td')[1];
+  const cloneTd = td.cloneNode(true);
+
+  row.insertBefore(cloneTd, row.lastElementChild);
+});
+
+const tdHead = thead.rows[0].querySelectorAll('th')[1];
+const cloneTdHead = tdHead.cloneNode(true);
+thead.rows[0].insertBefore(cloneTdHead, thead.rows[0].lastElementChild);
+
+const tdFoot = tfoot.rows[0].querySelectorAll('th')[1];
+const cloneTdFoot = tdFoot.cloneNode(true);
+tfoot.rows[0].insertBefore(cloneTdFoot, tfoot.rows[0].lastElementChild);
