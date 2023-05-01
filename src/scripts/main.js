23a425bc6ef3685729ca3dd2119a6b674c06e17c
Nick <tortickk8475@gmail.com>
2023-05-01 11:56:35 +0300

    chore: append element into table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const appendElem = () => {
+  const table = document.querySelector('table');
+
+  for (let i = 0; i < table.rows.length; i++) {
+    const getTd = table.rows[i].cells[1].innerHTML;
+
+    if (table.rows[i].cells[1].innerHTML === 'Position') {
+      table.rows[i].cells[3].insertAdjacentHTML('beforebegin', `
+          <th>${getTd}</th>
+        `);
+    } else {
+      table.rows[i].cells[3].insertAdjacentHTML('beforebegin', `
+          <td>${getTd}</td>
+        `);
+    }
+  }
+};
+
+appendElem();
