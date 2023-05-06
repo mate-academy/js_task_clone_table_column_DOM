c04b2669c202a181d610ae6e40fe2c6f3b03ac24
Ihor Zakharov <ihor.o.zakharov@gmail.com>
2023-05-06 15:50:56 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,23 @@
 'use strict';
 
 const duplicateSecondCell = (tableRow) => {
-  tableRow.lastElementChild.before(tableRow.children[1].cloneNode(true));
+  tableRow.lastElementChild.before(tableRow.cells[1].cloneNode(true));
 };
 
 const tableHeader = document.querySelector('thead > tr');
+
 if (tableHeader) {
   duplicateSecondCell(tableHeader);
 }
 
 const tableBody = document.querySelectorAll('tbody > tr');
+
 for (const tableRow of tableBody) {
   duplicateSecondCell(tableRow);
 }
 
 const tableFooter = document.querySelector('tfoot > tr');
+
 if (tableFooter) {
   duplicateSecondCell(tableFooter);
 }
-
