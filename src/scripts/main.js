9c393861889b743a97ffc63c8e5c6f8896301d74
Alla Gutsul <allagutsul1990@gmail.com>
2023-05-07 17:23:34 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const tableRows = table.rows;
+
+const cells = [...tableRows].map(item => item.cells[1]);
+
+for (let i = 0; i < cells.length; i++) {
+  (tableRows[i].cells[4]).before(cells[i].cloneNode(true));
+}
