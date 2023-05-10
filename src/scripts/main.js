a5fb047835564d1a16cab79ed24fefe47ddd981f
metin <metinbicaksiz@gmail.com>
2023-05-10 14:57:34 +0300

    updated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,6 @@
 'use strict';
 
-const table = document.querySelector('table');
-
-const heads = table.tHead.rows;
-const rows = table.tBodies[0].rows;
-const foots = table.tFoot.rows;
-
-for (let i = 0; i < heads.length; i++) {
-  const cell = heads[i].cells[1];
-  const clone = cell.cloneNode(true);
-  const lastCell = heads[i].cells[heads[i].cells.length - 1];
-
-  heads[i].insertBefore(clone, lastCell);
-}
+const rows = document.querySelectorAll('tr');
 
 for (let i = 0; i < rows.length; i++) {
   const cell = rows[i].cells[1];
@@ -20,12 +8,4 @@ for (let i = 0; i < rows.length; i++) {
   const lastCell = rows[i].cells[rows[i].cells.length - 1];
 
   rows[i].insertBefore(clone, lastCell);
-}
-
-for (let i = 0; i < foots.length; i++) {
-  const cell = foots[i].cells[1];
-  const clone = cell.cloneNode(true);
-  const lastCell = foots[i].cells[foots[i].cells.length - 1];
-
-  foots[i].insertBefore(clone, lastCell);
 }
