adc263e8ecaf2165c99c9861370401b38b80f8fe
DefaultGitUser <usingforgit@gmail.com>
2023-05-11 10:14:34 +0300

    Sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const tableEl = document.querySelector('table');
+const columnToCopy = 1;
+
+copyColumn(tableEl, columnToCopy);
+
+function copyColumn(table, toCopy = 1) {
+  [...table.children].forEach((child) => {
+    [...child.rows].forEach((row) => {
+      const additionalNode = row.lastElementChild.cloneNode();
+
+      additionalNode.textContent = row.cells[toCopy].textContent;
+
+      row.lastElementChild
+        .insertAdjacentHTML('beforebegin', additionalNode.outerHTML);
+    });
+  });
+}
