c15a17d1a6b2fbac65d57a235118cba759ae9b9f
Veronika Pastushkova <d9169393507@gmai.com>
2023-05-12 21:29:18 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,7 @@
 'use strict';
 
-// write your code here
+const rows = [...document.querySelector('table').rows];
+
+for (const row of rows) {
+  row.lastElementChild.before(row.cells[1].cloneNode(true));
+}
