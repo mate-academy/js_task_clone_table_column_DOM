06a8e549bf94922e2ff64f0c1ff3309a45ff48a0
Liliia Danylenko <liliya.danylenko96@gmail.com>
2023-05-16 14:11:48 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('tr');
+
+for (let i = 0; i < rows.length; i++) {
+  const row = document.querySelectorAll('tr')[i];
+
+  cloneColumn(row);
+}
+
+function cloneColumn(column) {
+  const copyColumn = column.children[1].cloneNode(true);
+
+  return column.children[4].before(copyColumn);
+}
