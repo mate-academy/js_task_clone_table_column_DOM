48e65cd74a51ee6c6c691c83c5c142ef9f640d33
Artem Kurinniy <artem.kyrinniy@gmail.com>
2023-05-16 23:37:32 +0300

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const allTrs = document.querySelectorAll('table tr');
+
+for (const tr of allTrs) {
+  const thElements = tr.getElementsByTagName('th');
+
+  if (thElements.length !== 0) {
+    const copyiedElement = document.createElement('th');
+
+    copyiedElement.innerText = thElements[1].innerText;
+    tr.append(copyiedElement);
+  } else {
+    const tdElements = tr.getElementsByTagName('td');
+    const copyiedElement = document.createElement('td');
+
+    copyiedElement.innerText = tdElements[1].innerText;
+    tr.append(copyiedElement);
+  }
+}
