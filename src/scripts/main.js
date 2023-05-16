8bad89bd9b39164dee62a306e3ec9c241f3f97db
Vitalii Parkhomenko <poroh1991ukr@gmail.com>
2023-05-17 00:03:04 +0300

    js_task-clone-table-column-DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const columnToCopy = 2;
+
+  const rowsData = document.querySelectorAll('tr');
+
+  for (const row of rowsData) {
+    const copyCell = row.children[columnToCopy - 1];
+    const columnToInsertNumber = row.children[row.children.length - 1];
+
+    copyAndInsertCell(copyCell, columnToInsertNumber);
+  }
+
+  function copyAndInsertCell(cellToCopy, cellToInsertBefore) {
+    const htmlToInsert = `<${cellToCopy.tagName.toLowerCase()}>`
+        + `${cellToCopy.textContent}`
+        + `</${cellToCopy.tagName.toLowerCase()}>`;
+
+    cellToInsertBefore.insertAdjacentHTML('beforebegin', htmlToInsert);
+  }
+});
