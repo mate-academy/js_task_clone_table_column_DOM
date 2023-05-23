e32075295ed63e6b5c892ede6c8ce02a3aa78505
Oleksandr Chernikov <pjatochkin2013@gmail.com>
2023-05-23 14:42:19 +0300

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+  let table = document.querySelector('table');
+  let rows = table.getElementsByTagName('tr');
+
+  for (var i = 0; i < rows.length; i++) {
+    let secondChild = rows[i].childNodes[3].cloneNode(true); 
+    let lastChild = rows[i].childNodes[9];
+    rows[i].insertBefore(secondChild, lastChild);
+  }
+
+  
