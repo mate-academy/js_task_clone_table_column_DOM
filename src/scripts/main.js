f11c5a06237d615f2106f349d74d620ad800b23d
Igor Galkin <igor_galkin@ukr.net>
2023-05-30 16:52:51 +0300

    1st commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+function copyColumn(copyIndex, pastIndex) {
+  for (let i = 0; i < tRows.length; i++) {
+    const currentRow = tRows[i];
+    const sourceCell = currentRow.cells[copyIndex];
+    let newCell;
+
+    if (i === 0 || i === tRows.length - 1) {
+      newCell = document.createElement('th');
+    } else {
+      newCell = document.createElement('td');
+    }
+
+    newCell.innerHTML = sourceCell.innerHTML;
+
+    currentRow.insertBefore(newCell, currentRow.cells[pastIndex]);
+  }
+}
+
+const table = document.querySelector('table');
+const tRows = table.rows;
+
+copyColumn(1, 4);
