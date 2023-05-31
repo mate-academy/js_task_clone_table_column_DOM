43c4d19a728a21a87980eb8e80f62311c1718ad3
Dmytro Vovk <bellboy.dv@gmail.com>
2023-05-31 19:00:12 +0300

    message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const tableRows = document.querySelectorAll('tr');
+
+for (let i = 0; i < tableRows.length; i++) {
+  const row = tableRows[i];
+  const cells = row.querySelectorAll('td, th');
+  const secondColumnCell = cells[1].cloneNode(true);
+  const lastCell = cells[cells.length - 1];
+
+  row.insertBefore(secondColumnCell, lastCell);
+}
