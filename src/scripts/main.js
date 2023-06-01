5274db23abbe7ff9d6b7450cba4fc3f8e6328328
Nadiia Romanskaya <97957968+NadiiaR@users.noreply.github.com>
2023-06-01 17:31:44 +0300

    task commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+// const table = document.querySelector('table');
+
+// console.log(table);
+
+// for (let i = 0; i < table.rows.length; i++) {
+//   const position = table.rows[i].cells[1];
+//   const salary = table.rows[i].cells[4];
+//   const positionCopy = position.cloneNode(true);
+
+//   table.rows[i].insertBefore(positionCopy, salary);
+// } *I want to leave this comment please
+
+const rows = document.querySelectorAll('tr');
+
+for (const row of rows) {
+  const cloned = row.cells[1].cloneNode(true);
+  const lastChild = row.cells[4];
+
+  row.insertBefore(cloned, lastChild);
+}
