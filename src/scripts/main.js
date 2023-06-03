adac55b5439211c1177c05c01a1c97361c161088
Roma Tsvindin <tsvindin1995@gmail.com>
2023-06-03 23:03:17 +0300

    js_task_clone_table_column_DOM solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write your code here
+const theTable = document.querySelector('table');
+
+function copyColumn(table, fromIndex, toIndex) {
+  const rows = table.rows;
+
+  for (let i = 0; i < rows.length; i++) {
+    const cells = rows[i].cells;
+
+    const sourceCell = cells[fromIndex];
+
+    const newCell = rows[i].insertCell(toIndex);
+
+    newCell.innerHTML = sourceCell.innerHTML;
+
+    const computedStyles = getComputedStyle(sourceCell);
+
+    for (let j = 0; j < computedStyles.length; j++) {
+      const styleName = computedStyles[j];
+      const styleValue = computedStyles.getPropertyValue(styleName);
+
+      newCell.style.setProperty(styleName, styleValue);
+    }
+  }
+}
+
+copyColumn(theTable, 1, 4);
