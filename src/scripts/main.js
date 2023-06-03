652ec54e4c5dedd70642b1fa3139e36f637a2dd6
Vasyl Kozak <sasdaiv@gmail.com>
2023-06-03 23:04:31 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const rows = table.querySelectorAll('tr');
+
+rows.forEach(row => {
+  const cells = row.querySelectorAll('td, th');
+  const contentToInsert = cells[1].innerHTML;
+  const newCell = document.createElement(cells[1].tagName);
+
+  newCell.innerHTML = contentToInsert;
+  row.insertBefore(newCell, cells[4]);
+});
