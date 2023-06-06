28ad2a48d92746e56fd68979d6d93d4b6453b968
Hishchak Tetiana <tanaiosered@ukr.net>
2023-06-06 09:05:40 +0300

    clone column

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (let i = 0; i < table.rows.length; ++i) {
+  const columbSecond = table.rows[i].cells[1];
+  const newColum = columbSecond.cloneNode(true);
+  const index = table.rows[i].cells.length - 1;
+
+  table.rows[i].cells[index].before(newColum);
+}
