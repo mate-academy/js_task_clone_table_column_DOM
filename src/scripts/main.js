4ff3fb727924e080e5ebe1416a9d95a886a6688a
Hishchak Tetiana <tanaiosered@ukr.net>
2023-06-06 09:06:16 +0300

    clone column

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,9 @@
 const table = document.querySelector('table');
 
 for (let i = 0; i < table.rows.length; ++i) {
-  const columbSecond = table.rows[i].cells[1];
-  const newColum = columbSecond.cloneNode(true);
+  const columnSecond = table.rows[i].cells[1];
+  const newColumn = columnSecond.cloneNode(true);
   const index = table.rows[i].cells.length - 1;
 
-  table.rows[i].cells[index].before(newColum);
+  table.rows[i].cells[index].before(newColumn);
 }
