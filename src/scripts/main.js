ef0d2c7e7394193d8895a5eb7af6cf4dbd8ed3b5
Mykola Plekan <zikzar@gmail.com>
2023-06-08 19:35:50 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,9 @@
 'use strict';
 
-const tableHead = document.querySelector('thead > tr');
-const tableBody = document.querySelector('tbody');
-const tableFoot = document.querySelector('tfoot > tr');
+const trElem = document.querySelectorAll('tr');
 
-const thElemHead = document.createElement('th');
-const thElemFoot = document.createElement('th');
+for (const elem of [...trElem]) {
+  const copyCell = [...elem.cells][1].cloneNode(true);
 
-thElemHead.textContent = tableHead.children[1].textContent;
-
-tableHead.insertBefore(thElemHead, tableHead.children[4]);
-
-for (const child of [...tableBody.children]) {
-  const tdElem = document.createElement('td');
-
-  tdElem.textContent = child.children[1].textContent;
-
-  child.insertBefore(tdElem, child.children[4]);
+  elem.insertBefore(copyCell, [...elem.cells][4]);
 }
-
-thElemFoot.textContent = tableHead.children[1].textContent;
-tableFoot.insertBefore(thElemFoot, tableFoot.children[4]);
