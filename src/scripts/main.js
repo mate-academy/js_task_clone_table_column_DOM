f6f64a4c7b8bc75af92a5c0f5928f9b4d4b2e28f
Mykola Plekan <zikzar@gmail.com>
2023-06-08 18:42:35 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const tableHead = document.querySelector('thead > tr');
+const tableBody = document.querySelector('tbody');
+const tableFoot = document.querySelector('tfoot > tr');
+
+const thElemHead = document.createElement('th');
+const thElemFoot = document.createElement('th');
+
+thElemHead.textContent = tableHead.children[1].textContent;
+
+tableHead.insertBefore(thElemHead, tableHead.children[4]);
+
+for (const child of [...tableBody.children]) {
+  const tdElem = document.createElement('td');
+
+  tdElem.textContent = child.children[1].textContent;
+
+  child.insertBefore(tdElem, child.children[4]);
+}
+
+thElemFoot.textContent = tableHead.children[1].textContent;
+tableFoot.insertBefore(thElemFoot, tableFoot.children[4]);
