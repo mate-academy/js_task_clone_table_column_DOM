2259953e24464c2499df2fb6c4ab080b92604b6b
spencerspe <spencerspe@gmail.com>
2023-07-13 14:30:51 +0100

    first  solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // write your code here
+
+const table = document.body.firstElementChild;
+
+for (let i = 0; i < table.rows.length; i++) {
+  const toInsertAfter = table.rows[i].childNodes[7];
+
+  const row = table.rows[i].childNodes[3];
+
+  const newRow = row.cloneNode(true);
+
+  toInsertAfter.after(newRow);
+}
