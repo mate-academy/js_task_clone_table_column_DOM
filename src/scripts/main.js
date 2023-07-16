220e6d0debe283c35c2d845252605b2b089b6d34
Rufat Mammadov <rufat.mammadov.m@gmail.com>
2023-07-16 23:55:14 +0300

    js task clone table DOM - first try

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const table = document.body.firstElementChild;
+
+for (let i = 0; i < table.rows.length; i++) {
+  const columnToClone = table.rows[i].cells[1];
+  const tagToCreate = document.createElement(columnToClone.tagName);
+
+  tagToCreate.textContent = columnToClone.textContent;
+
+  table.rows[i].cells[4].before(tagToCreate);
+}
