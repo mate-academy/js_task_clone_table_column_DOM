07836583c093f245153603a6f13a13f1474e6718
GGsmou <svyatoslav.golovin.2005@gmail.com>
2023-08-01 17:46:40 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+const thead = document.querySelector('thead');
+const headToCopy = thead.children[0].children[1].cloneNode(true);
+const lastChildIndex = thead.children[0].children.length - 1;
+
+const tbody = document.querySelector('tbody');
+const tbodyChildren = tbody.children;
+
+const tfoot = document.querySelector('tfoot');
+const footToCopy = tfoot.children[0].children[1].cloneNode(true);
+
+thead.children[0]
+  .insertBefore(headToCopy, thead.children[0].children[lastChildIndex]);
+
+for (let i = 0; i < tbodyChildren.length; i++) {
+  tbodyChildren[i].insertBefore(
+    tbodyChildren[i].children[1].cloneNode(true),
+    tbodyChildren[i].children[lastChildIndex],
+  );
+}
+
+tfoot.children[0]
+  .insertBefore(footToCopy, tfoot.children[0].children[lastChildIndex]);
