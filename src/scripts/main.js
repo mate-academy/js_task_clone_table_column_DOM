a4e0d0f7dc12f7dc7a80f96f41264426c573618f
Edgar Kocuba <edgarkocuba@gmail.com>
2023-08-20 21:28:07 +0300

    solution fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,5 +6,6 @@ const table = document.querySelector('table');
 const copyNode = table.cloneNode(true);
 
 for (let i = 0; i < table.rows.length; i++) {
-  table.rows[i].append(copyNode.rows[i].cells[1]);
+  table.rows[i].append(copyNode.rows[i].cells[4]);
+  table.rows[i].cells[4].replaceWith(copyNode.rows[i].cells[1]);
 }
