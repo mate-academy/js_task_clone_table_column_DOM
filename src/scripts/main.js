5b651cef5a6fdc057877ad837a4585530f6db48e
Dmytro Loboda <dmytro.loboda@gmail.com>
2023-08-24 04:02:56 +0800

    copy/paste column solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+function cloneAndInsertColumn(targetRow, columnIndex) {
+  const secondCell = targetRow.children[columnIndex];
+  const clonedCell = secondCell.cloneNode(true);
+  const lastCell = targetRow.lastElementChild;
+
+  targetRow.insertBefore(clonedCell, lastCell);
+}
+
+const table = document.querySelector('table');
+
+const headerRow = table.querySelector('thead tr');
+
+cloneAndInsertColumn(headerRow, 1);
+
+const footerRow = table.querySelector('tfoot tr');
+
+cloneAndInsertColumn(footerRow, 1);
+
+const rows = table.querySelectorAll('tbody tr');
+
+rows.forEach(row => cloneAndInsertColumn(row, 1));
