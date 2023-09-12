fbe210e9e1e9be02ff5717c09613f6adc45c22d1
Eugene Kulish <genekulish13@gmail.com>
2023-09-12 19:43:38 +0300

    solve the task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (let i = 0; i < table.rows.length; i++) {
+  const row = table.rows[i];
+  const clone = row.cells[1].cloneNode(true);
+
+  row.cells[3].after(clone);
+}
