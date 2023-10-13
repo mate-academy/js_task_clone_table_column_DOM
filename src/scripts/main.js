3d64a9e61dd3cf8c5451425ef1f45509c7dbf9ea
AndriiYelieva <andrew.yelieva@gmail.com>
2023-10-13 13:26:27 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const tableHead = table.querySelector('thead').querySelector('tr');
+const tableBody = table.querySelector('tbody').querySelectorAll('tr');
+const tableFoot = table.querySelector('tfoot').querySelector('tr');
+
+const th = document.createElement('th');
+
+th.innerText = tableHead.children[1].innerHTML;
+tableHead.children[4].before(th);
+
+const tf = document.createElement('th');
+
+tf.innerText = tableFoot.children[1].innerHTML;
+tableFoot.children[4].before(tf);
+
+tableBody.forEach(tr => {
+  const td = document.createElement('td');
+
+  td.innerText = tr.children[1].innerHTML;
+  tr.children[4].before(td);
+});
