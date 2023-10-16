a33e42bd6e545e1e09941138af5b40394c41667e
Andrii Rykal <andreuka79@gmail.com>
2023-10-16 13:46:08 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,7 @@
 'use strict';
 
-// write your code here
+const rows = document.body.firstElementChild.rows;
+
+for (let i = 0; i < rows.length; i++) {
+  rows[i].lastElementChild.before(rows[i].children[1].cloneNode(true));
+}
