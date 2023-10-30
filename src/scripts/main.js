369b45fad9e0d97a270c0d468ba45c3a266bddaa
alex <cebanoleksandr@gmail.com>
2023-10-30 10:47:05 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const tbody = document.querySelector('tbody');
+const thead = document.querySelector('thead');
+const tfoot = document.querySelector('tfoot');
+
+function cloneTableColumn(element) {
+  Array.from(element.children).map(row => {
+    const secondChild = row.children[1];
+    const cloneChild = secondChild.cloneNode(true);
+
+    row.insertBefore(cloneChild, row.children[row.children.length - 2]);
+  });
+}
+
+cloneTableColumn(thead);
+cloneTableColumn(tbody);
+cloneTableColumn(tfoot);
