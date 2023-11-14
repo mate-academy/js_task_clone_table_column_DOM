90e0748935c393596d86e0d4dbc8ed328de737e6
Liliya Kalinichenko <fedotova.liliya90@gmail.com>
2023-11-14 16:14:58 +0200

    task solution was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write your code here
+const tableRows = document.querySelector('table').rows;
+
+[...tableRows].forEach(row => {
+  const cellCopy = row.cells[1].cloneNode(true);
+  const lastCell = row.cells[row.cells.length - 1];
+
+  lastCell.before(cellCopy);
+});
