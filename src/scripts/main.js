543ef1d822b8213e76dd666debb3fb28bf2a80ea
IhorVerba <ihor.verbivskyi@gmail.com>
2023-11-27 16:39:49 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (const row of table.rows) {
+  const secondCellCopy = row.cells[1].cloneNode(true);
+  const lastCell = row.cells[row.cells.length - 1];
+
+  lastCell.before(secondCellCopy);
+}
