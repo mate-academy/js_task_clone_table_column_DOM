dcaaaa1d79978a6108a58fc5e8be0e03203b630a
Dominika Karmel <dkarmel@op.pl>
2023-11-28 21:57:08 +0100

    change of method

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,14 +4,14 @@ const rows = document.querySelectorAll('tr');
 
 for (const [index, row] of rows.entries()) {
   if (index === 0 || index === rows.length - 1) {
-    const tmpContent = row.cells[4].textContent;
+    const clonedCell = row.cells[4].cloneNode(true);
 
     row.cells[4].textContent = row.cells[1].textContent;
-    row.innerHTML += `<th>${tmpContent}</th>`;
+    row.appendChild(clonedCell);
   } else {
-    const tmpContent = row.cells[4].textContent;
+    const clonedCell = row.cells[4].cloneNode(true);
 
     row.cells[4].textContent = row.cells[1].textContent;
-    row.innerHTML += `<td>${tmpContent}</td>`;
+    row.appendChild(clonedCell);
   }
 }
