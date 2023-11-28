0a860c2da8778f2ecd03d45cb816501d967b55ef
Dominika Karmel <dkarmel@op.pl>
2023-11-28 14:50:37 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('tr');
+
+for (const [index, row] of rows.entries()) {
+  if (index === 0 || index === rows.length - 1) {
+    const tmpContent = row.cells[4].textContent;
+
+    row.cells[4].textContent = row.cells[1].textContent;
+    row.innerHTML += `<th>${tmpContent}</th>`;
+  } else {
+    const tmpContent = row.cells[4].textContent;
+
+    row.cells[4].textContent = row.cells[1].textContent;
+    row.innerHTML += `<td>${tmpContent}</td>`;
+  }
+}
