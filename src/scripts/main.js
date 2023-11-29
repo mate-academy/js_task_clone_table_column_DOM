7d7c8fd0b3d965258864f2e1544790076c7742ec
Dominika Karmel <dkarmel@op.pl>
2023-11-29 09:59:38 +0100

    code optimization

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,16 +2,9 @@
 
 const rows = document.querySelectorAll('tr');
 
-for (const [index, row] of rows.entries()) {
-  if (index === 0 || index === rows.length - 1) {
-    const clonedCell = row.cells[4].cloneNode(true);
+for (const row of rows) {
+  const clonedCell = row.cells[4].cloneNode(true);
 
-    row.cells[4].textContent = row.cells[1].textContent;
-    row.appendChild(clonedCell);
-  } else {
-    const clonedCell = row.cells[4].cloneNode(true);
-
-    row.cells[4].textContent = row.cells[1].textContent;
-    row.appendChild(clonedCell);
-  }
+  row.cells[4].textContent = row.cells[1].textContent;
+  row.appendChild(clonedCell);
 }
