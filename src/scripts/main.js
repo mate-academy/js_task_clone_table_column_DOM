3603067d936c3c616714132c52728a1a6596b10d
Юрий Мисюра <apriory13@meta.ua>
2023-12-04 14:24:00 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const rows = table.rows;
+
+[...rows].forEach((el, i, arr) => {
+  const newElement = arr[i].cells[1].cloneNode(true);
+  const referenceElement = arr[i].cells[arr[i].cells.length - 1];
+
+  arr[i].insertBefore(newElement, referenceElement);
+});
