3840959ece9015cbc29889130cb31bb824b2c81e
Joanna Konopacka <joanna.konopacka@hotmail.com>
2023-12-10 22:12:14 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
-'use strict';
+"use strict";
 
-// write your code here
+const rows = document.querySelectorAll("tr");
+
+rows.forEach((row, index) => {
+  const positionCellContent = row.cells[1].textContent;
+
+  if (index === 0 || index === rows.length - 1) {
+    const th = document.createElement("th");
+
+    th.textContent = positionCellContent;
+    row.insertBefore(th, row.cells[row.cells.length - 1]);
+  } else {
+
+    const td = document.createElement("td");
+
+    td.textContent = positionCellContent;
+    row.insertBefore(td, row.cells[row.cells.length - 1]);
+  }
+});
