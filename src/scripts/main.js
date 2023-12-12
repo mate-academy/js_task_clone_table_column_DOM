822f2a931b79a1187915fbb355e77e0abb8c47b5
Nataliia Henda <nataliehenda1296@gmail.com>
2023-12-12 12:39:56 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const tbody = document.querySelector('tbody');
+const thead = document.querySelector('thead');
+const tfoot = document.querySelector('tfoot');
+const listTbody = tbody.querySelectorAll('tr');
+const listThead = thead.querySelectorAll('tr');
+const listTfoot = tfoot.querySelectorAll('tr');
+
+function makeCopy(listElem, tag) {
+  listElem.forEach(elem => {
+    const tdList = elem.querySelectorAll(tag);
+
+    const nodeToCopy = tdList[1];
+    const copy = nodeToCopy.cloneNode(true);
+
+    tdList[4].before(copy);
+  });
+}
+
+makeCopy(listTbody, 'td');
+makeCopy(listThead, 'th');
+makeCopy(listTfoot, 'th');
