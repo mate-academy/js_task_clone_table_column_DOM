45408c1cba814eccd2692da940344377cfe73483
Oksana Vasyliuk <oksana.vasyliuk13@gmail.com>
2023-12-22 16:57:44 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write your code here
+const rows = [...document.querySelectorAll('tr')];
+
+rows.forEach(row => {
+  const cell = document.createElement(row.children[1].tagName.toLowerCase());
+
+  cell.textContent = row.children[1].textContent;
+  row.insertBefore(cell, row.children[row.children.length - 1]);
+});
