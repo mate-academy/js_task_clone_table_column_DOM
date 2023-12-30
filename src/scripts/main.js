f54e23b3ef2bfbf3e94df92ed39cca6663116a05
Dmytro Fefelov <reseneweb@gmail.com>
2023-12-30 21:19:34 +0200

    Develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const trs = document.querySelectorAll('tr');
+
+trs.forEach(tr => {
+  const item = tr.parentElement.tagName === 'THEAD'
+  || tr.parentElement.tagName === 'TFOOT'
+    ? document.createElement('th') : document.createElement('td');
+
+  item.textContent = tr.children[1].textContent;
+  tr.insertBefore(item, tr.children[4]);
+});
