9b6f78bd30fe09959fdcb13c826571190b89d936
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2024-01-07 19:52:48 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 // write your code here
+const INDEX_COLUNM = 1;
+const INDEX_NEW_COLUNM = 4;
+
+const rows = document.querySelectorAll('tr');
+
+rows.forEach(row => {
+  const cell = document.createElement(row.children[INDEX_COLUNM].localName);
+  const cellBeforeApp = row.children[INDEX_NEW_COLUNM];
+
+  cell.innerText = row.children[INDEX_COLUNM].textContent;
+
+  cellBeforeApp.parentNode.insertBefore(cell, cellBeforeApp);
+});
