df671ebe4da949d85322731051cfb5ba93604582
Robert Szałapak <robsza1212@gmail.com>
2024-01-21 20:38:17 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
-// write your code here
+const cellsInRow = document.querySelectorAll('tr');
+
+cellsInRow.forEach(element => {
+  element.lastElementChild
+    .insertAdjacentElement('beforebegin', element.cells[1].cloneNode(true));
+});
