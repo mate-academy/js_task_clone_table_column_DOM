ad92aa72445cc48a9ed6c46e28351e42d7df4029
Arkadiusz <stefanski.a00@gmail.com>
2024-02-02 08:20:07 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write your code here
+document.addEventListener('DOMContentLoaded', function() {
+  const table = document.querySelector('table');
+
+  const headerRow = table.querySelector('thead tr');
+
+  const newPositionHeader = document.createElement('th');
+
+  newPositionHeader.innerText = 'Position';
+
+  const salaryHeader = headerRow.querySelector('th:last-child');
+
+  salaryHeader.parentNode.insertBefore(newPositionHeader, salaryHeader);
+
+  const footerRow = table.querySelector('tfoot tr');
+
+  const newPositionFooter = document.createElement('th');
+
+  newPositionFooter.innerText = 'Position';
+
+  const salaryFooter = footerRow.querySelector('th:last-child');
+
+  salaryFooter.parentNode.insertBefore(newPositionFooter, salaryFooter);
+
+  const rows = Array.from(
+    table.getElementsByTagName('tbody')[0].getElementsByTagName('tr'),
+  );
+
+  rows.forEach(row => {
+    const position = row.getElementsByTagName('td')[1].innerText;
+
+    const newPositionCell = document.createElement('td');
+
+    newPositionCell.innerText = position;
+
+    const salaryCell = row.getElementsByTagName('td')[4];
+
+    salaryCell.parentNode.insertBefore(newPositionCell, salaryCell);
+  });
+});
