0c468071d1e739d6d40f575393df12d90280d1d0
dima.rogkov <dima.rogkov@cheitgroup.com>
2024-02-04 16:28:48 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
+/* eslint-disable comma-dangle */
 'use strict';
 
-// write your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const tableInit = () => {
+    const cellsInRow = document.querySelectorAll('tr');
+
+    if (cellsInRow.length) {
+      cellsInRow.forEach((element) => {
+        const clonedCell = element.cells[1].cloneNode(true);
+
+        element.lastElementChild.insertAdjacentElement(
+          'beforebegin',
+          clonedCell
+        );
+      });
+    }
+  };
+
+  tableInit();
+});
