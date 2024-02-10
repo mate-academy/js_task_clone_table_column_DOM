8508cf805769fad9c4eb7ccc5aeceb4644cf76f6
Siarhei Tsishkavets <kenny.sergey@gmail.com>
2024-02-10 10:43:22 +0100

    Add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const table = document.querySelectorAll('tr');
+
+table.forEach(element => {
+  const newTh = document.createElement(element.children[1].nodeName);
+
+  newTh.textContent = element.children[1].textContent;
+
+  element.children[3].insertAdjacentElement('afterend', newTh);
+});
