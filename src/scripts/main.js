a30cae32103e01dff2be3494397385a3c176cf84
Dmitriy Matvichuk <dmytromatviichuk1979@gmail.com>
2024-02-15 09:46:31 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,10 @@
 'use strict';
 
-const createTh = text => {
-  const th = document.createElement('th');
+const row = document.querySelectorAll('tr');
 
-  th.textContent = text;
+row.forEach(tr => {
+  const copiedElement = tr.children[1].cloneNode(true);
+  const lastElement = tr.lastElementChild;
 
-  return th;
-};
-
-const headRow = document.querySelector('thead tr');
-const bodyRows = document.querySelectorAll('tbody tr');
-const footRow = document.querySelector('tfoot tr');
-
-const newPositionTh = createTh('Position');
-
-headRow.insertBefore(newPositionTh.cloneNode(true), headRow.children[4]);
-footRow.insertBefore(newPositionTh.cloneNode(true), footRow.children[4]);
-
-const secondColumnTds = document.querySelectorAll('tbody tr td:nth-child(2)');
-
-bodyRows.forEach((row, i) => {
-  const newTd = document.createElement('td');
-
-  newTd.textContent = secondColumnTds[i].textContent;
-  row.insertBefore(newTd, row.children[4]);
+  lastElement.before(copiedElement);
 });
