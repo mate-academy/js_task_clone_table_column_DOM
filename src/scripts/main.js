bc32df4bc6c3928d432be9cbab981a2e36af1895
Vladyslav Filonenko <oladik05@gmail.com>
2024-03-21 00:08:27 +0200

    clone table column dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const list = document.querySelectorAll('tr');
+
+const clone = [...list].map((tr) => tr.children[1]);
+
+for (let i = 0; i < list.length; i++) {
+  list[i].children[4].before(clone[i].cloneNode(true));
+}
