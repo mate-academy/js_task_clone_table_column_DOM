572f3ac7b186f0b5cbc626686a861cf98e5feaa5
Semen Sokolovskyi <anori.fjord@gmail.com>
2024-03-26 18:11:16 +0200

    cloned column

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
 // write your code here
+const positions = document.querySelectorAll('tr');
+
+positions.forEach((pos) => {
+  const clone = pos.children[1].cloneNode(true);
+
+  pos.children[3].after(clone);
+});
