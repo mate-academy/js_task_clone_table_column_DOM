a2947d4ce14dc378b15e49b153b10467a2bde026
Mateusz Oślizło <amti4osl@gmail.com>
2024-03-27 13:41:46 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+function copyCells(rows, cellTag) {
+  for (const row of rows) {
+    const cell = document.createElement(cellTag);
+
+    cell.textContent = row.cells[1].textContent;
+    row.lastElementChild.before(cell);
+  }
+}
+
+const table = document.querySelector('table');
+
+copyCells(table.tHead.rows, 'th');
+copyCells(table.tBodies[0].rows, 'td');
+copyCells(table.tFoot.rows, 'th');
