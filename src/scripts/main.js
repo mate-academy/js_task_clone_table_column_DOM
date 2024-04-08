12ecf99ee551708f1a906784cf42d154cbbebd1a
vadymboichenkoAcupower <vadym.boichenko@acupower.co.uk>
2024-04-08 22:26:53 +0300

    simp;ify code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,11 @@
 'use strict';
 
-const thead = document.querySelector('thead').querySelector('tr');
-const tfoot = document.querySelector('tfoot').querySelector('tr');
-const tbodytr = document.querySelector('tbody').querySelectorAll('tr');
+const table = document.querySelector('table');
 
-const newHeader = document.createElement('th');
+const rowElements = table.querySelectorAll('tr');
 
-newHeader.textContent = 'Position';
+rowElements.forEach((elem) => {
+  const copyElem = elem.cells[1].cloneNode(true);
 
-const newFooter = document.createElement('th');
-
-newFooter.textContent = 'Position';
-thead.insertBefore(newHeader, thead.lastElementChild);
-tfoot.insertBefore(newFooter, tfoot.lastElementChild);
-
-tbodytr.forEach((item) => {
-  const bodyPositionCell = item.cells[1].cloneNode(true);
-  const bodyPositionHeader = document.createElement('td');
-
-  bodyPositionHeader.textContent = bodyPositionCell.textContent;
-  item.insertBefore(bodyPositionHeader, item.lastElementChild);
+  elem.cells[3].after(copyElem);
 });
