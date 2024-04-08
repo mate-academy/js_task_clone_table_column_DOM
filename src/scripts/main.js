bb42df867b496ed3a6f190e38381ab1eb6ea1f66
vadymboichenkoAcupower <vadym.boichenko@acupower.co.uk>
2024-04-08 19:25:35 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const thead = document.querySelector('thead').querySelector('tr');
+const tfoot = document.querySelector('tfoot').querySelector('tr');
+const tbodytr = document.querySelector('tbody').querySelectorAll('tr');
+
+const newHeader = document.createElement('th');
+
+newHeader.textContent = 'Position';
+
+const newFooter = document.createElement('th');
+
+newFooter.textContent = 'Position';
+
+thead.insertBefore(newHeader, thead.lastElementChild);
+tfoot.insertBefore(newFooter, tfoot.lastElementChild);
+
+tbodytr.forEach((item) => {
+  const bodytTh = document.createElement('td');
+
+  bodytTh.textContent = item.cells[1].innerText;
+  item.insertBefore(bodytTh, item.lastElementChild);
+});
