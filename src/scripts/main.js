d608aba06ca0e337ceb6dc8ea0e46906105a1e08
unknown <bohdan.lesyk.kn.2021@lpnu.ua>
2024-04-10 17:10:56 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+[...table.rows].forEach((tr) => {
+  const dublicatedItem = tr.children[1].cloneNode(true);
+
+  tr.children[4].before(dublicatedItem);
+});
