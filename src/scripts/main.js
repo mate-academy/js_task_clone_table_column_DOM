5cebe0456328358c6cfdf6b3eb6d3447b8b9fa76
Yurii Yepikhov <yuriiyepikhov@gmail.com>
2024-04-24 20:25:46 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
 // write your code here
+
+const table = document.querySelector('table');
+
+const rows = table.rows;
+
+for (const row of rows) {
+  const td2 = row.children[1];
+
+  row.insertBefore(td2.cloneNode(true), row.children[4]);
+}
