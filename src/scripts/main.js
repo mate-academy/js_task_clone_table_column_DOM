ff0d277e35fbe026a2afdc5c82ad4be05545ef47
Krzysztof <krzysiek.gugala@gmail.com>
2024-04-26 09:20:50 +0200

    solutionV1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const rows = table.rows;
+
+for (let i = 0; i < rows.length; i++) {
+  const row = rows[i];
+  const secondColumnCopy = row.cells[1].cloneNode(true);
+
+  row.insertBefore(secondColumnCopy, row.cells[4]);
+}
