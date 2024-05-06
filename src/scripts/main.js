6fb5e7fa041e7a0c524ca63d6b216b6e52af4697
Yuliia <kotyuliya24@gmail.com>
2024-05-05 20:43:00 -0400

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const table = document.querySelector('table');
-const rows = document.querySelectorAll('tr');
+// const table = document.querySelector('table');
+// const rows = document.querySelectorAll('tr');
 
-for (let i = 0; i < rows.length; i++) {
-  const clonedColumn = table.rows[i].cells[1].cloneNode(true);
-  const lastElementIndex = table.rows[i].cells.length - 1;
+// for (let i = 0; i < rows.length; i++) {
+//   const clonedColumn = table.rows[i].cells[1].cloneNode(true);
+//   const lastElementIndex = table.rows[i].cells.length - 1;
 
-  table.rows[i].cells[lastElementIndex].before(clonedColumn);
-}
+//   table.rows[i].cells[lastElementIndex].before(clonedColumn);
+// }
