2f8471b95e6908b045f3a6e55a09ac64355f7abf
Denis Hurskiy <denis.gurskiy@gmail.com>
2024-05-07 18:14:54 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write your code here
+const tabel = document.querySelector('table');
+const elements = [...tabel.children];
+
+const clone = (rows, position, newPosition) => {
+  rows.forEach((row) => {
+    const children = row.children;
+    const pastePosition =
+      newPosition > [...children].length ? [...children].length : newPosition;
+
+    const cell =
+      row.querySelector(`th:nth-child(${position})`) ||
+      row.querySelector(`td:nth-child(${position})`);
+
+    const place =
+      row.querySelector(`th:nth-child(${pastePosition})`) ||
+      row.querySelector(`td:nth-child(${pastePosition})`);
+
+    const newCell = cell.cloneNode(true);
+
+    if (pastePosition !== newPosition) {
+      place.after(newCell);
+    } else {
+      place.before(newCell);
+    }
+  });
+};
+
+elements.forEach((element) => {
+  const rows = element.querySelectorAll('tr');
+
+  clone(rows, 2, 5);
+});
