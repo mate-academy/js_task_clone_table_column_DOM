a30d676e359ea1490b61942df7807d48f2052ae9
Dmytro Levchenko <levchenkod91@gmail.com>
2024-05-28 19:47:44 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+[...table.rows].forEach((row) => {
+  const cell = row.cells[1];
+  const newCell = document.createElement(cell.tagName);
+
+  newCell.innerText = cell.innerText;
+  row.lastElementChild.before(newCell);
+});
