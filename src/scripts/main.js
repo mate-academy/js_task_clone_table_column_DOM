9dd33d411eeb595385f9d0d4f8a1b7b5b1ff78a4
Vitalii Nezdoiminoha <vitalii.nezdojminoga@gmail.com>
2024-06-03 13:04:14 +0300

    improvments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,35 +1,9 @@
 'use strict';
 
-const rows = document.querySelectorAll('table tr');
+const table = document.querySelector('table');
 
-const colHead = document.querySelectorAll('thead th');
+for (const row of table.rows) {
+  const rowCloned = row.cells[1].cloneNode(true);
 
-const colFooter = document.querySelectorAll('tfoot th');
-
-const rowBody = document.querySelectorAll('tbody tr');
-
-const secondColumn = [];
-
-rows.forEach((row) => {
-  const cells = row.querySelectorAll('td, th');
-
-  secondColumn.push(cells[1]);
-});
-
-colHead[colHead.length - 1].insertAdjacentHTML(
-  'beforebegin',
-  `<th>${secondColumn[0].textContent}</th>`,
-);
-
-colFooter[colFooter.length - 1].insertAdjacentHTML(
-  'beforebegin',
-  `<th>${secondColumn[secondColumn.length - 1].textContent}</th>`,
-);
-
-rowBody.forEach((row) => {
-  const newCell = document.createElement('td');
-
-  newCell.textContent = row.cells[1].textContent;
-
-  row.cells[row.cells.length - 1].insertAdjacentElement('beforebegin', newCell);
-});
+  row.insertBefore(rowCloned, row.cells[row.cells.length - 1]);
+}
