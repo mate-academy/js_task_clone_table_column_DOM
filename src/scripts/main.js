d1317b4db0e92b0e1b8ebdee99d138818675b576
Vitalii Nezdoiminoha <vitalii.nezdojminoga@gmail.com>
2024-06-03 12:12:39 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('table tr');
+
+const colHead = document.querySelectorAll('thead th');
+
+const colFooter = document.querySelectorAll('tfoot th');
+
+const rowBody = document.querySelectorAll('tbody tr');
+
+const secondColumn = [];
+
+rows.forEach((row) => {
+  const cells = row.querySelectorAll('td, th');
+
+  secondColumn.push(cells[1]);
+});
+
+colHead[colHead.length - 1].insertAdjacentHTML(
+  'beforebegin',
+  `<th>${secondColumn[0].textContent}</th>`,
+);
+
+colFooter[colFooter.length - 1].insertAdjacentHTML(
+  'beforebegin',
+  `<th>${secondColumn[secondColumn.length - 1].textContent}</th>`,
+);
+
+rowBody.forEach((row) => {
+  const newCell = document.createElement('td');
+
+  newCell.textContent = row.cells[1].textContent;
+
+  row.cells[row.cells.length - 1].insertAdjacentElement('beforebegin', newCell);
+});
