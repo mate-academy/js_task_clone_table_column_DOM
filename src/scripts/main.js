19457e442f044f6e6b2206a1f5bf7402c3a83727
Max <gcrazy007@gmail.com>
2024-06-08 01:56:20 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const tbody = document.querySelector('tbody');
+const thead = document.querySelector('thead');
+const tfoot = document.querySelector('tfoot');
+const th = document.createElement('th');
+const thFoot = document.createElement('th');
+
+th.textContent = thead.rows[0].cells[1].innerText;
+thFoot.textContent = th.innerText;
+
+thead.rows[0].cells[4].insertAdjacentElement('beforebegin', th);
+tfoot.rows[0].cells[4].insertAdjacentElement('beforebegin', thFoot);
+
+for (let i = 0; i < tbody.rows.length; i++) {
+  const row = tbody.rows[i];
+
+  const td = document.createElement('td');
+
+  td.textContent = row.cells[1].innerText;
+  row.cells[4].insertAdjacentElement('beforebegin', td);
+}
