e5cc5d31363bd7e113ed44a45c3fa0d553ff309d
SrTrace <levashov04sergey@gmail.com>
2024-06-08 13:52:27 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const tableRows = [...document.querySelector('table').rows];
+const columnIndexCopy = 1;
+const columnIndexPaste = 4;
+
+tableRows.forEach((row) => {
+  if (
+    row.cells.length <= columnIndexCopy ||
+    row.cells.length < columnIndexPaste
+  ) {
+    throw new Error('Table does not have enough cells');
+  }
+
+  const copyText = row.cells[columnIndexCopy].textContent;
+  const newCell = row.insertCell(columnIndexPaste);
+
+  newCell.textContent = copyText;
+});
