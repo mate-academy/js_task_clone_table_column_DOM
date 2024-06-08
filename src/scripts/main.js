ffa14d71640cbc7a6ac998fa443659f555a35605
Vladyslav <vladbuldenko@gmail.com>
2024-06-08 18:02:08 +0300

    VB solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (const row of table.rows) {
+  const secondCell = row.cells[1];
+  const clonedCell = secondCell.cloneNode(true);
+  const lastCell = row.lastElementChild;
+
+  lastCell.insertAdjacentElement('beforebegin', clonedCell);
+}
