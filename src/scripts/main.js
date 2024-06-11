e55c21a4ff1c40a74414c29f5630d1d18904c639
Den <clixqqk@gmail.com>
2024-06-11 17:18:49 +0300

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const secondColumnHeader = table.querySelector('thead tr th:nth-child(2)');
+const clonedHeader = secondColumnHeader.cloneNode(true);
+
+table
+  .querySelector('thead tr')
+  .insertBefore(clonedHeader, table.querySelector('thead tr th:last-child'));
+
+const rows = table.querySelectorAll('tbody tr');
+
+rows.forEach((row) => {
+  const secondColumnCell = row.querySelector('td:nth-child(2)');
+  const clonedCell = secondColumnCell.cloneNode(true);
+
+  row.insertBefore(clonedCell, row.querySelector('td:last-child'));
+});
+
+const footerRows = table.querySelectorAll('tfoot tr');
+
+footerRows.forEach((row) => {
+  const secondColumnFooterCell = row.querySelector('th:nth-child(2)');
+  const clonedFooterCell = secondColumnFooterCell.cloneNode(true);
+
+  row.insertBefore(clonedFooterCell, row.querySelector('th:last-child'));
+});
