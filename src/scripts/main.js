e7278c3a2cd507bba384ffd0b74475ff4c2f2780
Vladyslav Shylin <indiggoo228@gmail.com>
2024-06-15 12:56:06 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
 // write your code here
+const table = document.querySelector('table');
+
+const array = [];
+
+for (const row of table.rows) {
+  array.push(row.cells[1]);
+}
+
+for (const el of array) {
+  const row = el.parentElement;
+
+  const newEl = document.createElement(el.tagName.toLowerCase());
+
+  newEl.innerHTML = el.innerHTML;
+
+  row.insertBefore(newEl, row.cells[row.cells.length - 1]);
+}
