1b49919f7b8f5b8dc16b4db32ffd5a819ca01b93
Rusłan Karaniewski <karane88vka@gmail.com>
2024-06-16 23:56:47 +0200

    Task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const columns = document.querySelectorAll('tr');
+
+columns.forEach((el) => {
+  const posColumn = el.cells[1];
+  const posColumnClone = posColumn.cloneNode(true);
+  const lastColumn = el.cells[el.cells.length - 1];
+
+  el.insertBefore(posColumnClone, lastColumn);
+});
