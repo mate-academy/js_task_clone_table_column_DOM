b754ffb7a3ebfcbcf09198ed097a9042793e4fb7
MTS13 <mtstaras@gmail.com>
2024-06-17 20:45:35 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const rows = table.rows;
+
+for (let i = 0; i < rows.length; i++) {
+  const clonePosition = rows[i].cells[1].cloneNode(true);
+
+  rows[i].insertBefore(clonePosition, rows[i].cells[4]);
+}
