cc31a8bf3bea582f8c7a1d4272361e2440f1ad9a
Arthur Tolkachov <babanjerelo@gmail.com>
2024-06-17 17:32:48 +0300

    [Feature] cloned table column

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+function copyCell(row, indexFrom, indexTo) {
+  const cell = row.cells[indexFrom];
+
+  const cellCopy = cell.cloneNode(true);
+  const targetSibling = row.cells[indexTo - 1];
+
+  targetSibling.after(cellCopy);
+}
+
+function copyCells(tElement, indexFrom, indexTo) {
+  const { rows } = tElement;
+
+  [...rows].forEach((row) => {
+    copyCell(row, indexFrom, indexTo);
+  });
+}
+
+function copyColumn(table, indexFrom, indexTo) {
+  const { tHead, tBodies, tFoot } = table;
+  const tableElementsArray = [tHead, tBodies[0], tFoot];
+
+  tableElementsArray.forEach((tableElement) => {
+    copyCells(tableElement, indexFrom, indexTo);
+  });
+}
+
+const tableSelector = document.querySelector('table');
+
+copyColumn(tableSelector, 1, 4);
