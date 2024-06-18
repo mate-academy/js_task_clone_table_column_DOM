95e9441d4ec1436d1acbbd6146cc61cd6984fc2d
Vasil Liakhivskyi <vazilx@gmail.com>
2024-06-18 08:23:49 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const rows = [...table.rows];
+
+rows.forEach((row) => {
+  const cells = [...row.cells];
+
+  const cloneSecondCell = cells[1].cloneNode(true);
+
+  const firstPartCells = cells.slice(0, -1);
+
+  const newCells = [...firstPartCells, cloneSecondCell, cells.at(-1)];
+
+  newCells.forEach((cell) => {
+    row.append(cell);
+  });
+});
