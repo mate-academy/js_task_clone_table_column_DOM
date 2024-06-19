85c557659c237bd50541a62533cfa2a4d317a183
Marian <getmoodyup@gmail.com>
2024-06-19 17:14:01 +0300

    linters changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,10 +5,15 @@ const trHeading = table.querySelector('thead th:nth-child(2)').cloneNode(true);
 const trFooter = table.querySelector('tfoot th:nth-child(2)').cloneNode(true);
 const rows = table.querySelectorAll('tbody tr');
 
-table.querySelector('thead tr').insertBefore(trHeading, table.querySelector('thead th:last-child'));
-table.querySelector('tfoot tr').insertBefore(trFooter, table.querySelector('tfoot th:last-child'));
+table
+  .querySelector('thead tr')
+  .insertBefore(trHeading, table.querySelector('thead th:last-child'));
 
-rows.forEach(row => {
+table
+  .querySelector('tfoot tr')
+  .insertBefore(trFooter, table.querySelector('tfoot th:last-child'));
+
+rows.forEach((row) => {
   const secondCol = row.cells[1].cloneNode(true);
   const lastCol = row.cells[row.cells.length - 1];
 
