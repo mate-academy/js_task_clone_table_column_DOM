4c9fa069d477c2f032b2b37784271608770d293d
unknown <beliyandr@gmail.com>
2024-06-26 19:44:17 +0300

    created 0ne function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+function cloneColumnFromTable(tableName, numberColumn) {
+  const tableRows = [...tableName.querySelectorAll('tr')];
+  const arrList = tableRows.map((elem) => {
+    return elem.children[numberColumn - 1];
+  });
+
+  return arrList;
+}
+
+// function addColumnToTable(tableName, columnArr, position) {}
+
+const columnFromTable = cloneColumnFromTable(table, 2);
+
+console.log(columnFromTable);
