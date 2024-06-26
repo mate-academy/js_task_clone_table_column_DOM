fa8084807664a42297bc5916f5042dbe05c320a1
unknown <beliyandr@gmail.com>
2024-06-26 22:36:23 +0300

    &&

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,8 +11,21 @@ function cloneColumnFromTable(tableName, numberColumn) {
   return arrList;
 }
 
-// function addColumnToTable(tableName, columnArr, position) {}
+function addColumnToTable(tableName, columnArr, position) {
+  const column = [...columnArr];
+
+  for (let i = 0; i < tableName.rows.length; i++) {
+    // tableName.rows[i].cells[position].after(column[i]);
+
+    tableName.rows[i].cells[position].insertAdjacentHTML(
+      'afterbegin',
+      column[i],
+    );
+  }
+}
 
 const columnFromTable = cloneColumnFromTable(table, 2);
 
+addColumnToTable(table, columnFromTable, 2);
+// eslint-disable-next-line no-console
 console.log(columnFromTable);
