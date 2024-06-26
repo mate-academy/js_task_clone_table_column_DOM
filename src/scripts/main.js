8f72699bb2496c3f9e14eb620d707fd9f9256756
Daniil <piecyberpie@gmail.com>
2024-06-26 18:55:15 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 // write your code here
+const rows = document.querySelectorAll('tr');
+
+for (const row of rows) {
+  let cells = row.querySelectorAll('td');
+
+  if (cells.length === 0) {
+    cells = row.querySelectorAll('th');
+  }
+
+  const additionalSalaryColumn = cells[4].cloneNode(true);
+
+  // age moving right abit
+  cells[4].innerHTML = cells[3].innerHTML;
+
+  // copying position
+  cells[4].innerHTML = cells[1].innerHTML;
+
+  row.appendChild(additionalSalaryColumn);
+}
