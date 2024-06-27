6779ca57bd91b4e730ef9d71c2399626a159b88f
myemets <for.education1809@gmail.com>
2024-06-27 16:21:52 +0300

    simplify logic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,31 +2,11 @@
 
 // write your code here
 
-const tableTest = document.querySelector('table');
+const table = document.querySelectorAll('table tr');
+const cloneTr = [...table];
 
-function getColumn(table, indexOfColumn) {
-  const columnElements = [];
+cloneTr.forEach((row) => {
+  const dublicateTh = row.children[1].cloneNode(true);
 
-  for (let i = 0; i < table.getElementsByTagName('tr').length; i++) {
-    columnElements.push(table.rows[i].cells[indexOfColumn].textContent);
-  }
-
-  return columnElements;
-}
-
-function pasteColumn(table, columnElements, indexToPaste) {
-  const rowsCount = table.getElementsByTagName('tr').length;
-  const headerCell = table.rows[0].insertCell(indexToPaste);
-  const footerCell = table.rows[rowsCount - 1].insertCell(indexToPaste);
-
-  headerCell.outerHTML = `<th>${columnElements[0]}</th>`;
-  footerCell.outerHTML = `<th>${columnElements[0]}</th>`;
-
-  for (let i = 1; i < rowsCount - 1; i++) {
-    const newRowCell = table.rows[i].insertCell(indexToPaste);
-
-    newRowCell.innerText = columnElements[i];
-  }
-}
-
-pasteColumn(tableTest, getColumn(tableTest, 1), 4);
+  row.children[4].before(dublicateTh);
+});
