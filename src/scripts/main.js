18ab47cd7b289c758747763c285361eb9d102b21
MelnykVlad <21vladmel15@gmail.com>
2024-06-28 13:35:37 +0300

    add clone table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const tbodyRows = document.querySelectorAll('tbody tr');
+
+const theadRow = document.querySelector('thead tr');
+
+const tfootRow = document.querySelector('tfoot tr');
+
+const cloneRow = (row) => {
+  const cells = row.querySelectorAll('td, th');
+  const secondCollumn = cells[1].cloneNode(true);
+  const lastCollumn = cells.length - 1;
+
+  row.insertBefore(secondCollumn, cells[lastCollumn]);
+};
+
+tbodyRows.forEach((row) => cloneRow(row));
+
+cloneRow(theadRow);
+cloneRow(tfootRow);
