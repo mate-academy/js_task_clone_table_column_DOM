2ac07c0587e758b72bb1344a9f05c3fe903842eb
Liudmyla Hnydyn <gnydynluda@gmail.com>
2024-06-30 21:45:47 -0400

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const replacedCell = document.getElementsByTagName('tr');
+
+for (const cell of replacedCell) {
+  const secondElement = cell.querySelector(':nth-child(2)');
+  const cloneCell = secondElement.cloneNode(true);
+  const lastElement = cell.lastElementChild;
+
+  lastElement.before(cloneCell);
+}
