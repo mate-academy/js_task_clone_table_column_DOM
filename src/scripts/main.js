49df8c61a7a2954b825aff65822e267db76e0dbe
Svitlana-Vitryk <vitriksvitlana@gmail.com>
2024-07-05 15:37:00 +0300

    js_task_clone_table_column_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+document.addEventListener('DOMContentLoaded', function () {
+  const rows = document.querySelectorAll('table tr');
+
+  rows.forEach((row) => {
+    const cells = row.querySelectorAll('th, td');
+
+    const clonedCell = cells[1].cloneNode(true);
+
+    row.insertBefore(clonedCell, cells[cells.length - 1]);
+  });
+});
