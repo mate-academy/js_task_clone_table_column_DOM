a75226f1d66285a7fea9714a93a99160adb74965
Misha Vynnyk <vinnikmisha94@gmail.com>
2024-07-06 09:34:15 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (let i = 0; i < table.rows.length; i++) {
+  const td = table.rows[i].cells[1];
+  const tdCopy = document.createElement('td');
+  const lastId = table.rows[i].cells[table.rows[i].cells.length - 1];
+
+  tdCopy.innerHTML = td.innerHTML;
+
+  table.rows[i].insertBefore(tdCopy, lastId);
+}
