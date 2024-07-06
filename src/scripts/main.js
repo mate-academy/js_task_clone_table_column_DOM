83152821fff82256c086bb23ac33bd6598a94c97
Andrii <andriykostiuk1@gmail.com>
2024-07-06 16:19:14 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const copyPastleColumn = (tr, copyIndex, pastleIndex) => {
+  const copyNode = tr.children[copyIndex].cloneNode(true);
+
+  tr.children[pastleIndex - 1].insertAdjacentElement('afterend', copyNode);
+};
+
+[...table.children].forEach((tableElement) => {
+  [...tableElement.children].forEach((tr) => {
+    copyPastleColumn(tr, 1, 4);
+  });
+});
