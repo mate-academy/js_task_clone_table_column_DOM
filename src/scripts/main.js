6e624aceb5600deaa355b5b246b389a57db13cb4
Misha Vynnyk <vinnikmisha94@gmail.com>
2024-07-06 09:42:01 +0200

    correct

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,9 +5,9 @@ const table = document.querySelector('table');
 for (let i = 0; i < table.rows.length; i++) {
   const td = table.rows[i].cells[1];
   const tdCopy = document.createElement('td');
-  const lastId = table.rows[i].cells[table.rows[i].cells.length - 1];
+  const lastTd = table.rows[i].cells[table.rows[i].cells.length - 1];
 
   tdCopy.innerHTML = td.innerHTML;
 
-  table.rows[i].insertBefore(tdCopy, lastId);
+  table.rows[i].insertBefore(tdCopy, lastTd);
 }
