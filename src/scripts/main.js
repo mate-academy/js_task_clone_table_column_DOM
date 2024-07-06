fc6283af5b31749956171871653eb9c22bf062fb
Iryna Stefaniuk <irinagoyan@gmail.com>
2024-07-06 18:40:59 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const head = document.querySelector('thead tr');
+const body = document.querySelectorAll('tbody tr');
+const food = document.querySelector('tfoot tr');
+
+const cloneCellHead = head.cells[1].cloneNode(true);
+const cloneCellFood = food.cells[1].cloneNode(true);
+
+head.cells[4].insertAdjacentElement('beforebegin', cloneCellHead);
+
+food.cells[4].insertAdjacentElement('beforebegin', cloneCellFood);
+
+for (let i = 0; i < body.length; i++) {
+  const cellsBody = body[i].cells[1];
+  const cloneCellBody = cellsBody.cloneNode(true);
+
+  body[i].cells[4].insertAdjacentElement('beforebegin', cloneCellBody);
+}
