e41cbaedd57e701fb32facd4b0b5deafb96a1db0
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-07-10 23:51:27 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write your code here
+const tHeadRow = document.querySelector('thead tr');
+const tFootRow = document.querySelector('tfoot tr');
+const tBodyRows = document.querySelectorAll('tbody tr');
+
+function copyColumn(target, destination) {
+  const newTHeadCell = document.createElement('th');
+  const newTFootCell = document.createElement('th');
+
+  addCell(tHeadRow, newTHeadCell, target, destination);
+  addCell(tFootRow, newTFootCell, target, destination);
+
+  tBodyRows.forEach((tr, i) => {
+    const newTBodyCell = document.createElement('td');
+
+    addCell(tr, newTBodyCell, target, destination);
+  });
+}
+
+function addCell(row, cell, target, destination) {
+  cell.textContent = row.cells[target - 1].textContent;
+  row.cells[destination - 1].insertAdjacentElement('afterend', cell);
+}
+
+copyColumn(2, 4);
