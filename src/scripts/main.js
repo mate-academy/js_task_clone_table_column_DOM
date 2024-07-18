1299b73c9d3d4dd9b3ab518e7722a0decdd25965
Viktoriia <vkkishko@gmail.com>
2024-07-18 13:04:02 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
 // write your code here
+const table = document.querySelector('table');
+const rowsArr = [...table.rows];
+
+rowsArr.forEach((el) => {
+  const copiedCell = el.cells[1].cloneNode();
+
+  copiedCell.textContent = el.cells[1].textContent;
+
+  el.lastElementChild.before(copiedCell);
+});
