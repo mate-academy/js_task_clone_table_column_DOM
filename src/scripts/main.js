7e51a20f72087ce9bc14da6d7198ab83c888f8fc
Mykola Bilenkiy <nikolosknv@gmail.com>
2024-07-20 17:04:32 +0300

    remake

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,7 @@
 // write your code here
 const tbody = document.querySelector('tbody');
 const thead = document.querySelector('thead');
+const tfoot = document.querySelector('tfoot');
 
 for (const tr of tbody.rows) {
   const trPosition = tr.cells[1];
@@ -16,3 +17,9 @@ const thPosition = thRow.cells[1];
 const clonedThPosition = thPosition.cloneNode(true);
 
 thRow.insertBefore(clonedThPosition, thRow.cells[4]);
+
+const tfRow = tfoot.rows[0];
+const tfPosition = tfRow.cells[1];
+const clonedTfPosition = tfPosition.cloneNode(true);
+
+tfRow.insertBefore(clonedTfPosition, tfRow.cells[4]);
