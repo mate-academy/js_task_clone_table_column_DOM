de42ac30d926be89b799fe2627ee2787b0a90668
NataliMax <maximovs@1876gmail.com>
2024-07-22 00:30:14 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+function cloneColumn(part, el) {
+  const table = document.querySelector('table');
+
+  const secondColumn = table.querySelectorAll(`${part} tr ${el}:nth-child(2)`);
+
+  for (let i = 0; i <= secondColumn.length - 1; i++) {
+    const clonedColumn = secondColumn[i].cloneNode(true);
+    const lastColumn = table.querySelector(
+      `${part} tr:nth-child(${i + 1}) ${el}:last-child`,
+    );
+
+    lastColumn.parentNode.insertBefore(clonedColumn, lastColumn);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', cloneColumn('thead', 'th'));
+document.addEventListener('DOMContentLoaded', cloneColumn('tfoot', 'th'));
+document.addEventListener('DOMContentLoaded', cloneColumn('tbody', 'td'));
