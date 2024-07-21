656119bfb8d1bff1e7888ddb0e523d479bad119d
Roma Zuravliov <janmorozov1988@gmail.com>
2024-07-21 15:29:29 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+function cloneAndInsertColumn(section) {
+  for (const row of section.rows) {
+    const cloneCell = row.cells[1].cloneNode(true);
+
+    row.insertBefore(cloneCell, row.cells[row.cells.length - 1]);
+  }
+}
+
+if (table.tHead) {
+  cloneAndInsertColumn(table.tHead);
+}
+
+if (table.tBodies.length > 0) {
+  for (const tbody of table.tBodies) {
+    cloneAndInsertColumn(tbody);
+  }
+}
+
+if (table.tFoot) {
+  cloneAndInsertColumn(table.tFoot);
+}
