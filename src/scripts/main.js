3766a8e9394d1609fb97fa093a68c1f7d8f8d0ed
mrsvolodya <vovamursky@gmail.com>
2024-07-22 18:25:18 +0300

    cell-clone

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+[...table.rows].forEach((item) => {
+  const secondCell = item.cells[1].cloneNode(true);
+
+  item.cells[3].after(secondCell);
+});
