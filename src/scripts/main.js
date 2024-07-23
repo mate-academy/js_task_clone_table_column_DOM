f71109264e4f84cdbdb62cc1a39af36ccb377e73
Polina <polinabondar4@gmail.com>
2024-07-23 23:43:57 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
+const rows = [...document.querySelectorAll('tr')];
+
+rows.forEach((row) => {
+  const positionRow = row.children[1].cloneNode(true);
+
+  row.lastElementChild.before(positionRow);
+});
+
 // write your code here
