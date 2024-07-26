b49a5b5cd3c6a238455fb8113bb9bf7e64b6aad5
Denys209818 <deniskravcuk63@gmail.com>
2024-07-27 02:32:42 +0300

    Load solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 // write your code here
+const rows = document.getElementsByTagName('tbody')[0].querySelectorAll('tr');
+
+for (let i = 0; i < rows.length; i++) {
+  const tds = rows[i].querySelectorAll('td');
+
+  const last = tds.item(tds.length - 1);
+
+  const additional = document.createElement('td');
+
+  additional.innerHTML = tds[1].innerHTML;
+
+  last.insertAdjacentElement('beforebegin', additional);
+}
