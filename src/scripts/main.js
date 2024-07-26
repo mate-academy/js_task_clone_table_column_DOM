b3cef5f37ec7eec6e1712cba1336d369138e5243
Korolko Anastasiia <korolkonasta@gmail.com>
2024-07-26 17:19:25 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const rows = document.getElementsByTagName('tr');
+
+for (let i = 0; i < rows.length; i++) {
+  const cells = rows[i].children;
+
+  const clone = cells[1].cloneNode(true);
+
+  rows[i].insertBefore(clone, cells[cells.length - 1]);
+}
