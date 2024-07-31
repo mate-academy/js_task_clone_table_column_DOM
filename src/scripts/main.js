b068fcf867fc86b7f6321ba6f3b991eedccdd354
Max <max@energyhill.com>
2024-07-31 16:17:15 -0400

    v 1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (const row of table.rows) {
+  const positionCopy = document.createElement(row.cells[1].tagName);
+
+  positionCopy.innerHTML = row.cells[1].innerHTML;
+  row.insertBefore(positionCopy, row.cells[row.cells.length - 1]);
+}
