924f0e086005d4ce57f1a007c9b5c296b8db04b6
Kirill Gasan <dp160190gkj@gmail.com>
2024-08-02 22:26:43 +0300

    alternative option

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,33 +1,10 @@
 'use strict';
 
-const tr = [...document.querySelectorAll('tr')];
+const table = document.querySelector('table');
 
-const thHead = tr[0].querySelectorAll('th')[1];
-const cloneThHead = thHead.cloneNode(true);
-const newTh = document.createElement('th');
+for (const row of table.rows) {
+  const positionCopy = document.createElement(row.cells[1].tagName);
 
-newTh.textContent = cloneThHead.textContent;
-tr[0].insertBefore(newTh, tr[0].children[tr[0].children.length - 1]);
-
-const thFoot = tr[tr.length - 1].querySelectorAll('th')[1];
-const cloneThFoot = thFoot.cloneNode(true);
-const newTf = document.createElement('th');
-
-newTf.textContent = cloneThFoot.textContent;
-
-tr[tr.length - 1].insertBefore(
-  newTf,
-  tr[tr.length - 1].children[tr[tr.length - 1].children.length - 1],
-);
-
-tr.forEach((item) => {
-  const tdBody = item.querySelectorAll('td')[1];
-
-  if (tdBody) {
-    const cloneTdBody = tdBody.cloneNode(true);
-    const newTd = document.createElement('td');
-
-    newTd.textContent = cloneTdBody.textContent;
-    item.insertBefore(newTd, item.children[item.children.length - 1]);
-  }
-});
+  positionCopy.innerHTML = row.cells[1].innerHTML;
+  row.insertBefore(positionCopy, row.cells[row.cells.length - 1]);
+}
