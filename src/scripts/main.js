f8a2715785a7826ba06f4517515c142c5bf42fcb
Yuliia Kosenchuk <yuliia.kosenchuk@gmail.com>
2024-08-03 14:20:55 +0200

    change js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,24 +8,3 @@ for (const row of table.rows) {
   copyCell.innerHTML = row.cells[1].innerHTML;
   row.insertBefore(copyCell, row.cells[row.cells.length - 1]);
 }
-
-// Alternative version;
-// const table = document.querySelector('table');
-// const thead = table.querySelector('thead');
-// const tbody = table.querySelector('tbody');
-// const tfoot = table.querySelector('tfoot');
-
-// function cloneAndInsertColumn(section) {
-//   const rows = section.querySelectorAll('tr');
-
-//   rows.forEach((row) => {
-//     const cells = row.children;
-//     const clonedCell = cells[1].cloneNode(true);
-
-//     row.insertBefore(clonedCell, cells[cells.length - 1]);
-//   });
-// }
-
-// cloneAndInsertColumn(thead);
-// cloneAndInsertColumn(tbody);
-// cloneAndInsertColumn(tfoot);
