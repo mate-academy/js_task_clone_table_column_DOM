5fa42857d0315fd28214850fa45f8712f21e6673
Oleksii Symonets <alex.simonets@gmail.com>
2024-08-04 17:00:13 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const trow = document.getElementsByTagName('table')[0].rows;
+
+for (let i = 0; i < trow.length; i++) {
+  const secondColumnCell =
+    document.getElementsByTagName('table')[0].rows[i].cells[1];
+  const cloneSecondColumnCell = secondColumnCell.cloneNode(true);
+
+  document
+    .getElementsByTagName('table')[0]
+    .rows[i].lastElementChild.before(cloneSecondColumnCell);
+}
