fd7c7e1e598ea4cf2906f75c28e24abce93a632d
Mikhailo Koshilka <mkosilka@gmail.com>
2024-08-05 20:34:25 +0100

    Delete comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,29 +1,20 @@
 'use strict';
 
-// Додавання верхнього заголовку
 const tableHead = document.querySelector('thead');
 const firstRow = tableHead.querySelector('tr');
 const allHeads = firstRow.querySelectorAll('th');
-
 const copyPos = allHeads[1].textContent;
 const newPos = document.createElement('th');
-
 newPos.textContent = copyPos;
-
 firstRow.insertBefore(newPos, allHeads[allHeads.length - 1]);
 
-// Додавання нижнього заголовку
 const tableFot = document.querySelector('tfoot');
 const lastRow = tableFot.querySelector('tr');
 const allFoots = lastRow.querySelectorAll('th');
-
 const newFootPos = document.createElement('th');
-
 newFootPos.textContent = copyPos;
-
 lastRow.insertBefore(newFootPos, allFoots[allFoots.length - 1]);
 
-// Додавання позиції
 const body = document.querySelector('tbody');
 const AllRows = body.querySelectorAll('tr');
 
