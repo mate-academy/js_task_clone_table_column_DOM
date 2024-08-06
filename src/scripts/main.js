4d4137efb9c2599efe484337d3cbb7d7a87f8f3d
Yehor Gordiienko <egordienko319@gmail.com>
2024-08-06 12:46:18 +0300

    fisrt commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+function cloneTbody() {
+  const rows = document.querySelectorAll('tr');
+
+  rows.forEach((row) => {
+    const position = row.cells[1].textContent;
+
+    row.cells[4].textContent = position;
+
+    const salary = row.cells[4].textContent;
+
+    const newCell = row.insertCell(5);
+
+    newCell.textContent = salary;
+  });
+}
+
+cloneTbody();
