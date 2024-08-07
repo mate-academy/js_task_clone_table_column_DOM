605ff14dcce68d992f4f56485ce519c716c7cef9
voidmain69 <photoreznichenko@gmail.com>
2024-08-07 22:28:48 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
 // write your code here
+const table = document.querySelector('table');
+
+for (const row of table.rows) {
+  const position = row.cells[1].innerHTML;
+
+  const newCell = row.insertCell(4);
+
+  newCell.innerHTML = position;
+}
