e697194d47d44a6c243798c312781c91144c1254
Maksim Atanasov <atanasovmaksim56@gmail.com>
2024-08-07 13:48:28 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,7 @@
 'use strict';
 
-// write your code here
+const tableRows = [...document.getElementsByTagName('tr')];
+
+for (const row of tableRows) {
+  row.cells[3].after(row.cells[1].cloneNode(true));
+}
