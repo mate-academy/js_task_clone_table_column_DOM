479e2c1aa371c3f916b0f0fe32d54236e79a0124
Serhii Khobotov <hobbitura@gmail.com>
2024-08-10 16:20:38 +0300

    all tests passed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,13 @@
 'use strict';
 
 const trow = document.getElementsByTagName('table')[0].rows;
-console.log(trow);
+
+for (let i = 0; i < trow.length; i++) {
+  const secondColumnCell =
+    document.getElementsByTagName('table')[0].rows[i].cells[1];
+  const copySecondColumnCell = secondColumnCell.cloneNode(true);
+
+  document
+    .getElementsByTagName('table')[0]
+    .rows[i].lastElementChild.before(copySecondColumnCell);
+}
