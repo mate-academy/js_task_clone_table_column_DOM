94ea56d08a4c7046eb0d9baddf42ee450348256a
unknown <oleksandr.plaksin25@gmail.com>
2024-08-10 08:41:49 +0200

    solution2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,15 +5,10 @@ const table = document.querySelector('table');
 const rows = table.rows;
 
 for (let i = 0; i < rows.length; i++) {
-  if (i === 0 || i === rows.length - 1) {
-    const newCell = document.createElement('th');
+  const newCell = document.createElement(
+    i === 0 || i === rows.length - 1 ? 'th' : 'td',
+  );
 
-    newCell.innerText = rows[i].cells[1].innerText;
-    rows[i].insertBefore(newCell, rows[i].lastElementChild);
-  } else {
-    const newCell = document.createElement('td');
-
-    newCell.innerText = rows[i].cells[1].innerText;
-    rows[i].insertBefore(newCell, rows[i].lastElementChild);
-  }
+  newCell.innerText = rows[i].cells[1].innerText;
+  rows[i].insertBefore(newCell, rows[i].lastElementChild);
 }
