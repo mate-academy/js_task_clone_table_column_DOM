26db53fbb98580f2952707a2d9a35acad4995119
unknown <oleksandr.plaksin25@gmail.com>
2024-08-10 08:31:45 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
 // write your code here
+const table = document.querySelector('table');
+const rows = table.rows;
+
+for (let i = 0; i < rows.length; i++) {
+  if (i === 0 || i === rows.length - 1) {
+    const newCell = document.createElement('th');
+
+    newCell.innerText = rows[i].cells[1].innerText;
+    rows[i].insertBefore(newCell, rows[i].lastElementChild);
+  } else {
+    const newCell = document.createElement('td');
+
+    newCell.innerText = rows[i].cells[1].innerText;
+    rows[i].insertBefore(newCell, rows[i].lastElementChild);
+  }
+}
