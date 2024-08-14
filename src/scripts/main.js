370fe7452703b194b3ccce2e6e46fa92f1ab741e
malys@LAPTOP-DSQPC4U1 <alenamalyseva422@gmail.com>
2024-08-14 22:09:51 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write your code here
+function copyColumn(columnIndex) {
+  const table = document.querySelector('table');
+  const columnData = [];
+
+  for (let i = 0; i < table.rows.length; i++) {
+    const cell = table.rows[i].cells[columnIndex];
+
+    if (cell) {
+      columnData.push(cell.textContent.trim());
+    }
+  }
+
+  return columnData;
+}
+
+function insertColumn(data) {
+  const table = document.querySelector('table');
+
+  for (let i = 0; i < table.rows.length; i++) {
+    const newCell = table.rows[i].insertCell(4);
+
+    newCell.textContent = data[i];
+  }
+}
+
+insertColumn(copyColumn(1));
