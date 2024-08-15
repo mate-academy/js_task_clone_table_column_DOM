19605d3653fc8201f6c7a4e4ace1ed138ecefa52
vanvalera <115824927+vanvalera@users.noreply.github.com>
2024-08-15 13:49:23 +0300

    complete task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write your code here
+const columns = [...document.querySelectorAll('th')];
+const rows = [...document.querySelectorAll('tr')];
+
+function createNewTh(textContent) {
+  const th = document.createElement('th');
+
+  th.textContent = textContent;
+
+  return th;
+}
+
+const copiedColumn = columns[1].textContent;
+
+const newThContent = copiedColumn;
+
+columns[4].insertAdjacentElement('beforebegin', createNewTh(newThContent));
+
+columns[9].insertAdjacentElement('beforebegin', createNewTh(newThContent));
+
+rows.forEach((row) => {
+  const cells = [...row.querySelectorAll('td')];
+
+  if (cells.length > 1) {
+    const newCell = document.createElement('td');
+
+    newCell.textContent = cells[1].textContent;
+
+    row.insertBefore(newCell.cloneNode(true), cells[4]);
+  }
+});
