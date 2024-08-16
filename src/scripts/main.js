d4721fc9103653297813d592293b7fbed248fe41
iryna lysko <goiralysko@gmail.com>
2024-08-16 16:41:44 +0300

    clone table column

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const targetColumn = 1;
+const tHead = table.tHead.rows[0];
+const tFoot = table.tFoot.rows[0];
+const bodyRows = table.tBodies[0].rows;
+const newHeading = tHead.cells[targetColumn].cloneNode(true);
+const newFooterHeading = tFoot.cells[targetColumn].cloneNode(true);
+
+tHead.lastElementChild.before(newHeading);
+tFoot.lastElementChild.before(newFooterHeading);
+
+for (const row of bodyRows) {
+  const newRowData = row.cells[targetColumn].cloneNode(true);
+
+  row.lastElementChild.before(newRowData);
+}
