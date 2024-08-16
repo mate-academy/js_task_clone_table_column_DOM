1d0f43915a094bb614fc91595fd1b395bfe378b4
Hlib Vakhrameiev <g.vakhrameev@icloud.com>
2024-08-16 19:29:02 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const arrRows = table.rows;
+
+[...arrRows].forEach((row, i) => {
+  if (i > 0) {
+    const cloneTd = row.cells[1].cloneNode(true);
+
+    arrRows[i].cells[3].insertAdjacentElement('afterend', cloneTd);
+  } else {
+    const firstTh = row.cells[1].cloneNode(true);
+
+    const positionEl = row.cells[3];
+
+    positionEl.insertAdjacentElement('afterend', firstTh);
+  }
+});
