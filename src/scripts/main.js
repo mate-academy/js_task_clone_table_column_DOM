a914dff8ab98b5a9a9c4851275a3235fc4fa94cd
Hlib Vakhrameiev <g.vakhrameev@icloud.com>
2024-08-17 13:59:28 +0300

    improved solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,16 +3,8 @@
 const table = document.querySelector('table');
 const arrRows = table.rows;
 
-[...arrRows].forEach((row, i) => {
-  if (i > 0) {
-    const cloneTd = row.cells[1].cloneNode(true);
+[...arrRows].forEach((row) => {
+  const cloneTd = row.cells[1].cloneNode(true);
 
-    arrRows[i].cells[3].insertAdjacentElement('afterend', cloneTd);
-  } else {
-    const firstTh = row.cells[1].cloneNode(true);
-
-    const positionEl = row.cells[3];
-
-    positionEl.insertAdjacentElement('afterend', firstTh);
-  }
+  row.cells[3].insertAdjacentElement('afterend', cloneTd);
 });
