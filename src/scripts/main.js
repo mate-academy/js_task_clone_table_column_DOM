37f75453897057da49f92f2d539780512bc8e387
yursprava <yursprava@gmail.com>
2024-08-18 14:40:31 -0400

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write your code here
+const allRows = document.querySelectorAll('tr');
+
+for (const currRow of allRows) {
+  const currCellsCollect = currRow.cells;
+
+  const positionCell = currCellsCollect[1];
+
+  const salaryCell = currCellsCollect[4];
+
+  const thElements = currRow.querySelectorAll('th');
+
+  if (thElements.length > 0) {
+    const thAdd = document.createElement('th');
+
+    thAdd.innerHTML = positionCell.innerHTML;
+    currRow.append(thAdd);
+    currRow.insertBefore(thAdd, salaryCell);
+  } else {
+    const tdAdd = document.createElement('td');
+
+    tdAdd.innerHTML = positionCell.innerHTML;
+    currRow.append(tdAdd);
+    currRow.insertBefore(tdAdd, salaryCell);
+  }
+}
