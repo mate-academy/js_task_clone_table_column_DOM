9cd809c0443f0fc8c3174937ed2e2c2737833f09
Vlad Tkachuk <vlad.tkachuk.04@gmail.com>
2024-08-19 09:09:50 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+function copyPasteColumn(tableElement, indexCopy, indexPaste) {
+  const rows = [...tableElement.rows];
+
+  rows.forEach((row) => {
+    const cells = [...row.cells];
+    const copyCell = cells[indexCopy].cloneNode(true);
+
+    cells[indexPaste].after(copyCell);
+  });
+}
+
+copyPasteColumn(table, 1, 3);
