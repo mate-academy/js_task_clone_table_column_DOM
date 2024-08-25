a0748d5894a456620a4c2d539a0dd55bbf9d3303
sinner1993 <sergio1993goga@gmail.com>
2024-08-25 22:34:43 +0200

    qeewrwet

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+function copyAndPaste(element) {
+  const tr = [...element.querySelectorAll('tr')];
+
+  const secondColumn = tr.map((el) => el.children[1].innerText);
+
+  secondColumn.forEach((el, index, arr) => {
+    const item =
+      el === 'Position'
+        ? document.createElement('th')
+        : document.createElement('td');
+
+    item.textContent = el;
+    tr[index].insertBefore(item, tr[index].lastElementChild);
+  });
+}
+
+const tHead = table.querySelector('thead');
+const tBody = table.querySelector('tbody');
+const tFoot = table.querySelector('tfoot');
+
+copyAndPaste(tHead);
+copyAndPaste(tBody);
+copyAndPaste(tFoot);
