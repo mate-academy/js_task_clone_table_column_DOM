87437e47142d56d36ac3219ddfa375e136738a1d
Yevhenii Lutsenko <evgenlutscenko@icloud.com>
2024-08-26 23:21:09 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const tableRows = [...table.children]
+  .map((val) => {
+    return [...val.children];
+  })
+  .flat();
+
+const secondColumnValues = tableRows.map((val) => {
+  return [...val.children][1].textContent;
+});
+
+tableRows.forEach((val, i) => {
+  const tableData =
+    i === 0 || i === tableRows.length - 1
+      ? document.createElement('th')
+      : document.createElement('td');
+
+  tableData.textContent = secondColumnValues[i];
+  val.lastElementChild.previousSibling.after(tableData);
+});
