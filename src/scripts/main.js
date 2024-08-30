c205989b716a671519e4f358b534c13642e1a1d2
Mykyta Forostovets <122729163+Galenwood@users.noreply.github.com>
2024-08-30 17:53:20 +0300

    clone solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
 // write your code here
+const tb = document.querySelector('table');
+const rows = tb.querySelectorAll('tr');
+
+rows.forEach((row) => {
+  const cell = row.children;
+
+  const clonedCell = cell[1].cloneNode(true);
+
+  row.insertBefore(clonedCell, cell[cell.length - 1]);
+});
