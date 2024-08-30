fe7331206df6894c7a43c0b95702f13f8b1ea3a9
Maksym Mohyla <greenparksquad@gmail.com>
2024-08-30 11:56:30 +0300

    shortened code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,10 @@
 'use strict';
 
 const table = document.querySelector('table');
-const tbody = table.querySelector('tbody');
-const rows = tbody.querySelectorAll('tr');
-const thead = document.querySelector('thead');
-const theadRow = thead.firstElementChild;
-const secondPositionTh = document.createElement('th');
-const secondPositionThFoot = document.createElement('th');
-
-secondPositionTh.textContent = 'Position';
-secondPositionThFoot.textContent = 'Position';
-theadRow.insertBefore(secondPositionTh, theadRow.lastChild.previousSibling);
-
-const tfoot = document.querySelector('tfoot');
-const tfootRow = tfoot.firstElementChild;
-
-tfootRow.insertBefore(secondPositionThFoot, tfootRow.lastChild.previousSibling);
+const rows = table.querySelectorAll('tr');
 
 rows.forEach((row) => {
-  const positionCell = row.querySelector('td:nth-child(2)');
+  const positionCell = row.cells[1];
   const newPositionCell = positionCell.cloneNode(true);
 
   row.insertBefore(newPositionCell, row.lastChild.previousSibling);
