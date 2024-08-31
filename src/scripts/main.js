862fad4d0a221d107098770be7371924e6d16fd5
Tetyana Chuhai <chugaytatyana@gmail.&git config --global user.email chugaytatyana@gmail.>
2024-08-31 21:41:49 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (let i = 0; i < table.rows.length; i++) {
+  const row = table.rows[i];
+  const secondCell = row.querySelector('td:nth-child(2), th:nth-child(2)');
+  const cellClone = secondCell.cloneNode(true);
+  const lastCell = row.lastElementChild;
+
+  row.insertBefore(cellClone, lastCell);
+}
