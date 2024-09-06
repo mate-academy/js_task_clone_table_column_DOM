76cbcaacb3a8e41013f1a22bed884733425698f9
Pavlo Pukalo <misterkatlet@gmail.com>
2024-09-06 13:31:06 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (let i = 0; i < table.rows.length; i++) {
+  let copyEl;
+
+  if (i === 0 || i === table.rows.length - 1) {
+    copyEl = document.createElement('th');
+  } else {
+    copyEl = document.createElement('td');
+  }
+
+  copyEl.innerHTML = table.rows[i].cells[1].innerHTML;
+  table.rows[i].cells[4].insertAdjacentElement('beforebegin', copyEl);
+}
