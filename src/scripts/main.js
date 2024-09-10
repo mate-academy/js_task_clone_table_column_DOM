2b65c793f05247536b4e261b409fed7d930f3f6e
Valeria Chubarova <chubarova.valeria99@gmail.com>
2024-09-10 16:48:12 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+function cloneSecondColumn() {
+  const sections = ['thead', 'tbody', 'tfoot'];
+
+  sections.forEach((section) => {
+    const rows = table.querySelectorAll(`${section} tr`);
+
+    rows.forEach((row) => {
+      const cells = Array.from(row.querySelectorAll('td, th'));
+
+      const secondCell = cells[1].cloneNode(true);
+
+      row.insertBefore(secondCell, cells[cells.length - 1].previousSibling);
+    });
+  });
+}
+
+cloneSecondColumn();
