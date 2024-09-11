b5f5cbdfe23ce1e04c292040463b2d6547a1f35a
Maksym Fedorchuk <maxeffect.apollo@gmail.com>
2024-09-11 03:46:20 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write your code here
+const pageTable = document.querySelector('table');
+
+function cloneTableColumn(table, columnIndex, insertingIndex = 0) {
+  const tableHeadRow = table.querySelector('thead tr');
+  const tableHeadColumn = tableHeadRow.cells[columnIndex];
+  const clonedTableHeadColumn = tableHeadColumn.cloneNode(true);
+
+  const tableFooterRow = table.querySelector('tfoot tr');
+  const tableFooterColumn = tableFooterRow.cells[columnIndex];
+  const clonedTableFooterColumn = tableFooterColumn.cloneNode(true);
+
+  tableHeadRow.insertBefore(
+    clonedTableHeadColumn,
+    tableHeadRow.children[insertingIndex],
+  );
+
+  tableFooterRow.insertBefore(
+    clonedTableFooterColumn,
+    tableFooterRow.children[insertingIndex],
+  );
+
+  const tableBody = table.querySelector('tbody');
+  const tableBodyRows = tableBody.querySelectorAll('tr');
+
+  for (const tableBodyRow of tableBodyRows) {
+    const tableBodyColumn = tableBodyRow.cells[columnIndex];
+    const clonedTableBodyColumn = tableBodyColumn.cloneNode(true);
+
+    tableBodyRow.insertBefore(
+      clonedTableBodyColumn,
+      tableBodyRow.children[insertingIndex],
+    );
+  }
+}
+
+cloneTableColumn(pageTable, 1, 4);
