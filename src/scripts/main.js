ef8a30ae9f56c7930899966424733b68234d96bc
Zyrak Nur <noorzirak7@gmail.com>
2024-09-11 19:39:47 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+'use strict';
+
+const table = document.querySelector('table');
+const rows = [...table.querySelectorAll('tr')];
+
+rows.forEach((row) => {
+  const columns = [...row.children];
+  const secondColumnCopy = columns[1].cloneNode(true);
+
+  const updatedColumns = [...columns];
+
+  updatedColumns.splice(columns.length - 2, 0, secondColumnCopy);
+
+  row.innerHTML = '';
+
+  updatedColumns.forEach((column) => row.append(column));
+});
