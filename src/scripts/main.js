ff322b8e2e61cffb94849048b5bed27e5d09e161
Burchak Dmitry <burchak.dmitry@icloud.com>
2024-09-11 12:26:53 +0300

    task is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const thead = table.querySelector('thead');
+const tbody = table.querySelector('tbody');
+const tfoot = table.querySelector('tfoot');
+
+function changeColumns(...elements) {
+  elements.forEach((element) => {
+    const rows = [...element.querySelectorAll('tr')];
+
+    rows.forEach((row) => {
+      const children = row.children;
+      const lastChildren = children[children.length - 1];
+      const newCell = document.createElement(`${row.cells[0].tagName}`);
+
+      newCell.textContent = row.cells[1].textContent;
+      row.insertBefore(newCell, lastChildren);
+    });
+  });
+}
+
+changeColumns(thead, tbody, tfoot);
