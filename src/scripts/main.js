0c83af524f1680b58f3f9893e02e44324e968178
Vitaliy <vitaliklevytskyi07@gmail.com>
2024-09-12 17:13:00 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+function cloneAndInsertColumn() {
+  const rows = Array.from(table.querySelectorAll('tr'));
+
+  rows.forEach((row) => {
+    const secondCell = row.children[1];
+
+    if (secondCell) {
+      const clonedCell = secondCell.cloneNode(true);
+
+      row.insertBefore(clonedCell, row.children[row.children.length - 1]);
+    }
+  });
+}
+
+cloneAndInsertColumn();
