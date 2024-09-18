f443c6b45c7e3cfb2b774a5b6f586ba79d4fa83c
Roman Martseniuk <roman.martseniuk.developer@gmail.com>
2024-09-18 15:21:23 +0300

    complete task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
 // write your code here
+const table = document.querySelector('table');
+
+const rowsArr = Array.from(table.rows);
+
+rowsArr.forEach((el, index) => {
+  if (index === 0 || index === rowsArr.length - 1) {
+    const cell = document.createElement('th');
+
+    cell.innerText = el.cells[1].innerText;
+
+    el.insertBefore(cell, el.cells[el.cells.length - 1]);
+  } else {
+    const cell = el.insertCell(el.cells.length - 1);
+
+    cell.innerText = el.cells[1].innerText;
+    cell.style.cssText = el.style.cssText;
+  }
+});
