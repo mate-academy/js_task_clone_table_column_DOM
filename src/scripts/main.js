b4faecec5dc870c8c5f9274b78822ecb24c6ae85
Yevhen Rudnytskyi <yevhen.dodev@gmail.com>
2024-09-21 15:08:23 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const theadRow = table.querySelector('thead tr');
+const tbodyRows = table.querySelectorAll('tbody tr');
+const tfootRow = table.querySelector('tfoot tr');
+
+function cloneAndAppendSecondColumn(row) {
+  const secondCell = row.children[1];
+  const lastCell = row.lastElementChild;
+  const clonedCell = secondCell.cloneNode(true);
+
+  row.insertBefore(clonedCell, lastCell);
+}
+
+cloneAndAppendSecondColumn(theadRow);
+
+tbodyRows.forEach((row) => {
+  cloneAndAppendSecondColumn(row);
+});
+
+cloneAndAppendSecondColumn(tfootRow);
