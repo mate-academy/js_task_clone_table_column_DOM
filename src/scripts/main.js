53052d6e38c16062cf4f1c8f01b25bb8c4bddc05
associate2coder <olatsko@gmail.com>
2024-09-27 23:57:14 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+function cloneCell(row) {
+  const rowArr = Array.from(row.children);
+
+  const lastCell = rowArr[rowArr.length - 1];
+  const secondCellCopy = rowArr[1].cloneNode(true);
+
+  lastCell.before(secondCellCopy);
+}
+
+const tableArr = Array.from(document.querySelector('table').children);
+
+for (let i = 0; i < tableArr.length; i++) {
+  if (i === 1) {
+    const bodyRowsArr = Array.from(tableArr[i].children);
+
+    bodyRowsArr.forEach((row) => cloneCell(row));
+  } else {
+    const trNode = tableArr[i].firstElementChild;
+
+    cloneCell(trNode);
+  }
+}
