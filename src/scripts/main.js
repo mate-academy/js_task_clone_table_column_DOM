cff65eb8e660e2c6d9b2721354897a8483bc5ad8
associate2coder <olatsko@gmail.com>
2024-09-28 10:26:46 +0300

    simplify code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,9 @@
 'use strict';
 
-function cloneCell(row) {
-  const rowArr = Array.from(row.children);
+const table = document.querySelector('table');
 
-  const lastCell = rowArr[rowArr.length - 1];
-  const secondCellCopy = rowArr[1].cloneNode(true);
+for (const row of table.rows) {
+  const copyCell = row.cells[1].cloneNode(true);
 
-  lastCell.before(secondCellCopy);
-}
-
-const tableArr = Array.from(document.querySelector('table').children);
-
-for (let i = 0; i < tableArr.length; i++) {
-  if (i === 1) {
-    const bodyRowsArr = Array.from(tableArr[i].children);
-
-    bodyRowsArr.forEach((row) => cloneCell(row));
-  } else {
-    const trNode = tableArr[i].firstElementChild;
-
-    cloneCell(trNode);
-  }
+  row.lastElementChild.before(copyCell);
 }
