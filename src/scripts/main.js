df5334a53740935430a3d76586f3087456145090
Mykhailo Lapchynskyi <mykhailolapchynskyi@Dikaas-MacBook-Pro.local>
2024-09-28 17:12:19 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const copyColumn = (table, sourceColumnIndex, targetColumnIndex) => {
+  const rows = table.rows;
+
+  for (let i = 0; i < rows.length; i++) {
+    const sourceCell = rows[i].cells[sourceColumnIndex];
+    const newCell = sourceCell.cloneNode(true);
+
+    rows[i].insertBefore(newCell, rows[i].cells[targetColumnIndex]);
+  }
+};
+
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+const tfoot = document.querySelector('tfoot');
+
+copyColumn(thead, 1, 4);
+copyColumn(tbody, 1, 4);
+copyColumn(tfoot, 1, 4);
