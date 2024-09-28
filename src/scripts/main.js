b06fa883f661620a86bec49e3aced08fb2e7ce67
missk <rostock.vasiljev@gmail.com>
2024-09-28 23:23:56 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
 // write your code here
+
+const table = document.querySelector('table');
+const secondColumnIndex = 1;
+const lastColumnIndex = table.rows[0].cells.length - 1;
+
+const cloneCells = [];
+
+for (let i = 0; i < table.rows.length; i++) {
+  const cell = table.rows[i].cells[secondColumnIndex].cloneNode(true);
+  cloneCells.push(cell);
+}
+
+for (let i = 0; i < table.rows.length; i++) {
+  const newCell = table.rows[i].insertCell(lastColumnIndex);
+  newCell.appendChild(cloneCells[i]);
+}
