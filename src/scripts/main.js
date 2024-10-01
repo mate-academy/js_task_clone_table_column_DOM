f7542443cdc7bcda4bf49843422dba5cf9f5b51a
kovaltar <kovaltar@gmail.com>
2024-10-01 18:33:00 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 // write your code here
+const tableTr = document.querySelectorAll('tr');
+
+for (let i = 0; i < tableTr.length; i++) {
+  const tr = tableTr[i];
+
+  for (let j = 1; j < 2; j++) {
+    const trClone = tr.cells[j].cloneNode(true);
+
+    tr.insertBefore(trClone, tr.lastElementChild);
+  }
+}
