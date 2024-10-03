f06e58c47df60765301e66ad0956be0e9f28a8e3
Oleksii Karpov <alneon1@gmail.com>
2024-10-03 16:30:59 +0300

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+function copyPasteTableColumns(copyColumnIndex, pasteColumnIndex) {
+  const rows = document.querySelectorAll('table tr');
+
+  rows.forEach((row) => {
+    const cells = row.querySelectorAll('td, th');
+    const newCell = document.createElement(cells[copyColumnIndex].tagName);
+
+    newCell.textContent = cells[copyColumnIndex].textContent;
+
+    row.insertBefore(newCell, cells[pasteColumnIndex]);
+  });
+}
+
+copyPasteTableColumns(1, 4);
