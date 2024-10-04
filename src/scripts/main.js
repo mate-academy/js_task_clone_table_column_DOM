dd5d9df062ea7490d70935c84dac7d7b59d0b54e
Nazarii AlieKsieiev <nazarmetall@gmail.com>
2024-10-04 10:52:03 +0300

    Task was solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
+/* eslint-disable no-console */
 'use strict';
 
-// write your code here
+import { getColumn } from './helpers.js';
+
+const table = document.querySelector('table');
+
+function changeColumsData(dashboard, fromColumnIndex, toColumnIndex) {
+  const columnFrom = getColumn(dashboard, fromColumnIndex);
+  const columnTo = getColumn(dashboard, toColumnIndex);
+
+  columnTo.forEach((cell, index) => {
+    cell.after(columnFrom[index].cloneNode(true));
+  });
+}
+
+changeColumsData(table, 1, 3);
