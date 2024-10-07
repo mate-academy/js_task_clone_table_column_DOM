edb12f3f49b77b36301eba572969f0c6243d3010
Ivan <ivan.kartavy@icloud.com>
2024-10-07 15:06:38 +0300

    copyPaste table column

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 // write your code here
+const tr = [...document.querySelectorAll('tr')];
+
+tr.forEach((el) => {
+  if (el.children.length > 1) {
+    const secondCell = el.children[1];
+    const lastCell = el.children[el.children.length - 1];
+    const clonedCell = secondCell.cloneNode(true);
+
+    lastCell.insertAdjacentElement('beforebegin', clonedCell);
+  }
+});
