20cfae19abea05e7eb72f869398fec62ebcc7360
Igor <4430704@gmail.com>
2024-10-09 12:28:42 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const tableSrc = document.querySelector('table');
+
+const cloneColumn = (table, columnIndex) => {
+  for (const el of [...table.children]) {
+    const rows = [...el.children];
+
+    for (const row of rows) {
+      const cellClone = row.children[columnIndex].cloneNode(true);
+
+      row.children[row.children.length - 1].before(cellClone);
+    }
+  }
+};
+
+cloneColumn(tableSrc, 1);
