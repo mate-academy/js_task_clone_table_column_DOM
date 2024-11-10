930d2a4f2f257ff0216cee648daf9d33f55b1779
Roman Rusin <rgromkorusin12@gmail.com>
2024-11-10 19:37:21 +0200

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const columnIndex = 1;
+const table = document.querySelector('table');
+
+for (let i = 0; i < table.rows.length; i++) {
+  const row = table.rows[i];
+
+  const cellToClone = row.cells[columnIndex];
+  const clonedCell = cellToClone.cloneNode(true);
+
+  const insertPosition = row.cells.length - 1;
+
+  row.insertBefore(clonedCell, row.cells[insertPosition]);
+}
