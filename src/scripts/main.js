7b8ce076a37e8ecf030ec3f893250d88f311cfec
Volodymyr Vynnyk <vinnikvvv@gmail.com>
2025-01-07 18:12:07 +0200

    add cloned column

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const tableRows = table ? table.rows.length : 0;
+
+for (let rowNum = 0; rowNum < tableRows; rowNum++) {
+  const clonedCell = table.rows[rowNum].cells[1].cloneNode(true);
+
+  const lastCell = table.rows[rowNum].cells[4];
+
+  lastCell.before(clonedCell);
+}
+
+/* 'use strict';
+
+const rows = [...document.querySelectorAll('tr')];
+
+for (const row of rows) {
+  const position = row.children[1];
+  const clone = position.cloneNode(true);
+
+  row.lastElementChild.before(clone);
+}
+ */
