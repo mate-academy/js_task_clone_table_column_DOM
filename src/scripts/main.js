24f638da08bfd6f83ae5231a8e724b0ba529703e
Veronica Shyian <veronica.shyian@gmail.com>
2025-01-08 15:05:51 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('tr');
+
+rows.forEach((row) => {
+  const cellCopy = row.cells[4].cloneNode(true);
+
+  row.append(cellCopy);
+
+  row.cells[4].textContent = row.cells[1].textContent;
+});
