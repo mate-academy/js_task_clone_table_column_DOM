45c7cf99415728b142b2540bb5d0809d1a7c9f3e
Ruslan Ihnatenko <ua.ignatenko@gmail.com>
2025-01-10 04:27:05 +0100

    Task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const rows = table.querySelectorAll('tr');
+
+rows.forEach((row) => {
+  const cells = row.querySelectorAll('th, td');
+
+  const secondCloneColumn = cells[1].cloneNode(true);
+
+  row.insertBefore(secondCloneColumn, cells[cells.length - 1]);
+});
