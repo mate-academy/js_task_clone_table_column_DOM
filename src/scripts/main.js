2a76bedfe9bcdf556c0fd6cbfb93384e3a738969
Anton Tur <ant.tur85@gmail.com>
2025-01-16 16:16:23 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+Array.from(table.rows).forEach((row) => {
+  const cells = row.cells;
+
+  const secondColumn = cells[1].cloneNode(true);
+
+  row.insertBefore(secondColumn, cells[cells.length - 1]);
+});
