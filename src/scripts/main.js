fcfc044bf6ce926cf38401587880bd83cd3044bf
unknown <Olesyk08@gmail.com>
2025-01-17 13:20:40 +0200

    add task solution clone table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const rows = table.querySelectorAll('tr');
+
+for (let i = 0; i < rows.length; i++) {
+  const cells = rows[i].cells;
+  const clonedCell = cells[1].cloneNode(true);
+
+  rows[i].insertBefore(clonedCell, cells[cells.length - 1]);
+}
