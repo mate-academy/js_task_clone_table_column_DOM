0d03284b1debbe4fd1357eefa7d283b846eb63e8
Ruslan <gnatisin.ruslan1988@gmail.com>
2025-01-17 16:47:25 +0200

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write your code here
+const rows = [...document.querySelectorAll('table tr')];
+
+rows.forEach((row) => {
+  const cells = row.querySelectorAll('th, td');
+
+  if (cells.length > 1) {
+    const secondColumn = cells[1];
+    const lastColumn = cells[cells.length - 1];
+
+    const cloneColumn = secondColumn.cloneNode(true);
+
+    lastColumn.before(cloneColumn);
+  }
+});
