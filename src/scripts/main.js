90ad9ea51df5480e67f419537fae19f0f4de5e5d
artur.manuilenko <artur.manuilenko@yalantis.net>
2025-01-18 20:07:34 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
 // write your code here
+const table = document.getElementsByTagName('table')[0];
+
+[...table.rows].forEach((row) => {
+  const children = [...row.children];
+
+  children[3].after(children[1].cloneNode('deep'));
+});
