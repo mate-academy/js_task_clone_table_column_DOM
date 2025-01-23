1955651fa3395779cdfe69b094f8126b63af4c47
Alina Isakova <adorosh062@gmail.com>
2025-01-23 18:49:06 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const rows = table.querySelectorAll('tr');
+
+rows.forEach((row) => {
+  const cells = row.children;
+  const secondSell = cells[1];
+  const clone = secondSell.cloneNode(true);
+
+  const lastCell = cells[cells.length - 1];
+
+  lastCell.insertAdjacentElement('beforebegin', clone);
+});
