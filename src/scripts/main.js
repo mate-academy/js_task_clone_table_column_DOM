6948bcd0e62fdec87b803fb8a1d8620e132faf65
Mykyta Kalniev <nikitamal4ik@gmail.com>
2025-01-26 10:35:32 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+function cloneColumn(selector, enterPosition, clonedPosition) {
+  const table = document.querySelector(`${selector}`);
+
+  for (const el of table.children) {
+    const allLines = el.querySelectorAll('tr');
+
+    if (allLines.length > 0) {
+      for (const elem of allLines) {
+        const newClone = elem.children[enterPosition].cloneNode(true);
+
+        elem.children[clonedPosition - 1].after(newClone);
+      }
+    }
+  }
+}
+
+cloneColumn('table', 1, 4);
