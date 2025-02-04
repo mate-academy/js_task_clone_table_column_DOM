a36bdb4c62682739a837621d970affa96541d89a
Nadiia Bulmak <nadgaval@gmail.com>
2025-02-04 19:00:15 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
 // write your code here
+const table = document.querySelector('table');
+const rows = table.rows;
+
+for (const row of rows) {
+  const secondCell = row.cells[1];
+  const clonedCell = secondCell.cloneNode(true);
+  const lastCell = row.cells[row.cells.length - 1];
+
+  row.insertBefore(clonedCell, lastCell);
+}
