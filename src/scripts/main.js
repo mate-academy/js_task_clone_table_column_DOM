a9cc298a415a85e290fb61eee4985f648fb22156
Roman Zhyla <zhyla.roman25@gmail.com>
2025-02-09 20:46:08 +0100

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+function copyColumn(colIndex) {
+  const table = document.querySelector('table');
+
+  for (const row of table.rows) {
+    if (colIndex < row.cells.length) {
+      const cell = row.cells[colIndex].cloneNode(true);
+      const lastCell = row.cells[row.cells.length - 1];
+
+      row.insertBefore(cell, lastCell);
+    }
+  }
+}
+
+copyColumn(1);
