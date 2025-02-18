6bdd3ac6810e65da1176eb2e0f6fae76ad7fec40
Artem <stilon670@gmail.com>
2025-02-18 11:39:54 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+const tfoot = document.querySelector('tfoot');
+const copyTheadPosition = thead.rows[0].cells[1].cloneNode(true);
+const copyTfootPosition = tfoot.rows[0].cells[1].cloneNode(true);
+const allPosition = [...tbody.rows];
+
+allPosition.forEach((row) => {
+  row.insertBefore(row.cells[1].cloneNode(true), row.lastElementChild);
+});
+
+thead.rows[0].insertBefore(copyTheadPosition, thead.rows[0].lastElementChild);
+tfoot.rows[0].insertBefore(copyTfootPosition, tfoot.rows[0].lastElementChild);
