a2804059b06e5782894c599abc5c813f778c4216
Iryna <kapustyak.ira97@gmail.com>
2025-02-18 20:08:02 +0100

    SOLUTION

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const tableEl = document.querySelector('table');
+const trElList = Array.from(document.querySelectorAll('tr'));
+
+trElList.forEach(tr => {
+  const thElToDuplicate = tr.children[1]; 
+  const clonedTh = thElToDuplicate.cloneNode(true); 
+  
+  const ageBeforeClonedEl = tr.children[3];
+  ageBeforeClonedEl.after(clonedTh);
+});
