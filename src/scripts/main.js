fa5056166e959238ae692bdb5e6f53eda6b11159
Ivan Babiuk <whatifitryfirst@gmail.com>
2025-02-19 17:53:59 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
 // write your code here
+function cloneAndInsertColumn() {
+  const table = document.querySelector('table');
+  const rows = table.rows;
+
+  for (const row of rows) {
+    const cells = row.cells;
+
+    if (cells.length > 1) {
+      const clonnedCell = cells[1].cloneNode(true);
+
+      row.insertBefore(clonnedCell, cells[cells.length - 1]);
+    }
+  }
+}
+
+cloneAndInsertColumn();
