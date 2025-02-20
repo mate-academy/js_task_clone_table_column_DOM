795e331489a115f3dd799f370d41a99167f1bd3a
Vitalii Kornyliuk <vitalii.kornyliuk@aspose.com>
2025-02-20 20:13:21 +0300

    Copyed second column

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const rows = [...document.querySelectorAll('tr')];
+
+const getSecondCell = (dataRows, cellNumber) => {
+  dataRows.forEach((row) => {
+    const cell = row.children[cellNumber].cloneNode(true);
+
+    row.lastChild.previousSibling.before(cell);
+  });
+};
+
+getSecondCell(rows, 1);
