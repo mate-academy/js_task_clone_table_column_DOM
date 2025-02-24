329454f1a38dbc9b579cbe5707a29c799dbb9368
Verdysh Volodymyr <verdyshvladimir@gmail.com>
2025-02-24 09:32:50 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+function cloneSecondColumn(tableId) {
+  const table = document.querySelector(tableId);
+
+  for (const row of table.rows) {
+    const secondCell = row.cells[1];
+    const clonedCell = secondCell.cloneNode(true);
+
+    const lastCellIndex = row.cells.length - 1;
+
+    row.insertBefore(clonedCell, row.cells[lastCellIndex]);
+  }
+}
+
+cloneSecondColumn('table');
