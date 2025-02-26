597dc1505df3adc2488aa397cf4984ba799a35c3
Samarash Eugene <samarasha95@gmail.com>
2025-02-26 23:04:57 +0200

    task is ready

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+table.querySelectorAll('tr').forEach((row) => {
+  const indexCopy = 1;
+  const cell = row.children;
+
+  if (cell) {
+    const copyCell = row.children[indexCopy].cloneNode(true);
+
+    row.insertBefore(copyCell, cell[cell.length - 1]);
+  }
+});
