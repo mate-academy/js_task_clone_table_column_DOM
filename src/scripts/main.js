5f49309557ef686aa64039033e2bb8c419ae13a6
SofiiaPelenska <soniapelenskaoe2002@gmail.com>
2025-03-01 19:19:54 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const sections = [
+  table.querySelector('thead'),
+  table.querySelector('tbody'),
+  table.querySelector('tfoot'),
+];
+
+sections.forEach((section) => {
+  section.querySelectorAll('tr').forEach((row) => {
+    const currentPosition = row.querySelectorAll('td, th');
+    const cloned = currentPosition[1].cloneNode(true);
+
+    row.insertBefore(cloned, currentPosition[currentPosition.length - 1]);
+  });
+});
