d67f72b579fbc4325ede6b00eda921e06205f2ff
WalentynaSadowa <walentynasadowa28@gmail.com>
2025-03-01 19:14:01 +0100

    cloneTableColumn

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+document.addEventListener('DOMContentLoaded', function () {
+  const table = document.querySelector('table');
+
+  const rows = table.querySelectorAll('tbody tr');
+
+  rows.forEach((row) => {
+    const secondColumn = row.cells[1];
+
+    const clonedColumn = secondColumn.cloneNode(true);
+
+    row.insertBefore(clonedColumn, row.cells[row.cells.length - 1]);
+  });
+});
