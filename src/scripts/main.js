faea2a55260610f939cadb4842d30a9eb42056e0
SofiiaPelenska <soniapelenskaoe2002@gmail.com>
2025-03-01 19:25:41 +0200

    add task solution fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,6 +10,11 @@ const sections = [
 sections.forEach((section) => {
   section.querySelectorAll('tr').forEach((row) => {
     const currentPosition = row.querySelectorAll('td, th');
+
+    if (currentPosition.length < 2) {
+      return;
+    }
+
     const cloned = currentPosition[1].cloneNode(true);
 
     row.insertBefore(cloned, currentPosition[currentPosition.length - 1]);
