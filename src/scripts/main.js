511dbd6dcb02f1994962a76a76b568c2aa273512
Anton Polishchuk <hardixxxxx@gmail.com>
2025-03-02 21:18:38 +0100

    complete task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+[...table.rows].forEach((row) => {
+  const secondCellCopy = row.cells[1].cloneNode(true);
+
+  row.lastElementChild.before(secondCellCopy);
+});
