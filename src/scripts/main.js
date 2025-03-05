7b592deeea18d7b39858d4b2cad939c49db5ca1d
George <george.boujhenitsa@gmail.com>
2025-03-05 17:57:08 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write your code here
+// Top TH
+const getTopHead = document.querySelector('thead tr');
+const newTopTh = document.createElement('th');
+
+newTopTh.innerText = getTopHead.children[1].innerText;
+getTopHead.children[3].after(newTopTh);
+
+// Bottom TH
+const getBottomHead = document.querySelector('tfoot tr');
+const newBottomTh = document.createElement('th');
+
+newBottomTh.innerText = getBottomHead.children[1].innerText;
+getBottomHead.children[3].after(newBottomTh);
+
+// Adding List
+const positionsList = document.querySelectorAll('tbody tr');
+
+positionsList.forEach((item) => {
+  const newTd = document.createElement('td');
+
+  newTd.innerText = item.children[1].innerText;
+
+  item.children[3].after(newTd);
+});
