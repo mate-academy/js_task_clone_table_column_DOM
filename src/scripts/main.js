e72fff06353c892df51cf4abc10ca9b2302abfc0
Yaroslav <yarmoshyk96@gmail.com>
2025-03-06 11:08:06 +0200

    Add clone table column DOM solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const table = document.querySelector('table');
+  const cellsArray = Array.from(table.tHead.rows[0].cells);
+  const positionIndex = cellsArray.findIndex(
+    (cell) => cell.innerText === 'Position',
+  );
+  const lastIndex = cellsArray.length - 1;
+
+  for (const el of table.children) {
+    for (const row of el.rows) {
+      const positionElement = row.cells[positionIndex].cloneNode(true);
+
+      row.insertBefore(positionElement, row.cells[lastIndex]);
+    }
+  }
+});
