28e12cbbf2b68b22e3661ace5c4b8db2fa84c473
Ivan Herhi <ionherhi@gmail.com>
2025-03-08 13:10:36 +0100

    MainCamf

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const body = document.body;
+const table = body.querySelector('table');
+const tableChilds = Array.from(table.children);
+
+tableChilds.forEach((item) => {
+  const arrayTr = Array.from(item.children);
+
+    arrayTr.forEach((tr) => {
+      let element;
+
+      if (item === tableChilds[1]){
+        element = document.createElement('td')
+      } else {
+        element = document.createElement('th')
+      }
+
+      element.textContent = tr.children[1].textContent
+      tr.insertBefore(element, tr.lastChild)
+    })
+})
