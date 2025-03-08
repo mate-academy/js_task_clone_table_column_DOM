164dac882e210d60c2e2d3839c3b500db21914f5
Andriian <an.hrytsan@gmail.com>
2025-03-08 20:01:02 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const tableRows = table.rows;
+
+for (const row of tableRows) {
+  const cells = row.cells;
+  const positionCell = cells[1];
+  const salaryCell = cells[cells.length - 1];
+
+  const positionCellClone = positionCell.cloneNode(true);
+
+  salaryCell.before(positionCellClone);
+}
