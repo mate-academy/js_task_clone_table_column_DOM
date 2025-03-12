a8f34f89c3a492633ccc228944ed5a168f0ad78e
Lera <valeriamelesenko4@gmail.com>
2025-03-12 18:52:05 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
 // write your code here
+const thheader = document.querySelector('thead');
+const trElement = thheader.children[0];
+const secondTh = trElement.children[1];
+const secoTh = trElement.children[3];
+
+const secondThCopy = secondTh.cloneNode(true);
+
+secoTh.insertAdjacentElement('afterend', secondThCopy);
+
+const tfoot = document.querySelector('tfoot');
+const trElementFoot = tfoot.children[0];
+const secondThFoot = trElementFoot.children[1];
+const secoThFoot = trElementFoot.children[3];
+
+const secondThCopyFoot = secondThFoot.cloneNode(true);
+
+secoThFoot.insertAdjacentElement('afterend', secondThCopyFoot);
+
+const tbody = document.querySelector('tbody');
+const rows = tbody.querySelectorAll('tr');
+
+rows.forEach((row) => {
+  const originalTd = row.children[1];
+  const copiedTd = originalTd.cloneNode(true);
+
+  const targetTd = row.children[3];
+
+  targetTd.insertAdjacentElement('afterend', copiedTd);
+});
