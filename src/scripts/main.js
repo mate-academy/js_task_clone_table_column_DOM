695f4a8bb0cdad8e919f0fd0a68c5a65fc0e51ef
nadia vlasenko <nadiia@MacBook-Air-nadia.local>
2025-03-13 15:13:14 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const table = document.querySelector('table');
+
+  if (!table) {
+    return;
+  }
+
+  const rows = table.rows;
+
+  for (const row of rows) {
+    const clonedCell = row.cells[1].cloneNode(true);
+
+    row.insertBefore(clonedCell, row.cells[row.cells.length - 1]);
+  }
+});
