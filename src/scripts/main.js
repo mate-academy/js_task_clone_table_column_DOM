d95523c296388f5d9806746f31db072ca9676cbe
Illia <hetsko.ilya@gmail.com>
2025-03-17 01:30:39 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const tbody = table.querySelector('tbody');
+const tHead = table.querySelector('thead');
+const tFoot = table.querySelector('tfoot');
+
+const positionHeader = tHead.querySelector('th:nth-child(2)');
+const salaryHeader = tHead.querySelector('th:nth-child(5)');
+const positionFooter = tFoot.querySelector('th:nth-child(2)');
+const salaryFooter = tFoot.querySelector('th:nth-child(5)');
+
+const positionColumnCells = Array.from(tbody.querySelectorAll('tr')).map(
+  (row) => row.cells[1],
+);
+
+const newHeaderCell = document.createElement('th');
+const newFooterCell = document.createElement('th');
+
+newHeaderCell.textContent = positionHeader.textContent;
+tHead.querySelector('tr').insertBefore(newHeaderCell, salaryHeader);
+
+newFooterCell.textContent = positionFooter.textContent;
+tFoot.querySelector('tr').insertBefore(newFooterCell, salaryFooter);
+
+Array.from(tbody.querySelectorAll('tr')).forEach((row, index) => {
+  const newCell = document.createElement('td');
+
+  newCell.textContent = positionColumnCells[index].textContent;
+  row.insertBefore(newCell, row.cells[4]);
+});
