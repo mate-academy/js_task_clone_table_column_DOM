7f0e33e7cbf5d1f51d0d4221d2c61ea0be7dbed7
Andrii Filipenko <andreyfil1337@gmail.com>
2025-03-20 19:28:36 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write your code here
+function rowClonning() {
+  const sections = document.querySelector('table').children;
+
+  for (const section of sections) {
+    const rows = section.children;
+
+    for (const row of rows) {
+      const clonedCell =
+        row.firstElementChild.nextElementSibling.cloneNode(true);
+
+      if (row.firstElementChild.tagName === 'TH') {
+        const newTh = clonedCell;
+
+        row.lastElementChild.before(newTh);
+      } else {
+        const newTd = clonedCell;
+
+        row.lastElementChild.before(newTd);
+      }
+    }
+  }
+}
+
+rowClonning();
