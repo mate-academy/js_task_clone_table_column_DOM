c3672254dcdc8e13e08df24dff20b94a2aa4570d
i-melnyk <i.melnyk1985@gmail.com>
2025-03-20 12:43:37 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+function copyPastColumnTable(indexCellCopy, indexCellPast) {
+  const sections = [
+    document.querySelector('thead'),
+    document.querySelector('tbody'),
+    document.querySelector('tfoot'),
+  ];
+
+  sections.forEach((x) => {
+    for (const row of x.rows) {
+      const copyElem = row.cells[indexCellCopy].cloneNode(true);
+
+      row.cells[
+        indexCellPast < 0 ? row.cells.length + indexCellPast : indexCellPast
+      ].before(copyElem);
+    }
+  });
+}
+
+copyPastColumnTable(1, -1);
