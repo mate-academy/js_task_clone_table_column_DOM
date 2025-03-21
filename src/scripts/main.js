de105b46356f9ce7ef9e52ffe2968bffd4aea261
user <mariiaantoniv@gmail.com>
2025-03-21 11:21:05 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (let i = 0; i < table.rows.length; i++) {
+  const row = table.rows[i];
+  const rowLast = row.cells.length - 1;
+  const newRow = row.insertCell(rowLast);
+
+  newRow.textContent = row.cells[1]?.textContent || '';
+}
