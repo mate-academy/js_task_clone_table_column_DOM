f529e6dc6a3aeff122560e68164c28d21f50a6e4
Pamela Szczerbiak <pamela.szczerbiak@gmail.com>
2025-03-26 17:38:52 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write your code here
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const table = document.querySelector('table');
+  const thead = table.querySelector('thead tr');
+  const tbody = table.querySelectorAll('tbody tr');
+  const tfoot = table.querySelector('tfoot tr');
+
+  const lastColumnIndex = thead.children.length - 1;
+
+  function cloneColumn(columnIndex) {
+    const headerCell = thead.children[columnIndex].cloneNode(true);
+
+    thead.insertBefore(headerCell, thead.children[lastColumnIndex]);
+
+    tbody.forEach((row) => {
+      const cell = row.children[columnIndex].cloneNode(true);
+
+      row.insertBefore(cell, row.children[lastColumnIndex]);
+    });
+
+    const footerCell = tfoot.children[columnIndex].cloneNode(true);
+
+    tfoot.insertBefore(footerCell, tfoot.children[lastColumnIndex]);
+  }
+
+  cloneColumn(1);
+});
