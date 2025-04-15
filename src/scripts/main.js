8dacd44681992c524759d4c86cd40615b2871bf3
Diana <dinamitka2369@gmail.com>
2025-04-15 21:33:55 +0200

    result1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+const tbody = document.querySelector('tbody');
+const rows = tbody.querySelectorAll('tr');
+
+rows.forEach((row) => {
+  const secondCell = row.children[1];
+  const clone = secondCell.cloneNode(true);
+
+  row.insertBefore(clone, row.lastElementChild);
+});
+
+const thead = document.querySelector('thead');
+const rows1 = thead.querySelectorAll('tr');
+
+rows1.forEach((row) => {
+  const secondCell = row.children[1];
+  const clone = secondCell.cloneNode(true);
+
+  row.insertBefore(clone, row.lastElementChild);
+});
+
+const tfoot = document.querySelector('tfoot');
+const rows2 = tfoot.querySelectorAll('tr');
+
+rows2.forEach((row) => {
+  const secondCell = row.children[1];
+  const clone = secondCell.cloneNode(true);
+
+  row.insertBefore(clone, row.lastElementChild);
+});
