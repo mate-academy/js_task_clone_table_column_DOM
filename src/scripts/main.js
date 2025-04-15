3af7408f4fd872fd1506e3fd8d915b140e86c414
Yuliia Babych <jl.babych29@gmail.com>
2025-04-15 14:47:57 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const trList = document.querySelectorAll('tr');
+
+trList.forEach((el) => {
+  const thList = el.children;
+
+  const elClone = thList[1].cloneNode(true);
+
+  thList[thList.length - 1].insertAdjacentElement('beforebegin', elClone);
+});
