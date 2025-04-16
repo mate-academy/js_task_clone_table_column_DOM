3a9821dbf46875fe7e881f764527b5968d4c8bee
Daryna <kapitanckyk.dasha@gmail.com>
2025-04-16 11:58:31 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const sections = ['thead', 'tbody', 'tfoot'];
+
+sections.forEach((section) => {
+  const rows = table.querySelector(section).rows;
+
+  for (const row of rows) {
+    const cellToClone = row.cells[1];
+    const clonedCell = cellToClone.cloneNode(true);
+
+    row.insertBefore(clonedCell, row.cells[row.cells.length - 1]);
+  }
+});
