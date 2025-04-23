00e55aae4547085dcb0eb24abf58f6e1b78bf34e
Вася <pankulich654g@mail.com>
2025-04-23 21:58:11 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,22 @@
 'use strict';
+// прив'язка до вузлів
 
 const tableBody = document.querySelector('tbody');
-
 const tableHead = document.querySelector('thead');
-
 const tableFoot = document.querySelector('tfoot');
 
+// Текст для підпису колонки верх і низ
 const headName = tableHead.rows[0].cells[1].innerHTML;
 const footName = tableFoot.rows[0].cells[1].innerHTML;
 
+// теги для вставки верх і низ
 const lasthead = document.createElement('tr');
 const lastfoot = document.createElement('tr');
 
 lasthead.innerHTML = headName;
 lastfoot.innerHTML = footName;
 
-const arrayPosition = [];
-
-for (const a of tableBody.children) {
-  arrayPosition.push(a.children[1].textContent);
-}
-
+// Добаляєм новий верх і низ
 const elementHeadTooAdd = tableHead.children[0].children[4];
 
 elementHeadTooAdd.before(lasthead);
@@ -29,13 +25,12 @@ const elementFootTooAdd = tableFoot.children[0].children[4];
 
 elementFootTooAdd.before(lastfoot);
 
-for (const a of tableBody.children) {
-  const add = a.children[4];
+//
 
-  for (let i = 0; i < arrayPosition.length; i++) {
-    const newCell = document.createElement('td');
+for (const row of tableBody.children) {
+  const salaryCell = row.children[4]; // получаєм 4 елемент із основної таблиці
+  const newCell = document.createElement('td');
 
-    newCell.textContent = arrayPosition[i];
-    add.before(newCell);
-  }
+  newCell.textContent = row.children[1].textContent;
+  salaryCell.before(newCell);
 }
