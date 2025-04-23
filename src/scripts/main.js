e4af1f8f4c5c56d8ec3f84847a5bc63e274f99b0
Вася <pankulich654g@mail.com>
2025-04-23 18:41:12 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
-// write your code here
+const tableBody = document.querySelector('tbody');
+
+const tableHead = document.querySelector('thead');
+
+const tableFoot = document.querySelector('tfoot');
+
+const headName = tableHead.rows[0].cells[1].innerHTML;
+const footName = tableFoot.rows[0].cells[1].innerHTML;
+
+const lasthead = document.createElement('tr');
+const lastfoot = document.createElement('tr');
+
+lasthead.innerHTML = headName;
+lastfoot.innerHTML = footName;
+
+const arrayPosition = [];
+
+for (const a of tableBody.children) {
+  arrayPosition.push(a.children[1].textContent);
+}
+
+const elementHeadTooAdd = tableHead.children[0].children[4];
+
+elementHeadTooAdd.before(lasthead);
+
+const elementFootTooAdd = tableFoot.children[0].children[4];
+
+elementFootTooAdd.before(lastfoot);
+
+for (const a of tableBody.children) {
+  const add = a.children[4];
+
+  for (let i = 0; i < arrayPosition.length; i++) {
+    const newCell = document.createElement('td');
+
+    newCell.textContent = arrayPosition[i];
+    add.before(newCell);
+  }
+}
