310483c0040addd8468b77a423e5c28ef43f7aec
Olena Tatyuk <tatyukolena@gmail.com>
2025-05-13 11:57:19 -0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const theadRows = document.querySelectorAll('thead tr');
+const tbodyRows = document.querySelectorAll('tbody tr');
+const tfootRows = document.querySelectorAll('tfoot tr');
+
+function cloneAndInsertColumn(rows) {
+  rows.forEach((row) => {
+    const cells = Array.from(row.children);
+    const secondCell = cells[1].cloneNode(true);
+    const lastCell = cells[cells.length - 1];
+
+    row.insertBefore(secondCell, lastCell);
+  });
+}
+
+cloneAndInsertColumn(tbodyRows);
+cloneAndInsertColumn(theadRows);
+cloneAndInsertColumn(tfootRows);
