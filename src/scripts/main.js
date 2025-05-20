4cd76b22b6defdac3c7a08cf2987439024e43bb1
Orest Vaheman <ovaheman@gmail.com>
2025-05-20 22:21:31 +0300

    add check for 2 or more column

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,11 +2,14 @@
 
 const table = document.body.querySelector('table');
 const collectionRows = table.rows;
+const firstRow = table.rows[0];
 
-for (const row of collectionRows) {
-  const col = row.cells[1].cloneNode();
+if (firstRow.cells.length >= 2) {
+  for (const row of collectionRows) {
+    const col = row.cells[1].cloneNode();
 
-  col.textContent = row.cells[1].textContent;
+    col.textContent = row.cells[1].textContent;
 
-  row.lastElementChild.before(col);
+    row.lastElementChild.before(col);
+  }
 }
