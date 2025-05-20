c18f2080fec62230a898cea9310a39247dea0670
Orest Vaheman <ovaheman@gmail.com>
2025-05-20 21:57:19 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const table = document.body.querySelector('table');
+const collectionRows = table.rows;
+
+for (const row of collectionRows) {
+  const col = row.cells[1].cloneNode();
+
+  col.textContent = row.cells[1].textContent;
+
+  row.lastElementChild.before(col);
+}
