80112dc89665b6bef4fb3b9e1a5908f4d0bebdfe
Orest Vaheman <ovaheman@gmail.com>
2025-05-20 22:35:58 +0300

    fix check

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,17 @@
 
 const table = document.body.querySelector('table');
 const collectionRows = table.rows;
-const firstRow = table.rows[0];
 
-if (firstRow.cells.length >= 2) {
-  for (const row of collectionRows) {
-    const col = row.cells[1].cloneNode();
+for (let i = 0; i < collectionRows.length; i++) {
+  if (collectionRows[i].cells.length >= 2) {
+    const col = collectionRows[i].cells[1].cloneNode(true);
 
-    col.textContent = row.cells[1].textContent;
-
-    row.lastElementChild.before(col);
+    collectionRows[i].lastElementChild.before(col);
   }
 }
+
+// for (const row of collectionRows) {
+//       const col = row.cells[1].cloneNode(true);
+
+//       row.lastElementChild.before(col);
+//     }
