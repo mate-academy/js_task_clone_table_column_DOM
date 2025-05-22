6a283c3064094fc0968cc97d9a86e09e8aa4b476
Nikita Doroshenko <nikitokd05@gmail.com>
2025-05-22 15:02:30 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const positionHead = table.tHead.rows[0].cells[1];
+const positionFoot = table.tFoot.rows[0].cells[1];
+const copyPositionHead = positionHead.cloneNode(true);
+const copyPositionFoot = positionFoot.cloneNode(true);
+
+table.tHead.rows[0].cells[3].after(copyPositionHead);
+table.tFoot.rows[0].cells[3].after(copyPositionFoot);
+
+const rows = table.tBodies[0].rows;
+
+for (const row of rows) {
+  const positionCell = row.cells[1];
+  const copyCell = positionCell.cloneNode(true);
+
+  row.cells[3].after(copyCell);
+}
