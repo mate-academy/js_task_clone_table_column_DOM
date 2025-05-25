6ab9f211729ac3c6eeddbf9ab44ecbaed250cb26
z3rt <z3rt@MacBook-Pro-Aleksandr.local>
2025-05-25 18:56:37 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const tr = table.querySelectorAll('tr');
+
+for (let i = 0; i < tr.length; i++) {
+  if (tr[i].children.length < 2) {
+    continue;
+  }
+
+  const clonedTd = tr[i].children[1].cloneNode(true);
+  const lastTd = tr[i].lastElementChild;
+
+  tr[i].insertBefore(clonedTd, lastTd);
+}
