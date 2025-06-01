33d270c66507e32a0624a6e4c6acd2e2499917ad
Viktoriia <viktoriamyhailiak@gmail.com>
2025-06-01 22:03:18 +0300

    add a solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const rows = document.getElementsByTagName('tr');
+
+for (let i = 0; i < rows.length; i++) {
+  const lastCell = rows[i].lastElementChild;
+  let newCell = document.createElement('td');
+
+  if (i === 0) {
+    newCell = document.createElement('th');
+  }
+  newCell.innerHTML = rows[i].firstElementChild.nextElementSibling.innerHTML;
+
+  lastCell.before(newCell);
+}
