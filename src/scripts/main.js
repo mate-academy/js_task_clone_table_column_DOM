eb9143ee2ced5b0d31f3eefe8d42faa5e2156268
AzartiksMan <azartofgod@gmail.com>
2025-06-09 18:27:25 +0300

    Update solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,9 +5,11 @@ const table = document.querySelector('table');
 const cloneColumn = (item) => {
   const cells = item.children;
 
+  const newPlace = cells.length - 1;
+
   const cloned = cells[1].cloneNode(true);
 
-  item.insertBefore(cloned, cells[4]);
+  item.insertBefore(cloned, cells[newPlace]);
 };
 
 ['thead', 'tbody', 'tfoot'].forEach((section) => {
