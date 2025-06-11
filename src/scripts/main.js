0d8cc2e77c30535b78ac7009de5396d5e7bd4dcb
YuliaSabitova <yuliasab2017@mail.com>
2025-06-11 22:36:26 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const table = document.querySelector('table');
+
+  const cloneColIndex = 1;
+
+  const insertBeforeLast = (row, cloneIndex) => {
+    const cells = Array.from(row.cells);
+
+    if (cells.length < 2) {
+      return;
+    }
+
+    const cloneCell = cells[cloneIndex].cloneNode(true);
+    const lastIndex = cells.length - 1;
+
+    row.insertBefore(cloneCell, cells[lastIndex]);
+  };
+  const cloneColumn = (section) => {
+    if (!section) {
+      return;
+    }
+
+    for (const row of section.rows) {
+      insertBeforeLast(row, cloneColIndex);
+    }
+  };
+
+  cloneColumn(table.tHead);
+  cloneColumn(table.tBodies[0]);
+  cloneColumn(table.tFoot);
+});
