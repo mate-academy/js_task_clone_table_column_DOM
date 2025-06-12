6c8338eface137c906d25c1e20dcefc4629161af
Oleg Nepiuk <gtailde@gmail.com>
2025-06-12 19:07:53 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
 // write your code here
+const tr = document.querySelectorAll('tr');
+
+Array.from(tr).forEach((row) => {
+  const copyCell = row.children[1].cloneNode(true);
+  const pasteBeforeEl = row.children[row.childElementCount - 1];
+
+  row.insertBefore(copyCell, pasteBeforeEl);
+});
