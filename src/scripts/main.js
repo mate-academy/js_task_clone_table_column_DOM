f06fba21aed5a93d43cc5118938e7e9ecc1578c5
Oleksii Myronenko <oleksiimrnko@gmail.com>
2025-06-13 23:09:07 +0300

    add test solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const table = document.querySelector('table');
+  const rows = table.querySelectorAll('thead tr, tbody tr, tfoot tr');
+
+  rows.forEach((row) => {
+    const cells = Array.from(row.children);
+    const secondCell = cells[1];
+    const clonedCell = secondCell.cloneNode(true);
+
+    row.insertBefore(clonedCell, cells[cells.length - 1]);
+  });
+});
