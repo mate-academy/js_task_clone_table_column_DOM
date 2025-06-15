2a3c7d2f35231ea01d9520710f6e3b8e52326453
Valentyna Moskvinova <tina.moskwinova@gmail.com>
2025-06-15 10:55:55 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+function cloneAndInsertColumn(indexToClone, insertBeforeLast = true) {
+  const rows = Array.from(table.rows);
+
+  rows.forEach((row) => {
+    const cells = Array.from(row.cells);
+    const clone = cells[indexToClone].cloneNode(true);
+    const insertIndex = insertBeforeLast ? cells.length - 1 : cells.length;
+
+    row.insertBefore(clone, row.cells[insertIndex]);
+  });
+}
+
+cloneAndInsertColumn(1);
