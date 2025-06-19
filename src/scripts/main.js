fbb394bd84810eb68875a4e31485a91993064e17
Vanya Holovey <golovejvana9@gmail.com>
2025-06-19 21:43:33 +0300

    copypaste table column

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const thead = document.querySelector('thead');
+const tfoot = document.querySelector('tfoot');
+
+const theadPosition =
+  thead.firstElementChild.firstElementChild.nextElementSibling;
+const tfootPosition =
+  tfoot.firstElementChild.firstElementChild.nextElementSibling;
+
+thead.firstElementChild.lastElementChild.before(theadPosition.cloneNode(true));
+tfoot.firstElementChild.lastElementChild.before(tfootPosition.cloneNode(true));
+
+const tbody = document.querySelector('tbody');
+const tbodyChildren = [...tbody.children];
+
+for (const child of tbodyChildren) {
+  const td = child.firstElementChild.nextElementSibling;
+
+  child.lastElementChild.before(td.cloneNode(true));
+}
