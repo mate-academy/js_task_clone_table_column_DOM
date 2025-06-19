4bfa608640c95510e570a61350588e7ea7c7a7af
Savtenko Volodymyr <volodimer.sovtenko2018@gmail.com>
2025-06-19 17:13:06 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const thead = table.querySelector('thead');
+const tbody = table.querySelector('tbody');
+
+const headerRow = thead.querySelector('tr');
+const headerCells = headerRow.querySelectorAll('th');
+
+const clonedHeaderCell = headerCells[1].cloneNode(true);
+
+headerRow.insertBefore(
+  clonedHeaderCell,
+  headerCells[headerCells.length - 1].nextSibling,
+);
+
+const bodyRows = tbody.querySelectorAll('tr');
+
+bodyRows.forEach((row) => {
+  const cells = row.querySelectorAll('td');
+
+  const clonedCell = cells[1].cloneNode(true);
+
+  row.insertBefore(clonedCell, cells[cells.length - 1].nextSibling);
+});
