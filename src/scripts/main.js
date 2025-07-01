d8d7df405a5c1771145481edd4945ddb095001ce
Alina Yamchuk <alina.utc@gmail.com>
2025-07-01 14:26:13 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,14 +3,14 @@
 const rows = document.querySelectorAll('tr');
 
 rows.forEach(row => {
-  const cells = row.querySelectorAll('td');
+  const cells = row.querySelectorAll('td, th');
 
   if (cells.length < 2) return;
 
   if (row.dataset.cloned) return;
 
   const secondCell = cells[1];
-  const lastCell = cells[cells.length - 1];
+  const lastCell = cells[4];
 
   const copy = secondCell.cloneNode(true);
 
