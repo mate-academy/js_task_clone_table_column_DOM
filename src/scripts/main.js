2871cf80d3e944819d9db8c507659c949acfbda4
Your Namesimbochka <kinasvika@gmail.com>
2025-07-04 01:47:08 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const rows = Array.from(table.rows);
+
+const positionColumn = [];
+
+rows.forEach((element) => {
+  const secondCell = element.children[1];
+
+  if (secondCell) {
+    positionColumn.push(secondCell.cloneNode(true));
+  } else {
+    positionColumn.push(null);
+  }
+});
+
+for (let i = 0; i < positionColumn.length; i++) {
+  const cellToInsert = positionColumn[i];
+
+  if (cellToInsert) {
+    rows[i].lastElementChild.insertAdjacentElement('beforebegin', cellToInsert);
+  }
+}
