18df8730b3fc1eb2a5e5cb0db3de36a9e266223f
Adam <ad.kamieniarz@gmail.com>
2025-07-03 10:35:23 +0200

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const data = document.querySelectorAll('tr');
+
+for (let i = 0; i < data.length; i++) {
+  let copy = null;
+  let lastChild = null;
+
+  copy = data[i].children[1].cloneNode(true);
+  lastChild = data[i].children[4];
+
+  data[i].insertBefore(copy, lastChild);
+}
