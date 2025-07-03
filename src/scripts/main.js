e024d85e46d1bb04949faa50d2718ce32d092d9c
Bernavschi Natalia <natb@list.ru>
2025-07-03 08:58:46 +0300

    add task solution for js_task_clone_table_column_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const headTr = document.querySelector('thead tr');
+const secondTh = headTr.children[1];
+const lastHeadTh = headTr.lastElementChild;
+const newTh = secondTh.cloneNode(true);
+
+headTr.insertBefore(newTh, lastHeadTh);
+
+const footTr = document.querySelector('tfoot tr');
+const lastFootTh = footTr.lastElementChild;
+const newTh2 = secondTh.cloneNode(true);
+
+footTr.insertBefore(newTh2, lastFootTh);
+
+appendingClonedColumn(table);
+
+function appendingClonedColumn(tableParam) {
+  [...tableParam.tBodies].forEach((tBody) => {
+    [...tBody.rows].forEach((row) => {
+      const clone = row.children[1].cloneNode(true);
+
+      row.insertBefore(clone, row.lastElementChild);
+    });
+  });
+}
