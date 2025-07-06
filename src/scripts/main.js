6dcc277e979b9faf84aca31feed80cc9b98ea065
Dmytro Ostapenko <dr.dm.ostapenko@gmail.com>
2025-07-06 17:33:29 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
 // write your code here
+const table = document.querySelector('table');
+
+['thead', 'tbody', 'tfoot'].forEach((sec) => {
+  const section = table.querySelectorAll('sec');
+
+  if (!section) {
+    return;
+  }
+
+  section.querySelectorAll('tr').forEach((tr) => {
+    const src = tr.cells[1];
+
+    if (!src) {
+      return;
+    }
+
+    const clone = src.cloneNode(true);
+    const beforeNode = tr.cells[tr.cells.length - 1];
+
+    tr.insertBefore(clone, beforeNode);
+  });
+});
