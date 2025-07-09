f1ac82e15c4b6565d78941679a445874d36c6609
Rustam Herheman <gergemannn@gmail.com>
2025-07-09 19:10:27 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
 // write your code here
+const table = document.querySelector('table');
+const sections = ['thead', 'tbody', 'tfoot'];
+
+for (const sectionName of sections) {
+  const section = table.querySelector(sectionName);
+
+  if (section) {
+    for (const row of section.rows) {
+      const cells = row.cells;
+
+      if (section.length < 2) {
+        continue;
+      }
+
+      const secondCell = cells[1];
+      const lastIndex = cells.length - 1;
+      const clone = secondCell.cloneNode(true);
+
+      row.insertBefore(clone, cells[lastIndex]);
+    }
+  }
+}
