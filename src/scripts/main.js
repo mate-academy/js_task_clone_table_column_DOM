3641e09b303c4b3933a6126db8439089e5aa8aba
Vladyslav Stepanenko <vlad.step.186@gmail.com>
2025-07-09 12:16:48 +0300

    clone row

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const tr = document.querySelectorAll('tr');
+
+for (const row of tr) {
+  const cells = row.querySelectorAll('td, th');
+
+  if (cells.length >= 2) {
+    const clonedCell = cells[1].cloneNode(true);
+
+    row.insertBefore(clonedCell, cells[cells.length - 1]);
+  }
+}
