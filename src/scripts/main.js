3aa9fe5fb2163c10cf5e5375d6c1d4017444f0b6
Svyatoslav Kozelchuk-Panulin <svyatoslavkozelchuk@gmail.com>
2025-07-15 18:50:26 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const clone = (colIndexToClone) => {
+  const rows = table.querySelectorAll('tr');
+
+  rows.forEach((row) => {
+    const cells = row.children;
+
+    const insertIndex = cells.length - 1;
+
+    const cloneCell = cells[colIndexToClone].cloneNode(true);
+
+    row.insertBefore(cloneCell, cells[insertIndex]);
+  });
+};
+
+clone(1);
