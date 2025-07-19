794b9706a630cb68f70490ba4ecd4d059f848a07
Olga Berezhna <57196165+oberezhnay@users.noreply.github.com>
2025-07-19 17:26:02 +0300

    Solition

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const rows = [...table.rows];
+const positions = [];
+
+for (const row of rows) {
+  const cells = [...row.cells];
+
+  positions.push(cells[1].innerText);
+}
+
+for (let i = 0; i < rows.length; i += 1) {
+  const row = rows[i];
+  const cells = [...row.cells];
+
+  if (i === 0 || i === rows.length - 1) {
+    cells[cells.length - 1].insertAdjacentHTML(
+      'beforebegin',
+      `<th>${positions[i]}</th>`,
+    );
+  } else {
+    cells[cells.length - 1].insertAdjacentHTML(
+      'beforebegin',
+      `<td>${positions[i]}</td>`,
+    );
+  }
+}
