37b473d7aad53407f6c6e0c7739f4a3934af3299
Pavlo <marlana08022022@gmail.com>
2025-07-19 22:19:24 +0300

    chanche table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const tr = document.querySelectorAll('tr');
+
+const trArr = Array.from(tr);
+
+for (let i = 0; i < trArr.length; i++) {
+  const line = trArr[i].children;
+
+  const clone = line[1].cloneNode(true);
+
+  trArr[i].insertBefore(clone, line[line.length - 1]);
+}
