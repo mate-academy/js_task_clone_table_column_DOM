61b0df917cf9587fb0877d1b3cc92e29cd2b8a81
Denys Herych <fojjof2@gmail.com>
2025-07-22 16:08:48 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,9 +9,9 @@ arrRows.forEach((el, idx) => {
     // до першого і останнього елементу ми додамо інші елементи
     const th = document.createElement('th');
 
-    th.textContent = el.childNodes[3].textContent;
+    th.textContent = el.children[1].textContent;
     // це точна позиція "position" в таблиці
-    el.insertBefore(th, el.childNodes[9]);
+    el.insertBefore(th, el.children[4]);
     // вставляємо перед потрібним елеметом в кожному рядку
 
     return;
@@ -19,6 +19,6 @@ arrRows.forEach((el, idx) => {
 
   const td = document.createElement('td');
 
-  td.textContent = el.childNodes[3].textContent;
-  el.insertBefore(td, el.childNodes[9]);
+  td.textContent = el.children[1].textContent;
+  el.insertBefore(td, el.children[4]);
 });
