305b560ef54de83481d9a509c956fa433fe5d704
Ivan <rusev065@gmail.com>
2025-07-22 20:12:07 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
 // write your code here
+
+const theadRows = document.querySelectorAll('thead tr');
+const tbodyRows = document.querySelectorAll('tbody tr');
+const tfootRows = document.querySelectorAll('tfoot tr');
+
+theadRows.forEach((row) => {
+  const cells = row.querySelectorAll('th');
+  const cloned = cells[1].cloneNode(true);
+
+  row.insertBefore(cloned, cells[cells.length - 1]);
+});
+
+tbodyRows.forEach((row) => {
+  const cells = row.querySelectorAll('td');
+  const cloned = cells[1].cloneNode(true);
+
+  row.insertBefore(cloned, cells[cells.length - 1]);
+});
+
+tfootRows.forEach((row) => {
+  const cells = row.querySelectorAll('th');
+  const cloned = cells[1].cloneNode(true);
+
+  row.insertBefore(cloned, cells[cells.length - 1]);
+});
