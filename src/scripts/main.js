152b47ab5e16a45f3777488dc14550a0453d9d9e
Denys Herych <fojjof2@gmail.com>
2025-07-22 15:58:31 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+const rows = document.getElementsByTagName('tr');
+const arrRows = [...rows];
+// деструктиризація для того щоб використати forEach
+
+arrRows.forEach((el, idx) => {
+  if (idx === 0 || idx === arrRows.length - 1) {
+    // до першого і останнього елементу ми додамо інші елементи
+    const th = document.createElement('th');
+
+    th.textContent = el.childNodes[3].textContent;
+    // це точна позиція "position" в таблиці
+    el.insertBefore(th, el.childNodes[9]);
+    // вставляємо перед потрібним елеметом в кожному рядку
+
+    return;
+  }
+
+  const td = document.createElement('td');
+
+  td.textContent = el.childNodes[3].textContent;
+  el.insertBefore(td, el.childNodes[9]);
+});
