6cdc8cba6bbd90ea52e2ae46926b29105bb2f524
Vladyslav Nechepurenko <fablevlados@gmail.com>
2025-07-29 23:37:15 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
+// const { createElement } = require('react');
+
 // write your code here
+const table = document.querySelector('table');
+
+function cloneSecondColumn(section) {
+  const rows = section.querySelectorAll('tr');
+
+  for (const row of rows) {
+    const positionCell = row.children[1];
+    const newCell = document.createElement(positionCell.tagName.toLowerCase());
+
+    newCell.textContent = positionCell.textContent;
+
+    row.insertBefore(newCell, row.lastElementChild);
+  }
+}
+
+cloneSecondColumn(table.querySelector('thead'));
+cloneSecondColumn(table.querySelector('tbody'));
+cloneSecondColumn(table.querySelector('tfoot'));
