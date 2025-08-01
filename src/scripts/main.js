3824f06db8d7d04ffa0f7de7fdff5407e4bbe91a
Dariia Hryshchukova <dariagrisukova@gmail.com>
2025-08-02 00:16:53 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const arr = document.querySelectorAll('tbody tr');
+
+const headRow = document.querySelector('thead tr');
+const ths = headRow.querySelectorAll('th');
+const cloneTh = ths[1].cloneNode(true);
+
+const tfootRow = document.querySelector('tfoot tr');
+const tfs = tfootRow.querySelectorAll('th');
+const cloneF = tfs[1].cloneNode(true);
+
+headRow.insertBefore(cloneTh, ths[ths.length - 1]);
+tfootRow.insertBefore(cloneF, tfs[tfs.length - 1]);
+
+arr.forEach((el) => {
+  const arr1 = el.querySelectorAll('td');
+  const secondTd = arr1[1];
+  const clone = secondTd.cloneNode(true);
+  const lastTd = arr1[arr1.length - 1];
+
+  el.insertBefore(clone, lastTd);
+});
