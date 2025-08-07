112ea14f90efe70f669ea3b92bdb4bc2a4834a62
Dmytro Nykorovych <dev.dmytronykorovych@gmail.com>
2025-08-07 20:23:03 +0200

    Task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('tr');
+
+[...rows].forEach((row) => {
+  const cells = row.querySelectorAll('td, th');
+  const newCell = document.createElement(row.querySelector('th') ? 'th' : 'td');
+  const elem = cells[1];
+  const lastElem = cells[cells.length - 1];
+
+  if (elem) {
+    newCell.innerText = elem.innerText;
+
+    if (lastElem) {
+      lastElem.before(newCell);
+    }
+  }
+});
