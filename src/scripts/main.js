7d46f58d07faead353dedf0f2ed78bcebee58cfe
Nikolay Tyshchenko <nikolay.tyshchenko04@gmail.com>
2025-08-08 12:56:26 +0300

    solution for clone table column task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const tableHeaderRow = table.tHead.rows[0];
+
+cloneSecondColumn(tableHeaderRow);
+
+const tableBodyRows = table.tBodies[0].rows;
+
+Array.from(tableBodyRows).forEach((row) => {
+  cloneSecondColumn(row);
+});
+
+const tableFooterRow = table.tFoot.rows[0];
+
+cloneSecondColumn(tableFooterRow);
+
+function cloneSecondColumn(row) {
+  const cells = row.cells;
+  const clonedSecond = cells[1].cloneNode(true);
+  const clonedLast = cells[cells.length - 1].cloneNode(true);
+
+  row.replaceChild(clonedSecond, cells[cells.length - 1]);
+  row.appendChild(clonedLast);
+}
