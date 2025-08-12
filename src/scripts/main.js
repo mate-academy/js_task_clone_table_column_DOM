adaa1a06320d16122e45bb667e1d76cd567bdd2d
gOFFa <n4v4rm0re666@gmail.com>
2025-08-12 19:35:15 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
 // write your code here
+const tbody = document.querySelector('tbody');
+const thead = document.querySelector('thead');
+const tfoot = document.querySelector('tfoot');
+
+const positionTypeHead = thead.rows[0].cells[1].cloneNode(true);
+const positionTypeFoot = tfoot.rows[0].cells[1].cloneNode(true);
+
+thead.rows[0].cells[4].before(positionTypeHead);
+tfoot.rows[0].cells[4].before(positionTypeFoot);
+
+const rows = tbody.rows;
+
+for (let i = 0; i < rows.length; i++) {
+  const employeePosition = tbody.rows[i].cells[1].cloneNode(true);
+
+  tbody.rows[i].cells[4].before(employeePosition);
+}
