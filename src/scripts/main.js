0d22f4cf0b96598cbd57161c001674c99eb1ae37
Nikita Chupahin <nik123chupahin@gmail.com>
2025-08-18 19:44:58 +0300

    added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
 // write your code here
+const table = document.querySelector('table');
+const rows = Array.from(table.rows);
+
+rows.forEach((row) => {
+  const copiedEl = row.cells[1].outerHTML;
+  const lastEl = row.lastElementChild;
+
+  lastEl.insertAdjacentHTML('beforebegin', copiedEl);
+});
