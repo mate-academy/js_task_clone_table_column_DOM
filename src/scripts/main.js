5d59e07d91c21c237d837c8ef565ee47f2dd6cd4
Mykola <nyr3mvneov@gmail.com>
2025-08-18 14:58:04 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('table tbody tr');
+
+const positionColumn = Array.from(rows).map((row) => {
+  const cells = row.querySelectorAll('td');
+
+  return cells[1].textContent;
+});
+
+rows.forEach((row, i) => {
+  const cells = row.querySelectorAll('td');
+  const newTd = document.createElement('td');
+
+  newTd.textContent = positionColumn[i];
+  cells[3].after(newTd);
+});
+
+const headerRow = document.querySelector('table thead tr');
+
+if (headerRow) {
+  const ths = headerRow.querySelectorAll('th');
+  const newTh = document.createElement('th');
+
+  newTh.textContent = 'Position';
+  ths[3].after(newTh);
+}
+
+const footerRow = document.querySelector('table tfoot tr');
+
+if (footerRow) {
+  const ths = footerRow.querySelectorAll('th');
+  const newTh = document.createElement('th');
+
+  newTh.textContent = 'Position';
+  ths[3].after(newTh);
+}
