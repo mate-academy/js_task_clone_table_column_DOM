b8045eece2b5d11657a68d7b711e70a9e6ac2b86
BadOfficer <tarasbondarenko2005@gmail.com>
2025-08-23 02:49:41 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const rows = table.querySelectorAll('tr');
+
+rows.forEach((row) => {
+  const cells = row.querySelectorAll('td, th');
+
+  if (cells.length > 1) {
+    const secondCellCopy = cells[1].cloneNode(true);
+
+    cells[cells.length - 1].before(secondCellCopy);
+  }
+});
