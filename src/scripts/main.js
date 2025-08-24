060529f98eace45edfcd20f8bd1267288a0d7f94
Volodymyr Kolosov <kolosovvova0@gmail.com>
2025-08-24 21:13:04 +0300

    copy-paste

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const allTaible = document.querySelector('table');
+
+const headTaible = allTaible.tHead;
+
+const bodyTaible = allTaible.tBodies[0];
+
+const footTaible = allTaible.tFoot;
+
+function copyPaste(elementTaible) {
+  for (let i = 0; i < elementTaible.rows.length; i++) {
+    const elementTr = elementTaible.rows[i];
+    const copy = elementTr.cells[1].cloneNode(true);
+    const lastCells = elementTr.cells[elementTr.cells.length - 1];
+
+    elementTr.insertBefore(copy, lastCells);
+  }
+}
+
+copyPaste(headTaible);
+copyPaste(bodyTaible);
+copyPaste(footTaible);
