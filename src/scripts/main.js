a739d22e13b0d29f041b7e4021d047da69b179a8
kirky359 <lol783176@gmail.com>
2025-08-28 12:49:06 +0300

    task completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // write your code here
+
+const table = document.querySelector('table');
+
+for (const row of table.rows) {
+  if (row.cells.length < 2) {
+    continue;
+  }
+
+  const clone = row.cells[1].cloneNode(true);
+
+  row.insertBefore(clone, row.lastElementChild);
+}
