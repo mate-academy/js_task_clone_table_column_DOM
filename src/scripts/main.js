8ab8e7d952d0b65eff1331a0ba2dd5eea52b4e7f
Denys <zorosmg5@mail.com>
2025-09-10 12:05:38 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (const row of table.rows) {
+  // клон другої клітинки
+  const clone = row.cells[1].cloneNode(true);
+
+  // вставляємо перед останньою
+  row.insertBefore(clone, row.cells[row.cells.length - 1]);
+}
