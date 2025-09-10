2d3277130623f0380367a3caf8d83518ef1bdd10
Nazariy <nazariy.holovach2007@gmail.com>
2025-09-10 16:39:27 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+function insertClone(row) {
+  const cells = row.children;
+  const cellClone = cells[1].cloneNode(true);
+
+  row.insertBefore(cellClone, row.lastElementChild);
+}
+
+const tableHeader = document.querySelector('thead');
+const tableBody = document.querySelector('tbody');
+const headerRow = tableHeader.querySelector('tr');
+const tableRows = tableBody.querySelectorAll('tr');
+
+if (!tableBody || !tableHeader || !tableRows.length || !headerRow) {
+  throw new Error('Table elements are not found');
+}
+
+tableRows.forEach((row) => {
+  insertClone(row);
+});
+
+insertClone(headerRow);
