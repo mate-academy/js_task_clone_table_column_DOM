2d48b749c47234bcebfceda1eea947806892102a
Volodimir <volodimirbryatko@gmail.com>
2025-09-11 19:54:20 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 // write your code here
+document.querySelectorAll('table tr').forEach(function (item) {
+  if (item.querySelectorAll('th').length > 0) {
+    const lastSel = item.querySelector('th:nth-child(2)').cloneNode(true);
+
+    item.insertBefore(lastSel, item.querySelector('th:last-child'));
+  } else if (item.querySelectorAll('td').length > 0) {
+    const lastSel = item.querySelector('td:nth-child(2)').cloneNode(true);
+
+    item.insertBefore(lastSel, item.querySelector('td:last-child'));
+  }
+});
