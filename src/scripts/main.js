04d6e64eaabe7f82d795bd24b51be9885bd93098
Volodimir <volodimirbryatko@gmail.com>
2025-09-12 19:08:49 +0300

    solution2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,13 @@
 'use strict';
 
-// write your code here
-document.querySelectorAll('table tr').forEach(function (item) {
-  if (item.querySelectorAll('th').length > 0) {
-    const lastSel = item.querySelector('th:nth-child(2)').cloneNode(true);
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('table tr').forEach(function (row) {
+    const cells = row.cells;
 
-    item.insertBefore(lastSel, item.querySelector('th:last-child'));
-  } else if (item.querySelectorAll('td').length > 0) {
-    const lastSel = item.querySelector('td:nth-child(2)').cloneNode(true);
+    if (cells.length > 1) {
+      const clone = cells[1].cloneNode(true);
 
-    item.insertBefore(lastSel, item.querySelector('td:last-child'));
-  }
+      row.insertBefore(clone, cells[cells.length - 1]);
+    }
+  });
 });
