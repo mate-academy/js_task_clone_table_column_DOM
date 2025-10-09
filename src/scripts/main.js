7abc3745e6770340e906848eebe929dced178516
Artem Yakhno <artem.yakhno.ua@gmail.com>
2025-10-09 14:41:37 +0300

    fixed issues

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,28 +10,12 @@ function cloneColumnBeforeLast(tableEl, fromIndex = 1) {
   }
 
   insertIntoSection(tableEl.querySelector('thead'), fromIndex);
-  insertIntoSection(tableEl.querySelector('tbody'), fromIndex, true);
+  insertIntoSection(tableEl.querySelector('tbody'), fromIndex);
   insertIntoSection(tableEl.querySelector('tfoot'), fromIndex);
 }
 
-function insertIntoSection(sectionEl, fromIndex, isBody = false) {
+function insertIntoSection(sectionEl, fromIndex) {
   if (!sectionEl) {
-    return;
-  }
-
-  if (!isBody) {
-    const tr = sectionEl.rows?.[0];
-
-    if (!tr || tr.cells.length <= fromIndex) {
-      return;
-    }
-
-    const clone = tr.cells[fromIndex].cloneNode(true);
-
-    const beforeElem = tr.cells[tr.cells.length - 1] ?? null;
-
-    tr.insertBefore(clone, beforeElem);
-
     return;
   }
 
@@ -43,21 +27,12 @@ function insertIntoSection(sectionEl, fromIndex, isBody = false) {
 
   for (const row of rows) {
     if (row.cells.length <= fromIndex) {
-      return;
+      continue;
     }
-  }
 
-  const clones = [];
+    const clone = row.cells[fromIndex].cloneNode(true);
+    const beforeEl = row.cells[row.cells.length - 1] ?? null;
 
-  for (const row of rows) {
-    clones.push(row.cells[fromIndex].cloneNode(true));
-  }
-
-  let i = 0;
-
-  for (const row of rows) {
-    const beforeElem = row.cells[row.cells.length - 1] ?? null;
-
-    row.insertBefore(clones[i++], beforeElem);
+    row.insertBefore(clone, beforeEl);
   }
 }
