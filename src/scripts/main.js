8fda84a1fc8c67666467f3d12d8847ce85074ace
Dima <dima2003strik@gmail.com>
2025-10-13 04:11:20 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
 // write your code here
+const rows = document.querySelectorAll('table tr');
+
+rows.forEach(row => {
+  const cells = Array.from(row.querySelectorAll('th, td'));
+
+  const currentCell = cells.find((cell, index) => index === 1);
+  const lastCell = cells[cells.length - 1];
+
+  if (currentCell && lastCell) {
+    // Клонуємо другу комірку (з вмістом)
+    const clonedCell = currentCell.cloneNode(true);
+
+    // Вставляємо перед останньою
+    row.insertBefore(clonedCell, lastCell);
+  }
+});
