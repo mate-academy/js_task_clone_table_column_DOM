f98994a19d67c9ce584ea6569c44c264663cfa3a
wzfxc <maksohrimenko1@gmail.com>
2025-10-15 12:59:52 +0300

    v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const table = document.body.firstElementChild;
+
+const sectionArray = [table.tHead, table.tBodies[0], table.tFoot];
+
+sectionArray.filter(Boolean).forEach((section) => {
+  Array.from(section.rows).forEach((row) => {
+    const second = row.cells[1];
+    const copy = second.cloneNode(true);
+
+    row.insertBefore(copy, row.lastElementChild);
+  });
+});
