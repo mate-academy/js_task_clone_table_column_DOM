51d15ef530215a02f7e284fa1413d07d8b1d0797
Dmytro Popov <lort20178@gmail.com>
2025-10-25 11:16:45 +0300

    Add solution clone table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+['thead', 'tbody', 'tfoot'].forEach((section) => {
+  const rows = table.querySelectorAll(`${section} tr`);
+
+  rows.forEach((row) => {
+    const cells = row.children;
+    const clonedCell = cells[1].cloneNode(true);
+
+    row.insertBefore(clonedCell, cells[cells.length - 1]);
+  });
+});
