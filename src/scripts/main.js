3d0f9697a412ca93eab522742c561e99b84164ec
yahohulia <yaroslavhohulia@gmail.com>
2025-10-27 17:59:40 +0200

    added js solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+// do for loop, and for each iteration transfer to befor end
+
+// const td = table.rows[0].cells[1];
+// const beforEnd = table.rows[0].cells[3];
+for (let i = 0; i < table.rows.length; i++) {
+  const cloned = table.rows[i].cells[1].cloneNode(true);
+  const lengthRow = table.rows[i].cells.length;
+
+  table.rows[i].insertBefore(cloned, table.rows[i].cells[lengthRow - 1]);
+}
