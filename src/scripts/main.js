0c8a7c534641c0ea3e2497a40d42e27ce8918f4d
Paulina <paulina.zwolenik@gmail.com>
2025-11-06 18:37:54 +0100

    Add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const headerRow = table.querySelector('thead tr');
+const bodyRows = table.querySelectorAll('tbody tr');
+const footerRow = table.querySelector('tfoot tr');
+
+function cloneColumn(row) {
+  const cellToClone = row.cells[1];
+  const referenceCell = row.cells[4];
+  const clonedCell = cellToClone.cloneNode(true);
+
+  row.insertBefore(clonedCell, referenceCell);
+}
+
+cloneColumn(headerRow);
+
+bodyRows.forEach((row) => {
+  cloneColumn(row);
+});
+
+cloneColumn(footerRow);
