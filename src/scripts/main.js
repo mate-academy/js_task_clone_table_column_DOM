a8e9bb796ef7fc2edac10d6d2f26e0a06acae038
jacube1 <jakubfedorczyk@gmail.com>
2025-11-11 21:23:16 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const headerRow = table.querySelector('thead tr');
+const bodyRows = table.querySelectorAll('tbody tr');
+const footerRow = table.querySelector('tfoot tr');
+
+function cloneAndInsertColumn() {
+  const secondHeaderCell = headerRow.children[1];
+  const clonedHeaderCell = secondHeaderCell.cloneNode(true);
+  const lastHeaderCell = headerRow.children[headerRow.children.length - 1];
+
+  headerRow.insertBefore(clonedHeaderCell, lastHeaderCell);
+
+  bodyRows.forEach((row) => {
+    const secondCell = row.children[1];
+    const clonedCell = secondCell.cloneNode(true);
+    const lastCell = row.children[row.children.length - 1];
+
+    row.insertBefore(clonedCell, lastCell);
+  });
+
+  const secondFooterCell = footerRow.children[1];
+  const clonedFooterCell = secondFooterCell.cloneNode(true);
+  const lastFooterCell = footerRow.children[footerRow.children.length - 1];
+
+  footerRow.insertBefore(clonedFooterCell, lastFooterCell); // SKRÓCONA LINIA
+}
+
+cloneAndInsertColumn();
