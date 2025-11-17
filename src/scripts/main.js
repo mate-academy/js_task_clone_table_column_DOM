cdddeb77d4c6f7c1b08aa43ecfa911dc6765df4e
Viktor-1995 <madebyvityok@ukr.net>
2025-11-17 23:46:58 +0100

    1.0 solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
 // write your code here
+for (const tr of document.querySelectorAll('tr')) {
+  const el = tr.children[0].nodeName.toLowerCase();
+  const newTd = document.createElement(`${el}`);
+
+  newTd.textContent = tr.children[1].textContent;
+
+  tr.children[tr.children.length - 2].after(newTd);
+}
