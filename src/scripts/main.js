ef5e3bfbf5f5210db12b5435b7e55100aa9cbefc
A-Melnykk <969696245n@gmail.com>
2025-11-18 18:45:09 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const thead = document.querySelector('thead tr');
+const tbodyRows = document.querySelectorAll('tbody tr');
+const tfoot = document.querySelector('tfoot tr');
+
+const secondTh = thead.children[1];
+const clonedTh = secondTh.cloneNode(true);
+
+thead.insertBefore(clonedTh, thead.children[4]);
+
+tbodyRows.forEach((row) => {
+  const secondTd = row.children[1];
+  const clonedTd = secondTd.cloneNode(true);
+
+  row.insertBefore(clonedTd, row.children[4]);
+});
+
+const secondTf = tfoot.children[1];
+const clonedTf = secondTf.cloneNode(true);
+
+tfoot.insertBefore(clonedTf, tfoot.children[4]);
