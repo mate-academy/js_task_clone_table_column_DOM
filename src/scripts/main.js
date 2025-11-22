121fe77d54ffeda989fae0ea9a9587ceb3a4421a
horbenkoOleksandr <horbenko.oleksandr.v@gmail.com>
2025-11-22 14:52:38 +0200

    Table-dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const tableElemEmployee = document.querySelector('table');
+const allRowsEmployee = [...tableElemEmployee.rows];
+
+allRowsEmployee.forEach((row) => {
+  const secondCell = row.cells[1];
+  const lastCell = row.cells[row.cells.length - 1];
+
+  if (secondCell) {
+    const cloneCell = secondCell.cloneNode(true);
+
+    lastCell.before(cloneCell);
+  }
+});
