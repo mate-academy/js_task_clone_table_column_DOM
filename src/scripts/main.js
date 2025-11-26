2aad7f7aed07c06d3e4eb5a36e1c1efde9726327
Valentyna Chernova <valentyna19960410@gmail.com>
2025-11-26 22:20:09 +0200

    update solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,16 +4,18 @@
 const rows = document.querySelectorAll('tbody tr');
 const thead = document.querySelectorAll('thead tr, tfoot tr');
 
-thead.forEach(thead => {
-  const th = thead.querySelectorAll('th')[1];
+thead.forEach((theader) => {
+  const th = theader.querySelectorAll('th')[1];
   const clone = th.cloneNode(true);
-  const th4 = thead.querySelectorAll('th')[3];
-  th4.insertAdjacentElement('afterEnd', clone);
-})
+  const th4 = theader.querySelectorAll('th')[4];
 
-rows.forEach(row => {
+  th4.insertAdjacentElement('beforeBegin', clone);
+});
+
+rows.forEach((row) => {
   const td = row.querySelectorAll('td')[1];
   const clone = td.cloneNode(true);
-  const td4 = row.querySelectorAll('td')[3];
-  td4.insertAdjacentElement('afterEnd', clone);
-})
+  const td4 = row.querySelectorAll('td')[4];
+
+  td4.insertAdjacentElement('beforeBegin', clone);
+});
