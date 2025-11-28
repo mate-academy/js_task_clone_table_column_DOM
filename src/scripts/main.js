c2065b9f13cdf1bb5a24f19e2429f7310b80e07a
Tymofii <zrozik@MacBook-Air-Tymofii.local>
2025-11-28 09:59:37 +0200

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('tr');
+
+rows.forEach((row) => {
+  const cells = row.children;
+
+  if (cells.length > 1) {
+    const secondCell = cells[1];
+    const clone = secondCell.cloneNode(true);
+
+    row.insertBefore(clone, cells[cells.length - 1]);
+  }
+});
