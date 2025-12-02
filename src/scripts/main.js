de94bbe48aa5d486a5d32449a6b12352dcf98a25
SageUA <panchyshyn33@gmail.com>
2025-12-02 17:36:05 +0200

    add Soluton

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const rows = [...table.rows];
+
+rows.forEach((el) => {
+  const newEl = document.createElement(el.children[1].nodeName);
+
+  newEl.innerHTML = el.children[1].textContent;
+  el.insertBefore(newEl, el.cells[el.cells.length - 1]);
+});
