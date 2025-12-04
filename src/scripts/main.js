b75a719acd7cb20174bf1ef370059c6e22502835
Maksym Halushko <gallikillag@gmail.com>
2025-12-04 12:41:27 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('tr');
+
+rows.forEach((row) => {
+  const secondCell = row.cells[1];
+  const lastCell = row.cells[row.cells.length - 1];
+
+  if (!secondCell || !lastCell) {
+    return;
+  }
+
+  const cloneCell = secondCell.cloneNode(true);
+
+  row.insertBefore(cloneCell, lastCell);
+});
