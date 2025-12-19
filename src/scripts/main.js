e2590e63578049a55b6b8ee9f02de308b0facc4e
Yurii Kumpanenko <yuriikumpanenko@ukr.net>
2025-12-19 17:49:29 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('table tr');
+
+rows.forEach((row) => {
+  const cells = [...row.cells];
+  const clonedCell = cells[1].cloneNode(true);
+  const lastCell = cells[cells.length - 1];
+
+  lastCell.before(clonedCell);
+});
