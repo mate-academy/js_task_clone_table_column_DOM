64f629857fd4041c3112bfb9bbaa9d9a3b262f94
Dima Sidenko <sidenkodima2202@gmail.com>
2025-12-29 00:06:59 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // write your code here
+const table = document.querySelector('table');
+
+[...table.children].forEach((t) => {
+  [...t.children].forEach((tr) => {
+    const cloneRow5 = tr.cells[4].cloneNode(true);
+    const cloneRow2 = tr.cells[1].cloneNode(true);
+
+    tr.removeChild(tr.cells[4]);
+    tr.append(cloneRow2);
+    tr.append(cloneRow5);
+  });
+});
