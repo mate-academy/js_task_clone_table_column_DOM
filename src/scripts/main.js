0d7031d61b859d3d8ab98d2c9a74b4b87e23a0d0
Dima Sidenko <sidenkodima2202@gmail.com>
2025-12-29 09:53:44 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,11 +5,8 @@ const table = document.querySelector('table');
 
 [...table.children].forEach((t) => {
   [...t.children].forEach((tr) => {
-    const cloneRow5 = tr.cells[4].cloneNode(true);
     const cloneRow2 = tr.cells[1].cloneNode(true);
 
-    tr.removeChild(tr.cells[4]);
-    tr.append(cloneRow2);
-    tr.append(cloneRow5);
+    tr.insertBefore(cloneRow2, tr.cells[4]);
   });
 });
