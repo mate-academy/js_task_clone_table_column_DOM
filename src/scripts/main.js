2a3d0ace51e9b94567b4fdb4eb339f90fa1be7e1
Aleks Pashkevych <alekspashkevych@gmail.com>
2026-01-02 16:29:21 +0200

    task completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 // write your code here
+const table = document.body.querySelectorAll('thead, tbody, tfoot');
+
+for (const section in table) {
+  const rows = section.querySelectorAll('tr');
+
+  rows.forEach((row) => {
+    const cells = row.querySelectorAll('th, td');
+    const clonedCells = cells[1].cloneNode(true);
+
+    row.insertBefore(clonedCells, cells[cells.length - 1]);
+  });
+}
