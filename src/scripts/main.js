1d0befbe972197183a707feebf7208127a5fc357
Nazarii Lesniak <naziksay@gmail.com>
2026-01-07 14:47:14 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('tr');
+
+function getCloneColumn(list) {
+  list.forEach((row) => {
+    const secondCell = row.cells[1];
+    const lastCell = row.cells[row.cells.length - 1];
+    const cellClone = secondCell.cloneNode(true);
+
+    row.insertBefore(cellClone, lastCell);
+  });
+}
+
+getCloneColumn(rows);
