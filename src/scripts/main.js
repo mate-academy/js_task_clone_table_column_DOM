009960836e28083d3bef4215ea73f46f628a855c
DevT <dev.tymofiichuk@gmail.com>
2026-01-09 17:16:07 +0200

    Copy/Paste table column task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+function cloneTableColumn(tableElement, sourceIndex) {
+  for (const element of tableElement.children) {
+    for (const row of element.rows) {
+      const sourceCell = row.cells[sourceIndex];
+
+      if (!sourceCell) {
+        continue;
+      }
+
+      const insertBeforeIndex = row.cells.length - 1;
+
+      row.cells[insertBeforeIndex].before(sourceCell.cloneNode(true));
+    }
+  }
+}
+
+const table = document.querySelector('table');
+
+cloneTableColumn(table, 1);
