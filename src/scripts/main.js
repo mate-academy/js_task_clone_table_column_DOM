d51f54f5f91eca0cbace1c33c5a1ef39179a4b76
Maksym Tsyrulnyk <maximtsyrulnyk@gmail.com>
2026-01-11 18:48:57 +0200

    done Copy/Paste table column task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
 // write your code here
+const table = document.querySelector('table');
+const thead = document.querySelector('thead');
+const cells = thead.querySelectorAll('th');
+
+const tableSize = table.rows.length;
+const startCell = 1;
+const finishCell = cells.length - 1;
+
+for (let i = 0; i < tableSize; i++) {
+  const td = table.rows[i].cells[startCell];
+
+  table.rows[i].append(td.cloneNode(true));
+}
+
+for (let i = 0; i < tableSize; i++) {
+  const td = table.rows[i].cells[finishCell];
+
+  table.rows[i].append(td);
+}
