b4cfd4670c640a388c3e6da510f0244c3f534cb3
Andriy <andriit35@gmail.com>
2026-01-24 17:51:54 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const trs = document.querySelectorAll('tr');
+
+trs.forEach((tr) => {
+  const trArray = Array.from(tr.children);
+
+  const th = document.createElement('th');
+
+  th.textContent = trArray[1].textContent;
+
+  tr.insertBefore(th, trArray[trArray.length - 1]);
+});
