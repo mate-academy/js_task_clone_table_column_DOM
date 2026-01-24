3ef93d2b303be13fafb0263200f0e1b05b5465d7
Andriy <andriit35@gmail.com>
2026-01-24 17:58:16 +0200

    solution fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,9 +5,7 @@ const trs = document.querySelectorAll('tr');
 trs.forEach((tr) => {
   const trArray = Array.from(tr.children);
 
-  const th = document.createElement('th');
-
-  th.textContent = trArray[1].textContent;
+  const th = trArray[1].cloneNode(true);
 
   tr.insertBefore(th, trArray[trArray.length - 1]);
 });
