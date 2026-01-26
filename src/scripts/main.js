29a568ffc4148435aa451a5bb07bf5a658515aac
Nikita Serdyukov <nikita.serdyukov.20033002@gmail.com>
2026-01-26 23:36:53 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
 // write your code here
+const head = document.querySelector('thead tr');
+
+const headCell = head.children[1].cloneNode(true);
+
+head.insertBefore(headCell, head.children[head.children.length - 1]);
+
+const tbody = document.querySelectorAll('tbody tr');
+
+tbody.forEach((row) => {
+  const cell = row.children[1].cloneNode(true);
+
+  row.insertBefore(cell, row.children[row.children.length - 1]);
+});
+
+const foot = document.querySelector('tfoot tr');
+const footCell = foot.children[1].cloneNode(true);
+
+foot.insertBefore(footCell, foot.children[foot.children.length - 1]);
