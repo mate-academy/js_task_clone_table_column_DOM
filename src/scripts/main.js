b6c3dedfd30b74e4a9b64caeb239a1c2fcc46d06
Anastasia Sutkova <nxxqar@gmail.com>
2026-02-04 01:21:13 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const list = document.querySelectorAll('tr');
+
+list.forEach((row) => {
+  const cells = row.cells;
+  const targetCell = cells[1];
+  const clonedCell = targetCell.cloneNode(true);
+  const lastCell = cells[cells.length - 1];
+
+  row.append(clonedCell, lastCell);
+});
