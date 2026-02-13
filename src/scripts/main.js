292fd065200f0a9f1042ea7d2c667070042f1c54
Mykola Fatkullin <fatkullinmykola@gmail.com>
2026-02-13 02:25:04 +0200

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,13 +2,15 @@
 
 const table = document.querySelector('table');
 
-for (const row of table.rows) {
-  if (row.length < 2) {
-    continue;
+if (table) {
+  for (const row of table.rows) {
+    if (row.cells.length < 2) {
+      continue;
+    }
+
+    const cells = row.cells;
+    const clone = cells[1].cloneNode(true);
+
+    row.insertBefore(clone, cells[cells.length - 1]);
   }
-
-  const cells = row.cells;
-  const clone = cells[1].cloneNode(true);
-
-  row.insertBefore(clone, cells[cells.length - 1]);
 }
