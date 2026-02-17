a0bd131bd44f638c6612abefd1969021200e172f
naviailpach <ivan.chaplya13@gmail.com>
2026-02-17 15:21:17 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+function cloneSecondCell(rows) {
+  rows.forEach((tr) => {
+    if (tr.cells.length < 2) {
+      return;
+    }
+
+    const secondCell = tr.cells[1];
+    const lastCell = tr.cells[tr.cells.length - 1];
+    const clone = secondCell.cloneNode(true);
+
+    tr.insertBefore(clone, lastCell);
+  });
+}
+
+const allRows = [];
+
+if (table.tHead) {
+  allRows.push(...table.tHead.rows);
+}
+
+for (const tbody of table.tBodies) {
+  allRows.push(...tbody.rows);
+}
+
+if (table.tFoot) {
+  allRows.push(...table.tFoot.rows);
+}
+
+cloneSecondCell(allRows);
