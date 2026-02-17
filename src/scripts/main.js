a1dd52b519b270f967e66e514a2b2514c369607f
GabrielSpelchuk <gabrielspelciuc24@gmail.com>
2026-02-17 20:43:15 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
 // write your code here
+
+const table = document.querySelector('table');
+
+const selections = [
+  table.querySelector('thead'),
+  table.querySelector('tbody'),
+  table.querySelector('tfoot'),
+].filter(Boolean);
+
+function cloneSecondCell(row) {
+  if (row.cells.length < 2) {
+    return null;
+  }
+
+  const secondCell = row.cells[1];
+  const cloned = secondCell.cloneNode(true);
+
+  return cloned;
+}
+
+selections.forEach((selection) => {
+  const rows = [...selection.rows];
+
+  rows.forEach((row) => {
+    const newCell = cloneSecondCell(row);
+
+    if (!newCell) {
+      return true;
+    }
+
+    const lastCell = row.cells[row.cells - 1];
+
+    row.insertBefore(newCell, lastCell);
+  });
+});
