d590d26eefe632ae8beb670c8056cbb46b2d0052
Natalia Humenyk <gumenuknatalia53@gmail.com>
2026-02-19 21:44:04 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,25 @@
 'use strict';
 
 const table = document.querySelector('table');
+
+if (!table) {
+}
+
 const rows = table.querySelectorAll('tr');
 
 rows.forEach((row) => {
   const cells = row.children;
 
+  if (!cells || cells.length < 2) {
+    return;
+  }
+
   const secondCell = cells[1];
   const lastCell = cells[cells.length - 1];
+
+  if (!secondCell || !lastCell) {
+    return;
+  }
 
   const clonedCell = secondCell.cloneNode(true);
 
