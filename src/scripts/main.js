cce82a5d70641e49331ada17a856c26599b00dcf
Anatolii Lutai <toliklutai@gmail.com>
2026-02-22 20:27:16 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (const row of table.rows) {
+  if (row.cells.length > 1) {
+    const clone = row.cells[1].cloneNode(true);
+    const lastCell = row.cells[row.cells.length - 1];
+
+    row.insertBefore(clone, lastCell);
+  }
+}
