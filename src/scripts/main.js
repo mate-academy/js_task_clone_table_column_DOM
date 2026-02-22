d7b4aab64d4c3d3d62d28462cd8a493b1c49b4f2
Anatolii Lutai <toliklutai@gmail.com>
2026-02-22 20:59:45 +0200

    created a cloneColumn function to get the validation: if (!table) return;

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,20 @@
 'use strict';
 
-const table = document.querySelector('table');
+function cloneColumn() {
+  const table = document.querySelector('table');
 
-for (const row of table.rows) {
   if (!table) {
-    continue;
+    return;
   }
 
-  if (row.cells.length > 1) {
-    const clone = row.cells[1].cloneNode(true);
-    const lastCell = row.cells[row.cells.length - 1];
+  for (const row of table.rows) {
+    if (row.cells.length > 1) {
+      const clone = row.cells[1].cloneNode(true);
+      const lastCell = row.cells[row.cells.length - 1];
 
-    row.insertBefore(clone, lastCell);
+      row.insertBefore(clone, lastCell);
+    }
   }
 }
+
+cloneColumn();
