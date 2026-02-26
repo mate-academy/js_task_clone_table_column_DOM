8976b08cfc968a379571c6c0bffe778664879d32
uselessmorning <shtoykoigor@ukr.net>
2026-02-26 22:30:38 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
 // write your code here
+const rows = document.querySelectorAll('tr');
+
+rows.forEach((row) => {
+  const positionCell = row.cells[1];
+  const salaryCell = row.cells[row.cells.length - 1];
+  const clone = positionCell.cloneNode(true);
+
+  row.insertBefore(clone, salaryCell);
+});
