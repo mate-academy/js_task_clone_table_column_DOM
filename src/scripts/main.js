c72866b57d1c4ea33be27a82588f5bc1df6baa62
Yevhen Serdiuk <yevhen.srdk@gmail.com>
2026-03-11 18:43:36 +0200

    fixed bug in footer section and improved code readability

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,28 +1,22 @@
 'use strict';
 
-const tableHeadRow = document.querySelector('thead tr');
 const headerPosition = document
   .querySelector('thead tr th')
   .nextElementSibling.cloneNode(true);
 const footerPosition = document
   .querySelector('tfoot tr th')
   .nextElementSibling.cloneNode(true);
-const salary = document.querySelector('thead tr').lastElementChild;
-
+const headerSalary = document.querySelector('thead tr').lastElementChild;
+const footerSalary = document.querySelector('tfoot tr').lastElementChild;
 const tableBody = document.querySelector('tbody');
 const tableBodyRows = tableBody.children;
 
-const tableFooterRow = document.querySelector('tfoot tr');
-
-tableHeadRow.append(headerPosition);
-tableFooterRow.append(footerPosition);
-
-salary.replaceWith(headerPosition, salary);
+headerSalary.replaceWith(headerPosition, headerSalary);
+footerSalary.replaceWith(footerPosition, footerSalary);
 
 for (const row of tableBodyRows) {
   const positionCeil = row.cells[1].cloneNode(true);
   const salaryCeil = row.cells[4];
 
-  row.append(positionCeil);
   salaryCeil.replaceWith(positionCeil, salaryCeil);
 }
