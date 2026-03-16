b4817131f06dff9ec12b563892aa3d46771d4039
Mariana Klochnyk <klochnykmaryana@gmail.com>
2026-03-16 12:11:09 +0200

    edit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const rows = table.rows;
+
+for (const key of rows) {
+  const col = key.cells[1];
+  const newCol = col.cloneNode(true);
+
+  key.insertBefore(newCol, key.lastElementChild);
+}
