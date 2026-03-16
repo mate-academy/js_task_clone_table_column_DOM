d5d6b209602b97403599dc259cf594e91c86aaed
Taras Demianiuk <demtaras1@gamil.com>
2026-03-16 12:52:43 +0200

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write your code here
+const table = document.getElementsByTagName('table')[0];
+
+const headerRow = table.tHead.rows[0];
+
+const newHeader = document.createElement('th');
+
+headerRow.insertBefore(newHeader, headerRow.cells[headerRow.cells.length - 1]);
+
+newHeader.innerHTML = headerRow.cells[1].innerHTML;
+
+const tBody = document.getElementsByTagName('tbody')[0];
+
+const bodyTrs = tBody.getElementsByTagName('tr');
+
+Array.from(bodyTrs).forEach((tr) => {
+  const newTd = document.createElement('td');
+
+  tr.lastElementChild.before(newTd);
+  newTd.innerHTML = tr.cells[1].innerHTML;
+});
+
+const tFoot = table.getElementsByTagName('tfoot')[0];
+
+const foorHeader = document.createElement('th');
+
+tFoot.rows[0].lastElementChild.before(foorHeader);
+
+foorHeader.innerHTML = tFoot.rows[0].cells[1].innerHTML;
