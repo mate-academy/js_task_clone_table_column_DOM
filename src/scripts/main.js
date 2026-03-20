3f3eac4a8ae1203cfe160a3cc6cb3c658556ae96
Artem <muha13artem@gmail.com>
2026-03-20 17:08:45 +0200

    finish

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('tr');
+
+rows.forEach((row) => {
+  const cells = row.cells;
+
+  const target = cells[1];
+
+  const clone = target.cloneNode(true);
+
+  const last = cells[cells.length - 1];
+
+  row.insertBefore(clone, last);
+});
