51cb0e735a0b8c7622a2dd09c3abefcedbf99df2
Yurii Leniv <supusman32@gmail.com>
2026-03-24 18:14:01 +0200

    dsfsdgsd

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const k = document.querySelector('thead tr');
+const m = Array.from(k.children);
+
+m[m.length - 1].before(m[1]);
+
+const w = document.querySelector('tfoot th');
+const f = Array.from(w.children);
+
+f[f.length - 1].before(f[1]);
+
+const col = [...document.querySelectorAll('tbody tr')];
+
+for (let i = 0; i < col.length; i++) {
+  const s = Array.from(col[i].children);
+
+  s[4].before(s[1].cloneNode(true));
+}
