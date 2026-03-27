62d033c9693d8159070c8d8586f4bf2d6a797a36
Heorhii Minesko <heorhii.minesko@gmail.com>
2026-03-27 12:32:54 +0200

    added solution of the task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write your code here
+const tHeaders = document.querySelector('thead tr');
+const theaderPositionClone = tHeaders.cells[1].cloneNode(true);
+const tFooters = document.querySelector('tfoot tr');
+const tfooterPositionClone = tFooters.cells[1].cloneNode(true);
+
+tFooters.cells[4].before(tfooterPositionClone);
+tHeaders.cells[4].before(theaderPositionClone);
+
+const tbRows = document.querySelectorAll('tbody tr');
+
+tbRows.forEach((row) => {
+  const positionClone = row.cells[1].cloneNode(true);
+  const indexToAppend = row.cells[4];
+
+  indexToAppend.before(positionClone);
+});
