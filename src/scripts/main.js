a628b115357124718077d22ed6abf48718cb3366
Sidach Ruslan <flashangel4600@gmail.com>
2026-04-02 14:39:32 +0300

    add sulotion

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,45 @@
 'use strict';
 
-// write your code here
+/**
+ *
+ * @param {HTMLTableElement} elementTable
+ * @param {Number} fromIndex
+ * @param {Number} toIndex
+ */
+function cloneInsertColumn(elementTable, fromIndex, toIndex) {
+  /**
+   * @type {NodeListOf<HTMLTableCellElement>}
+   */
+  const elementTr = elementTable.querySelectorAll('tr');
+
+  const normalizedFrom = normalizeIndex(fromIndex, elementTr[0].cells.length);
+  const normalizedTo = normalizeIndex(toIndex, elementTr[0].cells.length);
+
+  elementTr.forEach((tr) => {
+    const copy = tr.children[normalizedFrom].cloneNode(true);
+
+    tr.children[normalizedTo - 1].after(copy);
+  });
+}
+
+function normalizeIndex(index, max) {
+  if (index > max) {
+    return max;
+  }
+
+  let normalizedIndex = index;
+
+  if (normalizedIndex < 0) {
+    normalizedIndex += max;
+  }
+
+  if (normalizedIndex < 0) {
+    normalizedIndex = 0;
+  }
+
+  return normalizedIndex;
+}
+
+const table = document.querySelector('table');
+
+cloneInsertColumn(table, 1, -1);
