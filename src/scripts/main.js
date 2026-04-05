6c3c89c9b7cfa89f77b424be2d2818c3c6ecc80f
Natalia Lunkevych <gadyka08@gmail.com>
2026-04-05 13:13:54 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (const row of table.rows) {
+  // знаходимо потрібну комірку в цьому рядку
+  const targetCell = row.cells[1];
+
+  // клон. її (true- копіює разом із вмістом)
+  const clone = targetCell.cloneNode(true);
+
+  // знах останню комірку щоб знати куди вписати клон
+  const lastCell = row.cells[row.cells.length - 1];
+
+  //  insertBefore(що_вставити, перед_чим)
+  row.insertBefore(clone, lastCell);
+}
