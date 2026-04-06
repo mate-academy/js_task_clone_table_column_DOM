d40fab61370b2673cdb367a0a3a50439dcf604e8
Oleh Skibinskyi <oleg.sk2002@gmail.com>
2026-04-06 17:35:56 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const allRows = document.querySelectorAll('tr');
+
+allRows.forEach(row => {
+  const cells = row.cells;
+  const secondColumn = cells[1];
+  const lastColumn = cells[cells.length - 1];
+  const clonedCell = secondColumn.cloneNode(true);
+
+  row.insertBefore(clonedCell, lastColumn);
+});
