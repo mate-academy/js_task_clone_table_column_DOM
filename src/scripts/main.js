a964bfe2fa3e56844eb93b92b0da11d9a73fb30d
Tymur <timurradkevic84@mail.com>
2026-04-07 20:40:41 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write your code here
+const tr = document.querySelectorAll('tr');
+
+tr.forEach((td) => {
+  const cells = td.children;
+
+  const secondCell = cells[1];
+  const lastCell = cells[cells.length - 1];
+
+  const copySecondCell = secondCell.cloneNode(true);
+
+  lastCell.before(copySecondCell);
+});
