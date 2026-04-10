f97b0cc7041e677346d7d712d655849d85d7bf88
Aakash Sadhu <aakashsadhu@hotmail.com>
2026-04-10 07:12:00 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('tr');
+
+rows.forEach((row) => {
+  const cell = row.children[1];
+  const clonedCell = cell.cloneNode(true);
+  const lastCell = row.children[row.children.length - 1];
+
+  row.insertBefore(clonedCell, lastCell);
+});
