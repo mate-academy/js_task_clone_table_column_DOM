45bab9dcf2e420262cd63dc673b1d0e5a643a74f
Yaroslav Kravchuk <Yaroslav.kravchyk@gmail.com>
2026-04-21 20:42:29 +0300

    add task solution 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('tr');
+
+rows.forEach((row) => {
+  const cells = row.children;
+  const cloned = cells[1].cloneNode(true);
+
+  row.insertBefore(cloned, cells[cells.length - 1]);
+});
