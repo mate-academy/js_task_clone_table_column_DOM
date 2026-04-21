07438dee9587e50374a397d8ef6ca779926644ba
Yaroslav Kravchuk <Yaroslav.kravchyk@gmail.com>
2026-04-21 20:58:15 +0300

    add task solution 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,11 @@ const rows = document.querySelectorAll('tr');
 
 rows.forEach((row) => {
   const cells = row.children;
+
+  if (cells.length < 2) {
+    return; // пропускаем такие строки
+  }
+
   const cloned = cells[1].cloneNode(true);
 
   row.insertBefore(cloned, cells[cells.length - 1]);
