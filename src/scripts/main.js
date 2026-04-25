8b68a31f6d83996fb75cf1c658363232316545df
Danylo Skepskyi <skepskyidanylo@gmail.com>
2026-04-25 16:35:42 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
 // write your code here
+function cloneColumn(initialNumber, insertNumber) {
+  const table = document.querySelector('table');
+  const rows = table.rows;
+
+  for (const row of rows) {
+    const cells = row.children;
+
+    const newCell = document.createElement(cells[initialNumber].tagName);
+
+    newCell.textContent = cells[initialNumber].textContent;
+
+    if (insertNumber >= cells.length) {
+      row.appendChild(newCell);
+    } else {
+      row.insertBefore(newCell, cells[insertNumber]);
+    }
+  }
+}
+
+cloneColumn(1, 4);
