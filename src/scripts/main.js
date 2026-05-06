37ac43083e539578b7d8296feeacebf5f1cd18df
Roman <yamshchikovr11@gmail.com>
2026-05-06 14:37:22 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
 // write your code here
+const column = [...document.querySelectorAll('tr')];
+
+column.forEach((item) => {
+  const clone = item.children[1].cloneNode(true);
+
+  item.children[item.children.length - 1].before(clone);
+});
