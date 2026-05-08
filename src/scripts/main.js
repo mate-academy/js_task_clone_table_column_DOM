bff13e4da8de0be8729863db016ca7485f5d85bd
Rostyslav <rooostik2016@gmail.com>
2026-05-08 23:25:24 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const lastColumns = [
+  ...document.querySelectorAll('th:last-child, td:last-child'),
+];
+
+const columnsToClone = [
+  ...document.querySelectorAll('th:nth-child(2), td:nth-child(2)'),
+];
+
+lastColumns.forEach((col, i) => {
+  const columnToInsert = columnsToClone[i].cloneNode(true);
+
+  col.before(columnToInsert);
+});
