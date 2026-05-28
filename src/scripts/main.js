f15b67d2a235cd59ef933e128f095d565c010e42
Damian Kurzydlo <duchozywia2024@gmail.com>
2026-05-28 22:48:43 +0100

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 // write your code here
+
+const table = document.querySelector('table');
+const rows = Array.from(table.rows);
+
+rows.forEach((row) => {
+  const clonedCell = row.cells[1].cloneNode(true);
+
+  const lastRowCell = row.cells[4];
+
+  row.insertBefore(clonedCell, lastRowCell);
+});
