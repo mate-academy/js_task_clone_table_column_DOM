9ba85c1c04ce4833c174d193a9f33af0f7ed809b
ILLYA SMYK <smykillya@gmail.com>
2026-06-01 21:52:56 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('tr');
+
+for (const row of rows) {
+  const secondCell = row.children[1];
+
+  const lastCell = row.children[row.children.length - 1];
+  const clone = secondCell.cloneNode(true);
+
+  row.insertBefore(clone, lastCell);
+}
