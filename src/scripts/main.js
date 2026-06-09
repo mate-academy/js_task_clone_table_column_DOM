162670eaf6577196255730ae7ba0fd5e8270a5cf
Anna Skobara <annaskobara14@gmail.com>
2026-06-09 11:53:15 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const allRows = document.querySelectorAll('table tr');
+
+for (const row of allRows) {
+  const cells = row.querySelectorAll('th, td');
+  const targetCell = cells[1];
+  const clonedCell = targetCell.cloneNode(true);
+  const lastCell = row.lastElementChild;
+
+  row.insertBefore(clonedCell, lastCell);
+}
