5d4209e787c26e1d9684e25c193e4be03bdb256e
Denys Boiko <big4ever6996@gmail.com>
2026-06-15 21:08:29 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const tableOfPeople = document.querySelector('table');
+
+for (const row of tableOfPeople.rows) {
+  const cellToCopy = row.cells[1];
+  const lastCell = row.cells[row.cells.length - 1];
+  const clonedCell = cellToCopy.cloneNode(true);
+
+  lastCell.before(clonedCell);
+}
