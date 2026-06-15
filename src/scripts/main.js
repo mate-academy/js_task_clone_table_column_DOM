bb07ba5f5ab1291ff53a4f2494931f26839292ab
Ihor Balakhmei <ihor@MacBook-Pro-Ihor.local>
2026-06-15 22:33:45 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const table = document.querySelector('table');
+
+  for (let i = 0; i < table.rows.length; i++) {
+    const row = table.rows[i];
+    const cell = row.cells[1];
+    const clone = cell.cloneNode(true);
+
+    row.insertBefore(clone, row.lastElementChild);
+  }
+});
