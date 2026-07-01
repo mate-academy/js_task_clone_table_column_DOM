1da63d94beb67ffd16cd52fba670e8c5015bbf1a
Skoulskiy <yaskevychilya@gmail.com>
2026-07-01 11:40:25 +0300

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
 // write your code here
+const table = document.querySelector('table');
+
+for (let i = 0; i < table.rows.length; i++) {
+  const row = table.rows[i];
+  const position = row.cells[1];
+  const lastIndex = row.cells[row.cells.length - 1];
+
+  row.insertBefore(position.cloneNode(true), lastIndex);
+}
