89db64b7b5faf537401599250b52c00396f09f14
w11ue <https://github.com/Vlad99M>
2026-07-11 23:25:42 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 // write your code here
+const rows = document.querySelectorAll('tr');
+
+rows.forEach((row) => {
+  const cells = row.querySelectorAll('th, td');
+
+  if (cells.length >= 2) {
+    const secondCell = cells[1];
+    const clonedCell = secondCell.cloneNode(true);
+    const lastCell = cells[cells.length - 1];
+
+    lastCell.parentNode.insertBefore(clonedCell, lastCell);
+  }
+});
