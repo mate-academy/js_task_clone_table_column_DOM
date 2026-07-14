8c5a4437932dc0e4c6db901c5bab38f1c5985bb4
Olga Bilokur <olyanderbilokur@gmail.com>
2026-07-15 00:09:43 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,16 +2,6 @@
 
 const rows = document.querySelectorAll('tr');
 
-// Крок 1. Клонуємо першу колонку і додаємо її в кінець
-rows.forEach((row) => {
-  if (row.cells.length > 0) {
-    const firstCellClone = row.cells[0].cloneNode(true);
-
-    row.appendChild(firstCellClone);
-  }
-});
-
-// Крок 2. Клонуємо другу колонку і вставляємо перед останньою
 rows.forEach((row) => {
   if (row.cells.length > 1) {
     const secondCellClone = row.cells[1].cloneNode(true);
