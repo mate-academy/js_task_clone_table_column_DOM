d822f3780e4fc762d332deac7552b7d2206b7d26
w11ue <https://github.com/Vlad99M>
2026-07-14 23:49:19 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,23 +9,12 @@ const rows = table.querySelectorAll('tr');
 rows.forEach((row) => {
   const cells = row.querySelectorAll('th, td');
 
-  if (cells.length >= 1) {
-    const firstCell = cells[0];
-    const clonedFirstCell = firstCell.cloneNode(true);
-
-    row.appendChild(clonedFirstCell);
-  }
-});
-
-rows.forEach((row) => {
-  const cells = row.querySelectorAll('th, td');
-
   if (cells.length >= 2) {
     const secondCell = cells[1];
     const clonedCell = secondCell.cloneNode(true);
-    const fifthCell = cells[4];
+    const lastCell = cells[cells.length - 1];
 
-    fifthCell.parentNode.insertBefore(clonedCell, fifthCell);
+    lastCell.parentNode.insertBefore(clonedCell, lastCell);
   }
 });
 
