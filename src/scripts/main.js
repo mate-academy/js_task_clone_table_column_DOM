c8aa8b1dd58bdd731e201abfaeffb6b671ed035a
Olga Bilokur <olyanderbilokur@gmail.com>
2026-07-14 20:42:26 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('tr');
+
+rows.forEach((row) => {
+  const cells = row.children;
+
+  const clone = cells[1].cloneNode(true);
+
+  row.insertBefore(clone, cells[cells.length - 1]);
+});
