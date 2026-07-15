fdb15573314cc9bd20eca4f4a500f89cbaa8f518
Vladyslav <vladislavkukhar66@gmail.com>
2026-07-15 20:22:39 +0300

    add changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,13 @@
 const tableRows = document.querySelectorAll('tr');
 
 tableRows.forEach((row) => {
+  // move first cells to the end of the table
+
+  // const firstCell = row.children[0];
+  // const clonedFirstCell = firstCell.cloneNode(true);
+
+  // row.append(clonedFirstCell);
+
   const targetCell = row.children[1];
   const clonedCell = targetCell.cloneNode(true);
   const cellIndex = row.children[4];
