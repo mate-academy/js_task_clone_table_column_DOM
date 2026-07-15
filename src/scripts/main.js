31eb0a704111c225d3c9225b510afaa99ab5f0a1
Vladyslav <vladislavkukhar66@gmail.com>
2026-07-15 20:13:04 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+// const tBody = document.querySelector('tbody');
+const tableRows = document.querySelectorAll('tr');
+
+tableRows.forEach((row) => {
+  const targetCell = row.children[1];
+  const clonedCell = targetCell.cloneNode(true);
+  const cellIndex = row.children[4];
+
+  cellIndex.before(clonedCell);
+});
