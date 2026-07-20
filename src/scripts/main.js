a8902bd011db06d54c7306793f42d665c1a98ebf
ParamooreXO <taraskull123@gmail.com>
2026-07-20 16:47:52 +0300

    solved issues

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,5 +5,5 @@ const tableRows = document.querySelector('table').rows;
 for (const row of tableRows) {
   const cellToCopy = row.cells[1];
 
-  row.cells[3].insertAdjacentElement('afterend', cellToCopy.cloneNode(true));
+  row.cells[4].insertAdjacentElement('beforebegin', cellToCopy.cloneNode(true));
 }
