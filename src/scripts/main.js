4e9585919f9c6f79b2d0c3f2a0bf4f51d7e59079
ptrodion <rdnpetrenko@gmail.com>
2026-07-20 21:42:03 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const arrayFromRows = Array.from(table.rows);
+
+function copyColumnForIndex(index) {
+  return arrayFromRows.map((row) => row.cells[index]);
+}
+
+function insertColumnBetween(rows, afterIndex, cellsToInsert) {
+  rows.forEach((row, i) => {
+    const referenceCell = row.cells[afterIndex];
+
+    const clonedCell = cellsToInsert[i].cloneNode(true);
+
+    row.insertBefore(clonedCell, referenceCell);
+  });
+}
+
+const insertColumns = copyColumnForIndex(1);
+
+insertColumnBetween(arrayFromRows, 4, insertColumns);
