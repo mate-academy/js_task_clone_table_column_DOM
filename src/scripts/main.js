b68368c2fbcb28a880cf2d9bc8cffc24848c6421
Vira Shmyhelska <vira.zykhor@gmail.com>
2026-07-22 11:27:40 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const values = [];
+
+for (const row of table.tBodies[0].rows) {
+  const value = row.cells[1].textContent;
+
+  values.push(value);
+}
+
+const headerCell = document.createElement('th');
+
+headerCell.textContent = table.tHead.rows[0].cells[1].textContent;
+
+table.tHead.rows[0].insertBefore(
+  headerCell,
+  table.tHead.rows[0].cells[table.tHead.rows[0].cells.length - 1],
+);
+
+for (const row of table.tBodies[0].rows) {
+  const newCell = document.createElement('td');
+
+  newCell.textContent = values[row.sectionRowIndex];
+  row.insertBefore(newCell, row.cells[row.cells.length - 1]);
+}
