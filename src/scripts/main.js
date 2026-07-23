a98725fbc0821bc8854dd50bbc0b9750203eba85
Ol. <holmate@dontsp.am>
2026-07-23 19:53:40 +0300

    Solution added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (const row of table.rows) {
+  const secondCell = row.cells[1];
+  const clone = secondCell.cloneNode(true);
+  const lastCell = row.cells[row.cells.length - 1];
+
+  lastCell.insertAdjacentElement('beforebegin', clone);
+}
