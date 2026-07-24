34bda690712e2e9e98bba8bd7cd06b9cce1aea60
Yehor Mishchenko <130080322+myehor@users.noreply.github.com>
2026-07-24 18:58:50 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
 // write your code here
+const table = document.querySelector('table');
+const rows = [...table.querySelectorAll('tr')];
+
+for (const row of rows) {
+  const arr = [...row.children];
+  const secondCell = arr[1].cloneNode(true);
+  const lastCell = arr[arr.length - 1];
+
+  lastCell.before(secondCell);
+}
