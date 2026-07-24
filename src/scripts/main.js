d6492bf2f7b2166118821df0bcc3e37c1f66ee2c
Danyliuk Yaroslav <unityismylove@gmail.com>
2026-07-24 19:58:35 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (const row of table.rows) {
+  const copy = row.cells[1].cloneNode(true);
+  const pos = row.cells[row.cells.length - 1];
+
+  row.insertBefore(copy, pos);
+}
