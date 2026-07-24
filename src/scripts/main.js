dee00ad73928e76ff95cb7bf557592db63f22acf
Yelyzaveta Romanovska <lizarom2608@gmail.com>
2026-07-24 20:45:24 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (const row of table.rows) {
+  const newCell = row.cells[1].cloneNode(true);
+
+  row.lastElementChild.before(newCell);
+}
