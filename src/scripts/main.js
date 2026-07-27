3c236edffe42648f8f0f6a28d448ba4616e52b69
Nik200309 <nikitapidlypnij@gmail.com>
2026-07-27 21:44:08 +0300

    First attempt

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
 // write your code here
+const table = document.querySelector('table');
+
+[...table.rows].forEach((row) => {
+  const columnToCopy = row.cells[1];
+  const clonedCell = columnToCopy.cloneNode(true);
+  const lastCell = row.cells[row.cells.length - 1];
+
+  row.insertBefore(clonedCell, lastCell);
+});
