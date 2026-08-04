a0324c6314b6beaeb7b0bbd617ef3bd20335dd1b
Maksym Babalich <17033001m@gmail.com>
2026-08-04 13:37:22 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const rows = table.rows;
+
+for (let i = 0; i < rows.length; i++) {
+  const textRow = rows[i].cells[1].textContent;
+  const position = rows[i].cells[3];
+
+  if (i === 0 || i === rows.length - 1) {
+    const newTh = document.createElement('th');
+
+    newTh.textContent = textRow;
+
+    position.after(newTh);
+    continue;
+  }
+
+  const newTd = document.createElement('td');
+
+  newTd.textContent = textRow;
+
+  position.after(newTd);
+}
