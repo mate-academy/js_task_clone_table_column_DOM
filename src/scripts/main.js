0556a45833d8765b12413e888e60b3a0d418fa16
Marichka <maria_volynchuk@ukr.net>
2026-08-08 23:57:54 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const tableRows = document.querySelectorAll('tr');
+
+for (let i = 0; i < tableRows.length; i++) {
+  const cloned = tableRows[i].children[1].cloneNode(true);
+
+  tableRows[i].lastElementChild.before(cloned);
+}
