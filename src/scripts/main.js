4577712c92ba13e05390beeffa0bb980bcf79bde
PrinceValue <princeossoa@gmail.com>
2026-08-12 15:22:36 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('tr');
+
+rows.forEach((row) => {
+  const cells = [...row.children];
+
+  const copy = cells[1].cloneNode(true);
+  const lastCell = cells[cells.length - 1];
+
+  row.insertBefore(copy, lastCell);
+});
