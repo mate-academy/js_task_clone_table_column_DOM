df19354d6cfdfeed5d636d742452395df1072a4f
SquareSquirrell <zettahedron@gmail.com>
2026-08-16 17:18:10 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+function duplicateSecondColumn(tableElement) {
+  if (!tableElement) {
+    return;
+  }
+
+  for (const row of tableElement.rows) {
+    const secondCell = row.cells[1];
+    const lastCell = row.cells[row.cells.length - 1];
+
+    if (secondCell && lastCell) {
+      const clonedCell = secondCell.cloneNode(true);
+
+      row.insertBefore(clonedCell, lastCell);
+    }
+  }
+}
+
+duplicateSecondColumn(table);
