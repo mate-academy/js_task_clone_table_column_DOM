17e9203102ac90eba5a13f09cf7cf365508100a8
Vladyslav <vlad.lozynsky@gmail.com>
2026-08-20 15:24:57 +0300

    Table column copied

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const allTableRowElements = [...document.querySelectorAll('tr')];
+
+allTableRowElements.forEach((row) => {
+  const childrenElements = row.children;
+  const tagNameOfElements = childrenElements[0].tagName.toLowerCase();
+  const newElement = document.createElement(tagNameOfElements);
+
+  newElement.textContent = childrenElements[1].textContent;
+
+  childrenElements[childrenElements.length - 1].before(newElement);
+});
