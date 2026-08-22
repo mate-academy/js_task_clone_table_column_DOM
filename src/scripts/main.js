1d2768043356de25385f55ec3b559ee6e34c6d9b
Gregory Lyon <ggylyon@gmail.com>
2026-08-23 00:37:34 +0300

    Added the solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const tableHead = table.querySelector('thead');
+
+const columnToCopyHead =
+  tableHead.querySelector('tr').firstElementChild.nextElementSibling;
+
+tableHead
+  .querySelector('tr')
+  .lastElementChild.before(columnToCopyHead.cloneNode(true));
+
+const tableFoot = table.querySelector('tfoot');
+
+const columnToCopyFoot =
+  tableFoot.querySelector('tr').firstElementChild.nextElementSibling;
+
+tableFoot
+  .querySelector('tr')
+  .lastElementChild.before(columnToCopyFoot.cloneNode(true));
+
+const tableBody = table.querySelector('tbody');
+
+for (const child of tableBody.children) {
+  const columnToCopyBody = child.firstElementChild.nextElementSibling;
+
+  child.lastElementChild.before(columnToCopyBody.cloneNode(true));
+}
