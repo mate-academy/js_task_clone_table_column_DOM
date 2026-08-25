e7b9d7f0d2557fe41071cc286fd4c6a1f1fa2ce4
ALex <sasha.hivuk@gmail.com>
2026-08-25 12:26:06 +0300

    clone table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
 // write your code here
+const strings = [...document.querySelectorAll('table tr')];
+
+strings.forEach((str) => {
+  const cells = str.children;
+  const clone = cells[1].cloneNode(true);
+
+  str.insertBefore(clone, cells[4]);
+});
