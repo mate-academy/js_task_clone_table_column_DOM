df6f819aa9135970dd4d22a1e64f33eac781a054
Bohdan Kulbaka <bkulbaka1990@gmail.com>
2026-08-28 16:19:26 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+for (const sections of table.children) {
+  for (const row of sections.children) {
+    const cloneSecondBLock = row.children[1].cloneNode(true);
+
+    row.children[4].before(cloneSecondBLock);
+  }
+}
