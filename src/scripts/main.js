c2e30034fbf4580bfea92db6031d344436f7018f
Bohdan Kulbaka <bkulbaka1990@gmail.com>
2026-08-28 16:33:04 +0300

    replace row.children[4] with row.children[row.children.length - 1]

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,6 +6,6 @@ for (const sections of table.children) {
   for (const row of sections.children) {
     const cloneSecondBLock = row.children[1].cloneNode(true);
 
-    row.children[4].before(cloneSecondBLock);
+    row.children[row.children.length - 1].before(cloneSecondBLock);
   }
 }
