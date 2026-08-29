ece9526d0c349b5b1319624825dfd84359cb0db4
AchekcSs_ <achekcss@gmail.com>
2026-08-29 22:45:33 +0300

    feat: clone and insert table column

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const tableElement = document.querySelector('table');
+
+[...tableElement.rows].forEach((row) => {
+  const cellCopy = row.cells[1].cloneNode(true);
+
+  row.insertBefore(cellCopy, row.cells[row.cells.length - 1]);
+});
