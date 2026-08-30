26adca15315828ac8671f199f949c2afc800e084
Jonereh <gnomushka0@gmail.com>
2026-08-30 09:27:21 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('tr');
+
+rows.forEach((row) => {
+  const columns = row.querySelectorAll('th , td');
+  const secondColumnClone = columns[1].cloneNode(true);
+
+  columns[columns.length - 1].before(secondColumnClone);
+});
