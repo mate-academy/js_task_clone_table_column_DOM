4c770cc2ff43cdac03733711a87bdb697d2603a7
Your Name <you@example.com>
2026-09-01 18:57:54 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const rows = [...table.rows];
+
+function insertColumn(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const cell = document.createElement('td');
+
+    cell.innerHTML = arr[i].cells[1].textContent;
+
+    const lastCellIndex = arr[i].cells.length - 1;
+
+    arr[i].cells[lastCellIndex].before(cell);
+  }
+
+  return arr;
+}
+
+insertColumn(rows);
