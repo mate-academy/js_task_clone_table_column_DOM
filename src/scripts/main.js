d5083ad286e6b800cc8f1ef2d9ae55f0ee7f002a
Your Name <you@example.com>
2026-09-01 19:50:51 +0300

    Solution_v2.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,10 +7,10 @@ function insertCloneColumnBefore(arr, clone, Before) {
   for (let i = 0; i < arr.length; i++) {
     const cell = arr[i].cells[clone].cloneNode(true);
 
-    arr[i].cells[Before].before(cell);
-  }
+    const cloneCell = arr[i].appendChild(cell);
 
-  return arr;
+    arr[i].cells[arr[i].cells.length - 2].before(cloneCell);
+  }
 }
 
 insertCloneColumnBefore(rows, 1, 4);
