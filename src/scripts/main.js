bddcb6d0e237dea0b7052dca665894c9b3f84302
Ruslan Sydorenko <letovletov111@gmail.com>
2026-09-04 08:27:43 +0200

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
 // write your code here
+const table = document.querySelector('table');
+const rows = Array.from(table.rows);
+
+rows.forEach((row) => {
+  const secondColumn = row.cells[1];
+  const copySecondColumn = secondColumn.cloneNode(true);
+  const lastColumn = row.cells[row.cells.length - 1];
+
+  lastColumn.before(copySecondColumn);
+});
