2d2133401d5af75e71c40b192b489d5cde6df7e0
Pianist <kosigorek@gmail.com>
2026-09-07 15:07:05 +0300

    finish result

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
 // write your code here
+const rows = document.querySelectorAll('table tr');
+
+rows.forEach((row) => {
+  const cells = row.children;
+  const secondCell = cells[1].cloneNode(true);
+  const lastCell = cells[cells.length - 1];
+
+  row.insertBefore(secondCell, lastCell);
+});
