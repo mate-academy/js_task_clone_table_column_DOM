c4735029cf07f97ec007cc8a5b24e51fc9629995
Mykola Averchenko <kolya.averchenko@gmail.com>
2026-09-12 14:46:35 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
-// write your code here
+// write your code
+const tableRows = document.querySelector('table').rows;
+
+for (const row of tableRows) {
+  row.cells[3].after(row.cells[1].cloneNode(true));
+}
